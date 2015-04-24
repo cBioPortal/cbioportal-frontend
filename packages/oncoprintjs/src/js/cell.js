1fff612168a54ff7da98199fa7552ff0107ea736
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2015-04-24 16:22:00 -0400

    towards more abstraction from rendering

--- packages/oncoprintjs/src/js/cell.js
+++ packages/oncoprintjs/src/js/cell.js
@@ -3,28 +3,34 @@ var $ = require('jquery');
 var _ = require('underscore');
 
 function D3SVGRuleSet() {
-	var rule_map = {};
+	this.rule_map = {};
 	this.addRule = function(condition, d3_shape, attrs, z_index) {
-		var rule_id = Object.keys(rule_map).length;
+		var rule_id = Object.keys(this.rule_map).length;
 		if (z_index === undefined) {
 			z_index = rule_id;
 		}
-		rule_map[rule_id] = new D3SVGRule(rule_id, condition, d3_shape, attrs, z_index);
+		this.rule_map[rule_id] = new D3SVGRule(rule_id, condition, d3_shape, attrs, z_index);
 		return rule_id;
 	}
 	this.removeRule = function(rule_id) {
-		rule_map[rule_id] = null;
+		this.rule_map[rule_id] = null;
 	}
 	this.getRules = function() {
 		// returns a list of lists of rules to render in the given order
 		var z_map = {};
-		_.each(rule_map, function(rule, rule_id) {
+		_.each(this.rule_map, function(rule, rule_id) {
 			z_map[rule.z_index] = z_map[rule.z_index] || [];
 			z_map[rule.z_index].push(rule);
 		});
 		return _.map(Object.keys(z_map).sort(), 
 			function(z) { return z_map[z];});
 	}
+	this.fromJSON = function(json_rules) {
+		this.rule_map = {};
+		_.each(json_rules, $.proxy(function(rule) {
+			this.addRule(rule.condition, rule.d3_shape, rule.attrs, rule.z_index);
+		}, this));
+	};
 }
 function D3SVGRule(rule_id, condition, d3_shape, attrs, z_index) {
 	this.rule_id = rule_id; 
@@ -42,20 +48,30 @@ function D3SVGRule(rule_id, condition, d3_shape, attrs, z_index) {
 		_.each(this.attrs, function(val, key) {
 			elts.attr(key, val);
 		});
-		console.log($('rect').length);
-		console.log($('g').length);
 	}
 }
 
 function D3SVGRenderer(track) {
-	var rule_set = new D3SVGRuleSet();
+	var self = this;
+	this.rule_set = new D3SVGRuleSet();
 	this.track = track;
 	this.config = this.track.config;
+	this.g;
+
+	this.data_key = function(d) {
+		return d[self.config.id_accessor];
+	};
+	this.setRuleSet = function(rs) {
+		this.rule_set = rs;
+	};
+	this.parseRuleSet = function(json_rules) {
+		this.rule_set.fromJSON(json_rules);
+	};
 	this.addRule = function(condition, d3_shape, attrs, z_index) {
-		return rule_set.addRule(condition, d3_shape, attrs, z_index);
+		return this.rule_set.addRule(condition, d3_shape, attrs, z_index);
 	};
 	this.removeRule = function(rule_id) {
-		rule_set.removeRule(rule_id);
+		this.rule_set.removeRule(rule_id);
 	};
 	this.renderInit = function(container, data, id_order) {
 		container.selectAll('g').remove();
@@ -63,32 +79,45 @@ function D3SVGRenderer(track) {
 		var config = this.config;
 		id_order = utils.invert_array(id_order);
 		// draw groups with empty rectangle hitzones
-		container.selectAll('g').data(data).enter().append('g').classed('cell', true)
+		this.g = container.selectAll('g').data(data, this.data_key).enter().append('g').classed('cell', true)
 			.attr('transform', function(d,i) { return utils.translate(
 				id_order[d[config.id_accessor]]*(config.cell_width + config.cell_padding)
 				, 0);
-			})
-			.append('rect').classed('hit', true)
-			.attr('width', config.cell_width)
-			.attr('height', config.cell_height)
-			.attr('stroke', 'rgba(0,0,0,0)')
-			.attr('fill', 'rgba(0,0,0,0)');
-		this.applyRules(container, data);
+			});
+		this.renderCells(container, data);
 	};
 	this.applyRule = function(d3_svg_rule, container, d3_data) {
 		var config = this.config;
 		var d3_filtered_data = d3_svg_rule.d3FilterData(d3_data);
-		var d3_g_selection = container.selectAll('g.cell').data(d3_filtered_data, function(d) { return d[config.id_accessor];});
+		var d3_g_selection = this.g.data(d3_filtered_data, this.data_key);
 		d3_svg_rule.apply(d3_g_selection);
 	};
-	this.applyRules = function(container, d3_data) {
-		var rule_lists = rule_set.getRules();
-		var self = this;
+	this.renderCells = function(container, d3_data) {
+		this.g.selectAll('*').remove();
+		var rule_lists = this.rule_set.getRules();
+		var config = this.config;
 		_.each(rule_lists, function(rule_list) {
 			_.each(rule_list, function(rule) {
 				self.applyRule(rule, container, d3_data);
 			});
 		});
+		// append hit rectangles
+		this.renderHitZones();
+	};
+	this.renderHitZones = function() {
+		this.g.selectAll('rect.hit').remove();
+		
+		var hits = this.g.append('rect').classed('hit', true)
+			.attr('width', this.config.cell_width)
+			.attr('height', this.config.cell_height)
+			.attr('stroke', 'rgba(0,0,0,0)')
+			.attr('fill', 'rgba(0,0,0,0)');
+		_.each(this.config.events, function(handler, evt) {
+			var wrapHandler = function(d,i) {
+				handler(d,i,d3.select(this));
+			};
+			self.g.on(evt, wrapHandler);
+		});			
 	};
 
 	/*this.update_order = function(container, data, id_order) {
@@ -100,7 +129,7 @@ function D3SVGRenderer(track) {
 									, 0);})
 	};*/
 };
-
+/*
 function GeneticAlterationRuleSet = function() {
 	// standard genetic alteration oncoprint settings
 	var ret = new D3SVGRuleSet();
@@ -115,6 +144,6 @@ function ContinuousDataRuleSet = function(data_member, attrs) {
 }
 
 function BinaryDataColorRuleSet = function(data_member, ) {
-}
+}*/
 
 module.exports = D3SVGRenderer;
--- packages/oncoprintjs/src/js/oncoprint.js
+++ packages/oncoprintjs/src/js/oncoprint.js
@@ -8,47 +8,65 @@ var defaultConfig = {
 	cell_width: 10,
 	cell_padding: 3,
 	track_padding: 2.5,
+	render: 'table',
 };
 
-function Oncoprint(container_selector_string, config) {
-	var d3_container = d3.select(container_selector_string);
+function Oncoprint(config) {
+	var self = this;
+	this.table;
 	this.config = $.extend({}, defaultConfig, config || {});
-	var id_order = [];
-	var track_order = []; 
-	var tracks = {};
-	var ids = {};
 
-	this.sortOnTrack = function(trackName, cmp) {
-		id_order = tracks[trackName].sort(cmp);
-		for (var i=0; i<track_order.length; i++) {
-			tracks[track_order[i]].update(id_order);
-		}
-	};
-	this.setTrackOrder = function(arr) {
-		track_order = arr;
-	};
-	this.addTrack = function(name, data, config) {
-		tracks[name] = new Track(data, config, this);
-		if (track_order.indexOf(name) === -1) {
-			track_order.push(name);
-		}
-		id_order = id_order.concat(_.difference(tracks[name].getIds(), id_order));
-		return tracks[name];
-	};
-	this.getTrack = function(name) {
-		return tracks[name];
+	this.id_order = [];
+	this.track_order = []; 
+	this.tracks = {};
+	this.ids = {};
+
+	if (this.config.render === 'table') {
+		this.renderer = new OncoprintTableRenderer(this);
 	}
-	this.renderInit = function() {
-		this.clear();
-		// build table
-		var d3_table = d3_container.append('table');
-		// delegate rendering to tracks
-		for (var i=0; i<track_order.length; i++) {
-			tracks[track_order[i]].renderInit(d3_table, id_order);
-		}
+
+	this.sortOnTrack = function(trackName, dataCmp) {
+		this.id_order = this.tracks[trackName].getIds(dataCmp);
+		_.each(this.track_order, function(trackName) {
+			self.tracks[trackName].resort(self.id_order);
+		});
 	};
-	this.clear = function() {
-		d3_container.selectAll("*").remove();
+
+	this.setTrackOrder = function(arr) {
+		this.track_order = arr;
+	};
+
+	this.appendTrack = function(name, data, config) {
+		if (name in this.tracks) {
+			return false;
+		}
+		this.tracks[name] = new Track(this, data, config);
+		this.track_order.push(name);
+		this.id_order = this.id_order.concat(_.difference(this.tracks[name].getIds(), this.id_order));
+		self.renderer.appendTrack(self.tracks[name]);	
+		return this.tracks[name];
+	};
+
+	this.getTrack = function(name) {
+		return this.tracks[name];
+	}
+}
+
+function OncoprintTableRenderer(oncoprint) {
+	this.oncoprint = oncoprint;
+	this.container;
+	this.table;
+
+	this.init = function() {
+		// clear all
+		this.container.selectAll('*').remove();
+		// put table
+		this.table = this.container.append('table');
+	}
+
+	this.appendTrack = function(track) {
+		var row = this.table.append('tr');
+		track.renderer.render(row);
 	}
 }
 
