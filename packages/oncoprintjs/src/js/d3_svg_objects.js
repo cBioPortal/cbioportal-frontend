0462d768c6164b8ee0100a7116b4d2fca3336bdf
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2015-04-27 16:02:36 -0400

    deleting file

--- packages/oncoprintjs/src/js/d3_svg_objects.js
+++
@@ -1,6 +0,0 @@
-var d3 = require('d3');
-
-module.exports = {};
-module.exports.makeD3SVGElement = function(tag) {
-	return d3.select(document.createElementNS('http://www.w3.org/2000/svg', tag));
-};
