5440d32faa6f55ca76210fb4ca40142d8845a58f
Aaron Lisman <alisman@smartling.com>
2016-08-29 10:16:41 -0400

    add some tests

---
+++ src/features/patient_view/clinical_information/ClinicalInformationContainer.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+import Immutable from 'immutable';
+import { default as reducer, actionTypes } from './duck';
+
+describe('', ()=>{
+
+    // it('clicking a note on active string fires onNoteClick handler', () => {
+    //
+    //     const onButtonClick = sinon.spy();
+    //
+    //     const wrapper = mount(
+    //
+    //     );
+    //
+    //     wrapper.find('.note').first().simulate('click');
+    //
+    //
+    //
+    //
+    //     console.log(onButtonClick.calledOnce);
+    // });
+
+});
---
+++ src/features/patient_view/clinical_information/duck.spec.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+import Immutable from 'immutable';
+import { default as reducer, actionTypes } from './duck';
+
+describe('clinical_information duck', ()=>{
+
+    let initialState;
+
+    beforeEach(()=>{
+        initialState = reducer(undefined,{ type: null });
+    });
+
+    it('by default reducer returns unmodified state', ()=>{
+
+        const nextState = reducer(initialState, { type:null });
+
+        assert.equal(initialState, nextState);
+
+    });
+
+    it('fetching action sets status to fetching', ()=>{
+
+        const action = {
+            type: actionTypes.FETCH,
+            status:'fetching'
+        };
+
+        const nextState = reducer(initialState, action);
+
+        assert.equal(nextState.get('status'), 'fetching');
+
+    });
+
+    it('fetching complete sets status to success and sets payload', ()=>{
+
+        const action = {
+            type: actionTypes.FETCH,
+            status:'success',
+            payload:{ patient:[1,2,3], samples:[4,5,6] }
+        };
+
+        const nextState = reducer(initialState, action);
+
+        assert.equal(nextState.get('status'), 'complete');
+
+    });
+
+
+
+
+
+
+});
