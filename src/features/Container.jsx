25c9ac711ef4283249e77054b6e6323738a5b669
aaronlisman <donkey00>
2016-10-04 17:53:18 -0400

    Don'g show pageheader when we're not in onePageApp context

--- src/features/Container.jsx
+++ src/features/Container.jsx
@@ -1,13 +1,17 @@
 import React, {PropTypes as T} from 'react';
-import PageHeader from 'shared/components/pageHeader.jsx';
+import ReactDOM from 'react-dom';
+import PageHeader from './pageHeader/PageHeader.jsx';
 
 
 export class Container extends React.Component {
 
-    changeRoute(newValue){
-        this.context.router.push(newValue);
+    componentDidMount() {
+        const headerNode = document.getElementById("reactHeader");
+        if (headerNode !== null) {
+            ReactDOM.render(<PageHeader router={this.context.router} currentRoutePath={ this.props.location.pathname } />,
+                headerNode);
+        }
     }
-
     renderChildren() {
         const childProps = {
             ...this.props,
@@ -16,13 +20,9 @@ export class Container extends React.Component {
         return React.Children.map(children,
             c => React.cloneElement(c, childProps));
     }
-
     render() {
-
-
         return (
             <div>
-                <PageHeader changeRoute={this.changeRoute.bind(this)} currentRoutePath={ this.props.location.pathname } />
                 <div>
                     {this.renderChildren()}
                 </div>
