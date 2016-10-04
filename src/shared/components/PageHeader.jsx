24ce40c52b5ee6ff42ebb269fd331128b072d42a
aaronlisman <donkey00>
2016-10-04 11:19:46 -0400

    fix submission of router form

--- src/shared/components/PageHeader.jsx
+++ src/shared/components/PageHeader.jsx
@@ -2,24 +2,21 @@ import React, { PropTypes as T } from 'react';
 
 class PageHeader extends React.Component {
 
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.changeRoute(this.routeInput.value);
+    }
+
     render() {
         return (
             <header className="clearfix">
                 <h1 className="pull-left">cBioPortal.com</h1>
-
-
-
                 <div className="pull-right">
-
-
-                    <form className="form-inline" onSubmit={(e) => e.preventDefault()}>
+                    <form className="form-inline" onSubmit={(e) => this.handleSubmit(e)}>
                         <div className="form-group">
-                                <input className="form-control" defaultValue={this.props.currentRoutePath} onBlur={(event) => this.props.changeRoute(event.target.value)} />
+                                <input className="form-control" defaultValue={this.props.currentRoutePath} ref={(c) => { this.routeInput = c; }} />
                         </div>
                     </form>
-
-
-
                 </div>
             </header>
         );
