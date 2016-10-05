73d46dd89b525a6326c609d44d6a3371da1e0240
aaronlisman <donkey00>
2016-10-05 12:59:49 -0400

    fix webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ var routeComponentRegex = /routes\/([^\/]+\/?[^\/]+).js$/;
 var config = {
 
     'entry': [
-        './src/app.jsx',
+        './src/appBootstrapper.jsx',
     ],
     'output': {
         path: './dist/',
