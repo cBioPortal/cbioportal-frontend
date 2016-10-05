995f46587c5ea54782c6d5d7ac5161caa261ef00
aaronlisman <donkey00>
2016-10-05 16:01:41 -0400

    remove features folder

--- src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.jsx
+++ src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.jsx
@@ -16,6 +16,7 @@ export default class ClinicalInformationPatientTable extends React.Component {
     render() {
         const rows = [];
 
+        ;
         this.props.data.forEach((item) => {
             rows.push(
                 <tr key={item.get('id')}>
@@ -36,8 +37,40 @@ export default class ClinicalInformationPatientTable extends React.Component {
                 <tbody>
                 {rows}
                 </tbody>
+
             </Table>
         );
+        // const headerCells = this.props.data.get('columns').map((col)=>{
+        //     return <th>{col.get('id')}</th>
+        // });
+        //
+        // const rows = this.props.data.get('items').map((row, key) => {
+        //     return (<tr key={key}>
+        //             <th>{row.get('name')}</th>
+        //             {
+        //                 this.props.data.get('columns').map((col)=> {
+        //                     if(col.get('id') in row.toJS()) {
+        //                         return <td>{row.get(col.get('id'))}</td>
+        //                     } else {
+        //                         return <td>N/A</td>
+        //                     }
+        //
+        //                 })
+        //             }
+        //
+        //         </tr>
+        //     );
+        // });
+        //
+        // return (
+        //     <Table striped>
+        //         <thead><tr>
+        //             <th></th>
+        //             { headerCells }
+        //         </tr></thead>
+        //         <tbody>{ rows }</tbody>
+        //     </Table>
+        // );
     }
 }
 
