0b072484075ca4ca49ce09ba7e285c49ceae6d8e
aaronlisman <donkey00>
2016-11-14 17:23:31 -0500

    migrate clincical information tables to use reactablemsk component
    intermediate
    
    migrate tables to datatables
    
    more
    
    typescript tests
    
    disable coverage reporting
    
    move tiles inline with copy buttons
    
    Use reactable for patient and sample tables
    
    blah
    
    fix branch
    
    
    Former-commit-id: a61dc3b4d166afa719f4ac5d933257d773935d8a

--- src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.tsx
+++ src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.tsx
@@ -2,74 +2,39 @@ import * as _ from 'lodash';
 import * as React from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import { Table } from 'react-bootstrap';
-import ReactZeroClipboard from 'react-zeroclipboard';
-import fileDownload from 'react-file-download';
-import {ClinicalData} from "../../../shared/api/CBioPortalAPI";
+import {ClinicalData} from '../../../shared/api/CBioPortalAPI';
+import renderIf from 'render-if';
+import { Table as DataTable } from 'reactableMSK';
+import TableExportButtons from '../../../shared/components/tableExportButtons/TableExportButtons';
 
 type TODO = any;
 
 export interface IClinicalInformationPatientTableProps {
     data: Array<ClinicalData>;
+    showTitleBar: Boolean;
 }
-
-let serializeTableData = function(tableData: Array<ClinicalData>) {
-
-    let content: Array<string> = [];
-    let delim = ',';
-
-    content.push('Attribute', delim, 'Value');
-
-    tableData.forEach((item: ClinicalData) => {
-        content.push('\r\n', (item.clinicalAttribute.displayName || 'Unknown'), delim, item.attrValue );
-    });
-
-    return content.join('');
-
-}
-
 
 class ClinicalInformationPatientTable extends React.Component<IClinicalInformationPatientTableProps, {}> {
 
-    private downloadData(){
-
-        fileDownload(serializeTableData(this.props.data), 'patient-clinical-attributes.csv');
-
-    }
-
     public render() {
-        const rows: JSX.Element[] = [];
 
-        _.each(this.props.data, (item: TODO) => {
-            rows.push(
-                <tr key={item.id}>
-                    <td>{item.clinicalAttribute.displayName}</td>
-                    <td>{item.value}</td>
-                </tr>
-            );
+        let tableData = _.map(this.props.data, (el: ClinicalData) =>{ return {
+            attribute: el.clinicalAttribute.displayName || '',
+            value: el.attrValue };
         });
 
         return (
             <div>
-                <ButtonGroup>
-                    <ReactZeroClipboard swfPath={require('react-zeroclipboard/assets/ZeroClipboard.swf')} getText={ serializeTableData.bind(this, this.props.data) }>
-                        <Button>Copy</Button>
-                    </ReactZeroClipboard>
 
-                    <Button onClick={ this.downloadData.bind(this) }>Download CSV</Button>
-                </ButtonGroup>
+                {renderIf(this.props.showTitleBar)(
+                    <div>
+                        <h4 className="pull-left">Patient</h4>
+                        <TableExportButtons className="pull-right" tableData={tableData} />
+                    </div>
+                )}
 
-                <Table striped style={{borderCollapse: 'unset', borderSpacing: '0px'}}>
-                    <thead>
-                    <tr>
-                        <th>Attribute</th>
-                        <th>Value</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {rows}
-                    </tbody>
+                <DataTable className='table table-striped' columns={[{ key:'attribute', label:'Attribute'},{ key:'value', label:'Value'}]} data={tableData} />
 
-                </Table>
             </div>
         );
     }
