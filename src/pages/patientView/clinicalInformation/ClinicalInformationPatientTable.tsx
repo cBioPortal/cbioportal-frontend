cd27143652271a7f723836706f184b2665696ccf
Andy Dufilie <andy.dufilie@gmail.com>
2016-11-18 12:55:03 -0500

    Implement ConnectorAPI
    Former-commit-id: fe81f514e992ca5c25209a236260085131b1ad53

--- src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.tsx
+++ src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.tsx
@@ -1,43 +1,34 @@
-import * as _ from 'lodash';
-import * as React from 'react';
-import { Button, ButtonGroup } from 'react-bootstrap';
-import { Table } from 'react-bootstrap';
-import {ClinicalData} from '../../../shared/api/CBioPortalAPI';
-import renderIf from 'render-if';
-import { Table as DataTable } from 'reactableMSK';
-import TableExportButtons from '../../../shared/components/tableExportButtons/TableExportButtons';
-
-type TODO = any;
+import * as React from "react";
+import {ClinicalData} from "../../../shared/api/CBioPortalAPI";
+import {Table as DataTable} from "reactableMSK";
+import TableExportButtons from "../../../shared/components/tableExportButtons/TableExportButtons";
 
 export interface IClinicalInformationPatientTableProps {
-    data: Array<ClinicalData>;
-    showTitleBar: Boolean;
+    data?: Array<ClinicalData>;
+    showTitleBar?: Boolean;
 }
 
-class ClinicalInformationPatientTable extends React.Component<IClinicalInformationPatientTableProps, {}> {
+export default class ClinicalInformationPatientTable extends React.Component<IClinicalInformationPatientTableProps, {}> {
 
     public render() {
 
-        let tableData = _.map(this.props.data, (el: ClinicalData) =>{ return {
+        let tableData = this.props.data && this.props.data.map((el: ClinicalData) => ({
             attribute: el.clinicalAttribute.displayName || '',
-            value: el.attrValue };
-        });
+            value: el.attrValue
+        }));
 
         return (
             <div>
-
-                {renderIf(this.props.showTitleBar)(
-                    <div>
-                        <h4 className="pull-left">Patient</h4>
-                        <TableExportButtons className="pull-right" tableData={tableData} />
-                    </div>
-                )}
-
+                {
+                    this.props.showTitleBar
+                    ?   <div>
+                            <h4 className="pull-left">Patient</h4>
+                            <TableExportButtons className="pull-right" tableData={tableData} />
+                        </div>
+                    :   null
+                }
                 <DataTable className='table table-striped' columns={[{ key:'attribute', label:'Attribute'},{ key:'value', label:'Value'}]} data={tableData} />
-
             </div>
         );
     }
 }
-
-export default ClinicalInformationPatientTable;
---
+++ src/pages/patientView/clinicalInformation/Connector.ts
@@ -0,0 +1,101 @@
+import getClinicalInformationData from './getClinicalInformationData';
+import {RootState} from "../../../redux/rootReducer";
+import {ClinicalData} from "../../../shared/api/CBioPortalAPI";
+import {ClinicalDataBySampleId} from "./getClinicalInformationData";
+import {IDispatch, Connector} from "../../../shared/lib/ConnectorAPI";
+import {IClinicalInformationContainerProps} from "./ClinicalInformationContainer";
+
+declare type TODO = any;
+
+const FETCH:'clinicalInformation/fetch' = 'clinicalInformation/fetch';
+const SET_TAB:'clinicalInformation/setTab' = 'clinicalInformation/setTab';
+export const actionTypes = {FETCH, SET_TAB};
+
+export type ClinicalInformationData = {
+    status?: 'fetching' | 'complete' | 'error',
+    activeTab?: number,
+    patient?: {
+        id: string,
+        clinicalData: Array<ClinicalData>
+    },
+    samples?: Array<ClinicalDataBySampleId>,
+    nodes?: TODO[]//PDXNode[],
+};
+
+export type ActionTypes = (
+    {type: typeof FETCH, status: 'fetching'}
+    | {type: typeof FETCH, status: 'error', error: Error}
+    | {type: typeof FETCH, status: 'success', payload: ClinicalInformationData}
+    | {type: typeof SET_TAB, activeTab: number }
+);
+
+export default new class ClinicalInformationConnector extends Connector<RootState, ClinicalInformationData, ActionTypes, IClinicalInformationContainerProps>
+{
+    initialState:ClinicalInformationData = {
+        status: 'fetching',
+        activeTab: 1,
+    };
+
+    mapDispatchToProps = {
+        loadClinicalInformationTableData: () => (dispatch:IDispatch<ActionTypes>) => { // this is a thunk
+            getClinicalInformationData().then(
+                (data) => {
+                    dispatch({
+                        type: FETCH,
+                        status: 'success',
+                        payload: data,
+                    });
+                }
+            );
+
+            dispatch({
+                type: FETCH,
+                status: 'fetching',
+            });
+        },
+        setTab: (activeTab:number) => ({
+            type: SET_TAB,
+            activeTab
+        })
+    };
+
+    mapStateToProps(state:RootState):IClinicalInformationContainerProps {
+        return {
+            samples: state.clinicalInformation.samples,
+            status: state.clinicalInformation.status,
+            patient: state.clinicalInformation.patient,
+            nodes: state.clinicalInformation.nodes,
+        };
+    }
+
+    reducer(state:ClinicalInformationData, action:ActionTypes) {
+        switch (action.type) {
+            case FETCH: {
+                switch (action.status) {
+                    case 'fetching':
+                        return this.mergeState(state, {'status': 'fetching'});
+
+                    case 'success':
+                        return this.mergeState(state, {
+                            status: 'complete',
+                            patient: action.payload.patient,
+                            nodes: action.payload.nodes,
+                            samples: action.payload.samples
+                        });
+
+                    case 'error':
+                        return this.mergeState(state, {'status': 'error'});
+
+                    default:
+                        return state;
+                }
+            }
+            case SET_TAB: {
+                return this.mergeState(state, {'activeTab': action.activeTab});
+            }
+            default: {
+                return state;
+            }
+        }
+    }
+};
