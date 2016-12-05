f10081a4a65e1b9ad8d12cdf8ca0ea7636cfc563
aaronlisman <donkey00>
2016-12-05 17:41:41 -0500

    get rid of tslint errors.  adjust patient table col width
    Former-commit-id: e15a7e3e0f5253565c9eff2b1ee002a3262c18d8

--- src/pages/datasetView/DatasetPage.tsx
+++ src/pages/datasetView/DatasetPage.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { connect } from 'react-redux';
 import { Table, unsafe }  from 'reactableMSK';
-import * as _ from 'underscore';
-import Connector, { DatasetDownloads } from './Connector';
+import Connector from './Connector';
 
 export interface IDatasetPageUnconnectedProps {
 
@@ -25,24 +23,28 @@ export default class DataSetPageUnconnected extends React.Component<IDatasetPage
             const rows: Array<any> = [];
             this.props.datasets.forEach((item) => {
                 const tempObj: any = {
-                    "CancerStudy": unsafe(`<a href='http://www.cbioportal.org/study?id=${ item.cancer_study_identifier }#summary' target='_blank'> ${ item.name } </a>  <a href='https://github.com/cBioPortal/datahub/blob/master/public/${ item.cancer_study_identifier }.tar.gz' download><i class='fa fa-download'></i></a>`),
-                    "Reference": unsafe(`<a target='_blank' href='https://www.ncbi.nlm.nih.gov/pubmed/${ item.pmid }'>${ item.citation }</a>`),
+                    CancerStudy: unsafe(`<a href='http://www.cbioportal.org/study?id=${ item.cancer_study_identifier }#summary' target='_blank'> ${ item.name } </a>  <a href='https://github.com/cBioPortal/datahub/blob/master/public/${ item.cancer_study_identifier }.tar.gz' download><i class='fa fa-download'></i></a>`),
+                    Reference: unsafe(`<a target='_blank' href='https://www.ncbi.nlm.nih.gov/pubmed/${ item.pmid }'>${ item.citation }</a>`),
                     "All": item.all,
-                    "Sequenced": item.sequenced,
-                    "CNA": item.cna,
+                    Sequenced: item.sequenced,
+                    CNA: item.cna,
                     "Tumor mRNA (RNA-Seq V2)": item.rna_seq_v2_mrna,
                     "Tumor mRNA (microarray)": item.microrna,
                     "Tumor miRNA": item.mrna,
                     "Methylation (HM27)": item.methylation_hm27,
-                    "RPPA": item.rppa,
-                    "Complete": item.complete
+                    RPPA: item.rppa,
+                    Complete: item.complete
                 };
                 rows.push(tempObj);
             });
-            return <Table className="table" data={rows} sortable={true}
-                          filterable={['CancerStudy','Reference','All','Sequenced','CNA','Tumor mRNA (RNA-Seq V2)','Tumor mRNA (microarray)','Tumor miRNA','Methylation (HM27)','RPPA','Complete']}/>;
+            return <Table
+                          className="table"
+                          data={rows}
+                          sortable={true}
+                          filterable={['CancerStudy','Reference','All','Sequenced','CNA','Tumor mRNA (RNA-Seq V2)','Tumor mRNA (microarray)','Tumor miRNA','Methylation (HM27)','RPPA','Complete']}
+            />;
         } else {
-            return <div>loading</div>
+            return <div>loading</div>;
         }
     }
 }
