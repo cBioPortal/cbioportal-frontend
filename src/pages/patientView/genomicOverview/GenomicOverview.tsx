4ef823612c365d741bbe609f054c91124c07e74b
Yichao Sun <yichao.sunny@gmail.com>
2016-12-08 18:41:38 -0500

    hello world

--- src/pages/patientView/genomicOverview/GenomicOverview.tsx
+++ src/pages/patientView/genomicOverview/GenomicOverview.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import $ from 'jquery';
+import Raphael from 'webpack-raphael';
+//import { createRaphaelCanvas } from './genomicOverviewHelper';
 
-export interface IClinicalInformationPatientTableProps {
-   data:Array<any>;
-}
 
-export default class GenomicOverview extends React.Component<IClinicalInformationPatientTableProps, {}> {
+export default class GenomicOverview extends React.Component<{}, {}> {
 
     constructor(){
 
@@ -13,43 +12,35 @@ export default class GenomicOverview extends React.Component<IClinicalInformatio
 
     }
 
-    shouldComponentUpdate(){
-
-        return false;
-
-    }
-
     fetchData() {
 
-        //use ajax againt new api
-
-        return Promise.resolve([1,2,3]);
-
-    }
-
-    componentDidMount(){
-
-        this.fetchData.then((data)=>{
-            let el = $('.genomicOverViewContainer');
-
-            // use rphael to render
-            //renderRap(this.rootNode);
-
-            //this.updateThing('blah');
-        });
+        // use ajax againt new api
+        // return Promise.resolve([1,2,3]);
+        // example: http://www.cbioportal.org/api-legacy/copynumbersegments?cancerStudyId=ov_tcga_pub&chromosome=17&sampleIds=TCGA-13-1510-01
 
     }
 
-    updateThing(data){
+    componentDidMount() {
 
-        el.text(data);
+        //this.fetchData.then((data=>{
+            var paper = new Raphael(document.getElementById('cna_segment_bar_chart_div'), 500, 500);
+            var circle = paper.circle(100, 100, 80);
+            for (var i = 0; i < 5; i += 1) {
+                var multiplier = i * 5;
+                paper.circle(250 + (2 * multiplier), 100 + multiplier, 50 - multiplier)
+            }
+            var rectangle = paper.rect(200, 200, 250, 100);
+            var ellipse = paper.ellipse(200, 400, 100, 50);
+        //});
+
     }
 
 
     public render() {
-
         return (
-            <div ref={(el)=>this.rootNode === el} className="genomicOverViewContainer">here i am</div>
+            <div className="genomicOverViewContainer" style={{ backgroundColor: 'red'}}>
+                <div id="cna_segment_bar_chart_div"></div>
+            </div>
         );
     }
 }
