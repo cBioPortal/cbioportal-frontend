8c5fbe2803d5be3a608f7bd293d799688305abcf
Onur Sumer <s.onur.sumer@gmail.com>
2016-12-08 18:12:07 -0500

    improved sample column formatter

---
+++ src/pages/patientView/mutation/column/SampleColumnFormatter.spec.js
@@ -0,0 +1,27 @@
+import {assert} from "chai";
+import {size} from "lodash";
+import SampleColumnFormatter from "./SampleColumnFormatter";
+
+describe('', () => {
+    const shortSampleId = "Short_Id";
+    const longSampleId = "This_is_a_quite_long_Sample_Id_in_my_opinion!";
+
+    it('sample id text is properly formatted', () => {
+
+        let text = SampleColumnFormatter.getText(shortSampleId);
+        assert.equal(text, shortSampleId); // should be same
+
+        text = SampleColumnFormatter.getText(longSampleId);
+        assert.equal(text.length, SampleColumnFormatter.MAX_LENGTH + SampleColumnFormatter.SUFFIX.length);
+        assert.isTrue(text.indexOf(SampleColumnFormatter.SUFFIX) != -1);
+    });
+
+    it('sample id style class is properly set', () => {
+
+        let style = SampleColumnFormatter.getStyleClass(shortSampleId);
+        assert.notEqual(style, SampleColumnFormatter.TOOLTIP_STYLE); // no tooltips for short ids
+
+        style = SampleColumnFormatter.getStyleClass(longSampleId);
+        assert.equal(style, SampleColumnFormatter.TOOLTIP_STYLE); // tooltips for long ids
+    });
+});
