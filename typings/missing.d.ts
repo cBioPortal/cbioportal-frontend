f5f267ff0892899ef1057beda8254212c9c1e5c4
Yichao Sun <yichao.sunny@gmail.com>
2016-12-09 12:17:46 -0500

    render genomic overview and datasets page

--- typings/missing.d.ts
+++ typings/missing.d.ts
@@ -5,3 +5,4 @@ declare module 'react-file-download';
 declare module 'react-zeroclipboard';
 declare module 'reactableMSK';
 declare module 'redux-seamless-immutable';
+declare module 'webpack-raphael';
