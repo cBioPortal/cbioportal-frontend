08524ea6d69c1fcfac916d7f26500c5c58a72fb3
Onur Sumer <s.onur.sumer@gmail.com>
2016-12-09 20:49:14 -0500

    added tests for protein change column formatter

--- src/shared/components/mutationTable/column/SampleColumnFormatter.spec.js
+++ src/shared/components/mutationTable/column/SampleColumnFormatter.spec.js
@@ -1,8 +1,10 @@
 import {assert} from "chai";
-import {size} from "lodash";
 import SampleColumnFormatter from "./SampleColumnFormatter";
 
-describe('', () => {
+/**
+ * @author Selcuk Onur Sumer
+ */
+describe('SampleColumnFormatter functions', () => {
     const shortSampleId = "Short_Id";
     const longSampleId = "This_is_a_quite_long_Sample_Id_in_my_opinion!";
 
