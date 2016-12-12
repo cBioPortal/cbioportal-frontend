d7d515bc6b2aa6d2e486c5cc548e4a50981213dd
Ino de Bruijn <ino@ino.pm>
2016-12-12 13:18:47 -0500

    Add clinical attributes styling from cBioPortal
    - Add clinicalAttributesUtil.js
        - use set innerHTML to prevent having to rewrite for JSX Element list
    - Add attribute selection styling of clinical attribute spans
    - Add some extra syling to please the eye

--- src/pages/patientView/patientHeader/PatientHeader.tsx
+++ src/pages/patientView/patientHeader/PatientHeader.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
+import {fromPairs} from 'lodash';
 import {OverlayTrigger, Popover} from 'react-bootstrap';
 
 import ClinicalInformationPatientTable from '../clinicalInformation/ClinicalInformationPatientTable';
-import SampleInline from './SampleInline';
 import {ClinicalInformationData} from "../Connector";
-import { ClinicalDataBySampleId } from "../../../shared/api/api-types-extended";
+import {getSpans} from '../clinicalInformation/lib/clinicalAttributesUtil.js';
 
 import styles from './styles.module.scss';
 
@@ -15,22 +15,11 @@ export default class PatientHeader extends React.Component<IPatientHeaderProps, 
 
         return (
             <div className={styles.patientHeader}>
-                <div>
-                    <i className="fa fa-female fa-2 genderIcon hidden" aria-hidden="true"></i>
-                    {this.props.patient && this.getOverlayTriggerPatient(this.props.patient)}
-                </div>
-                {this.props.samples && this.props.samples.map((s, n) => this.getOverlayTriggerSample(s, n))}
+                <i className="fa fa-female fa-2 genderIcon hidden" aria-hidden="true"></i>
+                {this.props.patient && this.getOverlayTriggerPatient(this.props.patient)}
             </div>
         );
 
-    }
-
-    private getPopoverSample(sample: ClinicalDataBySampleId, sampleNumber: number) {
-        return (
-            <Popover key={sampleNumber} id={'popover-sample-' + sampleNumber}>
-                <ClinicalInformationPatientTable showTitleBar={false} data={sample.clinicalData} />
-            </Popover>
-        );
     }
 
     private getPopoverPatient(patient: ClinicalInformationData['patient']) {
@@ -52,25 +41,11 @@ export default class PatientHeader extends React.Component<IPatientHeaderProps, 
             >
                 <span>
                     {patient.id}
+                    <span className='clinical-spans' id='patient-attributes' dangerouslySetInnerHTML={{__html:
+                        getSpans(fromPairs(patient.clinicalData.map((x) => [x.clinicalAttributeId, x.value])), 'lgg_ucsf_2014')}}>
+                    </span>
                 </span>
             </OverlayTrigger>
         );
     }
-
-    private getOverlayTriggerSample(sample: ClinicalDataBySampleId, sampleNumber: number) {
-        return (
-            <OverlayTrigger
-                delayHide={100}
-                key={sampleNumber}
-                trigger={['hover', 'focus']}
-                placement='bottom'
-                overlay={this.getPopoverSample(sample, sampleNumber + 1)}
-            >
-                <span>
-                    <SampleInline sample={sample} sampleNumber={sampleNumber + 1} />
-                </span>
-            </OverlayTrigger>
-        );
-    }
-
 }
