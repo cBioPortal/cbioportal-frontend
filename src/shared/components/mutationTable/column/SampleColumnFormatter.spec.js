026c436d99fb332d22bd7c48fddb7c99095db42f
Onur Sumer <s.onur.sumer@gmail.com>
2016-12-13 19:59:02 -0500

    improved type definitions and column formatters

--- src/shared/components/mutationTable/column/SampleColumnFormatter.spec.js
+++ src/shared/components/mutationTable/column/SampleColumnFormatter.spec.js
@@ -5,25 +5,32 @@ import SampleColumnFormatter from "./SampleColumnFormatter";
  * @author Selcuk Onur Sumer
  */
 describe('SampleColumnFormatter functions', () => {
-    const shortSampleId = "Short_Id";
-    const longSampleId = "This_is_a_quite_long_Sample_Id_in_my_opinion!";
+    const shortSampleId = {columnData: "Short_Id"};
+    const longSampleId = {columnData: "This_is_a_quite_long_Sample_Id_in_my_opinion!"};
 
     it('sample id text is properly formatted', () => {
+        // text and display should be the same for short sample ids
+        assert.equal(SampleColumnFormatter.getTextValue(shortSampleId),
+                     SampleColumnFormatter.getDisplayValue(shortSampleId));
 
-        let text = SampleColumnFormatter.getText(shortSampleId);
-        assert.equal(text, shortSampleId); // should be same
+        // text and display values should be different,
+        // and truncated size should have fixed number of characters
+        assert.equal(SampleColumnFormatter.getDisplayValue(longSampleId).length,
+                     SampleColumnFormatter.MAX_LENGTH + SampleColumnFormatter.SUFFIX.length);
 
-        text = SampleColumnFormatter.getText(longSampleId);
-        assert.equal(text.length, SampleColumnFormatter.MAX_LENGTH + SampleColumnFormatter.SUFFIX.length);
-        assert.isTrue(text.indexOf(SampleColumnFormatter.SUFFIX) != -1);
+        // suffix should be appended in the end
+        assert.isTrue(SampleColumnFormatter.getDisplayValue(longSampleId).indexOf(
+                          SampleColumnFormatter.SUFFIX) > -1);
     });
 
     it('sample id style class is properly set', () => {
 
-        let style = SampleColumnFormatter.getStyleClass(shortSampleId);
-        assert.notEqual(style, SampleColumnFormatter.TOOLTIP_STYLE); // no tooltips for short ids
+        // no tooltips for short ids
+        assert.notEqual(SampleColumnFormatter.getStyleClass(shortSampleId.columnData),
+                        SampleColumnFormatter.TOOLTIP_STYLE);
 
-        style = SampleColumnFormatter.getStyleClass(longSampleId);
-        assert.equal(style, SampleColumnFormatter.TOOLTIP_STYLE); // tooltips for long ids
+        // tooltips for long ids
+        assert.equal(SampleColumnFormatter.getStyleClass(longSampleId.columnData),
+                     SampleColumnFormatter.TOOLTIP_STYLE);
     });
 });
