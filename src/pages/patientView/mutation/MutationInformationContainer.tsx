0ae848e785fc8ad92b9f46978084a1215c6e3975
Onur Sumer <s.onur.sumer@gmail.com>
2016-12-16 17:13:13 -0500

    added partial support for column visibility option
    Former-commit-id: 0502f5e6d84ffc2d08eba0d86c55b87aa0132a4a

--- src/pages/patientView/mutation/MutationInformationContainer.tsx
+++ src/pages/patientView/mutation/MutationInformationContainer.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import mockData from "./mock/mutationData.json";
 import MutationTable from "../../../shared/components/mutationTable/MutationTable";
+import {IColumnDefMap} from "../../../shared/components/enhancedReactTable/IEnhancedReactTableProps";
 
 export interface IMutationInformationContainerProps {
     // setTab?: (activeTab:number) => void;
@@ -10,7 +11,11 @@ export interface IMutationInformationContainerProps {
 export default class MutationInformationContainer extends React.Component<IMutationInformationContainerProps, {}> {
     public render() {
         // TODO properly customize table for patient view specific columns!!!
-        let columns = {
+        let columns:IColumnDefMap = {
+            sampleId: {
+                name: "Sample Id", // name does not matter when the column is "excluded"
+                visible: "excluded"
+            },
             tumors: {
                 name: "Tumors"
             },
