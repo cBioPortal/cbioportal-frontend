ab6b4e0de5986c1d7574adf65e4ccf101fff4f06
Yichao Sun <yichao.sunny@gmail.com>
2016-12-20 18:14:55 -0500

    mutation events (count) bar chart
    Former-commit-id: 98a1f1ea5d2c86c0bfaae29a8d86b07b1e82875b

--- src/pages/patientView/genomicOverview/GenomicOverview.tsx
+++ src/pages/patientView/genomicOverview/GenomicOverview.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as $ from 'jquery';
 import * as _ from 'underscore';
-import queryString from "query-string";
 import CBioPortalAPI from "shared/api/CBioPortalAPI";
-import { GenomicOverviewConfig, createRaphaelCanvas, getChmInfo, plotChromosomes, plotCnSegs } from './genomicOverviewHelper';
+//import { GenomicOverviewConfig, createRaphaelCanvas, getChmInfo, plotChromosomes, plotCnSegs } from './genomicOverviewHelper';
+import * as genomicOverviewHelper from './genomicOverviewHelper'
 import {CopyNumberSegment} from "../../../shared/api/CBioPortalAPI";
 
 export default class GenomicOverview extends React.Component<{}, {}> {
@@ -16,12 +16,19 @@ export default class GenomicOverview extends React.Component<{}, {}> {
 
     fetchData() {
 
-        const qs = queryString.parse(location.search);
-        var p = Promise.resolve(
-            //$.get("http://www.cbioportal.org/api-legacy/copynumbersegments?cancerStudyId=" + qs.cancer_study_id + "&chromosome=17&sampleIds=P04_Pri")
-            $.get("http://www.cbioportal.org/api-legacy/copynumbersegments?cancerStudyId=ov_tcga_pub&chromosome=17&sampleIds=TCGA-24-2035-01")
-        );
-        return p;
+        var fetchJSON = function(url) {
+            return new Promise((resolve, reject) => {
+                $.getJSON(url)
+                    .done((json) => resolve(json))
+                    .fail((xhr, status, err) => reject(status + err.message));
+            });
+        }
+        var itemUrls = [
+                "http://www.cbioportal.org/api-legacy/copynumbersegments?cancerStudyId=ov_tcga_pub&chromosome=17&sampleIds=TCGA-24-2024-01",
+                "https://raw.githubusercontent.com/onursumer/cbioportal-frontend/enhanced-react-table/src/pages/patientView/mutation/mock/mutationData.json"
+            ],
+            itemPromises = _.map(itemUrls, fetchJSON);
+        return Promise.all(itemPromises);
 
     }
 
@@ -29,13 +36,27 @@ export default class GenomicOverview extends React.Component<{}, {}> {
 
         this.fetchData().then( apiResult => {
 
-            // transform API result
-            let raphaelData: any = {};
+            var cnaResult = apiResult[0];
+            var mutationResult = apiResult[1];
 
-            let sampleId = _.uniq(_.pluck(apiResult, 'sample'))[0];
+            // --- construct params ---
+            let sampleId = _.uniq(_.pluck(cnaResult, 'sample'))[0]; //TODO: multiple samples
+            var config = genomicOverviewHelper.GenomicOverviewConfig(2, 1000); //TODO: nRows, width
+            // --- end of params ---
 
-            raphaelData[sampleId] = [];
-            _.each(apiResult, function(_dataObj: any) {
+            // --- raphael config ---
+            var paper = genomicOverviewHelper.createRaphaelCanvas('genomic_overview_div', config);
+            // --- end of raphael config ---
+            
+            // --- chromosome chart ---
+            var chmInfo = genomicOverviewHelper.getChmInfo();
+            genomicOverviewHelper.plotChromosomes(paper,config,chmInfo);
+            // --- end of chromosome chart ---
+
+            // --- CNA bar chart ---
+            let cnaRaphaelData: any = {};
+            cnaRaphaelData[sampleId] = [];
+            _.each(cnaResult, function(_dataObj: any) {
                 var _tmp: Array<any> = [];
                 _tmp.push(_dataObj.sample);
                 _tmp.push(_dataObj.chr);
@@ -43,15 +64,14 @@ export default class GenomicOverview extends React.Component<{}, {}> {
                 _tmp.push(_dataObj.start);
                 _tmp.push(_dataObj.numProbes);
                 _tmp.push(_dataObj.value);
-                raphaelData[sampleId].push(_tmp);
+                cnaRaphaelData[sampleId].push(_tmp);
             });
+            genomicOverviewHelper.plotCnSegs(paper, config, chmInfo, 0, cnaRaphaelData[sampleId], 1, 3, 2, 5, sampleId);
+            // --- end of CNA bar chart ---
 
-            // render cna segment bar chart
-            var config = GenomicOverviewConfig(1, 1000);
-            var paper = createRaphaelCanvas('cna_segment_bar_chart_div', config);
-            var chmInfo = getChmInfo();
-            plotChromosomes(paper,config,chmInfo);
-            plotCnSegs(paper, config, chmInfo, 0, raphaelData[sampleId], 1, 3, 2, 5, sampleId);
+            // --- mutation events bar chart ---
+            genomicOverviewHelper.plotMuts(paper, config, chmInfo, 1, mutationResult, sampleId);
+            // --- end of mutation events bar chart ---
 
         });
 
@@ -61,7 +81,7 @@ export default class GenomicOverview extends React.Component<{}, {}> {
     public render() {
         return (
             <div className="genomicOverViewContainer" style={{ backgroundColor: '#F0FFFF'}}>
-                <div id="cna_segment_bar_chart_div"></div>
+                <div id="genomic_overview_div"></div>
             </div>
         );
     }
