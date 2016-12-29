6a21f3a1575d6535a9fd817d7b04bbca90325d7d
aaronlisman <donkey00>
2016-12-29 15:14:52 -0500

    work in progres son testing the tracks component
    Former-commit-id: cfc40f93fed7027c54b66bdeb9f307227d678100

--- src/pages/patientView/genomicOverview/GenomicOverview.tsx
+++ src/pages/patientView/genomicOverview/GenomicOverview.tsx
@@ -5,6 +5,9 @@ export default class GenomicOverview extends React.Component<{ data: any }, {}> 
 
     public render() {
 
+
+        console.log(this.props.data);
+
         return (
             <div>
                 <Tracks data={this.props.data} />
---
+++ src/pages/patientView/genomicOverview/Tracks.spec.jsx
@@ -0,0 +1,20 @@
+import mockData from './mockData.js';
+import { React } from 'react';
+import Tracks from './Tracks';
+import { shallow, mount } from 'enzyme';
+import { assert } from 'chai';
+
+describe('', ()=>{
+
+   it('', ()=>{
+
+       const wrapper = mount(<Tracks data={mockData} />);
+       //console.log(mockData);
+
+
+
+   })
+
+
+
+});
