ed3776c8ebf9d50b4f30df5a5d7716d135cfb4ee
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-01-06 17:31:54 -0500

    use api data in VAF plot
    Former-commit-id: ef7b754d8b89ed85df533af9761769229ce68e8a

--- src/pages/patientView/PatientViewPage.tsx
+++ src/pages/patientView/PatientViewPage.tsx
@@ -18,8 +18,6 @@ import { RequestStatus } from "../../shared/api/api-types-extended";
 import { default as CBioPortalAPI, Mutation }  from "../../shared/api/CBioPortalAPI";
 import renderIf from 'render-if';
 import queryString from "query-string";
-import {mockData as vafPlotMockData, mockColors as vafPlotMockColors, mockOrder as vafPlotMockOrder, mockLabels as vafPlotMockLabels} from './vafPlot/mockData';
-import {ThumbnailExpandVAFPlot} from './vafPlot/ThumbnailExpandVAFPlot';
 
 export interface IPatientViewPageProps {
     store?: RootState;
@@ -146,20 +144,17 @@ export default class PatientViewPage extends React.Component<IPatientViewPagePro
 
     public render() {
 
-        let plotComp = <ThumbnailExpandVAFPlot
-            data={vafPlotMockData}
-            colors={vafPlotMockColors}
-            labels={vafPlotMockLabels}
-            order={vafPlotMockOrder}
-            overlayPlacement="right"
-        />;
-
         return (
             <div>
 
                 {
                     renderIf(this.state.mutationData && this.state.cnaSegmentData)(
-                        <GenomicOverview mutations={this.state.mutationData} cnaSegments={this.state.cnaSegmentData} />
+                        <GenomicOverview mutations={this.state.mutationData}
+                                         cnaSegments={this.state.cnaSegmentData}
+                                         sampleOrder={mockData.order}
+                                         sampleLabels={mockData.labels}
+                                         sampleColors={mockData.colors}
+                        />
                     )
                 }
 
