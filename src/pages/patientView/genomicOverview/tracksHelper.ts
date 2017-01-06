2a20b6ea2967364f3d1caa405d861f24fb641bc7
aaronlisman <donkey00>
2017-01-06 12:44:17 -0500

    formatting and custom search box for mutation table

--- src/pages/patientView/genomicOverview/tracksHelper.ts
+++ src/pages/patientView/genomicOverview/tracksHelper.ts
@@ -255,7 +255,7 @@ function addToolTip(node: any, tip: any,showDelay: any, position: any) {
     //     param['show'] = { delay: showDelay };
     // if (position)
     //     param['position'] = position;
-    $(node).qtip(param);
+    ($(node) as any).qtip(param);
 }
 
 function underlineText(textElement: any, p: any) {
