6b6a04acb7f27374d7c7a87b70b22da9dd4be132
aaronlisman <donkey00>
2017-01-09 16:57:33 -0500

    fix ts errors

--- src/pages/patientView/genomicOverview/GenomicOverview.tsx
+++ src/pages/patientView/genomicOverview/GenomicOverview.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import Tracks from './Tracks';
 
 
-export default class GenomicOverview extends React.Component<{ mutations: any, cnaSegments:any, plotComponent: any }, {}> {
+export default class GenomicOverview extends React.Component<{ mutations: any, cnaSegments:any }, {}> {
 
 
     public render() {
 
         return (
             <div>
-                <Tracks mutations={this.props.mutationData} cnaSegments={this.props.cnaSegmentData} />
+                <Tracks mutations={this.props.mutations} cnaSegments={this.props.cnaSegments} />
             </div>
         );
     }
