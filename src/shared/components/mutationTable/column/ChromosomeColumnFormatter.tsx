8db00d9676e9a156cdf29263852f393d00cf6864
Onur Sumer <s.onur.sumer@gmail.com>
2017-01-09 18:18:25 -0500

    fixed the custom sort function interface, added the chromosome column, updated column order for the default columns,
    Former-commit-id: 5bea2b4a2e0e208d1726c504e90c4cd175ca0e8e

---
+++ src/shared/components/mutationTable/column/ChromosomeColumnFormatter.tsx
@@ -0,0 +1,62 @@
+import {IColumnFormatterData}
+    from "../../../../shared/components/enhancedReactTable/IColumnFormatter";
+import GeneColumnFormatter from "./GeneColumnFormatter";
+
+/**
+ * @author Selcuk Onur Sumer
+ */
+export default class ChromosomeColumnFormatter
+{
+    public static getDataFromRow(rowData:any)
+    {
+        let geneData = GeneColumnFormatter.getDataFromRow(rowData);
+
+        return geneData.chromosome;
+    }
+
+    public static sortFunction(a:string, b:string):number
+    {
+        let aValue = ChromosomeColumnFormatter.extractSortValue(a);
+        let bValue = ChromosomeColumnFormatter.extractSortValue(b);
+
+        return aValue > bValue ? 1 : -1;
+    }
+
+    public static extractSortValue(chromosome:string)
+    {
+        const numerical:RegExp = /[0-9]+/g;
+
+        let matched:RegExpMatchArray|null = chromosome.match(numerical);
+        let value:number = -1;
+
+        // if no match, then search for X or Y
+        if (matched)
+        {
+            value = parseInt(matched[0]);
+        }
+        else if (chromosome.toLowerCase().indexOf("x") > -1)
+        {
+            value = 23;
+        }
+        else if (chromosome.toLowerCase().indexOf("y") > -1)
+        {
+            value = 24;
+        }
+
+        return value;
+    }
+
+    public static getData(data:IColumnFormatterData)
+    {
+        let chromosome;
+
+        if (data.columnData) {
+            chromosome = data.columnData;
+        }
+        else {
+            chromosome = ChromosomeColumnFormatter.getDataFromRow(data.rowData);
+        }
+
+        return chromosome;
+    }
+}
