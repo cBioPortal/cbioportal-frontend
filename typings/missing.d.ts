47250cc03e62d7e3350bb220f17b2fc1ac2569aa
aaronlisman <donkey00>
2017-01-11 15:11:19 -0500

    upgrade react to 15.4.2. add column hid/select for mutation table

--- typings/missing.d.ts
+++ typings/missing.d.ts
@@ -12,3 +12,4 @@ declare module 'render-if';
 declare type PartialPick<T, K extends keyof T> = Partial<Pick<T, K>>;
 declare module 'rc-tooltip';
 declare module 'underscore';
+declare module 'react-if';
