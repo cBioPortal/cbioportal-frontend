3f9fbe1f97ab89aacf83b63d3f896a4ec2f78e05
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-01-24 16:58:49 -0500

    Use TablePaginationControls for patient cohort nav
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/shared/components/tablePaginationControls/TablePaginationControls.tsx
+++ src/shared/components/tablePaginationControls/TablePaginationControls.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Button, ButtonGroup, FormGroup, FormControl, InputGroup} from 'react-bootstrap';
 import styles from "./tablePaginationControls.module.scss";
+import { If } from 'react-if';
 
 const SHOW_ALL_PAGE_SIZE = -1;
 
@@ -10,15 +11,24 @@ export interface ITablePaginationControlsProps {
     itemsPerPageOptions?:number[];
     showAllOption?:boolean;
     textBetweenButtons?:string;
+    firstButtonContent?: string | JSX.Element;
     previousButtonContent?: string | JSX.Element;
     nextButtonContent?: string | JSX.Element;
+    lastButtonContent?: string | JSX.Element;
+    showFirstPage?:boolean;
+    showLastPage?:boolean;
+    showItemsPerPageSelector?:boolean;
     onChangeItemsPerPage?:(itemsPerPage:number)=>any;
+    onFirstPageClick?:()=>any;
     onPreviousPageClick?:()=>any;
     onNextPageClick?:()=>any;
+    onLastPageClick?:()=>any;
     className?:string;
+    marginLeft?: number;
+    firstPageDisabled?:boolean;
     previousPageDisabled?:boolean;
     nextPageDisabled?:boolean;
-    marginLeft?: number;
+    lastPageDisabled?:boolean;
 }
 
 export class TablePaginationControls extends React.Component<ITablePaginationControlsProps, {}> {
@@ -28,11 +38,18 @@ export class TablePaginationControls extends React.Component<ITablePaginationCon
         itemsPerPageOptions: [10, 25, 50, 100],
         showAllOption: true,
         textBetweenButtons: "text btwn buttons",
+        firstButtonContent: (<i className='fa fa-angle-double-left'/>),
         previousButtonContent: (<i className='fa fa-angle-left'/>),
         nextButtonContent: (<i className='fa fa-angle-right'/>),
+        lastButtonContent: (<i className='fa fa-angle-double-right'/>),
+        showItemsPerPageSelector:true,
+        showFirstPage:false,
+        showLastPage:false,
         onChangeItemsPerPage: ()=>0,
+        onFirstPageClick:()=>0,
         onPreviousPageClick: ()=>0,
         onNextPageClick: ()=>0,
+        onLastPageClick: ()=>0,
         className: "",
         marginLeft: 5,
         previousPageDisabled:false,
@@ -57,6 +74,11 @@ export class TablePaginationControls extends React.Component<ITablePaginationCon
         return (
             <div className={this.props.className} style={{marginLeft: this.props.marginLeft}}>
                 <ButtonGroup bsSize="sm">
+                    <If condition={!!this.props.showFirstPage}>
+                        <Button key="firstPageBtn" disabled={!!this.props.firstPageDisabled} onClick={this.props.onFirstPageClick}>
+                            {this.props.firstButtonContent}
+                        </Button>
+                    </If>
                     <Button key="prevPageBtn" disabled={!!this.props.previousPageDisabled} onClick={this.props.onPreviousPageClick}>
                         {this.props.previousButtonContent}
                     </Button>
@@ -66,17 +88,24 @@ export class TablePaginationControls extends React.Component<ITablePaginationCon
                     <Button key="nextPageBtn" disabled={!!this.props.nextPageDisabled} onClick={this.props.onNextPageClick}>
                         {this.props.nextButtonContent}
                     </Button>
+                    <If condition={!!this.props.showLastPage}>
+                        <Button key="lastPageBtn" disabled={!!this.props.lastPageDisabled} onClick={this.props.onLastPageClick}>
+                            {this.props.lastButtonContent}
+                        </Button>
+                    </If>
                 </ButtonGroup>
 
-                <FormGroup bsSize="sm" className={styles["form-select"]}>
-                    <FormControl
-                        componentClass="select"
-                        value={this.props.itemsPerPage}
-                        onChange={this.handleChangeItemsPerPage as React.FormEventHandler<any>}
-                    >
-                        {pageSizeOptionElts}
-                    </FormControl>
-                </FormGroup>
+                <If condition={!!this.props.showItemsPerPageSelector}>
+                    <FormGroup bsSize="sm" className={styles["form-select"]}>
+                        <FormControl
+                            componentClass="select"
+                            value={this.props.itemsPerPage}
+                            onChange={this.handleChangeItemsPerPage as React.FormEventHandler<any>}
+                        >
+                            {pageSizeOptionElts}
+                        </FormControl>
+                    </FormGroup>
+                </If>
             </div>
         );
     }
