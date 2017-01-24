0e46c5b44599c5b078b7f2a550348264c066ea4b
aaronlisman <donkey00>
2017-01-24 14:05:40 -0500

    use rc-tooltip for the sample bullet tooltip
    Former-commit-id: 6770eb0f32054c4b5c7af152f16eeec34f827c7b

--- src/pages/patientView/sampleManager.tsx
+++ src/pages/patientView/sampleManager.tsx
@@ -5,6 +5,15 @@ import SampleInline from './patientHeader/SampleInline';
 import {ClinicalDataBySampleId} from "../../shared/api/api-types-extended";
 import ClinicalInformationPatientTable from "./clinicalInformation/ClinicalInformationPatientTable";
 import {Sample} from "../../shared/api/CBioPortalAPI";
+import Tooltip from 'rc-tooltip';
+
+// we need this to account for issue with rc-tooltip when dealing with large tooltip overlay content
+export function placeArrow(tooltipEl: any) {
+    const arrowEl = tooltipEl.querySelector('.rc-tooltip-arrow');
+    const targetEl = this.getRootDomNode();  // eslint-disable-line no-invalid-this
+    arrowEl.style.left = '10px';
+}
+
 
 class SampleManager {
 
@@ -34,31 +43,56 @@ class SampleManager {
 
     getOverlayTriggerSample(sample: ClinicalDataBySampleId, sampleIndex: number) {
 
-        let sampleNumberText: number = sampleIndex+1;
+        const sampleNumberText: number = sampleIndex+1;
 
-        return (
-            <OverlayTrigger
-                delayHide={100}
-                key={sampleNumberText}
-                trigger={['hover', 'focus']}
-                placement='bottom'
-                overlay={this.getPopoverSample(sample, sampleNumberText)}
+        const align = {
+            points: ['tl', 'tr'], // align top left point of sourceNode with top right point of targetNode
+            offset: [0, 20], // the offset sourceNode by 10px in x and 20px in y,
+            targetOffset: ['0','0'], // the offset targetNode by 30% of targetNode width in x and 40% of targetNode height in y,
+        };
+
+
+        return (<Tooltip
+            placement='topRight'
+            trigger={['hover', 'focus']}
+            overlay={this.getPopoverSample(sample, sampleNumberText)}
+            arrowContent={<div className="rc-tooltip-arrow-inner" />}
+            destroyTooltipOnHide={false}
+            onPopupAlign={placeArrow}
+
             >
-                <svg width="12" height="12">
-                    <SampleInline
-                        sample={sample}
-                        sampleNumber={sampleNumberText}
-                    />
-                </svg>
-            </OverlayTrigger>
-        );
+            <svg width="12" height="12">
+                <SampleInline
+                             sample={sample}
+                             sampleNumber={sampleNumberText}
+                         />
+            </svg>
+        </Tooltip>);
+
+        // return (
+        //     <OverlayTrigger
+        //         delayHide={100}
+        //         key={sampleNumberText}
+        //         trigger={['hover', 'focus']}
+        //         placement='bottom'
+        //         overlay={this.getPopoverSample(sample, sampleNumberText)}
+        //     >
+        //         <svg width="12" height="12">
+        //             <SampleInline
+        //                 sample={sample}
+        //                 sampleNumber={sampleNumberText}
+        //             />
+        //         </svg>
+        //     </OverlayTrigger>
+        // );
     }
 
     getPopoverSample(sample: ClinicalDataBySampleId, sampleNumber: number) {
         return (
-            <Popover key={sampleNumber} id={'popover-sample-' + sampleNumber}>
+            <div style={{ maxHeight:400, overflow:'auto' }}>
+                <h5>{ sample.id }</h5>
                 <ClinicalInformationPatientTable showTitleBar={false} data={sample.clinicalData} />
-            </Popover>
+            </div>
         );
     }
 
