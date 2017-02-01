d13d22c3b2efe069c52594699974453b091fc6d5
Andy Dufilie <andy.dufilie@gmail.com>
2017-02-01 14:37:55 -0500

    Add missing remoteData.ts

---
+++ src/shared/api/remoteData.ts
@@ -0,0 +1,24 @@
+import * as seamlessImmutable from 'seamless-immutable';
+import {
+    MobxPromiseInputParamsWithDefault,
+    MobxPromiseUnionTypeWithDefault,
+    MobxPromiseInputUnion,
+    MobxPromiseUnionType,
+    MobxPromiseClass
+} from "./MobxPromise";
+
+/**
+ * Constructs a MobxPromise which will call seamlessImmutable.from() on the result and the default value.
+ */
+export function remoteData<R>(input:MobxPromiseInputParamsWithDefault<R>):MobxPromiseUnionTypeWithDefault<R>;
+export function remoteData<R>(input:MobxPromiseInputUnion<R>, defaultResult: R):MobxPromiseUnionTypeWithDefault<R>;
+export function remoteData<R>(input:MobxPromiseInputUnion<R>):MobxPromiseUnionType<R>;
+export function remoteData<R>(input:MobxPromiseInputUnion<R>, defaultResult?: R)
+{
+    input = MobxPromiseClass.normalizeInput(input);
+    const invoke = input.invoke;
+    input.invoke = () => MobxPromiseClass.toPromiseLike(invoke()).then(result => seamlessImmutable.from(result));
+    input.default = seamlessImmutable.from(input.default);
+    defaultResult = seamlessImmutable.from(defaultResult);
+    return new MobxPromiseClass(input, defaultResult) as MobxPromiseUnionTypeWithDefault<R>;
+}
