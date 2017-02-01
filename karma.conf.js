06030b8772547890524291c17a32e8a0d495f117
Ino de Bruijn <ino@ino.pm>
2017-02-01 18:53:56 -0500

    add code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,21 @@ process.env.NODE_ENV = 'test';
 var webpackConfig = require('./webpack.config');
 
 module.exports = function (config) {
+    var preprocessors = ['webpack', 'sourcemap'];
+    var plugins = [
+        'karma-mocha',
+        'karma-chai',
+        'karma-webpack',
+        'karma-phantomjs-launcher',
+        'karma-spec-reporter',
+        'karma-sourcemap-loader',
+    ];
+    // change preprocessors and plugins if coverage report is requested
+    if (config.reporters.indexOf('coverage') > -1) {
+        preprocessors.push('coverage');
+        plugins.push('karma-coverage');
+    }
+
     config.set({
         basePath: '',
         frameworks: ['mocha', 'chai'],
@@ -21,7 +36,7 @@ module.exports = function (config) {
 
         preprocessors: {
             // add webpack as preprocessor
-            'tests.webpack.js': ['webpack', 'sourcemap','coverage']
+            'tests.webpack.js': preprocessors,
 
         },
 
@@ -31,24 +46,14 @@ module.exports = function (config) {
         webpackServer: {
             noInfo: true
         },
-
-        plugins: [
-            'karma-mocha',
-            'karma-chai',
-            'karma-webpack',
-            'karma-phantomjs-launcher',
-            'karma-spec-reporter',
-            'karma-sourcemap-loader',
-            'karma-coverage'
-        ],
-
-        reporters: ['spec','coverage'],
+        plugins: plugins,
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         browsers: ['PhantomJS'],
         singleRun: !argv.watch,
         coverageReporter: {
+            includeAllSources: true,
             reporters: [
                 {type: 'lcov'},
                 {type: 'text-summary'}
