188dab6082b67e210d87e07d34c3418f597a5771
aaronlisman <donkey00>
2017-02-08 12:21:21 -0500

    Add code coverage reporting
    - Use istanbul instrumenter loader and coverageIstanbulReporter
    - Output lcov for codecov.io, update Travis to upload lcov file
    - Exclude API clients from testing
    - Since source files are not included in coverage report if they are not
      imported in a spec files, a bash script for generating spec files was made.
      The script uses a template to generate them.
    - Add the generated spec files for ts/tsx source files w/o one.
    
    
    Former-commit-id: fd1a549aab444996259e6853e1dce668abd892db

---
+++ src/pages/patientView/mutation/column/AnnotationColumnFormatter.spec.jsx
@@ -0,0 +1,21 @@
+import * as AnnotationColumnFormatter from './AnnotationColumnFormatter';
+import React from 'react';
+import { assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+
+describe('AnnotationColumnFormatter', () => {
+
+    before(()=>{
+
+    });
+
+    after(()=>{
+
+    });
+
+    it('what does it do?', ()=>{
+
+    });
+
+});
