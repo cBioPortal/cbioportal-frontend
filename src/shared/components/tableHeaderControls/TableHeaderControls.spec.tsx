d712ada7ba931d9f1356cd4f6744540b814cca6a
aaronlisman <donkey00>
2017-02-10 11:47:09 -0500

    tests for TableHeaderControls
    Former-commit-id: b521428578bde539bc31cde3902f50ba88551329

--- src/shared/components/tableHeaderControls/TableHeaderControls.spec.tsx
+++ src/shared/components/tableHeaderControls/TableHeaderControls.spec.tsx
@@ -1,16 +1,31 @@
+import * as React from 'react';
 import {assert} from "chai";
 import TableHeaderControls from "./TableHeaderControls";
 import sinon from 'sinon';
+import {mount, shallow, ReactWrapper} from 'enzyme';
+import {ColumnVisibilityControls} from "../columnVisibilityControls/ColumnVisibilityControls";
+import {TablePaginationControls} from "../tablePaginationControls/TablePaginationControls";
+import {ITableHeaderControlsProps} from "./TableHeaderControls";
 
 
-/**
- * @author Selcuk Onur Sumer
- */
 describe('TableHeaderControls', () => {
+
+    let wrapper: ReactWrapper<ITableHeaderControlsProps,{}>;
+    let inputStub: sinon.SinonStub;
+
+    before(()=> {
+        inputStub = sinon.stub(TableHeaderControls.prototype, "handleInput");
+
+        wrapper = mount(<TableHeaderControls searchClassName={searchClassName} copyDownloadClassName={copyClassName}  />);
+    });
+
+    after(()=>{
+        inputStub.restore();
+    });
 
     it('bindCopyButton is called only when there is a copy button and props flag is true', ()=>{
 
-        const mockInstance = {
+        let mockInstance = {
             bindCopyButton: sinon.stub(),
             props: { showCopyAndDownload: true },
             _copyButton: {}
@@ -19,11 +34,8 @@ describe('TableHeaderControls', () => {
         TableHeaderControls.prototype.componentDidMount.apply(mockInstance);
 
         assert.isTrue(mockInstance.bindCopyButton.calledOnce);
-    });
 
-    it('bindCopyButton is called only when there is a copy button and props flag is true', ()=>{
-
-        const mockInstance = {
+        mockInstance = {
             bindCopyButton: sinon.stub(),
             props: { showCopyAndDownload: false },
             _copyButton: {}
@@ -32,6 +44,52 @@ describe('TableHeaderControls', () => {
         TableHeaderControls.prototype.componentDidMount.apply(mockInstance);
 
         assert.isFalse(mockInstance.bindCopyButton.called);
+    });
+
+    const copyClassName = "copyClassName";
+    const searchClassName = "searchClassName";
+
+
+    it('Renders child controls according to prop flags', ()=>{
+
+
+        wrapper.setProps({
+            showHideShowColumnButton:false,
+            showPagination: false,
+            showCopyAndDownload: false,
+            showSearch:false
+        });
+        assert.equal( wrapper.find(ColumnVisibilityControls).length,  0 );
+        assert.equal( wrapper.find(TablePaginationControls).length, 0 );
+        assert.equal( wrapper.find(`.${copyClassName}`).length, 0 );
+        assert.equal( wrapper.find(`.${searchClassName}`).length, 0 );
+
+        wrapper.setProps({
+            showHideShowColumnButton:true,
+            showPagination: true,
+            showCopyAndDownload: true,
+            showSearch:true
+        }).update();
+
+        assert.equal( wrapper.find(ColumnVisibilityControls).length,  1 );
+        assert.equal( wrapper.find(TablePaginationControls).length, 1 );
+        assert.equal( wrapper.find(`.${copyClassName}`).length, 1 );
+        assert.equal( wrapper.find(`.${searchClassName}`).length, 1 );
+
+
+
+    });
+
+    it('input to search box causes handleInput to be called', ()=>{
+
+
+        var input = wrapper.find('input.tableSearchInput');
+
+        input.simulate('input', {key: 'Enter'})
+
+        assert.isTrue(inputStub.called);
+
+
     });
 
 });
