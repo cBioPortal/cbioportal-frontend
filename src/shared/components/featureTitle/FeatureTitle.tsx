5abd0b9265610bb6465d38c19e1c8a2201ae2854
aaronlisman <donkey00>
2017-02-15 11:35:14 -0500

    implement cna table in new tab of patient view
    Former-commit-id: 73413157e41c5e676d350749968aeb48692a98ce

--- src/shared/components/featureTitle/FeatureTitle.tsx
+++ src/shared/components/featureTitle/FeatureTitle.tsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import Spinner from "react-spinkit";
-import { If } from 'react-if';
+import { If, Else } from 'react-if';
 
 export interface IFeatureTitleProps {
     isLoading:Boolean;
     title:string;
     className?:string;
+    isHidden?:Boolean;
 }
 
 export default class FeatureTitle extends React.Component<IFeatureTitleProps, {}> {
 
     public render() {
-        return (
-            <h4 className={this.props.className || ''}>{this.props.title}
-                <If condition={this.props.isLoading}>
-                    <Spinner spinnerName="three-bounce" style={{ display:'inline-block', marginLeft:10 }} noFadeIn={true}  />
-                </If>
-            </h4>
+       return (
+            <If condition={this.props.isHidden}>
+
+                <Else><h4 className={this.props.className || ''}>{this.props.title}
+                    <If condition={this.props.isLoading}>
+                        <Spinner spinnerName="three-bounce" style={{ display:'inline-block', marginLeft:10 }} noFadeIn={true}  />
+                    </If>
+                </h4></Else>
+            </If>
         );
     }
 }
