8735bf83fe22b26cbfd2c5f28f4c329863cb9c76
Andy Dufilie <andy.dufilie@gmail.com>
2017-03-02 15:56:01 -0500

    Add config properties and typings
    Former-commit-id: e59dcb346c4fcd38e8ee943960328b6d45c2514d

--- src/config/IAppConfig.ts
+++ src/config/IAppConfig.ts
@@ -3,4 +3,10 @@ export interface IAppConfig {
     hotspotsApiRoot: string;
     hotspots3DApiRoot: string;
     tissueImageCheckUrl: string;
+    cancerStudySearchPresets: string[];
+    priorityStudies: PriorityStudies;
 }
+
+export type PriorityStudies = {
+	[category:string]: string[]
+};
--- src/config/production.config.ts
+++ src/config/production.config.ts
@@ -1,11 +1,23 @@
 import {IAppConfig} from "./IAppConfig";
 
-const config = {
+const config:IAppConfig = {
     apiRoot: 'cbioportal-rc.herokuapp.com/api',
     hotspotsApiRoot: 'cancerhotspots.org',
     hotspots3DApiRoot: '3dhotspots.org/3d',
-    tissueImageCheckUrl: '//cancer.digitalslidearchive.net/local_php/get_slide_list_from_db_groupid_not_needed.php?slide_name_filter='
-
-} as IAppConfig;
+    tissueImageCheckUrl: '//cancer.digitalslidearchive.net/local_php/get_slide_list_from_db_groupid_not_needed.php?slide_name_filter=',
+    cancerStudySearchPresets: [
+        'tcga',
+        'tcga -provisional',
+        'tcga -moratorium',
+        'tcga OR icgc',
+        '-"cell line"',
+        'prostate mskcc',
+        'esophageal OR stomach',
+        'serous',
+        'breast',
+    ],
+    priorityStudies: {
+    },
+};
 
 export default config;
