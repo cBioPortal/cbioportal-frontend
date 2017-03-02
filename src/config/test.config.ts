10a2e20c6c55d2adcebf02f5f4d5404a6c98382a
aaronlisman <donkey00>
2017-03-02 17:20:57 -0500

    add test.config.js to fix tests
    Former-commit-id: 0ad9b0fe78a95fd5772d643b6654b84708c789fe

---
+++ src/config/test.config.ts
@@ -0,0 +1,25 @@
+import {IAppConfig} from "./IAppConfig";
+
+const config:IAppConfig = {
+    apiRoot: 'cbioportal-rc.herokuapp.com/api',
+    hotspotsApiRoot: 'cancerhotspots.org',
+    hotspots3DApiRoot: '3dhotspots.org/3d',
+    tissueImageCheckUrl: '//cancer.digitalslidearchive.net/local_php/get_slide_list_from_db_groupid_not_needed.php?slide_name_filter=',
+    cancerStudySearchPresets: [
+        'tcga',
+        'tcga -provisional',
+        'tcga -moratorium',
+        'tcga OR icgc',
+        '-"cell line"',
+        'prostate mskcc',
+        'esophageal OR stomach',
+        'serous',
+        'breast',
+    ],
+    priorityStudies: {
+        'Shared institutional Data Sets': ['mskimpact', 'cellline_mskcc'],
+        'Priority Studies': ['blca_tcga_pub', 'coadread_tcga_pub', 'brca_tcga_pub2015'], // for demo
+    },
+};
+
+export default config;
