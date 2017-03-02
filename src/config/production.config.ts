89b2e0648a498d1b85086ac5321265a6884d0e29
aaronlisman <donkey00>
2017-03-02 14:08:36 -0500

    add missing configuration files
    Former-commit-id: 75fd23e2c6b6c2a6394ed4976b57191be599073c

---
+++ src/config/production.config.ts
@@ -0,0 +1,10 @@
+import {IAppConfig} from "./IAppConfig";
+
+const config = {
+    apiRoot: 'cbioportal-rc.herokuapp.com/api',
+    hotspotsApiRoot: 'cancerhotspots.org',
+    hotspots3DApiRoot: '3dhotspots.org/3d',
+
+} as IAppConfig;
+
+export default config;
