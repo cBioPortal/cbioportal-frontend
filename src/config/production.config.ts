40312602b7fbbe83eea3e2f132daafa42c676ccb
aaronlisman <donkey00>
2017-03-02 15:05:33 -0500

    add missing prop on production.config.ts
    Former-commit-id: 5824fd8b785d4808b83088b4ddab5f2d392c4d66

--- src/config/production.config.ts
+++ src/config/production.config.ts
@@ -4,6 +4,7 @@ const config = {
     apiRoot: 'cbioportal-rc.herokuapp.com/api',
     hotspotsApiRoot: 'cancerhotspots.org',
     hotspots3DApiRoot: '3dhotspots.org/3d',
+    tissueImageCheckUrl: '//cancer.digitalslidearchive.net/local_php/get_slide_list_from_db_groupid_not_needed.php?slide_name_filter='
 
 } as IAppConfig;
 
