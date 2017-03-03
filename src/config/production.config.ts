3ab8a331ea23663b1b725b881bcd4854e68c8627
aaronlisman <donkey00>
2017-03-03 17:26:22 -0500

    Apply proxy to urls in new configuration files
    Former-commit-id: 3c67f0dabbe50600081fb4eaaf2cf4de8dba3dd4

--- src/config/production.config.ts
+++ src/config/production.config.ts
@@ -2,10 +2,10 @@ import {IAppConfig} from "./IAppConfig";
 
 const config:IAppConfig = {
     apiRoot: 'cbioportal-rc.herokuapp.com/api',
-    hotspotsApiRoot: 'cancerhotspots.org',
-    hotspots3DApiRoot: '3dhotspots.org/3d',
-    oncoKbApiRoot: 'oncokb.org/api/v1',
-    tissueImageCheckUrl: '//cancer.digitalslidearchive.net/local_php/get_slide_list_from_db_groupid_not_needed.php?slide_name_filter=',
+    hotspotsApiRoot: 'cbioportal-rc.herokuapp.com/proxy/cancerhotspots.org',
+    hotspots3DApiRoot: 'cbioportal-rc.herokuapp.com/proxy/3dhotspots.org/3d',
+    oncoKbApiRoot: 'cbioportal-rc.herokuapp.com/proxy/oncokb.org/api/v1',
+    tissueImageCheckUrl: 'cbioportal-rc.herokuapp.com/proxy/cancer.digitalslidearchive.net/local_php/get_slide_list_from_db_groupid_not_needed.php?slide_name_filter=',
     cancerStudySearchPresets: [
         'tcga',
         'tcga -provisional',
