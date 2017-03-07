9e38d15a26282873125d8b7eed3ed66d31bbf305
Andy Dufilie <andy.dufilie@gmail.com>
2017-03-07 15:12:51 -0500

    Use mobxpromise npm module and @cached decorator
    Former-commit-id: 7bfef5ce5f9547d2999a99216a0100894fb72663

--- src/shared/api/remoteData.ts
+++ src/shared/api/remoteData.ts
@@ -1,24 +1,7 @@
 import * as seamlessImmutable from 'seamless-immutable';
-import {
-    MobxPromiseInputParamsWithDefault,
-    MobxPromiseUnionTypeWithDefault,
-    MobxPromiseInputUnion,
-    MobxPromiseUnionType,
-    MobxPromiseClass,
-    default as MobxPromise
-} from "./MobxPromise";
+import {createMobxPromiseFactory} from 'mobxpromise';
 
 /**
  * Constructs a MobxPromise which will call seamlessImmutable.from() on the result and the default value.
  */
-export function remoteData<R>(input:MobxPromiseInputParamsWithDefault<R>):MobxPromiseUnionTypeWithDefault<R>;
-export function remoteData<R>(input:MobxPromiseInputUnion<R>, defaultResult: R):MobxPromiseUnionTypeWithDefault<R>;
-export function remoteData<R>(input:MobxPromiseInputUnion<R>):MobxPromiseUnionType<R>;
-export function remoteData<R>(input:MobxPromiseInputUnion<R>, defaultResult?: R)
-{
-    input = MobxPromiseClass.normalizeInput(input, defaultResult);
-    const invoke = input.invoke;
-    input.invoke = () => invoke().then(result => seamlessImmutable.from(result));
-    input.default = seamlessImmutable.from(input.default);
-    return new MobxPromise(input);
-}
+export const remoteData = createMobxPromiseFactory(seamlessImmutable.from);
