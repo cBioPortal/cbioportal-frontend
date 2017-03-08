65925310587e31e2cf4ea737a722881e007bf7f7
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-03-08 16:46:14 -0500

    finish all columns except annotation
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/patientView/mutation/column/AlleleFreqColumnFormatter.tsx
+++ src/pages/patientView/mutation/column/AlleleFreqColumnFormatter.tsx
@@ -7,6 +7,7 @@ import {compareNumberLists} from '../../../../shared/lib/SortUtils';
 import 'rc-tooltip/assets/bootstrap_white.css';
 import {MutationTableRowData} from "../../../../shared/components/mutationTable/IMutationTableProps";
 import {Mutation} from "../../../../shared/api/generated/CBioPortalAPI";
+import SampleManager from "../../sampleManager";
 
 export default class AlleleFreqColumnFormatter {
     static barWidth = 6;
@@ -14,10 +15,13 @@ export default class AlleleFreqColumnFormatter {
     static maxBarHeight = 12;
     static indexToBarLeft = (n:number) => n*(AlleleFreqColumnFormatter.barWidth + AlleleFreqColumnFormatter.barSpacing);
 
-    public static renderFunction(data:IColumnFormatterData<MutationTableRowData>, columnProps:any) {
-        const sampleOrder = columnProps.sampleManager.getSampleIdsInOrder();
+    public static renderFunction(mutations:Mutation[], sampleManager:SampleManager|null) {
+        if (!sampleManager) {
+            return (<span></span>);
+        }
+
+        const sampleOrder = sampleManager.getSampleIdsInOrder();
         const barX = sampleOrder.reduce((map:{[s:string]:number}, sampleId:string, i:number) => {map[sampleId] = AlleleFreqColumnFormatter.indexToBarLeft(i); return map;}, {});
-        const mutations:Array<Mutation> = data.rowData || [];
         const sampleElements = mutations.map(function(mutation:Mutation) {
             const altReads = mutation.tumorAltCount;
             const refReads = mutation.tumorRefCount;
@@ -29,11 +33,11 @@ export default class AlleleFreqColumnFormatter {
             const barY = AlleleFreqColumnFormatter.maxBarHeight - barHeight;
 
 
-            const bar = (<rect x={barX[mutation.sampleId]} y={barY} width={AlleleFreqColumnFormatter.barWidth} height={barHeight} fill={columnProps.sampleManager.getColorForSample(mutation.sampleId)}/>);
+            const bar = (<rect x={barX[mutation.sampleId]} y={barY} width={AlleleFreqColumnFormatter.barWidth} height={barHeight} fill={sampleManager.getColorForSample(mutation.sampleId)}/>);
 
             const circleRadius = 6;
             const sampleId = mutation.sampleId;
-            const component = columnProps.sampleManager.getComponentForSample(sampleId);
+            const component = sampleManager.getComponentForSample(sampleId);
 
             const text = (<span>
                     <strong>{Math.round(100*freq)/100}</strong> {`(${altReads} variant reads out of ${altReads+refReads} total)`}
@@ -42,43 +46,57 @@ export default class AlleleFreqColumnFormatter {
                 sampleId, bar, component, text, freq
             };
         });
-        const sampleToElements = sampleElements.reduce((map:{[s:string]:any}, elements:any) => {if (elements) { map[elements.sampleId] = elements }; return map; }, {});
+        const sampleToElements = sampleElements.reduce((map:{[s:string]:any}, elements:any) => {if (elements) { map[elements.sampleId] = elements } return map; }, {});
         const elementsInSampleOrder = sampleOrder.map((sampleId:string) => sampleToElements[sampleId]).filter((x:any) => !!x);
         const tooltipLines = elementsInSampleOrder.map((elements:any)=>(<span key={elements.sampleId}>{elements.component}  {elements.text}<br/></span>));
         const freqs = sampleOrder.map((sampleId:string) => (sampleToElements[sampleId] && sampleToElements[sampleId].freq) || undefined);
         const bars = elementsInSampleOrder.map((elements:any)=>elements.bar);
 
-        return (<Td key={data.name} column={data.name} value={freqs}>
-            <If condition={columnProps.sampleManager.samples.length === 1}>
-            <Then>
-                <span>{ (!isNaN(freqs[0]) ? Math.round(100*freqs[0])/100 : '') }</span>
-            </Then>
-            <Else>
-                <If condition={tooltipLines.length > 0}>
-                    <DefaultTooltip
-                        placement="left"
-                        overlay={tooltipLines}
-                        arrowContent={<div className="rc-tooltip-arrow-inner"/>}
-                    >
+        let ret;
+        if (sampleManager.samples.length === 1) {
+            ret = (<span>{ (!isNaN(freqs[0]) ? Math.round(100*freqs[0])/100 : '') }</span>);
+        } else if (tooltipLines.length > 0) {
+            const overlay = (<span>{tooltipLines}</span>);
+            ret = (<DefaultTooltip
+                placement="left"
+                overlay={overlay}
+                arrowContent={<div className="rc-tooltip-arrow-inner"/>}
+            >
 
-                        <svg
-                            width={AlleleFreqColumnFormatter.getSVGWidth(sampleOrder.length)}
-                            height={AlleleFreqColumnFormatter.maxBarHeight}
-                        >
-                            {bars}
-                        </svg>
-                    </DefaultTooltip>
-                </If>
-            </Else>
-            </If>
-        </Td>);
+                <svg
+                    width={AlleleFreqColumnFormatter.getSVGWidth(sampleOrder.length)}
+                    height={AlleleFreqColumnFormatter.maxBarHeight}
+                >
+                    {bars}
+                </svg>
+            </DefaultTooltip>);
+        } else {
+            ret = (<span></span>);
+        }
+        return ret;
     }
 
     public static getSVGWidth(numSamples:number) {
         return numSamples*AlleleFreqColumnFormatter.barWidth + (numSamples-1)*AlleleFreqColumnFormatter.barSpacing
     }
 
-    public static sortFunction(a:number[], b:number[]):number {
-        return compareNumberLists(a, b);
+    public static getSortValue(d:Mutation[], sampleManager:SampleManager|null) {
+        if (!sampleManager) {
+            return [Number.POSITIVE_INFINITY];
+        }
+
+        // frequencies in sample order
+        const sampleToMutation = d.reduce((map:{[s:string]:Mutation}, next:Mutation)=>{
+            map[next.sampleId] = next;
+            return map;
+        }, {});
+        return sampleManager.getSampleIdsInOrder().map(sampleId=>sampleToMutation[sampleId]).map(mutation=>{
+            const altReads = mutation.tumorAltCount;
+            const refReads = mutation.tumorRefCount;
+            if ((altReads < 0) || (refReads < 0)) {
+                return null;
+            }
+            return (altReads / (altReads + refReads));
+        });
     }
 }
--- src/shared/components/mutationTable/column/SampleColumnFormatter.spec.tsx
+++ src/shared/components/mutationTable/column/SampleColumnFormatter.spec.tsx
@@ -17,36 +17,16 @@ describe('SampleColumnFormatter', () => {
         sampleId: "This_is_a_quite_long_Sample_Id_in_my_opinion!"
     });
 
-    const shortSampleId = {
-        name: "Sample",
-        columnData: mutationShort.sampleId
-    };
-
-    const longSampleId = {
-        name: "Sample",
-        columnData: mutationLong.sampleId
-    };
-
     let componentShort:ReactWrapper<any, any>;
     let componentLong:ReactWrapper<any, any>;
 
-    const tableData = [[mutationShort], [mutationLong]];
-
     before(() => {
-        let data = {
-            name: "Sample",
-            tableData,
-            rowData: [mutationShort]
-        };
+        let data = [mutationShort];
 
         // mount a single cell component (Td) for the mutation with short sample id
         componentShort = mount(SampleColumnFormatter.renderFunction(data));
 
-        data = {
-            name: "Sample",
-            tableData,
-            rowData: [mutationLong]
-        };
+        data = [mutationLong];
 
         // mount a single cell component (Td) for the mutation with long sample id
         componentLong = mount(SampleColumnFormatter.renderFunction(data));
@@ -54,28 +34,28 @@ describe('SampleColumnFormatter', () => {
 
     it('sample id text is properly formatted', () => {
         // text and display should be the same for short sample ids
-        assert.equal(SampleColumnFormatter.getTextValue(shortSampleId),
-                     SampleColumnFormatter.getDisplayValue(shortSampleId));
+        assert.equal(SampleColumnFormatter.getTextValue([mutationShort]),
+                     SampleColumnFormatter.getDisplayValue([mutationShort]));
 
         // text and display values should be different,
         // and truncated size should have fixed number of characters
-        assert.equal(SampleColumnFormatter.getDisplayValue(longSampleId).length,
+        assert.equal(SampleColumnFormatter.getDisplayValue([mutationLong]).length,
                      SampleColumnFormatter.MAX_LENGTH + SampleColumnFormatter.SUFFIX.length);
 
         // suffix should be appended in the end
-        assert.isTrue(SampleColumnFormatter.getDisplayValue(longSampleId).indexOf(
+        assert.isTrue(SampleColumnFormatter.getDisplayValue([mutationLong]).indexOf(
                           SampleColumnFormatter.SUFFIX) > -1);
     });
 
     it('sample id style class is properly set', () => {
 
         // no tooltips for short ids
-        assert.notEqual(SampleColumnFormatter.getTooltipValue(shortSampleId.columnData),
-                        SampleColumnFormatter.getTextValue(shortSampleId));
+        assert.notEqual(SampleColumnFormatter.getTooltipValue(SampleColumnFormatter.getTextValue([mutationShort])),
+                        SampleColumnFormatter.getTextValue([mutationShort]));
 
         // tooltip value should be the same as text value for long ids
-        assert.equal(SampleColumnFormatter.getTooltipValue(longSampleId.columnData),
-                     SampleColumnFormatter.getTextValue(longSampleId));
+        assert.equal(SampleColumnFormatter.getTooltipValue(SampleColumnFormatter.getTextValue([mutationLong])),
+            SampleColumnFormatter.getTextValue([mutationLong]));
     });
 
     it('renders sample display value', () => {
@@ -83,13 +63,6 @@ describe('SampleColumnFormatter', () => {
             'Display value is correct for short sample id');
         assert.isFalse(componentLong.find(`span`).text().indexOf("This_is_a_quite_long_Sample_Id_in_my_opinion!") > -1,
             'Display value for long sample id should not be equal to the actual value');
-    });
-
-    it('sets component cell value property', () => {
-        assert.equal(componentShort.prop("value"), "Short_Id",
-            'Cell (Td) value property is correct for short sample id');
-        assert.equal(componentLong.prop("value"), "This_is_a_quite_long_Sample_Id_in_my_opinion!",
-            'Cell (Td) value property is correct for long sample id');
     });
 
     it('generates component tooltip', () => {
