13173f70ea37ebacc773723cce5db5dce5e93bf3
Onur Sumer <s.onur.sumer@gmail.com>
2017-03-17 16:13:51 -0400

    made Copy Number Alterations table compatible with the latest column definition

--- src/pages/patientView/copyNumberAlterations/CopyNumberTableWrapper.spec.tsx
+++ src/pages/patientView/copyNumberAlterations/CopyNumberTableWrapper.spec.tsx
@@ -1,4 +1,3 @@
-import { renderAlterationTypes, AlterationTypes } from './CopyNumberTableWrapper';
 import React from 'react';
 import { assert } from 'chai';
 import { shallow, mount } from 'enzyme';
@@ -7,23 +6,11 @@ import {PatientViewPageStore} from "../clinicalInformation/PatientViewPageStore"
 
 describe('CopyNumberTableWrapper', () => {
 
-
     before(()=>{
+
     });
 
     after(()=>{
-
-    });
-
-    it('CNA column renderer shows correct text based on alteration value', ()=>{
-
-        let output = mount(renderAlterationTypes(-2));
-
-        assert.equal(output.text(), 'DeepDel');
-
-        output = mount(renderAlterationTypes(2));
-
-        assert.equal(output.text(), 'AMP');
 
     });
 
