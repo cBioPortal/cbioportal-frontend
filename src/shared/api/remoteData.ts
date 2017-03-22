e4edc8e2a37c9a01dc78ebb0a94f3de39e1a1495
Andy Dufilie <andy.dufilie@gmail.com>
2017-03-22 14:23:32 -0400

    Fix indentation

--- src/shared/api/remoteData.ts
+++ src/shared/api/remoteData.ts
@@ -5,22 +5,22 @@ import {MobxPromiseImpl, MobxPromise, MobxPromiseFactory, MobxPromiseInputUnion,
  * Constructs a MobxPromise which will call seamlessImmutable.from() on the result and the default value.
  */
 export const remoteData:MobxPromiseFactory = function<R>(input:MobxPromiseInputUnion<R>, defaultResult?:R) {
-	const normalizedInput = MobxPromiseImpl.normalizeInput(input, defaultResult);
-	const {invoke, onError} = normalizedInput;
-	const mobxPromise = new MobxPromise({
-		...input,
-		invoke: () => invoke().then(seamlessImmutable.from),
-		default: seamlessImmutable.from(normalizedInput.default),
-		onError: error => {
-			if (onError)
-			{
-				onError(error);
-			}
-			else if (!hasObservers(mobxPromise, 'error'))
-			{
-				console.log(`Unhandled ${error}`);
-			}
-		},
-	});
-	return mobxPromise;
+    const normalizedInput = MobxPromiseImpl.normalizeInput(input, defaultResult);
+    const {invoke, onError} = normalizedInput;
+    const mobxPromise = new MobxPromise({
+        ...input,
+        invoke: () => invoke().then(seamlessImmutable.from),
+        default: seamlessImmutable.from(normalizedInput.default),
+        onError: error => {
+            if (onError)
+            {
+                onError(error);
+            }
+            else if (!hasObservers(mobxPromise, 'error'))
+            {
+                console.log(`Unhandled ${error}`);
+            }
+        },
+    });
+    return mobxPromise;
 };
