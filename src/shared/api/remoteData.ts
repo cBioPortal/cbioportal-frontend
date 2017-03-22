a868199f45a81ff09e71c3f4df167b02e9a7652b
Andy Dufilie <andy.dufilie@gmail.com>
2017-03-22 14:15:36 -0400

    Make remoteData() log unhandled errors

--- src/shared/api/remoteData.ts
+++ src/shared/api/remoteData.ts
@@ -1,7 +1,26 @@
 import * as seamlessImmutable from 'seamless-immutable';
-import {createMobxPromiseFactory} from 'mobxpromise';
+import {MobxPromiseImpl, MobxPromise, MobxPromiseFactory, MobxPromiseInputUnion, hasObservers} from 'mobxpromise';
 
 /**
  * Constructs a MobxPromise which will call seamlessImmutable.from() on the result and the default value.
  */
-export const remoteData = createMobxPromiseFactory(seamlessImmutable.from);
+export const remoteData:MobxPromiseFactory = function<R>(input:MobxPromiseInputUnion<R>, defaultResult?:R) {
+	const normalizedInput = MobxPromiseImpl.normalizeInput(input, defaultResult);
+	const {invoke, onError} = normalizedInput;
+	const mobxPromise = new MobxPromise({
+		...input,
+		invoke: () => invoke().then(seamlessImmutable.from),
+		default: seamlessImmutable.from(normalizedInput.default),
+		onError: error => {
+			if (onError)
+			{
+				onError(error);
+			}
+			else if (!hasObservers(mobxPromise, 'error'))
+			{
+				console.log(`Unhandled ${error}`);
+			}
+		},
+	});
+	return mobxPromise;
+};
