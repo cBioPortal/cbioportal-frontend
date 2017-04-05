ef580bce08db9b8bf8e2b3dfd4906437b11f8ad0
Onur Sumer <s.onur.sumer@gmail.com>
2017-04-05 19:10:53 -0400

    removed unused imports and components

--- src/pages/patientView/mutation/column/ProteinChangeColumnFormatter.spec.tsx
+++ src/pages/patientView/mutation/column/ProteinChangeColumnFormatter.spec.tsx
@@ -1,7 +1,5 @@
 import ProteinChangeColumnFormatter from './ProteinChangeColumnFormatter';
 import styles from './style/proteinChange.module.scss';
-import {IColumnFormatterData} from "shared/components/enhancedReactTable/IColumnFormatter";
-import {MutationTableRowData} from "shared/components/mutationTable/IMutationTableProps";
 import {initMutation} from "test/MutationMockUtils";
 import {Mutation} from "shared/api/generated/CBioPortalAPI";
 import React from 'react';
