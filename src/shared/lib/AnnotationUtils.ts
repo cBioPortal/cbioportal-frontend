03dd9ca58d5e76f018b1690fd36010fff6bccd6b
Onur Sumer <s.onur.sumer@gmail.com>
2017-04-06 15:46:43 -0400

    improved hotspot mutation matching

--- src/shared/lib/AnnotationUtils.ts
+++ src/shared/lib/AnnotationUtils.ts
@@ -1,7 +1,12 @@
+import * as _ from 'lodash';
 import {CosmicMutation} from "shared/api/generated/CBioPortalAPIInternal";
 import {ICosmicData} from "shared/components/mutationTable/column/CosmicColumnFormatter";
-import {IMyCancerGenome, IMyCancerGenomeData} from "pages/patientView/mutation/column/AnnotationColumnFormatter";
+import {
+    IMyCancerGenome, IMyCancerGenomeData, IHotspotIndex, IHotspotLookup
+} from "pages/patientView/mutation/column/AnnotationColumnFormatter";
 import {HotspotMutation} from "../api/generated/CancerHotspotsAPI";
+import {Mutation} from "../api/generated/CBioPortalAPI";
+import HotspotSet from "./HotspotSet";
 
 /**
  * Utility functions related to annotation data.
@@ -45,18 +50,140 @@ export function geneToMyCancerGenome(myCancerGenomes:IMyCancerGenome[]):IMyCance
     return map;
 }
 
-export function geneAndProteinPosToHotspots(hotspots:HotspotMutation[]):{[key:string]: boolean}
+export function indexHotspots(hotspots:HotspotMutation[], defaultHotspotType: string = "hotspot"): IHotspotIndex
 {
-    // key: geneSymbol_proteinPosition
-    // (protienPosition: start[_end])
-    const map: {[key:string]: boolean} = {};
+    const index:IHotspotIndex = {};
 
-    // create a map for a faster lookup
-    hotspots.forEach(function(hotspot:HotspotMutation) {
-        const positions = hotspot.residue.match(/[0-9]+/g) || []; // start (and optionally end) positions
-        const key = [hotspot.hugoSymbol.toUpperCase()].concat(positions).join("_");
-        map[key] = true;
+    hotspots.forEach((hotspot:HotspotMutation) => {
+        const geneSymbol = hotspot.hugoSymbol.toUpperCase();
+        let geneIndex = index[geneSymbol];
+
+        if (!geneIndex) {
+            geneIndex = index[geneSymbol] = {};
+        }
+
+        const hotspotType = hotspot.type || defaultHotspotType;
+        let lookup = geneIndex[hotspotType];
+
+        if (!lookup)
+        {
+            lookup = {
+                hotspotMutations: [],
+                hotspotSet: new HotspotSet([]) // this is a placeholder to avoid TS errors.
+                                               // overwritten by the initHotspotSets function.
+            };
+
+            geneIndex[hotspotType] = lookup;
+        }
+
+        lookup.hotspotMutations.push(hotspot);
     });
 
-    return map;
+    initHotspotSets(index);
+
+    return index;
+}
+
+export function initHotspotSets(hotspotIndex: IHotspotIndex)
+{
+    _.keys(hotspotIndex).forEach((geneSymbol:string) => {
+        const geneIndex = hotspotIndex[geneSymbol];
+
+        _.keys(geneIndex).forEach((hotspotType:string) => {
+            const lookup = geneIndex[hotspotType];
+
+            const intervals = lookup.hotspotMutations.map(
+                (hotspot:HotspotMutation):[number, number] => {
+                    // start (and optionally end) positions
+                    const positions = hotspot.residue.match(/[0-9]+/g) || [];
+
+                    let start = -1;
+                    let end = -1;
+
+                    if (positions.length > 0) {
+                        start = parseInt(positions[0]);
+                        end = start;
+                    }
+
+                    // overwrite end if exists
+                    if (positions.length > 1) {
+                        end = parseInt(positions[1]);
+                    }
+
+                    return [start, end];
+                }
+            );
+
+            lookup.hotspotSet = new HotspotSet(intervals);
+        });
+    });
+}
+
+export function isHotspot(mutation:Mutation, index:IHotspotIndex):boolean
+{
+    let isHotspot = false;
+    let hotspotType:string|null = null;
+    const mutationType = mutation.mutationType && mutation.mutationType.toLowerCase();
+
+    if (mutationType)
+    {
+        if (mutationType.indexOf("missense") > -1) {
+            hotspotType = "single residue";
+        }
+        else if (mutationType.indexOf("inframe") > -1 ||
+            mutationType.indexOf("in_frame") > -1 ||
+            mutationType.indexOf("in frame") > -1)
+        {
+            hotspotType = "in-frame indel";
+        }
+    }
+
+    if (hotspotType)
+    {
+        const geneSymbol = mutation.gene && mutation.gene.hugoGeneSymbol;
+        const geneIndex = index[geneSymbol];
+
+        if (geneIndex)
+        {
+            const lookup = geneIndex[hotspotType];
+
+            if (lookup) {
+                isHotspot = checkHotspot(mutation, lookup.hotspotSet);
+            }
+        }
+    }
+
+    return isHotspot;
+}
+
+export function is3dHotspot(mutation:Mutation, index:IHotspotIndex):boolean
+{
+    let isHotspot = false;
+
+    const geneSymbol = mutation.gene && mutation.gene.hugoGeneSymbol;
+    const geneIndex = index[geneSymbol];
+
+    if (geneIndex) {
+        _.values(geneIndex).forEach((lookup: IHotspotLookup) => {
+            isHotspot = checkHotspot(mutation, lookup.hotspotSet);
+        });
+    }
+
+    return isHotspot;
+}
+
+function checkHotspot(mutation: Mutation, hotspotSet: HotspotSet)
+{
+    let isHotspot = false;
+    const start = mutation.proteinPosStart;
+
+    // only check for valid start position values
+    if (start >= 0)
+    {
+        // if proteinPosEnd value is not valid, use start as end
+        const end = mutation.proteinPosEnd >= start ? mutation.proteinPosEnd : undefined;
+        isHotspot = hotspotSet.check(start, end);
+    }
+
+    return isHotspot;
 }
