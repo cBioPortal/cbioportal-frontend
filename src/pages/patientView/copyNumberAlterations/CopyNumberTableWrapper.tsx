81b13f0511047a5929c9a317722b7a920d96b29a
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-04-06 14:18:11 -0400

    Change firstSortDirection to defaultSortDirection
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 261e6375189354726a103ecc25d79c8688ea93cf

--- src/pages/patientView/copyNumberAlterations/CopyNumberTableWrapper.tsx
+++ src/pages/patientView/copyNumberAlterations/CopyNumberTableWrapper.tsx
@@ -90,7 +90,7 @@ export default class CopyNumberTableWrapper extends React.Component<{ store:Pati
                 }
             },
             tooltip: (<span>Alteration frequency in cohort</span>),
-            firstSortDirection: "desc",
+            defaultSortDirection: "desc",
             order: 80
         });
 
