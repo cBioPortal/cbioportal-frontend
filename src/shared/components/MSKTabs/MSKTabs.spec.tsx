63e816471e08fce8237921f3a69ba3e364afcf85
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-04-11 14:20:25 -0400

    MSKTab loading and hide functionality, to solve problem
    Issue is that when URL linking directly to a tab, if that tabs content is pending
    so that the tab doesnt exist yet, then it starts at the first tab, then snaps to the
    new one when it appears, which is not ideal for a few reasons. Its also causing an SVG bug
    in the Genomic Tracks feature which we don't feel like fixing.
    
    So this makes it easy for a tab to be marked as loading, so that it shows a loading spinner
    instead of the given content; also, it can be dynamically hidden, using a prop value instead
    of ternary logic or <If> or anything like that.
    
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/shared/components/MSKTabs/MSKTabs.spec.tsx
+++ src/shared/components/MSKTabs/MSKTabs.spec.tsx
@@ -1,22 +1,26 @@
 import { MSKTab, MSKTabs } from './MSKTabs';
 import React from 'react';
 import { assert } from 'chai';
-import { shallow, mount } from 'enzyme';
+import {shallow, mount, ReactWrapper} from 'enzyme';
 import sinon from 'sinon';
+import Spinner from "react-spinkit";
 
-describe('IMutationTableProps', () => {
+describe('MSKTabs', () => {
 
     let tabs: any;
+
+    function tabText(tabs:ReactWrapper<any,any>):string[] {
+        return tabs.find('ul.nav-tabs').find('li').map(x=>x.text());
+    }
 
     beforeEach(()=>{
 
         tabs = mount(
-          <MSKTabs onTabClick={()=>{}}>
-              <MSKTab id="one" linkText="One">One</MSKTab>
-              <MSKTab linkText="Two" id="two">Two</MSKTab>
+          <MSKTabs>
+              <MSKTab id="one" linkText="One"><span className="content">One</span></MSKTab>
+              <MSKTab linkText="Two" id="two"><span className="content">Two</span></MSKTab>
           </MSKTabs>
         );
-
     });
 
     after(()=>{
@@ -44,6 +48,39 @@ describe('IMutationTableProps', () => {
         tabs.setProps({ activeTabId:"one" });
         assert.isTrue(tabs.find(MSKTab).at(1).hasClass('hidden'));
         assert.isFalse(tabs.find(MSKTab).at(0).hasClass('hidden'));
+    });
+
+    it('does not display tabs that have hide={true}', ()=>{
+        let tabs2 = mount(<MSKTabs>
+            <MSKTab id="one" linkText="One"><span className="content">One</span></MSKTab>
+            <MSKTab linkText="Two" id="two" hide={true}><span className="content">Two</span></MSKTab>
+        </MSKTabs>);
+        assert.deepEqual(tabText(tabs2), ["One"]);
+    });
+
+    it('does not display the content of tabs that have loading={true}, instead showing a spinner; and ' +
+        'does not show a tab with loading={true} unless it is the active tab', ()=>{
+
+        tabs.setProps({ activeTabId: "one" });
+        const tab = tabs.find(MSKTab).at(0);
+        let span:ReactWrapper<any,any> = tab.find("span").at(0);
+        assert(span.exists(), "the span exists");
+        assert.equal(span.text(), "One");
+
+        let tabs2 = mount(<MSKTabs activeTabId="one">
+            <MSKTab id="one" linkText="One" loading={true}><span className="content">One</span></MSKTab>
+            <MSKTab linkText="Two" id="two"><span className="content">Two</span></MSKTab>
+        </MSKTabs>);
+
+        const tab2 = tabs2.find(MSKTab).at(0);
+        span = tab2.find("span").at(0);
+        assert(!span.exists(), "the span does not exist for a loading tab");
+        assert(tab2.find(Spinner).at(0).exists(), "a loading tab contains a spinner element");
+        assert.deepEqual(tabText(tabs2), ["One", "Two"], "both tabs visible");
+
+        tabs2.setProps({ activeTabId: "two" });
+        assert.deepEqual(tabText(tabs2), ["Two"], "only one tab visible, since the other is loading");
+
     });
 
 });
