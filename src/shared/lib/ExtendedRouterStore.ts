97be2b4a8a76780a885dbe4d1e883e22e4221487
Andy Dufilie <andy.dufilie@gmail.com>
2017-04-13 11:05:10 -0400

    Use 'url' package for building and parsing urls

--- src/shared/lib/ExtendedRouterStore.ts
+++ src/shared/lib/ExtendedRouterStore.ts
@@ -1,18 +1,11 @@
 import { RouterStore } from 'mobx-react-router';
 import { action, computed } from 'mobx';
 import * as _ from 'lodash';
-import * as $ from 'jquery';
-import URI from 'urijs';
+import URL, {QueryParams} from 'url';
 
-// NOTE: there's a problem with mobx-react-router types so we have to type a few things here
-// which should be derived from typings of super class
-export default class ExtendedRouterStore extends (RouterStore as FunctionConstructor) {
+export default class ExtendedRouterStore extends RouterStore {
 
-    push: (newHash: string)=>void;
-
-    location: any; // for some reason cannot get TS to recongize super class
-
-    @action updateRoute(newParams: any, path = this.location.pathname) {
+    @action updateRoute(newParams: QueryParams, path = this.location.pathname) {
 
         let newQuery = _.clone(this.location.query);
 
@@ -24,13 +17,10 @@ export default class ExtendedRouterStore extends (RouterStore as FunctionConstru
             }
         });
 
-        // note that param handles encoding of parameter values
-        let params = $.param(newQuery);
-
         // put a leading slash if there isn't one
-        path = (path[0] === '/') ? path : '/' + path;
+        path = URL.resolve('/', path);
 
-        this.push( `${path}?${ params }` );
+        this.push( URL.format({pathname: path, query: newQuery}) );
 
     }
 
