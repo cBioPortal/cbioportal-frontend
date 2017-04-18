8a1368793d69e40f389d6b5fc3823c1badd19ee8
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-04-18 18:02:22 -0400

    Hide CNA mRNA column if no mrna genetic profile id
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/patientView/copyNumberAlterations/CopyNumberTableWrapper.spec.tsx
+++ src/pages/patientView/copyNumberAlterations/CopyNumberTableWrapper.spec.tsx
@@ -9,22 +9,24 @@ function hasColumn(tableWrapper:ReactWrapper<any, any>, columnName:string):boole
     return (columns.indexOf(columnName) > -1);
 }
 
-function getTable(samples:string[]):ReactWrapper<any, any> {
+function getTable(samples:string[], mrnaGeneticProfileId?:string):ReactWrapper<any, any> {
     return mount(<CopyNumberTableWrapper
         sampleManager={null}
         sampleIds={samples}
         gisticData={{}}
         status="available"
         data={[]}
+        mrnaExprRankGeneticProfileId={mrnaGeneticProfileId}
     />);
 }
 
 describe("CopyNumberTableWrapper", ()=>{
 
-    it("shows mrna expr column iff theres one sample", ()=>{
-        assert(hasColumn(getTable(["sampleA"]), "mRNA Expr."), "Has with one sample");
-        assert(!hasColumn(getTable([]), "mRNA Expr."), "Doesn't have with 0 samples");
-        assert(!hasColumn(getTable(["sampleA", "sampleB"]), "mRNA Expr."), "Doesn't have with 2 samples");
+    it("shows mrna expr column iff theres one sample and a genetic profile id", ()=>{
+        assert(hasColumn(getTable(["sampleA"], "id"), "mRNA Expr."), "Has with one sample and id");
+        assert(!hasColumn(getTable(["sampleA"]), "mRNA Expr."), "Doesnt have with one sample and no id");
+        assert(!hasColumn(getTable([], "id"), "mRNA Expr."), "Doesn't have with 0 samples and id");
+        assert(!hasColumn(getTable(["sampleA", "sampleB"], "id"), "mRNA Expr."), "Doesn't have with 2 samples and id");
     });
 
     it("shows tumors column iff theres more than one sample", ()=>{
