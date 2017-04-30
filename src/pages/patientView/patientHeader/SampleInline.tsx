5ebea4a2cf5641ebea900812e4f319342b94202f
Ino de Bruijn <ino@ino.pm>
2017-04-30 19:16:37 -0400

    Fix cBioPortal/cbioportal#2306 uncalled indicator
    In mutation table on the patient view show a light sample label if the mutation
    wasn't called, but the number of supporting alt reads > 0. This works if
    mutations are imported with a `*_mutations_uncalled` profile. It's useful for
    cases where the mutation is seen in the sequencing data, but we can't call the
    mutation confidently because it's there in such low abundance. This is common
    in ctDNA.

--- src/pages/patientView/patientHeader/SampleInline.tsx
+++ src/pages/patientView/patientHeader/SampleInline.tsx
@@ -1,23 +1,22 @@
 import * as React from "react";
 import {SampleLabelHTML} from "../../../shared/components/sampleLabel/SampleLabel";
 import { ClinicalDataBySampleId } from "../../../shared/api/api-types-extended";
-import {fromPairs} from 'lodash';
 
 
 interface ISampleInlineProps {
     sample: ClinicalDataBySampleId;
     sampleNumber: number;
     sampleColor: string;
-    showClinical: boolean;
+    fillOpacity: number;
 }
 
 export default class SampleInline extends React.Component<ISampleInlineProps, {}> {
     public render() {
-        const { sample, sampleNumber, sampleColor } = this.props;
+        const { sample, sampleNumber, sampleColor, fillOpacity } = this.props;
 
 
         return (
-            <SampleLabelHTML color={sampleColor} label={(sampleNumber).toString()} />
+            <SampleLabelHTML fillOpacity={fillOpacity} color={sampleColor} label={(sampleNumber).toString()} />
         );
     }
 }
