ba9f33f4d196aeb384fd481c34602d45b17f450f
Ino de Bruijn <ino@ino.pm>
2017-04-30 19:16:37 -0400

    Fix cBioPortal/cbioportal#2306 uncalled indicator
    In mutation table on the patient view show a light sample label if the mutation
    wasn't called, but the number of supporting alt reads > 0. This works if
    mutations are imported with a `*_mutations_uncalled` profile. It's useful for
    cases where the mutation is seen in the sequencing data, but we can't call the
    mutation confidently because it's there in such low abundance. This is common
    in ctDNA.

--- src/pages/patientView/mutation/column/TumorColumnFormatter.spec.tsx
+++ src/pages/patientView/mutation/column/TumorColumnFormatter.spec.tsx
@@ -1,8 +1,9 @@
-import * as TumorColumnFormatter from './TumorColumnFormatter';
+import TumorColumnFormatter from './TumorColumnFormatter';
 import React from 'react';
 import { assert } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
+import {GENETIC_PROFILE_MUTATIONS_SUFFIX, GENETIC_PROFILE_UNCALLED_MUTATIONS_SUFFIX} from '../../../../shared/constants';
 
 describe('TumorColumnFormatter', () => {
 
@@ -14,8 +15,24 @@ describe('TumorColumnFormatter', () => {
 
     });
 
-    it('what does it do?', ()=>{
-
+    it('test get present samples', ()=>{
+        let testData = [
+            {sampleId:'A',
+             geneticProfileId:GENETIC_PROFILE_MUTATIONS_SUFFIX
+            },
+            {sampleId:'B',
+             geneticProfileId:GENETIC_PROFILE_UNCALLED_MUTATIONS_SUFFIX,
+             tumorAltCount: 0,
+            },
+            {sampleId:'C',
+             geneticProfileId:GENETIC_PROFILE_UNCALLED_MUTATIONS_SUFFIX,
+             tumorAltCount: 5,
+            },
+        ];
+        let presentSamples = TumorColumnFormatter.getPresentSamples(testData);
+        assert(presentSamples['A'], "sample A is present and is called");
+        assert(!('B' in presentSamples), "sample B mutation is not present because it has 0 supporting reads");
+        assert(presentSamples['C'] === false, "sample C mutation is present and is uncalled with > 0 supporting reads");
     });
 
 });
