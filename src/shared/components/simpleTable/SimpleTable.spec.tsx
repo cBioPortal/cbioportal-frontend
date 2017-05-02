00f4095a565c63c7a18de0973ba901309a54a094
Tamba Monrose <tamba@mit.edu>
2017-05-02 12:50:46 -0400

    Add className package and tests.

--- src/shared/components/simpleTable/SimpleTable.spec.tsx
+++ src/shared/components/simpleTable/SimpleTable.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { assert } from 'chai';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import sinon from 'sinon';
 import SimpleTable from "./SimpleTable";
 
@@ -8,28 +8,40 @@ describe('SimpleTable', () => {
 
     let table:any;
 
-    before(()=>{
+    const headers:any = [<th>one</th>];
+    const rows:any = [<tr><td>data</td></tr>, <tr><td>data</td></tr>];
+    const classNameTrue:string = 'test-class';
+    const classNameFalse:undefined = undefined;
 
-        let headers:any = [<th>one</th>];
-        let rows:any = [<tr><td>data</td></tr>, <tr><td>data</td></tr>];
+    before(()=> {
 
         table = shallow(<SimpleTable headers={headers} rows={rows} />);
-    });
-
-    after(()=>{
 
     });
 
-    it('renders 3 rows if passed headers and two rows', ()=>{
+    after(()=> {
+
+    });
+
+    it('renders 3 rows if passed headers and two rows', ()=> {
         assert.equal( table.find('tr').length, 3);
     });
 
-    it('renders no result message when rows is empty array', ()=>{
+    it('renders no result message when rows is empty array', ()=> {
         table.setProps({rows:[]})
         assert.isTrue( table.containsMatchingElement(<td colSpan={1}>There are no results.</td>));
 
         table.setProps({rows:[], noRowsText:"Hello there!!!!"})
         assert.isTrue( table.containsMatchingElement(<td colSpan={1}>Hello there!!!!</td>));
     });
+
+    it('has class name of className prop if provided', ()=> {
+        assert.isTrue(shallow(<SimpleTable className={classNameTrue} headers={headers} rows={rows}/>).hasClass('test-class'));
+    });
+
+    it('does not have class name of \'undefined\' if className prop is undefined', ()=> {
+        assert.isFalse(shallow(<SimpleTable className={classNameFalse} headers={headers} rows={rows}/>).hasClass('undefined'));
+    });
+
 
 });
