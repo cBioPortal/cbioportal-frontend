c4d234c4b65b644c68b78f051d4ccea26d1790d6
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-05-02 20:00:28 -0400

    Add sortBy to num mutations and cancer type columns
    And move num mutations code into MutationCountColumnFormatter
    
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: b5a6d403b1151cb860c7089f5b278078ba9830d3

---
+++ src/shared/components/mutationTable/column/MutationCountColumnFormatter.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import LazyLoadedTableCell from "shared/lib/LazyLoadedTableCell";
+import {Mutation, MutationCount} from "../../../api/generated/CBioPortalAPI";
+import MutationCountCache from "../../../cache/MutationCountCache";
+import MutationTable, {IMutationTableProps} from "../MutationTable";
+
+export default class MutationCountColumnFormatter {
+    public static makeRenderFunction<P extends IMutationTableProps>(table:MutationTable<P>) {
+        return LazyLoadedTableCell(
+            (d:Mutation[])=>{
+                const mutationCountCache:MutationCountCache|undefined = table.props.mutationCountCache;
+                if (mutationCountCache) {
+                    return mutationCountCache.get(d[0].sampleId);
+                } else {
+                    return {
+                        status: "error",
+                        data: null
+                    };
+                }
+            },
+            (t:MutationCount)=>(<span>{t.mutationCount}</span>),
+            "Mutation count not available for this sample."
+        );
+    }
+
+    public static sortBy(d:Mutation[], mutationCountCache?:MutationCountCache) {
+        let ret;
+        if (mutationCountCache) {
+            const cacheDatum = mutationCountCache.get(d[0].sampleId);
+            if (cacheDatum && cacheDatum.data) {
+                ret = cacheDatum.data.mutationCount;
+            } else {
+                ret = null;
+            }
+        } else {
+            ret = null;
+        }
+        return ret;
+    }
+}
