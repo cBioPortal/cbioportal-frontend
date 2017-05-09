b090b5280fee271df5a82be09ccb147ce20d22be
aaronlisman <donkey00>
2017-05-09 12:17:24 -0400

    fix error reporting issue

--- src/appBootstrapper.jsx
+++ src/appBootstrapper.jsx
@@ -75,7 +75,8 @@ superagent.Request.prototype.end = function (callback) {
     return end.call(this, (error, response) => {
 
         if (error) {
-            Raven.captureException(response.error,{
+
+            Raven.captureException(((response && response.error) || error),{
                 tags: { network:true }
             });
         }
@@ -83,7 +84,7 @@ superagent.Request.prototype.end = function (callback) {
         if (redirecting) {
             return;
         }
-        if (response.statusCode === 401) {
+        if (response && response.statusCode === 401) {
             var storageKey = `redirect${Math.floor(Math.random() * 1000000000000)}`
             localStorage.setItem(storageKey, window.location.hash);
             const loginUrl = `//${getHost()}/?spring-security-redirect=${encodeURIComponent(window.location.pathname)}${encodeURIComponent(window.location.search)}${encodeURIComponent('#/restore?key=' + storageKey)}`;
