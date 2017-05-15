bb2a53e75328b7e15471db2e0fe3083532cf3abc
Onur Sumer <s.onur.sumer@gmail.com>
2017-05-15 13:53:32 -0400

    added 3d structure visualizer panel
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>

--- src/pages/resultsView/mutation/Mutations.tsx
+++ src/pages/resultsView/mutation/Mutations.tsx
@@ -51,6 +51,7 @@ export default class Mutations extends React.Component<IMutationsPageProps, {}>
                             pmidCache={this.props.store.pmidCache}
                             cancerTypeCache={this.props.store.cancerTypeCache}
                             mutationCountCache={this.props.store.mutationCountCache}
+                            pdbHeaderCache={this.props.store.pdbHeaderCache}
                             myCancerGenomeData={this.props.store.myCancerGenomeData}
                         />
                     </MSKTab>
