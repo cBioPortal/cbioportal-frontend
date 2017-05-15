290dfb854394fcee362bb651d7d9485c255d45b3
Onur Sumer <s.onur.sumer@gmail.com>
2017-05-15 14:27:52 -0400

    added a static 3D panel into MutationMapper for testing purposes
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>

--- src/pages/resultsView/mutation/MutationMapper.tsx
+++ src/pages/resultsView/mutation/MutationMapper.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import {observer} from "mobx-react";
+import {observable} from "mobx";
+import {Button, ButtonGroup} from 'react-bootstrap';
 import LoadingIndicator from "shared/components/loadingIndicator/LoadingIndicator";
+import StructureViewerPanel from "shared/components/structureViewer/StructureViewerPanel";
 import DiscreteCNACache from "shared/cache/DiscreteCNACache";
 import OncoKbEvidenceCache from "shared/cache/OncoKbEvidenceCache";
 import PubMedCache from "shared/cache/PubMedCache";
@@ -28,13 +31,84 @@ export interface IMutationMapperProps {
 @observer
 export default class MutationMapper extends React.Component<IMutationMapperProps, {}>
 {
+    @observable protected is3dPanelOpen = false;
+
     constructor(props: IMutationMapperProps) {
         super(props);
+
+        this.open3dPanel = this.open3dPanel.bind(this);
+        this.close3dPanel = this.close3dPanel.bind(this);
     }
 
     public render() {
         return (
             <div>
+                {
+                    (this.is3dPanelOpen) && (
+                        // TODO this is a static instance for temporary testing purposes only.
+                        // pdbId, chainId and residues should not be static...
+                        <StructureViewerPanel
+                            pdbId="3pxe"
+                            chainId="B"
+                            pdbHeaderCache={this.props.pdbHeaderCache}
+                            onClose={this.close3dPanel}
+                            residues={[
+                                {
+                                    positionRange: {
+                                        start: {
+                                            position: 122
+                                        },
+                                        end: {
+                                            position: 122
+                                        }
+                                    },
+                                    color: "#FF0000"
+                                },
+                                {
+                                    positionRange: {
+                                        start: {
+                                            position: 1710
+                                        },
+                                        end: {
+                                            position: 1710
+                                        }
+                                    },
+                                    color: "#FF0000"
+                                },
+                                {
+                                    positionRange: {
+                                        start: {
+                                            position: 1835
+                                        },
+                                        end: {
+                                            position: 1835
+                                        }
+                                    },
+                                    color: "#00FFFF"
+                                },
+                                {
+                                    positionRange: {
+                                        start: {
+                                            position: 1815
+                                        },
+                                        end: {
+                                            position: 1815
+                                        }
+                                    },
+                                    color: "#00FF00",
+                                    highlighted: true
+                                }
+                            ]}
+                        />
+                    )
+                }
+
+                <ButtonGroup className="pull-right">
+                    <Button className="btn-sm" onClick={this.open3dPanel}>
+                        3D Structure »
+                    </Button>
+                </ButtonGroup>
+
                 <LoadingIndicator isLoading={this.props.store.mutationData.isPending || this.props.store.gene.isPending} />
                 {
                     (this.props.store.mutationData.isComplete && this.props.store.gene.result) && (
@@ -63,5 +137,13 @@ export default class MutationMapper extends React.Component<IMutationMapperProps
                 }
             </div>
         );
+    }
+
+    private open3dPanel() {
+        this.is3dPanelOpen = true;
+    }
+
+    private close3dPanel() {
+        this.is3dPanelOpen = false;
     }
 }
---
+++ src/shared/components/structureViewer/StructureVisualizer.ts
@@ -0,0 +1,170 @@
+import {IPdbPositionRange} from "shared/model/Pdb";
+import {convertPdbPosToResCode} from "shared/lib/PdbUtils";
+
+export interface IResidueSpec {
+    positionRange: IPdbPositionRange;
+    color: string;
+    highlighted?: boolean;
+}
+
+export enum ProteinScheme {
+    CARTOON, SPACE_FILLING, TRACE, BALL_AND_STICK, RIBBON
+}
+
+// UNIFORM: single color, effective for all schemes
+// SECONDARY_STRUCTURE: not effective for space-filling scheme
+// ATOM_TYPE: effective only for space-filling scheme
+// NC_RAINBOW: not effective for space-filling scheme
+export enum ProteinColor {
+    UNIFORM, SECONDARY_STRUCTURE, NC_RAINBOW, ATOM_TYPE
+}
+
+// SELECTED: display side chain for only selected mutations
+// ALL: display side chain for all mapped mutations
+// NONE: do not display side chain atoms
+export enum SideChain {
+    ALL, SELECTED, NONE
+}
+
+// MUTATION_TYPE: use mutation colors for type
+// UNIFORM: use a single color
+// NONE: do not color (use default atom colors)
+export enum MutationColor {
+    UNIFORM, MUTATION_TYPE, NONE
+}
+
+export interface IStructureVisualizerProps {
+    proteinScheme: ProteinScheme;
+    proteinColor: ProteinColor;
+    sideChain: SideChain;
+    mutationColor: MutationColor;
+    // when set to false, restricts to protein only (hide other atoms)
+    displayBoundMolecules: boolean;
+    // PDB database URI
+    pdbUri?: string;
+    // base color of the whole structure
+    baseColor?: string;
+    // background color
+    backgroundColor?: string;
+    // colors for special structures
+    // structure color takes effect only when corresponding flag is set
+    structureColors?: {
+        alphaHelix: string;
+        betaSheet: string;
+        loop: string;
+    };
+    // translucency (opacity) of the whole structure
+    baseTranslucency?: number;
+    // color of the selected chain
+    chainColor?: string;
+    // translucency (opacity) of the selected chain
+    chainTranslucency?: number;
+    // uniform color of the mutated residues
+    uniformMutationColor?: string;
+    // color of the user-selected mutations
+    highlightColor?: string;
+}
+
+abstract class StructureVisualizer
+{
+    public static defaultProps = {
+        pdbUri: "http://www.rcsb.org/pdb/files/",
+        proteinScheme: ProteinScheme.CARTOON,
+        displayBoundMolecules: true,
+        backgroundColor: "#FFFFFF",
+        baseColor: "#DDDDDD",
+        structureColors: {
+            alphaHelix: "#FFA500",
+            betaSheet: "#0000FF",
+            loop: "#DDDDDD"
+        },
+        baseTranslucency: 5,
+        chainColor: "#888888",
+        chainTranslucency: 0,
+        proteinColor: ProteinColor.UNIFORM,
+        mutationColor: MutationColor.MUTATION_TYPE,
+        uniformMutationColor: "#8A2BE2",
+        highlightColor: "#FFDD00",
+        sideChain: SideChain.SELECTED
+    };
+
+    // abstract methods
+    protected abstract selectAll(): any;
+    protected abstract setScheme(scheme: ProteinScheme, selector?: any): any;
+    protected abstract setColor(color: string, selector?: any, style?: any): any;
+    protected abstract setTransparency(transparency: number, selector?: any, style?: any): any;
+    protected abstract selectChain(chainId: string): any;
+    protected abstract cpkColor(selector?: any, style?: any): any;
+    protected abstract selectAlphaHelix(chainId: string): any;
+    protected abstract selectBetaSheet(chainId: string): any;
+    protected abstract rainbowColor(selector?: any, style?: any): any;
+    protected abstract hideBoundMolecules(): any;
+    protected abstract updateResidueStyle(residues: IResidueSpec[],
+                                          chainId: string,
+                                          props: IStructureVisualizerProps,
+                                          style?: any): any;
+
+    /**
+     * Updates the visual style (scheme, coloring, selection, etc.)
+     */
+    public updateVisualStyle(residues: IResidueSpec[],
+                             chainId: string,
+                             props: IStructureVisualizerProps)
+    {
+        const defaultProps = StructureVisualizer.defaultProps;
+
+        let selector = this.selectAll(); // select everything
+        let style = this.setScheme(props.proteinScheme, selector); // show selected style view
+
+        // do the initial (uniform) coloring
+
+        this.setColor(props.baseColor || defaultProps.baseColor, selector, style); // set base color
+        this.setTransparency(props.baseTranslucency || defaultProps.baseTranslucency, selector, style);
+        selector = this.selectChain(chainId);
+        this.setColor(props.chainColor || defaultProps.chainColor, selector, style); // set chain color
+        this.setTransparency(props.chainTranslucency || defaultProps.chainTranslucency, selector, style);
+
+        // additional coloring for the selected chain
+
+        if (props.proteinColor === ProteinColor.ATOM_TYPE)
+        {
+            selector = this.selectChain(chainId);
+            this.cpkColor(selector, style);
+        }
+        else if (props.proteinColor === ProteinColor.SECONDARY_STRUCTURE)
+        {
+            // color secondary structure (for the selected chain)
+            selector = this.selectAlphaHelix(chainId); // select alpha helices
+            this.setColor((props.structureColors || defaultProps.structureColors).alphaHelix, selector, style);
+            selector = this.selectBetaSheet(chainId); // select beta sheets
+            this.setColor((props.structureColors || defaultProps.structureColors).betaSheet, selector, style);
+        }
+        else if (props.proteinColor === ProteinColor.NC_RAINBOW)
+        {
+            // color the chain by rainbow coloring scheme (gradient coloring)
+            selector = this.selectChain(chainId);
+            this.rainbowColor(selector, style);
+        }
+
+        this.updateResidueStyle(residues, chainId, props, style);
+
+        if (!props.displayBoundMolecules) {
+            this.hideBoundMolecules();
+        }
+    }
+
+    protected convertPositionsToResCode(positions: IPdbPositionRange[])
+    {
+        let residueCodes: string[] = [];
+
+        // convert positions to script positions
+        positions.forEach((range: IPdbPositionRange) => {
+            residueCodes = residueCodes.concat(convertPdbPosToResCode(range));
+        });
+
+        return residueCodes;
+    };
+}
+
+export default StructureVisualizer;
+
