72dd845d8838538cf78308d20f6a3f7f9c2879c6
Ino de Bruijn <ino@ino.pm>
2017-05-23 15:23:31 -0400

    Add extra visual indicators for uncalled mutations
    - On mouseover of label in mutation table indicate the mutation wasn't called
    - In allele freq mouseover indicate which mutations were not called and color
      labels accordingly
    
    
    Former-commit-id: a4acfeea2789cd05ad889401ad2011707c5a241c

--- src/pages/patientView/mutation/column/AlleleFreqColumnFormatter.spec.tsx
+++ src/pages/patientView/mutation/column/AlleleFreqColumnFormatter.spec.tsx
@@ -1,21 +1,55 @@
-import * as AlleleFreqColumnFormatter from './AlleleFreqColumnFormatter';
+import AlleleFreqColumnFormatter from './AlleleFreqColumnFormatter';
+import {GENETIC_PROFILE_UNCALLED_MUTATIONS_SUFFIX, GENETIC_PROFILE_MUTATIONS_SUFFIX} from '../../../../shared/constants';
 import React from 'react';
 import { assert } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 
-describe('AlleleFreqColumnFormatter', () => {
+describe.only('AlleleFreqColumnFormatter', () => {
 
-    before(()=>{
-
-    });
-
-    after(()=>{
+    before(()=> {
 
     });
 
-    it('what does it do?', ()=>{
+    after(()=> {
 
     });
 
+    it('uncalled mutations component w/o reads should have 0 opacity', ()=> {
+        const uncalledMutationWithoutSupport = {
+            geneticProfileId:`study_${GENETIC_PROFILE_UNCALLED_MUTATIONS_SUFFIX}`,
+            tumorAltCount:0,
+            tumorRefCount:10
+        };
+        const res = AlleleFreqColumnFormatter.getComponentForSampleArgs(uncalledMutationWithoutSupport);
+        assert(res.opacity === 0);
+    });
+    it('uncalled mutations component w supporting reads should have >0 and <1 opacity', ()=> {
+        const uncalledMutationWithSupport = {
+            geneticProfileId:`study_${GENETIC_PROFILE_UNCALLED_MUTATIONS_SUFFIX}`,
+            tumorAltCount:1,
+            tumorRefCount:10
+        };
+        const res = AlleleFreqColumnFormatter.getComponentForSampleArgs(uncalledMutationWithSupport);
+        assert(res.opacity > 0 && res.opacity < 1);
+    });
+    it('called mutations component w supporting reads should have 1 opacity', ()=> {
+        const calledMutation = {
+            geneticProfileId:`study_${GENETIC_PROFILE_MUTATIONS_SUFFIX}`,
+            tumorAltCount:1,
+            tumorRefCount:10
+        };
+        const res = AlleleFreqColumnFormatter.getComponentForSampleArgs(calledMutation);
+        assert(res.opacity === 1);
+    });
+    it('sampleElement should have the text (uncalled)', ()=> {
+        const uncalledMutationWithSupport = {
+            sampleId:'1',
+            geneticProfileId:`study_${GENETIC_PROFILE_UNCALLED_MUTATIONS_SUFFIX}`,
+            tumorAltCount:1,
+            tumorRefCount:10
+        };
+        const res = AlleleFreqColumnFormatter.convertMutationToSampleElement(uncalledMutationWithSupport, 'red', 5, {});
+        assert(res && mount(res.text).text().indexOf('(uncalled)') !== -1);
+    });
 });
