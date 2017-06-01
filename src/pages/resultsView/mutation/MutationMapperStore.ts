a34a9b00fc2c21c7bcace4b8416cc5b0155a9b69
Onur Sumer <s.onur.sumer@gmail.com>
2017-06-01 16:13:27 -0400

    now initializing the pdb chain data store with sorted alignment data
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>
    
    Former-commit-id: 12a4c2fc861d5919788b0aa87db1e8d9b99c39de

--- src/pages/resultsView/mutation/MutationMapperStore.ts
+++ src/pages/resultsView/mutation/MutationMapperStore.ts
@@ -9,6 +9,7 @@ import {IOncoKbData} from "shared/model/OncoKB";
 import {IHotspotData} from "shared/model/CancerHotspots";
 import {IPdbChain} from "shared/model/Pdb";
 import {calcPdbIdNumericalValue} from "shared/lib/PdbUtils";
+import {lazyMobXTableSort} from "shared/components/lazyMobXTable/LazyMobXTable";
 import {
     indexHotspotData, fetchHotspotsData, fetchCosmicData, fetchOncoKbData,
     fetchMutationData, generateSampleIdToTumorTypeMap, generateDataQueryFilter,
@@ -167,6 +168,19 @@ export class MutationMapperStore {
         return mergePdbAlignments(this.alignmentData);
     }
 
+    @computed get sortedMergedAlignmentData(): IPdbChain[] {
+        const sortMetric = (pdbChain: IPdbChain) => [
+            pdbChain.identity,         // first, sort by identity
+            pdbChain.alignment.length, // then by alignment length
+            pdbChain.identityPerc,     // then by identity percentage
+            // current sort metric cannot handle mixed values so generating numerical values for strings
+            ...calcPdbIdNumericalValue(pdbChain.pdbId, true), // then by pdb id (A-Z): always returns an array of size 4
+            -1 * pdbChain.chain.charCodeAt(0)                 // then by chain id (A-Z): chain id is always one char
+        ];
+
+        return lazyMobXTableSort(this.mergedAlignmentData, sortMetric, false);
+    }
+
     @computed get indexedHotspotData(): IHotspotData|undefined {
         return indexHotspotData(this.hotspotData);
     }
@@ -180,19 +194,7 @@ export class MutationMapperStore {
     }
 
     @cached get pdbChainDataStore(): IMobXApplicationDataStore<IPdbChain> {
-        const dataStore = new SimpleMobXApplicationDataStore<IPdbChain>(this.mergedAlignmentData);
-
-        // define sort metric and direction at initialization time
-        dataStore.sortAscending = false;
-        dataStore.sortMetric = (pdbChain: IPdbChain) => [
-            pdbChain.identity,         // first, sort by identity
-            pdbChain.alignment.length, // then by alignment length
-            pdbChain.identityPerc,     // then by identity percentage
-            // current sort metric cannot handle mixed values so generating numerical values for strings
-            ...calcPdbIdNumericalValue(pdbChain.pdbId, true), // then by pdb id (A-Z): always returns an array of size 4
-            -1 * pdbChain.chain.charCodeAt(0)                 // then by chain id (A-Z): chain id is always one char
-        ];
-
-        return dataStore;
+        // initialize with sorted merged alignment data
+        return new SimpleMobXApplicationDataStore<IPdbChain>(this.sortedMergedAlignmentData);
     }
 }
