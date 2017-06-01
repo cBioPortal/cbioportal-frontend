e5bd48f947ffd10af4cf68df13bb6dea0a91e35d
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-06-01 15:49:55 -0400

    Move highlighted position into data store
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/resultsView/mutation/MutationMapperDataStore.ts
+++ src/pages/resultsView/mutation/MutationMapperDataStore.ts
@@ -5,20 +5,31 @@ import {Mutation} from "../../../shared/api/generated/CBioPortalAPI";
 import {action, computed, observable} from "mobx";
 import Immutable from "seamless-immutable";
 
-type SelectedPositions = {[position:string]:boolean};
-type ImmutableSelectedPositions = SelectedPositions & Immutable.ImmutableObject<SelectedPositions>;
+type PositionAttr = {[position:string]:boolean};
+type ImmutablePositionAttr = PositionAttr & Immutable.ImmutableObject<PositionAttr>;
 
 export default class MutationMapperDataStore extends SimpleMobXApplicationDataStore<Mutation[]>{
-    @observable.ref private selectedPositions:ImmutableSelectedPositions;
+    @observable.ref private selectedPositions:ImmutablePositionAttr;
+    @observable.ref private highlightedPositions:ImmutablePositionAttr;
 
     @action public setPositionSelected(position:number, newVal:boolean) {
-        const toMerge:SelectedPositions = {};
+        const toMerge:PositionAttr = {};
         toMerge[position+""] = newVal;
-        this.selectedPositions = this.selectedPositions.merge(toMerge) as ImmutableSelectedPositions;
+        this.selectedPositions = this.selectedPositions.merge(toMerge) as ImmutablePositionAttr;
+    }
+
+    @action public setPositionHighlighted(position:number, newVal:boolean) {
+        const toMerge:PositionAttr = {};
+        toMerge[position+""] = newVal;
+        this.highlightedPositions = this.highlightedPositions.merge(toMerge) as ImmutablePositionAttr;
     }
 
     @action public clearSelectedPositions() {
-        this.selectedPositions = Immutable.from<SelectedPositions>({});
+        this.selectedPositions = Immutable.from<PositionAttr>({});
+    }
+
+    @action public clearHighlightedPositions() {
+        this.highlightedPositions = Immutable.from<PositionAttr>({});
     }
 
     public isPositionSelected(position:number) {
@@ -32,7 +43,9 @@ export default class MutationMapperDataStore extends SimpleMobXApplicationDataSt
 
     constructor(data:Mutation[][]) {
         super(data);
-        this.selectedPositions = Immutable.from<SelectedPositions>({});
+        this.selectedPositions = Immutable.from<PositionAttr>({});
+        this.highlightedPositions = Immutable.from<PositionAttr>({});
         this.dataSelector = (d:Mutation[])=>!!this.selectedPositions[d[0].proteinPosStart+""];
+        this.dataHighlighter = (d:Mutation[])=>!!this.highlightedPositions[d[0].proteinPosStart+""];
     }
 }
