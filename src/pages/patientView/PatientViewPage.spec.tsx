ab51a35d6ae9bfaebaca35d24cc761e4ae4a1c5c
aaronlisman <donkey00>
2017-06-08 11:24:29 -0400

    update patient page title when patient or sample is set. fixes #https://github.com/cBioPortal/cbioportal/issues/2536
    Former-commit-id: 8c723ad31d981e957919b09adfe56766ccd093b9

--- src/pages/patientView/PatientViewPage.spec.tsx
+++ src/pages/patientView/PatientViewPage.spec.tsx
@@ -1,21 +1,9 @@
-import * as PatientViewPage from './PatientViewPage';
+//import {  } from './PatientViewPage';
 import React from 'react';
 import { assert } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 
 describe('PatientViewPage', () => {
-
-    before(()=>{
-
-    });
-
-    after(()=>{
-
-    });
-
-    it('what does it do?', ()=>{
-
-    });
 
 });
