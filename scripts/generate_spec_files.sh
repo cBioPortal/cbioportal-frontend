2c336743a3e1e3ebf3da6588a19954af5e53bef8
Ino de Bruijn <ino@ino.pm>
2017-07-05 14:04:56 -0400

    Run spec file generation on Travis for coverage
    The code coverage only works on source files that are imported in a spec file.
    This generates spec files for each source file before running the tests.

--- scripts/generate_spec_files.sh
+++ scripts/generate_spec_files.sh
@@ -1,7 +1,7 @@
 # generate spec files for tsx files w/o them
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for f in $(cd ${SCRIPT_DIR}/../ && git ls-files | grep -E 'ts(x){0,1}$' | grep -v spec | grep -v api | grep -v redux | grep -v appShell | grep -v typings)
+for f in $(cd ${SCRIPT_DIR}/../ && git ls-files | grep -E 'ts(x){0,1}$' | grep -v spec | grep -v api | grep -v redux | grep -v appShell | grep -v typings | grep -v config | grep -v oql-parser.d)
 do 
         (test -e ${f/.ts*/.spec.ts} || test -e ${f/.ts*/.spec.tsx}) || \
             cat ${SCRIPT_DIR}/spec.tsx.template | \
