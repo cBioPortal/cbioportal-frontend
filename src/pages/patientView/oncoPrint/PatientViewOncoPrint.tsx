5b017bbd657cf37163e1537080c6b7f95ac19996
pamela-wu <pamela.wu@nyumc.org>
2017-07-24 13:05:42 -0400

    transposed oncoprint

--- src/pages/patientView/oncoPrint/PatientViewOncoPrint.tsx
+++ src/pages/patientView/oncoPrint/PatientViewOncoPrint.tsx
@@ -20,26 +20,30 @@ export default class PatientViewOncoPrint extends React.Component<IPatientViewOn
   }
 
   componentDidMount() {
-    const fake_data:any = [
-      {gene:'ABC',
-       data:[{gene: 'ABC', sample:'biopsy-1', vaf: 0.5},
-             {gene: 'ABC', sample:'biopsy-2', vaf: 0.7},
-             {gene: 'ABC', sample:'biopsy-3', vaf: 0.9},
-             {gene: 'ABC', sample:'biopsy-4', vaf: 1.0},
+    
+    const onco_data:any = [
+      {sample:'biopsy-1',
+       data:[{gene: 'ABC', sample:'biopsy-1', vaf: 0.1},
+             {gene: 'DEF', sample:'biopsy-1', vaf: 0.5},
+             {gene: 'GHI', sample:'biopsy-1', vaf: 0.9},
             ],
       },
-      {gene:'DEF',
-       data:[{gene: 'DEF', sample:'biopsy-1', vaf: 1.0},
-             {gene: 'DEF', sample:'biopsy-2', vaf: 0.9},
+      {sample:'biopsy-2',
+       data:[{gene: 'ABC', sample:'biopsy-2', vaf: 0.9},
+             {gene: 'DEF', sample:'biopsy-2', vaf: 0.5},
+             {gene: 'GHI', sample:'biopsy-2', vaf: 0.1},
+            ],
+      },
+      {sample:'biopsy-3',
+       data:[{gene: 'ABC', sample:'biopsy-3', vaf: 0.3},
              {gene: 'DEF', sample:'biopsy-3', vaf: 0.7},
-             {gene: 'DEF', sample:'biopsy-4', vaf: 0.5},
+             {gene: 'GHI', sample:'biopsy-3', vaf: 0.3},
             ],
       },
-      {gene:'GHI',
-       data:[{gene: 'GHI', sample:'biopsy-1', vaf: 0.8},
-             {gene: 'GHI', sample:'biopsy-2', vaf: 0.3},
-             {gene: 'GHI', sample:'biopsy-3', vaf: 0.8},
-             {gene: 'GHI', sample:'biopsy-4', vaf: 0.2},
+      {sample:'biopsy-4',
+       data:[{gene: 'ABC', sample:'biopsy-4', vaf: 0.8},
+             {gene: 'DEF', sample:'biopsy-4', vaf: 0.2},
+             {gene: 'GHI', sample:'biopsy-4', vaf: 0.6},
             ],
       },
     ];
@@ -48,11 +52,11 @@ export default class PatientViewOncoPrint extends React.Component<IPatientViewOn
         return new newOncoprint(params.ctr_selector, params.width);
     }
 
-    var oncoprint = makeOncoprint(window.Oncoprint, {ctr_selector: "#patient-view-oncoprint", width: 100 + fake_data.length * 50});
+    var oncoprint = makeOncoprint(window.Oncoprint, {ctr_selector: "#patient-view-oncoprint", width: 100 + onco_data.length * 50});
     oncoprint.suppressRendering();
 
     var share_id = null;
-    for (var i=0; i<fake_data.length; i++) {
+    for (var i=0; i<onco_data.length; i++) {
       const track_params:any = {
         'rule_set_params': {
             'type': 'gradient',
@@ -65,13 +69,13 @@ export default class PatientViewOncoPrint extends React.Component<IPatientViewOn
         },
         'has_column_spacing': true,
         'track_padding': 5,
-        'label': fake_data[i].gene,
+        'label': onco_data[i].sample,
         'sortCmpFn': function(d1:any, d2:any) {return 0;},
         'target_group': 0,
         'removable': true,
       };
       var new_hm_id = oncoprint.addTracks([track_params])[0];
-      fake_data[i].track_id = new_hm_id;
+      onco_data[i].track_id = new_hm_id;
       if (i === 0) {
         share_id = new_hm_id;
       } else {
@@ -82,9 +86,9 @@ export default class PatientViewOncoPrint extends React.Component<IPatientViewOn
     oncoprint.hideIds([], true);
     oncoprint.keepSorted(false);
 
-    for (let i=0; i<fake_data.length; i++) {
-      oncoprint.setTrackData(fake_data[i].track_id, fake_data[i].data, 'sample');
-      oncoprint.setTrackInfo(fake_data[i].track_id, fake_data[i].gene);
+    for (let i=0; i<onco_data.length; i++) {
+      oncoprint.setTrackData(onco_data[i].track_id, onco_data[i].data, 'gene');
+      oncoprint.setTrackInfo(onco_data[i].track_id, onco_data[i].sample);
     }
 
     oncoprint.keepSorted(true);
