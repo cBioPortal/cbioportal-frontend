8dd407a119fb50b04a199e7c08c923e043c9ea81
Onur Sumer <s.onur.sumer@gmail.com>
2017-07-26 16:23:04 -0400

    formatted mutation status column, and now using the same protein change renderer for both mutation tables
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>
    
    Former-commit-id: 5139f1a029594f024915a757dded92e7f77e6ef4

--- src/shared/components/mutationTable/column/ProteinChangeColumnFormatter.spec.tsx
+++ src/shared/components/mutationTable/column/ProteinChangeColumnFormatter.spec.tsx
@@ -1,10 +1,57 @@
 import {assert} from "chai";
 import ProteinChangeColumnFormatter from "./ProteinChangeColumnFormatter";
+import styles from './proteinChange.module.scss';
+import {initMutation} from "test/MutationMockUtils";
+import {Mutation} from "shared/api/generated/CBioPortalAPI";
+import {mount, ReactWrapper} from 'enzyme';
 
 /**
  * @author Selcuk Onur Sumer
  */
 describe('ProteinChangeColumnFormatter', () => {
+    const germlineMutation:Mutation = initMutation({
+        proteinChange: "Q616K",
+        mutationStatus: "Germline"
+    });
+
+    const somaticMutation:Mutation = initMutation({
+        proteinChange: "Q616L",
+        mutationStatus: "Somatic"
+    });
+
+    let germlineComponent: ReactWrapper<any, any>;
+    let somaticComponent:ReactWrapper<any, any>;
+
+    before(() => {
+        let data = [germlineMutation];
+
+        // mount a single cell component (Td) for a germline mutation
+        germlineComponent = mount(ProteinChangeColumnFormatter.renderWithMutationStatus(data));
+
+        data = [somaticMutation];
+
+        // mount a single cell component (Td) for a somatic mutation
+        somaticComponent = mount(ProteinChangeColumnFormatter.renderWithMutationStatus(data));
+    });
+
+    it('renders protein change display value', () => {
+        assert.isTrue(germlineComponent.find(`.${styles.proteinChange}`).exists(),
+            'Germline mutation should have the protein change value');
+        assert.isTrue(germlineComponent.find(`.${styles.proteinChange}`).text().indexOf("Q616K") > -1,
+            'Protein change value for germline mutation is correct');
+        assert.isTrue(somaticComponent.find(`.${styles.proteinChange}`).exists(),
+            'Somatic mutation should have the protein change value');
+        assert.isTrue(somaticComponent.find(`.${styles.proteinChange}`).text().indexOf("Q616L") > -1,
+            'Protein change value for somatic mutation is correct');
+    });
+
+    it('renders germline indicator', () => {
+        assert.isTrue(germlineComponent.find(`.${styles.germline}`).exists(),
+            'Germline mutation should have the additional germline indicator');
+        assert.isFalse(somaticComponent.find(`.${styles.germline}`).exists(),
+            'Somatic mutation should not have the additional germline indicator');
+    });
+
     const a = "E746_A750del";
     const b = "E747_T749del";
     const c = "K754E";
