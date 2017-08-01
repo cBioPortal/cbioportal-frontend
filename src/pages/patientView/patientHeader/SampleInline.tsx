b9a13cb4fc86298f186ce191f6df3994bbedf85e
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-07-31 21:12:21 -0400

    Remove minHeight in DefaultTooltip
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/patientView/patientHeader/SampleInline.tsx
+++ src/pages/patientView/patientHeader/SampleInline.tsx
@@ -3,8 +3,8 @@ import {If, Else, Then} from 'react-if';
 import {SampleLabelHTML} from "shared/components/sampleLabel/SampleLabel";
 import {ClinicalDataBySampleId} from "shared/api/api-types-extended";
 import ClinicalInformationPatientTable from "../clinicalInformation/ClinicalInformationPatientTable";
-import DefaultTooltip from "shared/components/DefaultTooltip";
-import {placeArrowBottomLeft} from "shared/components/DefaultTooltip";
+import DefaultTooltip from "shared/components/defaultTooltip/DefaultTooltip";
+import {placeArrowBottomLeft} from "shared/components/defaultTooltip/DefaultTooltip";
 
 interface ISampleInlineProps {
     sample: ClinicalDataBySampleId;
