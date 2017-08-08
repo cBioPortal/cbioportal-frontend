bfdff1cb59ef59182a9cab74078ddbad687b4dec
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-08-08 14:56:33 -0400

    Genomic Overview changes
    (1) Show MUT track for a sample iff its sequenced
    (2) Position tooltips for right-side percentage and mutation count, so that they dont go off screen
    
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: ed4db6ce8ffab5dce7aa024184c40e3cb1b1cdeb

--- src/pages/patientView/genomicOverview/Tracks.tsx
+++ src/pages/patientView/genomicOverview/Tracks.tsx
@@ -10,6 +10,7 @@ interface TracksPropTypes {
     mutations:Array<Mutation>;
     cnaSegments:Array<CopyNumberSeg>;
     sampleManager:SampleManager;
+    sequencedSamples:{[sampleId:string]:boolean};
     width:number;
 }
 
@@ -23,8 +24,8 @@ export default class Tracks extends React.Component<TracksPropTypes, {}> {
 
         // --- construct params ---
         let uniqCnasampleIds = _.uniq(_.map(this.props.cnaSegments, 'sampleId'));
-        let uniqMutSampleIds = _.uniq(_.map(this.props.mutations, 'sampleId'));
-        var config = tracksHelper.GenomicOverviewConfig(uniqCnasampleIds.length + uniqMutSampleIds.length, this.props.width);
+        let sequencedSamples = this.props.sampleManager.samples.filter(x=>!!this.props.sequencedSamples[x.id]);
+        var config = tracksHelper.GenomicOverviewConfig(uniqCnasampleIds.length + sequencedSamples.length, this.props.width);
         // --- end of params ---
 
         // --- raphael config ---
@@ -78,29 +79,27 @@ export default class Tracks extends React.Component<TracksPropTypes, {}> {
 
 
         // --- mutation events bar chart ---
-        _.each(this.props.sampleManager.samples, (sample: ClinicalDataBySampleId) => {
-            if (_.includes(uniqMutSampleIds, sample.id)) {
-                var _trackData = _.filter(this.props.mutations, function (_mutObj: any) {
-                    return _mutObj.sampleId === sample.id;
-                });
-                tracksHelper.plotMuts(paper, config, chmInfo, rowIndex, _trackData, sample.id);
-                rowIndex = rowIndex + 1;
+        _.each(sequencedSamples, (sample: ClinicalDataBySampleId) => {
+            var _trackData = _.filter(this.props.mutations, function (_mutObj: any) {
+                return _mutObj.sampleId === sample.id;
+            });
+            tracksHelper.plotMuts(paper, config, chmInfo, rowIndex, _trackData, sample.id);
+            rowIndex = rowIndex + 1;
 
-                if (this.props.sampleManager.samples.length > 1) {
-                    const id = `#mutTrack${sample.id}`;
-                    const $container = $(id);
-                    const pos = {x: parseInt($container.attr('x')) - 10, y: parseInt($container.attr('y')) - 5};
-                    const $newContainer = $(`<svg id="${id}" height="12" width="12" />`);
-                    $newContainer.attr(pos);
-                    $container.replaceWith($newContainer);
+            if (this.props.sampleManager.samples.length > 1) {
+                const id = `#mutTrack${sample.id}`;
+                const $container = $(id);
+                const pos = {x: parseInt($container.attr('x')) - 10, y: parseInt($container.attr('y')) - 5};
+                const $newContainer = $(`<svg id="${id}" height="12" width="12" />`);
+                $newContainer.attr(pos);
+                $container.replaceWith($newContainer);
 
-                    let comp: any = this.props.sampleManager.getComponentForSample(sample.id);
+                let comp: any = this.props.sampleManager.getComponentForSample(sample.id);
 
-                    ReactDOM.render(
-                        comp,
-                        $newContainer[0]
-                    );
-                }
+                ReactDOM.render(
+                    comp,
+                    $newContainer[0]
+                );
             }
         });
         // --- end of mutation events bar chart ---
