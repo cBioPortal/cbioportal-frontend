45f66b5eb9a29d57b8fffdf1020677616a5a1847
Onur Sumer <s.onur.sumer@gmail.com>
2017-08-11 19:25:19 -0400

    using germline specific sample list id instead of 12_245_PARTC_CONSENTED (fix https://github.com/cBioPortal/cbioportal/issues/2918)
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>
    
    Former-commit-id: a2cd9343dd160c6ee2d9ed3a23831f742b2143e6

--- src/pages/resultsView/mutation/MutationRateSummary.tsx
+++ src/pages/resultsView/mutation/MutationRateSummary.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import { Mutation } from "shared/api/generated/CBioPortalAPI";
-import {germlineMutationRate, somaticMutationRate, somaticMutationRateBySample} from "shared/lib/MutationUtils";
+import {germlineMutationRate, somaticMutationRate} from "shared/lib/MutationUtils";
+import {MobxPromise} from "mobxpromise";
+import {observer} from "mobx-react";
+import DefaultTooltip from "shared/components/defaultTooltip/DefaultTooltip";
 
 export interface IMutationRateSummaryProps {
     mutations: Mutation[];
     hugoGeneSymbol: string;
     sampleIds: string[];
-    patientIdsStatus: "pending" | "error" | "complete";
-    patientIds?: string[];
-    mskImpactGermlineConsentedPatientIdsStatus: "pending" | "error" | "complete";
-    mskImpactGermlineConsentedPatientIds?: string[];
+    germlineConsentedSampleIds: MobxPromise<string[]>;
 }
 
+@observer
 export default class MutationRateSummary extends React.Component<IMutationRateSummaryProps, {}>
 {
     public somaticMutationFrequency(): JSX.Element
@@ -19,43 +20,53 @@ export default class MutationRateSummary extends React.Component<IMutationRateSu
         let rate = 0;
 
         if (this.props.sampleIds.length > 0) {
-            rate = somaticMutationRateBySample(this.props.hugoGeneSymbol, this.props.mutations, this.props.sampleIds);
-        }
-        else if (this.props.patientIds && this.props.patientIds.length > 0) {
-            rate = somaticMutationRate(this.props.hugoGeneSymbol, this.props.mutations, this.props.patientIds);
+            rate = somaticMutationRate(this.props.hugoGeneSymbol, this.props.mutations, this.props.sampleIds);
         }
 
         return (
             <div data-test="somaticMutationRate">
                 <label>Somatic Mutation Frequency:</label>
                 {rate.toFixed(1)}%
+
+                <DefaultTooltip
+                    placement="right"
+                    overlay={(<span>{'Percentage of samples with a somatic mutation in ' + this.props.hugoGeneSymbol}</span>)}
+                >
+                    <i className='glyphicon glyphicon-info-sign' style={{'marginLeft':5}}></i>
+                </DefaultTooltip>
             </div>
         );
     }
 
     public germlineMutationFrequency(): JSX.Element
     {
-        let patientIds: string[]|undefined;
+        let sampleIds: string[]|undefined;
 
-        if (this.props.patientIdsStatus !== "pending" &&
-            this.props.mskImpactGermlineConsentedPatientIdsStatus !== "pending")
+        if (this.props.germlineConsentedSampleIds.status !== "pending")
         {
             // for germline mutation rate
-            // - only use germline constented patients for impact study
-            // - in all other cases assume that every patient had germline testing
+            // - only use germline constented samples for impact study
+            // - in all other cases assume that every sample had germline testing
             //   if > 0 germline mutations are found
-            patientIds = (
-                this.props.mskImpactGermlineConsentedPatientIds &&
-                this.props.mskImpactGermlineConsentedPatientIds.length > 0
-            ) ? this.props.mskImpactGermlineConsentedPatientIds : this.props.patientIds;
+            sampleIds = (
+                this.props.germlineConsentedSampleIds &&
+                this.props.germlineConsentedSampleIds.result &&
+                this.props.germlineConsentedSampleIds.result.length > 0
+            ) ? this.props.germlineConsentedSampleIds.result : this.props.sampleIds;
         }
 
-        const gmr = patientIds ? germlineMutationRate(this.props.hugoGeneSymbol, this.props.mutations, patientIds) : 0;
+        const gmr = sampleIds ? germlineMutationRate(this.props.hugoGeneSymbol, this.props.mutations, sampleIds) : 0;
 
         return (
             <div data-test='germlineMutationRate' className={(gmr > 0) ? '' : 'invisible' }>
                 <label>Germline Mutation Frequency:</label>
                 {(gmr > 0) ? `${gmr.toFixed(1)}%` : '--'}
+                <DefaultTooltip
+                    placement="right"
+                    overlay={(<span>{'Percentage of samples with a germline mutation in ' + this.props.hugoGeneSymbol}</span>)}
+                >
+                    <i className='glyphicon glyphicon-info-sign' style={{'marginLeft':5}}></i>
+                </DefaultTooltip>
             </div>
         );
     }
