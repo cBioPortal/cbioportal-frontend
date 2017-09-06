607763e5aecf3f082108b66902b253dd5ea3cf75
Ersin Ciftci <arseniusthegreat@gmail.com>
2017-09-06 13:16:13 -0400

    Update to the latest API
    Former-commit-id: 10785fd2e446735d29694aea01a8ae743f6ea63f

--- src/pages/resultsView/ResultsViewPageStore.ts
+++ src/pages/resultsView/ResultsViewPageStore.ts
@@ -190,7 +190,7 @@ export class ResultsViewPageStore {
         invoke: () => {
             const sampleHasData:{[sampleUid:string]:boolean} = {};
             for (const data of this.clinicalDataForSamples.result) {
-                sampleHasData[toSampleUuid(data.clinicalAttribute.studyId, data.entityId)] = true;
+                sampleHasData[toSampleUuid(data.clinicalAttribute.studyId, data.sampleId)] = true;
             }
             return Promise.resolve(this.samples.result.filter(sample=>{
                 return !sampleHasData[toSampleUuid(sample.studyId, sample.sampleId)];
