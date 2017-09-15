a9daaf514067ba66f1c9b0e50e944b77a97ca125
Tamba Monrose <tamba@mit.edu>
2017-09-15 08:58:54 -0400

    Update cancer summary chart.

--- src/shared/components/cancerSummary/SummaryBarGraph.tsx
+++ src/shared/components/cancerSummary/SummaryBarGraph.tsx
@@ -3,7 +3,7 @@ import * as _ from 'lodash';
 import {computed, observable} from "mobx";
 import { ChartTooltipItem } from '@types/chart.js';
 import Chart, {ChartLegendItem} from 'chart.js';
-import {IBarGraphConfigOptions} from './CancerSummaryContent';
+import {IBarGraphConfigOptions, IBarGraphDataset} from './CancerSummaryContent';
 import {observer} from "mobx-react";
 import './styles.scss';
 
@@ -16,23 +16,20 @@ interface ISummaryBarGraphProps {
     setPdfAnchor:any;
 }
 
-interface IChartLegendItem extends ChartLegendItem {
-    datasetIndex: number;
-}
-
 @observer
 export default class SummaryBarGraph extends React.Component<ISummaryBarGraphProps, {}> {
 
+    @observable private chartContainer:HTMLElement;
     @observable private chartTarget:HTMLCanvasElement;
-    @observable.ref private chart:any;
+    private chart:any;
     @observable private chartConfig:any={type:'bar'};
     @observable private chartContainerWidth = 0;
+
 
     constructor() {
         super();
 
         this.updateChart = this.updateChart.bind(this);
-        this.getChartOptions = this.getChartOptions.bind(this);
     }
 
     private getTooltipOptions(tooltipModel: any, data:IBarGraphConfigOptions, chartOptions:any) {
@@ -46,7 +43,7 @@ export default class SummaryBarGraph extends React.Component<ISummaryBarGraphPro
             tooltipEl = document.createElement('div');
             tooltipEl.id = 'chartjs-tooltip';
             tooltipEl.innerHTML = "<div></div>";
-            document.body.appendChild(tooltipEl);
+            document.getElementsByClassName('cancer-summary-chart-container')[0].appendChild(tooltipEl);
         }
 
         // Hide if no tooltip
@@ -77,7 +74,7 @@ export default class SummaryBarGraph extends React.Component<ISummaryBarGraphPro
 
             let innerHtml = (
                 `<div><b>Summary for ${tooltipModel.title[0]}</b></div>
-                    <div>Gene altered in ${altTotalPercent}% of ${totalCases} cases</div>
+                    <div>Gene altered in ${_.round(altTotalPercent, 2)}% of ${totalCases} cases</div>
                     <table class="table table-striped">
                         <thead>
                             <tr>
@@ -108,36 +105,46 @@ export default class SummaryBarGraph extends React.Component<ISummaryBarGraphPro
         // Display, position, and set styles for font
         tooltipEl.style.opacity = '1';
         // tooltipEl.style.display = 'block';
-        tooltipEl.style.left = position.left + tooltipModel.caretX + 10 + 'px';
-        tooltipEl.style.top = position.top + tooltipModel.caretY + 5 + 'px';
+        tooltipEl.style.left =  tooltipModel.caretX + 35 + 'px';
+        tooltipEl.style.top = tooltipModel.caretY + 5 + 'px';
     }
 
     private componentDidMount() {
         this.chartConfig.data = this.props.data;
-        this.chartConfig.options = this.getChartOptions(1000);
+        this.chartConfig.options = this.chartOptions;
         this.chart = new Chart(this.chartTarget, this.chartConfig);
-        this.updateChartWidth();
-    }
-
-    private updateChartWidth() {
-        const container = document.getElementsByClassName('cancer-summary-chart-container')[0] as HTMLElement;
-        if (container){
-            this.chartContainerWidth = container.offsetWidth;
-        }
     }
 
     private componentDidUpdate() {
         this.updateChart();
-        this.updateChartWidth();
     }
 
     private updateChart() {
         this.chartConfig.data = this.props.data;
-        this.chartConfig.options = this.getChartOptions(1);
-        this.chart.update();
+        this.chartConfig.options = {};
+        this.chartConfig.options = this.chartOptions;
+        this.chart.update({duration: 0});
     }
 
-    private getChartOptions(duration: number) {
+    private getLegendNames(id:string) {
+        const names = {
+            mutated: "Mutation",
+            amplified: "Amplification",
+            deleted: "Deletion",
+            fusion: "Fusion",
+            gain: "Gain",
+            mrnaExpressionUp: "mRNA Upregulation",
+            mrnaExpressionDown: "mRNA Downregulation",
+            protExpressionUp: "Protein Upregulation",
+            protExpressionDown: "Protein Downregulation",
+            multiple: "Multiple Alterations",
+            homdel: "Deep Deletion",
+            hetloss: "Shallow Deletion"
+        };
+        return names[id] || id;
+    }
+
+    private get chartOptions() {
         const {data} = this.props;
         const that = this;
 
@@ -166,7 +173,8 @@ export default class SummaryBarGraph extends React.Component<ISummaryBarGraphPro
                     gridLines: {display: false},
                     stacked: true,
                     ticks: {
-                        maxRotation: 80
+                        maxRotation: 90,
+                        autoSkip: false
                     }
                 }],
                 yAxes: [{
@@ -185,18 +193,26 @@ export default class SummaryBarGraph extends React.Component<ISummaryBarGraphPro
                 }]
             },
             legend: {
+                // position: 'bottom',
                 display: this.props.legend,
                 labels: {
-                    filter: function (chartItem: IChartLegendItem) {
-                        //TODO: change integer to length of different mutation types
-                        if (that.props.legend === false) return false;
-                        if (chartItem.datasetIndex < 4) return true;
-                        return false;
-                    },
+                    generateLabels:(chart:any) => {
+                        const {data:cdata} = chart;
+                        if (cdata.labels.length && cdata.datasets.length) {
+                            return _.uniqBy(cdata.datasets, 'label').map((dataset:IBarGraphDataset, i) => {
+                                return {
+                                    text: this.getLegendNames(dataset.label),
+                                    fillStyle: dataset.backgroundColor,
+                                    index: i
+                                };
+                            });
+                        } else {
+                            return [];
+                        }
+                    }
                 }
             },
             animation: {
-                duration,
                 onComplete: () => {
                     this.toImagePdf();
                 }
@@ -205,8 +221,10 @@ export default class SummaryBarGraph extends React.Component<ISummaryBarGraphPro
     }
 
     private toImagePdf() {
-        const png = this.chart.toBase64Image();
-        this.props.setPngAnchor(png);
+        if (this.chart) {
+            const png = this.chart.toBase64Image();
+            this.props.setPngAnchor(png);
+        }
         if (this.chartTarget) {
             const pdf = this.chartTarget.toDataURL();
             this.props.setPdfAnchor(pdf);
@@ -226,7 +244,7 @@ export default class SummaryBarGraph extends React.Component<ISummaryBarGraphPro
             errorMessage = <div className="cancer-summary-error-message">No alteration plot data.</div>;
         }
         return (
-            <div className="cancer-summary-chart-container">
+            <div ref={el => this.chartContainer = el} className="cancer-summary-chart-container">
                 {errorMessage}
                 <canvas ref={el => this.chartTarget = el} width="100%" height="600"/>
             </div>
