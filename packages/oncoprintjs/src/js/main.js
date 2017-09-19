3a7057789bf905df35b3365b14f2501ab06f74c3
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-09-19 17:55:39 -0400

    Another attempt to make it work as node package

--- packages/oncoprintjs/src/js/main.js
+++ packages/oncoprintjs/src/js/main.js
@@ -1,1 +1,7 @@
-window.Oncoprint = require('./oncoprint.js');
+var OncoprintJS = require('./oncoprint.js');
+
+if (typeof window !== "undefined") {
+  window.Oncoprint = OncoprintJS;
+}
+
+module.exports = OncoprintJS;
