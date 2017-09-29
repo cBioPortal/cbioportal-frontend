db45c5d266a889771684e4c00c173a5fb481959e
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-09-29 14:44:44 -0400

    fix study column formatter

--- src/shared/components/mutationTable/column/StudyColumnFormatter.tsx
+++ src/shared/components/mutationTable/column/StudyColumnFormatter.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import DefaultTooltip from "shared/components/defaultTooltip/DefaultTooltip";
-import {CancerStudy, GeneticProfile, Mutation} from "shared/api/generated/CBioPortalAPI";
+import {CancerStudy, MolecularProfile, Mutation} from "shared/api/generated/CBioPortalAPI";
 import TruncatedText from "../../TruncatedText";
 import {getStudySummaryUrl} from "../../../api/urls";
 
 export default class StudyColumnFormatter
 {
-    private static getStudy(d:Mutation[], geneticProfileIdToGeneticProfile?:{[geneticProfileId:string]:GeneticProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}):CancerStudy|null {
-        if (!geneticProfileIdToGeneticProfile || !studyIdToStudy)
+    private static getStudy(d:Mutation[], molecularProfileIdToMolecularProfile?:{[molecularProfileId:string]:MolecularProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}):CancerStudy|null {
+        if (!molecularProfileIdToMolecularProfile || !studyIdToStudy)
             return null;
 
-        const geneticProfileId = d[0].geneticProfileId;
-        const geneticProfile = geneticProfileIdToGeneticProfile[geneticProfileId];
+        const molecularProfileId = d[0].molecularProfileId;
+        const geneticProfile = molecularProfileIdToMolecularProfile[molecularProfileId];
         if (!geneticProfile)
             return null;
         const study = studyIdToStudy[geneticProfile.studyId];
         return study || null;
     }
-    public static renderFunction(d:Mutation[], geneticProfileIdToGeneticProfile?:{[geneticProfileId:string]:GeneticProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}) {
-        const study = StudyColumnFormatter.getStudy(d, geneticProfileIdToGeneticProfile, studyIdToStudy);
+    public static renderFunction(d:Mutation[], molecularProfileIdToMolecularProfile?:{[molecularProfileId:string]:MolecularProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}) {
+        const study = StudyColumnFormatter.getStudy(d, molecularProfileIdToMolecularProfile, studyIdToStudy);
         if (!study) {
             return <span/>;
         } else {
@@ -36,8 +36,8 @@ export default class StudyColumnFormatter
         }
     }
 
-    public static getTextValue(d:Mutation[], geneticProfileIdToGeneticProfile?:{[geneticProfileId:string]:GeneticProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}) {
-        const study = StudyColumnFormatter.getStudy(d, geneticProfileIdToGeneticProfile, studyIdToStudy);
+    public static getTextValue(d:Mutation[], molecularProfileIdToMolecularProfile?:{[molecularProfileId:string]:MolecularProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}) {
+        const study = StudyColumnFormatter.getStudy(d, molecularProfileIdToMolecularProfile, studyIdToStudy);
         if (!study) {
             return "";
         } else {
@@ -45,7 +45,7 @@ export default class StudyColumnFormatter
         }
     }
 
-    public static filter(d:Mutation[], filterStringUpper:string, geneticProfileIdToGeneticProfile?:{[geneticProfileId:string]:GeneticProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}) {
+    public static filter(d:Mutation[], filterStringUpper:string, geneticProfileIdToGeneticProfile?:{[geneticProfileId:string]:MolecularProfile}, studyIdToStudy?:{[studyId:string]:CancerStudy}) {
         const study = StudyColumnFormatter.getStudy(d, geneticProfileIdToGeneticProfile, studyIdToStudy);
         if (!study) {
             return false;
