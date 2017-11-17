51cbcfa5abb1524d6f46b4f60d8d4266bda01590
Ino de Bruijn <ino@ino.pm>
2017-11-17 18:10:37 -0500

    Update genome nexus and use new endpoints lollipop
    We now return the pfam ids and ranges as part of ensembl canonical transcript
    response. Update lollipop store and plot accordingly.

--- src/shared/lib/PfamUtils.spec.ts
+++ src/shared/lib/PfamUtils.spec.ts
@@ -1,71 +1,35 @@
 import * as _ from 'lodash';
 import { assert } from 'chai';
-import {PfamDomain} from "shared/api/generated/GenomeNexusAPI";
+import {PfamDomainRange} from "shared/api/generated/GenomeNexusAPI";
 import {generatePfamDomainColorMap} from "./PfamUtils";
 
-let domains: PfamDomain[];
+let domains: PfamDomainRange[];
 
 before(() => {
     domains = [{
-        geneId: "ENSG00000198742",
-        geneSymbol: "SMURF1",
-        pfamDomainDescription: "Domain 2",
-        pfamDomainEnd: 11,
         pfamDomainId: "PF0002",
-        pfamDomainName: "Dmn2",
         pfamDomainStart: 7,
-        proteinId: "ENSP0001",
-        transcriptId: "ENST00000361125"
+        pfamDomainEnd: 9,
     }, {
-        geneId: "ENSG00000198742",
-        geneSymbol: "SMURF1",
-        pfamDomainDescription: "Domain 2",
-        pfamDomainEnd: 29,
         pfamDomainId: "PF0002",
-        pfamDomainName: "Dmn2",
         pfamDomainStart: 23,
-        proteinId: "ENSP0001",
-        transcriptId: "ENST00000361125"
+        pfamDomainEnd: 29,
     }, {
-        geneId: "ENSG00000198742",
-        geneSymbol: "SMURF1",
-        pfamDomainDescription: "Domain 1",
-        pfamDomainEnd: 5,
         pfamDomainId: "PF0001",
-        pfamDomainName: "Dmn1",
         pfamDomainStart: 2,
-        proteinId: "ENSP0001",
-        transcriptId: "ENST00000361125"
+        pfamDomainEnd: 5,
     }, {
-        geneId: "ENSG00000198742",
-        geneSymbol: "SMURF1",
-        pfamDomainDescription: "Domain 1",
-        pfamDomainEnd: 89,
         pfamDomainId: "PF0001",
-        pfamDomainName: "Dmn1",
         pfamDomainStart: 61,
-        proteinId: "ENSP0001",
-        transcriptId: "ENST00000361125"
+        pfamDomainEnd: 89,
     }, {
-        geneId: "ENSG00000198742",
-        geneSymbol: "SMURF1",
-        pfamDomainDescription: "Domain 4",
-        pfamDomainEnd: 41,
         pfamDomainId: "PF0004",
-        pfamDomainName: "Dmn4",
         pfamDomainStart: 31,
-        proteinId: "ENSP0001",
-        transcriptId: "ENST00000361125"
+        pfamDomainEnd: 41,
     }, {
-        geneId: "ENSG00000198742",
-        geneSymbol: "SMURF1",
-        pfamDomainDescription: "Domain 3",
-        pfamDomainEnd: 17,
         pfamDomainId: "PF0003",
-        pfamDomainName: "Dmn3",
         pfamDomainStart: 13,
-        proteinId: "ENSP0003",
-        transcriptId: "ENST00000361368"
+        pfamDomainEnd: 17,
     }];
 });
 
