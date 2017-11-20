4f009944583606504c7783842901cd062d3f5857
aaronlisman <donkey00>
2017-11-20 16:50:16 -0500

    Improve detection of overlap detection based on patterns in studyId
    Former-commit-id: 9aefbf1b65998389ddc005e23d2c1336b7766ca6

--- src/shared/lib/getOverlappingStudies.ts
+++ src/shared/lib/getOverlappingStudies.ts
@@ -1,19 +1,19 @@
 import {CancerStudy} from "../api/generated/CBioPortalAPI";
 import * as _ from 'lodash';
 
-//testIt
 export default function getOverlappingStudies(studies: CancerStudy[]):CancerStudy[][] {
     const groupedTCGAStudies = _.reduce(studies,(memo, study:CancerStudy)=>{
         if (/_tcga/.test(study.studyId)) {
-            const initial = study.studyId.match(/^[^_]*_[^_]*/);
+            const initial = study.studyId.replace(/(_\d\d\d\d|_pub|(_pub\d\d\d\d))$/,'');
             if (initial) {
-                if (initial[0] in memo) {
-                    memo[initial[0]].push(study);
+                if (initial in memo) {
+                    memo[initial].push(study);
                 } else {
-                    memo[initial[0]] = [study];
+                    memo[initial] = [study];
                 }
             }
         }
+
         return memo;
     }, {} as { [studyId:string]:CancerStudy[] });
     return _.filter(groupedTCGAStudies, (grouping)=>grouping.length > 1);
