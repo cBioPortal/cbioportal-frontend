2f97d88b5ba7328bc8091f88194d2b1f1910083c
Fedde Schaeffer <fedde@thehyve.nl>
2018-01-19 19:20:23 +0100

    Add geneset score alt. type key to central list
    As the strings used in profile metadata directly affect compatibility
    with the backend and data loading pipeline, it makes sense to keep them
    in a centralised place rather than hard-code them scattered around the
    codebase.
    
    
    Former-commit-id: e16a4677c75d8355cb03445d9617694fde1ecc87

--- src/pages/resultsView/ResultsViewPageStore.ts
+++ src/pages/resultsView/ResultsViewPageStore.ts
@@ -74,8 +74,9 @@ export const AlterationTypeConstants = {
     COPY_NUMBER_ALTERATION: 'COPY_NUMBER_ALTERATION',
     MRNA_EXPRESSION: 'MRNA_EXPRESSION',
     PROTEIN_LEVEL: 'PROTEIN_LEVEL',
-    FUSION: 'FUSION'
-}
+    FUSION: 'FUSION',
+    GENESET_SCORE: 'GENESET_SCORE'
+};
 
 export interface ExtendedAlteration extends Mutation, GeneMolecularData {
     molecularProfileAlterationType: MolecularProfile["molecularAlterationType"];
@@ -1294,11 +1295,10 @@ export class ResultsViewPageStore {
             this.molecularProfilesInStudies
         ],
         invoke: () => {
-            const GENESET_SCORE = "GENESET_SCORE";
             const applicableProfiles = _.filter(
                 this.molecularProfilesInStudies.result!,
                 profile => (
-                    profile.molecularAlterationType === GENESET_SCORE
+                    profile.molecularAlterationType === AlterationTypeConstants.GENESET_SCORE
                     && profile.showProfileInAnalysisTab
                 )
             );
