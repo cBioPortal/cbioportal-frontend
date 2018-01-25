75284a08d0ee53765ba4fc2219f1e14b2dd5a392
aaronlisman <donkey00>
2018-01-25 13:53:03 -0500

    Add local identifier to browserstack jobs so concurrent jobs don't fail eachother.

--- end-to-end-tests/wdio/travis.conf.js
+++ end-to-end-tests/wdio/travis.conf.js
@@ -1,5 +1,11 @@
 var browserstack = require('browserstack-local');
 
+var path = require('path');
+//var VisualRegressionCompare = require('wdio-visual-regression-service/compare');
+
+//var getScreenshotName = require('./getScreenshotName');
+const localIdentifier = `foobar_${Date.now()}`
+
 exports.config = {
     //
     // ==================
@@ -44,10 +50,13 @@ exports.config = {
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
         maxInstances: 5,
-        //
-        browserName: 'chrome',
+        'os': 'OS X',
+        'os_version': 'El Capitan',
+        'browser': 'Chrome',
+        'browser_version': '63.0',
         build: 'webdriver-browserstack',
-        'browserstack.local': true
+        'browserstack.local': true,
+        'browserstack.localIdentifier': localIdentifier
     }],
     //
     // ===================
@@ -110,6 +119,19 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: ['browserstack'],
+
+    // visualRegression: {
+    //     compare: new VisualRegressionCompare.LocalCompare({
+    //         referenceName: getScreenshotName(path.join(process.cwd(), 'screenshots/reference')),
+    //         screenshotName: getScreenshotName(path.join(process.cwd(), 'screenshots/screen')),
+    //         diffName: getScreenshotName(path.join(process.cwd(), 'screenshots/diff')),
+    //         misMatchTolerance:5
+    //     }),
+    //     viewportChangePause: 300,
+    //     viewports: [{ width: 1600, height: 1000 }],
+    //     orientations: ['landscape', 'portrait'],
+    // },
+
     user: process.env.BROWSERSTACK_USERNAME,
     key: process.env.BROWSERSTACK_ACCESS_KEY,
     // Framework you want to run your specs with.
@@ -240,22 +262,22 @@ exports.config = {
      */
     // onComplete: function(exitCode) {
     // }
-	// Code to start browserstack local before start of test
-	onPrepare: function (config, capabilities) {
-	  console.log("Connecting local");
-	  return new Promise(function(resolve, reject){
-	    exports.bs_local = new browserstack.Local();
-	    exports.bs_local.start({'key': exports.config.key }, function(error) {
-	  	if (error) return reject(error);
-	  	console.log('Connected. Now testing...');
+    // Code to start browserstack local before start of test
+    onPrepare: function (config, capabilities) {
+        console.log("Connecting local");
+        return new Promise(function(resolve, reject){
+            exports.bs_local = new browserstack.Local();
+            exports.bs_local.start({'key': exports.config.key, localIdentifier }, function(error) {
+                if (error) return reject(error);
+                console.log('Connected. Now testing...');
 
-	  	resolve();
-	    });
-	  });
-	},
+                resolve();
+            });
+        });
+    },
 
-	// Code to stop browserstack local after end of test
-	onComplete: function (capabilties, specs) {
-	  exports.bs_local.stop(function() {});
-	}
+    // Code to stop browserstack local after end of test
+    onComplete: function (capabilties, specs) {
+        exports.bs_local.stop(function() {});
+    }
 }
