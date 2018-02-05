1482bb6494b69bff035d01a0a2aeaff250836211
Karthik Kalletla <kalletlakarthik@gmail.com>
2018-02-05 13:21:53 -0500

    Updates to query interface for multi-study query
    Former-commit-id: 41114bf797d29e12d55384b5a254d6950b5ccad7

--- src/shared/components/query/DataTypePrioritySelector.spec.tsx
+++ src/shared/components/query/DataTypePrioritySelector.spec.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 import {shallow} from "enzyme";
-import {profileAvailability, radioButtons} from "./DataTypePrioritySelector";
+import {checkBoxes} from "./DataTypePrioritySelector";
 import {AlterationTypeConstants} from "../../../pages/resultsView/ResultsViewPageStore";
 import {MolecularProfile} from "../../api/generated/CBioPortalAPI";
 import {stringListToSet} from "../../lib/StringUtils";
@@ -12,98 +12,18 @@ import {QueryStore} from "./QueryStore";
 expect.extend(expectJSX);
 
 describe("DataTypePrioritySelector", ()=>{
-    describe("profileAvailability", ()=>{
-        it("returns correct profile availability result in case of zero profiles", ()=>{
-            assert.deepEqual(profileAvailability([]), {mutation:false, cna:false});
-        });
-        it("returns correct profile availability result in case of one profile", ()=>{
-            let profiles = [{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: true
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:true, cna:false});
-
-            profiles = [{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: false
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:false, cna:false});
-
-            profiles = [{
-                molecularAlterationType: AlterationTypeConstants.COPY_NUMBER_ALTERATION,
-                showProfileInAnalysisTab: true
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:false, cna:true});
-
-            profiles = [{
-                molecularAlterationType: AlterationTypeConstants.COPY_NUMBER_ALTERATION,
-                showProfileInAnalysisTab: false
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:false, cna:false});
-        });
-        it("returns correct profile availability result in case of two profiles", ()=>{
-            let profiles = [{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: true
-            },{
-                molecularAlterationType: AlterationTypeConstants.COPY_NUMBER_ALTERATION,
-                showProfileInAnalysisTab: true
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:true, cna:true});
-
-            profiles = [{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: false
-            },{
-                molecularAlterationType: AlterationTypeConstants.COPY_NUMBER_ALTERATION,
-                showProfileInAnalysisTab: true
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:false, cna:true});
-
-            profiles = [{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: true
-            },{
-                molecularAlterationType: AlterationTypeConstants.COPY_NUMBER_ALTERATION,
-                showProfileInAnalysisTab: false
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:true, cna:false});
-
-            profiles = [{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: false
-            },{
-                molecularAlterationType: AlterationTypeConstants.COPY_NUMBER_ALTERATION,
-                showProfileInAnalysisTab: false
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:false, cna:false});
-        });
-        it("returns correct profile availability result in case of several profiles", ()=>{
-            let profiles = [{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: true
-            },{
-                molecularAlterationType: AlterationTypeConstants.MUTATION_EXTENDED,
-                showProfileInAnalysisTab: false
-            },{
-                molecularAlterationType: AlterationTypeConstants.COPY_NUMBER_ALTERATION,
-                showProfileInAnalysisTab: false
-            }] as MolecularProfile[];
-            assert.deepEqual(profileAvailability(profiles), {mutation:true, cna:false});
-        });
-    });
-    describe("radioButtons",()=>{
+    describe("checkBoxes",()=>{
         it("shows the right buttons when theres mutation and cna profiles available",()=>{
-            const buttonLabels = stringListToSet(radioButtons({mutation:true, cna:true}, {} as QueryStore).map(x=>x.props.label));
-            assert.deepEqual(buttonLabels, stringListToSet(["Mutation and CNA", "Only Mutation", "Only CNA"]));
+            const buttonLabels = stringListToSet(checkBoxes({mutation:true, cna:true}, {} as QueryStore).map(x=>x.props.label));
+            assert.deepEqual(buttonLabels, stringListToSet(["Mutation", "Copy number alterations"]));
         });
         it("shows the right buttons when theres just mutation profiles available",()=>{
-            const buttonLabels = stringListToSet(radioButtons({mutation:true, cna:false}, {} as QueryStore).map(x=>x.props.label));
+            const buttonLabels = stringListToSet(checkBoxes({mutation:true, cna:false}, {} as QueryStore).map(x=>x.props.label));
             assert.deepEqual(buttonLabels, stringListToSet(["Mutation"]));
         });
         it("shows the right buttons when theres just cna profiles available",()=>{
-            const buttonLabels = stringListToSet(radioButtons({mutation:false, cna:true}, {} as QueryStore).map(x=>x.props.label));
-            assert.deepEqual(buttonLabels, stringListToSet(["CNA"]));
+            const buttonLabels = stringListToSet(checkBoxes({mutation:false, cna:true}, {} as QueryStore).map(x=>x.props.label));
+            assert.deepEqual(buttonLabels, stringListToSet(["Copy number alterations"]));
         });
     })
 });
