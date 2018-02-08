8f27fbf3505d23883c4f8652f75a20845997f827
Ino de Bruijn <ino@ino.pm>
2018-02-08 17:20:22 -0500

    Add env files per branch
    Instead of only specifying one environment in my-index, we can now select the
    master or rc environment by setting `export BRANCH_ENV=rc` or `export
    BRANCH_ENV=master` and running `eval $(./scripts/env_vars.sh)`. That gets the
    exports for each particular branch.` In webpack we then use the DefinePlugin to
    get the correct environment variables. This means developers have to do an
    extra step of selecting what env variables to use.
    
    CircleCI finds out what branch the PR is pointing to automatically and gets the
    appropriate env variables.
    
    
    Former-commit-id: 4587169238a157e1ed0052a0da138e4de980be2f

--- scripts/env_vars.sh
+++ scripts/env_vars.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 # eval output of this file to get appropriate env variables e.g. eval "$(./env_vars.sh)"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RED='\033[0;31m'
+NC='\033[0m'
 
-echo "export CBIOPORTAL_URL=http://$(grep 'apiRoot' ${SCRIPT_DIR}/../my-index.ejs | cut -d: -f2 | cut -d, -f1 | tr -d "'" | tr -d [:space:] | tr -d ';')"
-echo "export GENOME_NEXUS_URL=http://$(grep 'genomeNexusApiUrl' ${SCRIPT_DIR}/../my-index.ejs | cut -d: -f2 | cut -d, -f1 | tr -d "'" | tr -d [:space:] | tr -d ';')"
+if [[ "$CIRCLECI" ]]; then
+    # on circle ci determine env variables based on branch or in case of PR
+    # what branch the PR is pointing to
+    if [[ "$CIRCLE_PULL_REQUEST" ]]; then
+        BRANCH=$(curl "${CIRCLE_PULL_REQUEST}" | grep -oE 'cBioPortal/cbioportal-frontend:[^"]*' | cut -d: -f2)
+    else
+        BRANCH=$CIRCLE_BRANCH
+    fi
+    cat $SCRIPT_DIR/../env/${BRANCH}.sh
+elif [[ "$BRANCH_ENV" ]]; then
+    cat $SCRIPT_DIR/../env/${BRANCH_ENV}.sh
+
+    # override with custom exports if they exist
+    if [[ -f ${SCRIPT_DIR}/env/custom.sh ]]; then
+        cat ${SCRIPT_DIR}/env/custom.sh
+    fi
+else
+    echo -e "${RED}No desired BRANCH_ENV variable set${NC}"
+    echo -e "${RED}set with e.g. export BRANCH_ENV=master${NC}"
+    echo -e "${RED}or export BRANCH_ENV=rc${NC}"
+    exit 1
+fi
