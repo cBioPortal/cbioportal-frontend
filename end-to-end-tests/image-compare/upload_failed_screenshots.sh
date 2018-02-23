ef380c38d72dad02a23c2ab9f678ed3bed331967
aaronlisman <donkey00>
2018-02-23 17:56:40 -0500

    upload screenshots to circleci artifacts and refine review/comparison interface
    Former-commit-id: 00b743736e8e6337cd3ef24b810ca14212ee5173

--- end-to-end-tests/image-compare/upload_failed_screenshots.sh
+++ end-to-end-tests/image-compare/upload_failed_screenshots.sh
@@ -7,67 +7,25 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-upload_image() {
-    local png=${1}
-
-    # curl -s -F "clbin=@$png" https://clbin.com
-    # clbin.com is down, using ptpb.pw instead
-    curl -F "c=@$png" https://ptpb.pw/ | grep url | cut -d' ' -f2
-}
-
-
 cd ${DIR}/../screenshots
 if (ls diff/*.png 2> /dev/null > /dev/null); then
     references=()
-    refs_uploaded=()
-    diffs_uploaded=()
-    screenshots_uploaded=()
-
     for diff in diff/*.png; do
         reference=${diff/diff/reference}
         references=( ${references[@]} $reference )
-
-        # upload screenshots when using Travis or Circle CI
-        if [[ ${TRAVIS} || ${CIRCLECI} ]]; then
-
-            screenshot=${diff/diff/screen}
-            diffs_uploaded=( ${diffs_uploaded[@]} "$(upload_image $diff)" )
-            refs_uploaded=( ${refs_uploaded[@]} "$(upload_image $reference)" )
-            screenshots_uploaded=( ${screenshots_uploaded[@]} "$(upload_image $screenshot)" )
-        fi
+        echo "FAILED SCREENSHOT TEST: ${reference}"
     done
 
-	# show dev how to download failing test screenshots
-	echo -e "${RED}SCREENSHOT TESTS FAILED!${NC}"
+    echo "var errorImages = '${references[@]}'.split(' ')" > ${DIR}/errors.js
+
+
     if [[ ${TRAVIS} || ${CIRCLECI} ]]; then
-        echo "TO DOWNLOAD FAILING SCREENSHOTS TO LOCAL REPO ROOT RUN:"
-        for ((i=0; i < ${#references[@]}; i++)); do
-            reference=${references[$i]}
-            url=${screenshots_uploaded[$i]}
-            echo "curl '"${url}"' > end-to-end-tests/screenshots/${reference}"
-        done
+        echo "${YELLOW}See Artifacts tab above for image comparison UI (imageCompare.html)${NC}"
+    else
+        repo_url=file://${PWD}/../..
+        echo -e "${GREEN}COPY+PASTE in BROWSER TO COMPARE FAILED SCREENSHOTS: ${YELLOW}${repo_url}/end-to-end-tests/imageCompare.html${NC}";
     fi
 
-    echo "CHECK OUT FAILED SCREENSHOTS:"
-	for ((i=0; i < ${#references[@]}; i++)); do
-        reference=${references[$i]}
 
-        if [[ ${TRAVIS} || ${CIRCLECI} ]]; then
-            ref_url=${refs_uploaded[$i]}
-            diff_url=${diffs_uploaded[$i]}
-            url=${screenshots_uploaded[$i]}
-            if [[ ${TRAVIS} ]]; then
-                repo_url=http://rawgit.com/${TRAVIS_REPO_SLUG}/${TRAVIS_COMMIT}
-            else
-                repo_url=http://rawgit.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_SHA1}
-            fi
-        else
-            repo_url=file://${PWD}/../..
-            ref_url=../screenshots/${reference}
-            url=../screenshots/${reference/reference/screen}
-            diff_url=../screenshots/${reference/reference/diff}
-        fi
-        echo -e "COPY+PASTE in BROWSER TO COMPARE FAILED SCREENSHOT: ${YELLOW}${repo_url}/end-to-end-tests/image-compare/index.html?img1=${ref_url}&img2=${url}&label1=reference&label2=$(git rev-parse --short HEAD)&screenshot_name=end-to-end-tests/screenshots/${reference}&diff_img=${diff_url}${NC}"
-    done
 fi
 exit 1 # always exit 1 since we are assuming that you call this when screenshots have failed
