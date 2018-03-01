e98b23b7affd3a7bf38b4dc9d24107ff42a1597b
aaronlisman <donkey00>
2018-03-01 14:48:21 -0500

    tweak test and screenshot index. update screenshots. update apis.

--- end-to-end-tests/specs/home.spec.js
+++ end-to-end-tests/specs/home.spec.js
@@ -8,7 +8,7 @@ const CBIOPORTAL_URL = process.env.CBIOPORTAL_URL.replace(/\/$/, "");
 
 describe('homepage', function() {
 
-    this.retries(2);
+  
 
     before(()=>{
         goToUrlAndSetLocalStorage(CBIOPORTAL_URL);
@@ -198,18 +198,15 @@ describe('homepage', function() {
 
 describe('patient page', function(){
 
-    this.retries(2);
+    before(()=>{
+        goToUrlAndSetLocalStorage(CBIOPORTAL_URL);
+    });
 
     it('oncokb indicators show up and hovering produces oncocard', function(){
 
         browser.url(`${CBIOPORTAL_URL}/case.do#/patient?studyId=ucec_tcga_pub&caseId=TCGA-BK-A0CC`);
-        // browser.setViewportSize({ height:1400, width:1000 });
 
-
-        browser.waitUntil( function(){
-            let el = $('.tab-content table td span');
-            return (el.value && browser.elementIdText(el.value.ELEMENT).value === 'PPP2R1A')
-        });
+        browser.waitForExist('span=PPP2R1A');
 
         // find oncokb image
         var oncokbIndicator = $('[data-test="oncogenic-icon-image"]');
@@ -217,13 +214,15 @@ describe('patient page', function(){
 
         // move over oncokb image (this is deprecated, but there is no new
         // function yet)
+
+        browser.pause(3000);
         browser.moveToObject('[data-test="oncogenic-icon-image"]',5,5);
 
         var oncokbCard = $('[data-test="oncokb-card"]');
 
         oncokbCard.waitForExist(30000);
 
-        assert.equal(browser.getText('.tip-header').toLowerCase(), 'PPP2R1A S256F in Uterine Serous Carcinoma/Uterine Papillary Serous Carcinoma'.toLowerCase());
+        assert.equal(browser.getText('.tip-header').toLowerCase(), 'TP53 Q331* in uterine serous carcinoma/uterine papillary serous carcinoma'.toLowerCase());
 
     });
 
@@ -231,7 +230,7 @@ describe('patient page', function(){
 
 describe('cross cancer query', function() {
 
-    this.retries(2);
+  
 
     it('should show cross cancer bar chart with TP53 in title when selecting multiple studies and querying for TP53', function() {
         browser.url(`${CBIOPORTAL_URL}`);
@@ -265,7 +264,7 @@ describe('cross cancer query', function() {
 
 describe('single study query', function() {
 
-    this.retries(2);
+  
 
     before(()=>{
         goToUrlAndSetLocalStorage(CBIOPORTAL_URL);
@@ -409,7 +408,7 @@ describe("results page", function() {
 
 describe('oncoprint', function() {
 
-    this.retries(2);
+  
 
     before(()=>{
         goToUrlAndSetLocalStorage(CBIOPORTAL_URL);
@@ -834,7 +833,7 @@ describe('oncoprint', function() {
 describe('case set selection in front page query form', function(){
     var selectedCaseSet_sel = 'div[data-test="CaseSetSelector"] span.Select-value-label[aria-selected="true"]';
 
-    this.retries(2);
+  
 
     beforeEach(function() {
         goToUrlAndSetLocalStorage(CBIOPORTAL_URL);
@@ -990,7 +989,7 @@ describe('case set selection in front page query form', function(){
 describe('case set selection in modify query form', function(){
     var selectedCaseSet_sel = 'div[data-test="CaseSetSelector"] span.Select-value-label[aria-selected="true"]';
 
-    this.retries(2);
+  
 
     beforeEach(function(){
         var url = `${CBIOPORTAL_URL}/index.do?cancer_study_id=coadread_tcga_pub&Z_SCORE_THRESHOLD=2&RPPA_SCORE_THRESHOLD=2&data_priority=0&case_set_id=coadread_tcga_pub_rppa&gene_list=KRAS%2520NRAS%2520BRAF&geneset_list=+&tab_index=tab_visualize&Action=Submit&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=coadread_tcga_pub_mutations&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=coadread_tcga_pub_gistic`;
@@ -1079,7 +1078,7 @@ describe('case set selection in modify query form', function(){
 
 describe('genetic profile selection in modify query form', function(){
 
-    this.retries(2);
+  
 
     beforeEach(function(){
         var url = `${CBIOPORTAL_URL}/index.do?cancer_study_id=chol_tcga&Z_SCORE_THRESHOLD=2.0&RPPA_SCORE_THRESHOLD=2.0&data_priority=0&case_set_id=chol_tcga_all&gene_list=EGFR&geneset_list=+&tab_index=tab_visualize&Action=Submit&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=chol_tcga_mutations&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=chol_tcga_gistic&genetic_profile_ids_PROFILE_PROTEIN_EXPRESSION=chol_tcga_rppa_Zscores`;
