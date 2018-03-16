6d91db9ae93e9250dff84d0a6416dd560b652e3a
Fedde Schaeffer <fedde@thehyve.nl>
2018-03-16 18:45:19 +0100

    Expose a function to filter data by merged track
    The Oncoprint will need to filter data while retaining information on
    which genes are queried as part of a single merged track query. In order
    to do this, add a use-case in which the filter function returns an
    unflattened structure, correcting mismatches in the function's documented
    and declared output type in the process.
    
    
    Former-commit-id: 15f7d95c6e737092fc234ed21eb3e709ee5098c9

--- src/shared/lib/oql/oqlfilter.d.ts
+++ src/shared/lib/oql/oqlfilter.d.ts
@@ -6,7 +6,7 @@
 
 //var filterData = function (oql_query, data, _accessors, opt_default_oql, opt_by_oql_line, opt_mark_oql_regulation_direction)
 
-import {SingleGeneQuery} from "./oql-parser";
+import {SingleGeneQuery, MergedGeneQuery} from "./oql-parser";
 import {AnnotatedMutation, ExtendedAlteration} from "../../../pages/resultsView/ResultsViewPageStore";
 import {NumericGeneMolecularData, Mutation} from "../../api/generated/CBioPortalAPI";
 
@@ -14,11 +14,20 @@ type OQLAlterationFilterString = string;
 
 export type OQLLineFilterOutput<T> = {
     gene: string;
-    parsed_oql_line: SingleGeneQuery[];
+    parsed_oql_line: SingleGeneQuery;
     oql_line: string;
     data: T[];
 };
 
+export type MergedTrackLineFilterOutput<T> = {
+    list: OQLLineFilterOutput<T>[],
+    label?: string
+};
+export type UnflattenedOQLLineFilterOutput<T> = (
+    OQLLineFilterOutput<T> | MergedTrackLineFilterOutput<T>
+);
+
+/* Interprets datatypes statements and flattens out merged track queries. */
 export declare function parseOQLQuery(
     oql_query: string,
     opt_default_oql?: OQLAlterationFilterString
@@ -28,3 +37,10 @@ export declare function filterCBioPortalWebServiceData(oql_query:string, data:(M
 
 export declare function filterCBioPortalWebServiceDataByOQLLine(oql_query:string, data:(AnnotatedMutation | NumericGeneMolecularData)[], accessors:any, default_oql:string): OQLLineFilterOutput<ExtendedAlteration&AnnotatedMutation>[];
 export declare function filterCBioPortalWebServiceDataByOQLLine(oql_query:string, data:(Mutation | NumericGeneMolecularData)[], accessors:any, default_oql:string): OQLLineFilterOutput<ExtendedAlteration>[];
+
+export declare function filterCBioPortalWebServiceDataByUnflattenedOQLLine(
+    oql_query: string,
+    data: (AnnotatedMutation|NumericGeneMolecularData)[],
+    accessors:any,
+    default_oql:string
+): UnflattenedOQLLineFilterOutput<ExtendedAlteration&AnnotatedMutation>[];
