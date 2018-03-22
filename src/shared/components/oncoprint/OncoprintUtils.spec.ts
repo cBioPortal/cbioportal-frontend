75dba8907745cf8e7b5fe1fb1b9a964e1b6d5488
Fedde Schaeffer <fedde@thehyve.nl>
2018-03-22 17:43:27 +0100

    Make genetic track labelling logic testable
    Former-commit-id: 2f5476ab4ea4022826f723d308e9441a5eeef69d

--- src/shared/components/oncoprint/OncoprintUtils.spec.ts
+++ src/shared/components/oncoprint/OncoprintUtils.spec.ts
@@ -1,8 +1,71 @@
+import {
+    makeGeneticTrackWith,
+    percentAltered
+} from "./OncoprintUtils";
+import * as _ from 'lodash';
 import {assert} from 'chai';
-import {percentAltered} from "./OncoprintUtils";
 
-describe('OncoprintUtils', ()=>{
-    describe('percentAltered',()=>{
+describe('OncoprintUtils', () => {
+    describe('makeGeneticTrackWith', () => {
+        const makeMinimalCoverageRecord = () => ({
+            byGene: {}, allGenes: [],
+            notProfiledByGene: {}, notProfiledAllGenes: []
+        });
+        const makeMinimal3Patient3GeneStoreProperties = () => ({
+            samples: [],
+            patients: [
+                { 'patientId': 'TCGA-02-0001', 'studyId': 'gbm_tcga', 'uniquePatientKey': 'VENHQS0wMi0wMDAxOmdibV90Y2dh' },
+                { 'patientId': 'TCGA-02-0003', 'studyId': 'gbm_tcga', 'uniquePatientKey': 'VENHQS0wMi0wMDAzOmdibV90Y2dh' },
+                { 'patientId': 'TCGA-02-0006', 'studyId': 'gbm_tcga', 'uniquePatientKey': 'VENHQS0wMi0wMDA2OmdibV90Y2dh' }
+            ],
+            coverageInformation: {
+                samples: {},
+                patients: {
+                    'VENHQS0wMi0wMDAxOmdibV90Y2dh': makeMinimalCoverageRecord(),
+                    'VENHQS0wMi0wMDAzOmdibV90Y2dh': makeMinimalCoverageRecord(),
+                    'VENHQS0wMi0wMDA2OmdibV90Y2dh': makeMinimalCoverageRecord()
+                }
+            },
+            sequencedSampleKeysByGene: {},
+            sequencedPatientKeysByGene: {'BRCA1': [], 'PTEN': [], 'TP53': []},
+            selectedMolecularProfiles: []
+        });
+        const makeMinimal3Patient3GeneCaseData = () => ({
+            samples: {},
+            patients: {
+                'VENHQS0wMi0wMDAxOmdibV90Y2dh': [],
+                'VENHQS0wMi0wMDAzOmdibV90Y2dh': [],
+                'VENHQS0wMi0wMDA2OmdibV90Y2dh': []
+            }
+        });
+        const MINIMAL_TRACK_INDEX = 0;
+
+        it('if queried for a plain gene, labels the track based on that query', () => {
+            // given store properties for three patients and query data for
+            // a single gene
+            const storeProperties = makeMinimal3Patient3GeneStoreProperties();
+            const queryData = {
+                cases: makeMinimal3Patient3GeneCaseData(),
+                oql: {
+                    gene: 'TP53',
+                    oql_line: 'TP53;',
+                    parsed_oql_line: {gene: 'TP53', alterations: []},
+                    data: []
+                }
+            };
+            // when the track formatting function is called with this query
+            const trackFunction = makeGeneticTrackWith({
+                sampleMode: false,
+                ...storeProperties
+            });
+            const track = trackFunction(queryData, MINIMAL_TRACK_INDEX);
+            // then it returns a track with the same label and OQL
+            assert.equal(track.label, 'TP53');
+            assert.equal(track.oql, 'TP53;');
+        });
+    });
+
+    describe('percentAltered', () => {
         it("returns the percentage with no decimal digits, for percentages >= 3", ()=>{
             assert.equal(percentAltered(3,100), "3%");
             assert.equal(percentAltered(20,100), "20%");
