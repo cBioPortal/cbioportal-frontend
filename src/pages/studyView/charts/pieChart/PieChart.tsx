df8e0647ddf2a2aa6a97168b4bfc3cf38f8b17e3
Karthik Kalletla <kalletlakarthik@gmail.com>
2018-05-02 09:56:08 -0400

    Add pie chart slice tooltip and increase pie radius
    Former-commit-id: a2010a5fb9ad937834f1352fbeb45eb86b919b46

--- src/pages/studyView/charts/pieChart/PieChart.tsx
+++ src/pages/studyView/charts/pieChart/PieChart.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import styles from "./styles.module.scss";
 import { StudyViewPageStore, ClinicalAttributeDataWithMeta, ClinicalDataType } from "pages/studyView/StudyViewPage";
 import { observer } from "mobx-react";
-import { VictoryPie, VictoryContainer, VictoryLabel } from 'victory';
-import { ClinicalAttribute } from "shared/api/generated/CBioPortalAPI";
+import { VictoryPie, VictoryContainer, VictoryLabel, VictoryTooltip } from 'victory';
 import { ClinicalDataCount } from "shared/api/generated/CBioPortalAPIInternal";
-import { observable } from "mobx";
+import { observable, computed } from "mobx";
 import _ from "lodash";
+
 export interface IPieChartProps {
   data: ClinicalAttributeDataWithMeta;
   filters:string[];
@@ -93,6 +93,12 @@ export class PieChart extends React.Component<IPieChartProps, {}> {
     }
   }];
 
+  @computed get totalCount(){
+    return this.props.data.counts.reduce((acc:number,next)=>{
+      return acc+next.count
+    },0)
+  }
+
   public render() {
     let data:ClinicalDataCount[] = this.props.data.counts;
     let chartFilters = this.props.filters;
@@ -106,18 +112,18 @@ export class PieChart extends React.Component<IPieChartProps, {}> {
     return (
         <VictoryPie
           colorScale={COLORS}
-          containerComponent={<VictoryContainer responsive={false} />}
+          containerComponent={<VictoryContainer responsive={false}/>}
           width={200}
           height={200}
-          labelRadius={25}
-          labels={(d:any) => d.y}
+          labelRadius={30}
+          padding={35}
+          //to hide label if the angle is too small(currently set to 20 degrees)
+          labels={(d:any) => ((d.y*360)/this.totalCount)<20?'':d.y}
           data={result}
           events={this.events}
-          labelComponent={<CustomLabel/>}
+          labelComponent={<CustomTooltip />}
           style={{
-            data: {
-              fillOpacity: 0.9
-            },
+            data: { fillOpacity: 0.9 },
             labels: { fill: "white" }
           }}
         />
@@ -126,23 +132,20 @@ export class PieChart extends React.Component<IPieChartProps, {}> {
 
 }
 
-//to hide label if the angle is too small(currently set to 20 degrees)
-class CustomLabel extends React.Component {
-  static defaultEvents = VictoryLabel.defaultEvents
+class CustomTooltip extends React.Component<{}, {}> {
+  static defaultEvents = VictoryTooltip.defaultEvents
   render() {
     const d:any = this.props;
-    let text = d.text;
-    if(d.slice){
-      let degrees = Math.abs((d.slice.endAngle as number) - (d.slice.startAngle as number))*180/3.14159;
-      if(degrees<20){
-        text = '';
-      }
-    }
     return (
       <g>
-        <VictoryLabel
-            {...this.props} 
-            text={text}/>
+        <VictoryTooltip {...this.props}
+                        pointerLength={0}
+                        cornerRadius={0}
+                        text={`${d.datum.x}: ${d.datum.y}`}
+                        style={{ fontSize: '10px',fontFamily: "Verdana, Arial, sans-serif" }}
+                        flyoutStyle={{ strokeWidth: '0' }}
+        />
+        <VictoryLabel {...this.props}/>
       </g>
     );
   }
