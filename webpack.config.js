295e22476b422f118b219813176cae5e6575997b
aaronlisman <lismanBusiness@gmail.com>
2018-05-17 10:24:18 -0400

    Move legacy javascript for IGV iframe to new project

--- webpack.config.js
+++ webpack.config.js
@@ -133,6 +133,9 @@ var config = {
         new CopyWebpackPlugin([
             {from: './common-dist', to: 'reactapp'},
             {from: './src/globalStyles/prefixed-bootstrap.min.css', to: 'reactapp/prefixed-bootstrap.min.css'},
+            {from: './src/shared/legacy/igv.min.js', to: 'reactapp/igv.min.js'},
+            {from: './src/shared/legacy/igv.css', to: 'reactapp/igv.css'},
+            {from: './src/shared/legacy/igv_webstart.js', to: 'reactapp/igv_webstart.js'},
             {from: './src/globalStyles/prefixed-bootstrap.min.css.map', to: 'reactapp/prefixed-bootstrap.min.css.map'}
         ]) // destination is relative to dist directory
     ],
