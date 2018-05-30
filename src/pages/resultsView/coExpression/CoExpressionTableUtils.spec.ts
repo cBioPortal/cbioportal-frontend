28d82cf9d8702e1ed459419399e59d0c34023181
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-05-30 17:35:14 -0400

    Fix cytoband filtering
    1) Fix bug introduced by earlier commit where the cytoband filter
    no longer returns true for input "-"
    
    2) Make cytoband matching only happen from the beginning of the string,
    ie "12p" will be filtered out by "-12p" but not by "-2p"
    
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/resultsView/coExpression/CoExpressionTableUtils.spec.ts
+++ src/pages/resultsView/coExpression/CoExpressionTableUtils.spec.ts
@@ -8,13 +8,36 @@ describe("CoExpressionTableUtils", ()=>{
             assert.isFalse(cytobandFilter({ cytoband: null } as any as CoExpression, "asdfa"));
         });
         it("returns false if no match", ()=>{
-            assert.isFalse(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "hi"));
+            assert.isFalse(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "hi"))
         });
-        it("returns true if match", ()=>{
-            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "ell"));
+        it("returns false if match in middle", ()=>{
+            assert.isFalse(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "ell"));
+        });
+        it("returns true if match at beginning", ()=>{
+            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "h"));
+            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "he"));
+            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "hel"));
+            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "hell"));
+            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "hello"));
+        });
+        it("returns false if no match for complete query", ()=>{
+            assert.isFalse(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "hellothere"));
+        });
+        it("returns true on empty input", ()=>{
+            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, ""));
+        });
+        it("returns true if minus sign at beginning and nothing following", ()=>{
+            assert.isTrue(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "-"));
         });
         it("returns false if match but minus sign at beginning of filter string", ()=>{
-            assert.isFalse(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "-ell"));
+            assert.isFalse(cytobandFilter({ cytoband: "hello" } as any as CoExpression, "-he"));
+            assert.isFalse(cytobandFilter({ cytoband: "17p" } as any as CoExpression, "-17p"));
+        });
+        it("returns true if no match but minus sign at beginning", ()=>{
+            assert.isTrue(cytobandFilter({ cytoband: "17p" } as any as CoExpression, "-3p"));
+        });
+        it("returns true if match in middle but minus sign at beginning", ()=>{
+            assert.isTrue(cytobandFilter({ cytoband: "17p" } as any as CoExpression, "-7p"));
         });
     });
 });
