052e6e358c7f7c0483859a401a6a71c6f51e1c03
Karthik Kalletla <kalletlakarthik@gmail.com>
2018-06-06 10:22:07 -0400

    Add new functionality to study view refactoring
    Add delete chart functionality in study view
    
    Add Tooltip table to pie chart
    
    Fix pie chart interaction performance
    
    Add table chart to new study view
    
    Add switching chart type and cleanup
    
    Implement Add chart feature and more cleanup
    
    Update API

--- src/pages/studyView/charts/pieChart/PieChart.tsx
+++ src/pages/studyView/charts/pieChart/PieChart.tsx
@@ -1,30 +1,44 @@
 import * as React from "react";
-import styles from "./styles.scss";
 import { observer } from "mobx-react";
 import { VictoryPie, VictoryContainer, VictoryLabel, Slice } from 'victory';
-import { observable, computed } from "mobx";
+import { observable, computed, action, toJS } from "mobx";
 import _ from "lodash";
-import { COLORS, UNSELECTED_COLOR, NA_COLOR } from "pages/studyView/StudyViewUtils";
+import { UNSELECTED_COLOR } from "pages/studyView/StudyViewUtils";
 import CBIOPORTAL_VICTORY_THEME from "shared/theme/cBioPoralTheme";
 import { AbstractChart } from "pages/studyView/charts/ChartContainer";
-import { ClinicalDataCount } from "shared/api/generated/CBioPortalAPIInternal";
 import ifndef from "shared/lib/ifndef";
+import { bind } from "bind-decorator";
+import { ClinicalDataCountWithColor } from "pages/studyView/StudyViewPageStore";
+import classnames from 'classnames';
+import ClinicalTable from "pages/studyView/table/ClinicalTable";
+import {If} from 'react-if';
 
 export interface IPieChartProps {
-    data: ClinicalDataCount[];
+    data: ClinicalDataCountWithColor[];
     filters:string[];
     onUserSelection:(values:string[])=>void;
+    active:boolean,
+    placement:'left'|'right'
 }
 
 @observer
 export default class PieChart extends React.Component<IPieChartProps, {}> implements AbstractChart{
 
-    // used in saving slice color
-    private colorSet:{[id:string]:string} = {};
     private svgContainer: any;
 
     constructor(props: IPieChartProps) {
         super(props);
+    }
+
+    @bind
+    private onUserSelection(filter:string) {
+        let filters = toJS(this.props.filters);
+        if(_.includes(filters,filter)){
+            filters = _.filter(filters, obj=> obj !== filter);
+        }else{
+            filters.push(filter);
+        }
+        this.props.onUserSelection(filters);
     }
 
     private get userEvents(){
@@ -37,19 +51,30 @@ export default class PieChart extends React.Component<IPieChartProps, {}> implem
                         {
                             target: "data",
                             mutation: (props:any) => {
-                                let filters = self.props.filters;
-                                if(_.includes(filters,props.datum.x)){
-                                    filters = _.filter(filters, filter=> filter !== props.datum.x);
-                                }else{
-                                    filters.push(props.datum.x);
-                                }
-                                self.props.onUserSelection(filters);
+                                this.onUserSelection(props.datum.value);
                             }
                         }
                     ];
                 }
             }
         }];
+    }
+
+    @observable isTooltipHovered: boolean = false;
+    @observable tooltipHighlightedRow:string|undefined = undefined;
+
+    @bind
+    @action private highlightedRow(value:string): void {
+        this.tooltipHighlightedRow = value;
+    }
+
+    @bind
+    @action private inTooltip(flag:boolean): void {
+        this.isTooltipHovered = flag;
+    }
+
+    @computed private get showTooltip(){
+        return this.props.active || this.isTooltipHovered
     }
 
     public downloadData() {
@@ -75,21 +100,12 @@ export default class PieChart extends React.Component<IPieChartProps, {}> implem
             b. No  -> add appropriate slice style properties depending on the filters
     */
     @computed get annotatedData() {
-        return this.props.data.map(slice => {
-            let color = this.colorSet[slice.value];
-            if (_.isUndefined(color)) {
-                if (slice.value.toLowerCase().includes('na')) {
-                    color = NA_COLOR;
-                } else {
-                    color = COLORS[Object.keys(this.colorSet).length];
-                }
-                this.colorSet[slice.value] = color;
-            }
+        return _.map(this.props.data, slice => {
             if (_.isEmpty(this.props.filters)) {
-                return { ...slice, fill: color };
+                return { ...slice, fill: slice.color};
             } else {
                 if (_.includes(this.props.filters, slice.value)) {
-                    return { ...slice, fill: color, stroke: "#cccccc", strokeWidth: 3 };
+                    return { ...slice, fill: slice.color, stroke: "#cccccc", strokeWidth: 3 };
                 } else {
                     return { ...slice, fill: UNSELECTED_COLOR, fillOpacity: '0.5' };
                 }
@@ -97,38 +113,85 @@ export default class PieChart extends React.Component<IPieChartProps, {}> implem
         })
     }
 
+    @computed get pieChartData(){
+        let data = this.annotatedData
+        if(!_.isUndefined(this.tooltipHighlightedRow)){
+            data = _.map(data,(obj:any)=>{
+                if(_.isEqual(obj.value, this.tooltipHighlightedRow)){
+                    return _.assign({ fillOpacity: '0.5' }, obj)
+                }
+                return obj;
+            });
+        }
+        return data;
+    }
+
+    @bind
+    private tooltipMouseEnter(): void {
+        this.isTooltipHovered = true;
+    }
+
+    @bind
+    private tooltipMouseLeave(): void {
+        this.isTooltipHovered = false;
+    }
+
     public render() {
-        //to hide label if the angle is too small(currently set to 20 degrees)
+        let left = _.isEqual(this.props.placement,'right')? '195px' : '-350px'
         return (
-            <VictoryPie
-                containerComponent={<VictoryContainer
-                                        theme={CBIOPORTAL_VICTORY_THEME}
-                                        standalone={false}
-                                        containerRef={(ref: any) => this.svgContainer = ref}
-                                    />}
-                width={190}
-                height={185}
-                labelRadius={30}
-                padding={30}
-                labels={(d:any) => ((d.y*360)/this.totalCount)<20?'':d.y}
-                data={this.annotatedData}
-                dataComponent={<CustomSlice/>}
-                events={this.userEvents}
-                labelComponent={<VictoryLabel/>}
-                style={{
-                    data: { 
-                        fill: (d:any) => ifndef(d.fill, "0x000000"),
-                        stroke: (d:any) => ifndef(d.stroke, "0x000000"),
-                        strokeWidth: (d:any) => ifndef(d.strokeWidth, 0),
-                        fillOpacity: (d:any) => ifndef(d.fillOpacity, 1)
-                    },
-                    labels: { 
-                        fill: "white" 
-                    }
-                }}
-                x="value"
-                y="count"
-            />
+            <div>
+                <If condition={this.showTooltip}>
+                    <div
+                        className={ classnames('popover', this.props.placement) }
+                        onMouseLeave={() => this.tooltipMouseLeave()}
+                        onMouseEnter={() => this.tooltipMouseEnter()}
+                        style={{ display: 'block', position: 'absolute', left: left, width: '350px', maxWidth: '350px' }}>
+        
+                        <div className="arrow" style={{ top: 20 }}></div>
+                        <div className="popover-content">
+                            <ClinicalTable 
+                                data={this.props.data}
+                                filters={this.props.filters}
+                                highlightedRow={this.highlightedRow}
+                                onUserSelection={this.props.onUserSelection}
+                            />
+                        </div>
+                    </div>
+                </If>
+
+                
+                <VictoryPie
+                    theme={CBIOPORTAL_VICTORY_THEME}
+                    containerComponent={<VictoryContainer
+                                            responsive={false}
+                                            containerRef={(ref: any) => this.svgContainer = ref}
+                                        />}
+                    width={190}
+                    height={180}
+                    labelRadius={30}
+                    padding={30}
+                    //to hide label if the angle is too small(currently set to 20 degrees)
+                    labels={(d:any) => ((d.count*360)/this.totalCount)<20?'':d.count}
+                    data={this.pieChartData}
+                    dataComponent={<CustomSlice/>}
+                    labelComponent={<VictoryLabel/>}
+                    events={this.userEvents}
+                    style={{
+                        data: { 
+                            fill: (d:any) => ifndef(d.fill, "0x000000"),
+                            stroke: (d:any) => ifndef(d.stroke, "0x000000"),
+                            strokeWidth: (d:any) => ifndef(d.strokeWidth, 0),
+                            fillOpacity: (d:any) => ifndef(d.fillOpacity, 1)
+                        },
+                        labels: { 
+                            fill: "white" 
+                        }
+                    }}
+                    x="value"
+                    y="count"
+                    eventKey="value"
+                />
+            </div>
         );
     }
 
@@ -139,8 +202,8 @@ class CustomSlice extends React.Component<{}, {}> {
         const d:any = this.props;
         return (
         <g>
-            <Slice {...this.props}/>
-            <title>{`${d.datum.x}:${d.datum.y}`}</title>
+            <Slice {...this.props} active={true}/>
+            <title>{`${d.datum.value}:${d.datum.count}`}</title>
         </g>
         );
     }
