43f3dd34157ec6cd8c50ddc5c94860ec4098cb8b
Ersin Ciftci <arseniusthegreat@gmail.com>
2018-06-07 10:29:40 -0400

    Show sample counts next to profile name in Enrichments

--- src/pages/resultsView/enrichments/EnrichmentsDataSetDropdown.tsx
+++ src/pages/resultsView/enrichments/EnrichmentsDataSetDropdown.tsx
@@ -6,18 +6,19 @@ import styles from "./styles.module.scss";
 import { MolecularProfile } from 'shared/api/generated/CBioPortalAPI';
 import autobind from 'autobind-decorator';
 
-export interface IDataSetDropdownProps {
+export interface IEnrichmentsDataSetDropdownProps {
     dataSets: MolecularProfile[];
     onChange: (molecularProfile: MolecularProfile) => void;
     selectedValue: string;
+    molecularProfileIdToProfiledSampleCount:{[molecularProfileId:string]:number};
 }
 
 @observer
-export default class DataSetDropdown extends React.Component<IDataSetDropdownProps, {}> {
+export default class EnrichmentsDataSetDropdown extends React.Component<IEnrichmentsDataSetDropdownProps, {}> {
 
     @autobind
     private change(e: any) {
-        this.props.onChange(JSON.parse(e.target.value));
+        this.props.onChange(this.props.dataSets.find(d => d.molecularProfileId === e.target.value)!);
     }
 
     public render() {
@@ -29,7 +30,8 @@ export default class DataSetDropdown extends React.Component<IDataSetDropdownPro
 
         const options: JSX.Element[] = [];
         this.props.dataSets.forEach(dataSet => {
-            options.push(<option value={JSON.stringify(dataSet)}>{dataSet.name}</option>);
+            const sampleCount = this.props.molecularProfileIdToProfiledSampleCount[dataSet.molecularProfileId];
+            options.push(<option value={dataSet.molecularProfileId}>{dataSet.name} ({sampleCount} sample{sampleCount !== 1 ? "s" : ""})</option>);
         });
 
         return (
