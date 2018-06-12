08c8131a77fff2d3bf5dc15ceb5951ca210a807a
aaronlisman <lismanBusiness@gmail.com>
2018-06-12 11:46:51 -0400

    run unit tests in chrome. fix stubbing of network requests for tests.

--- src/global.spec.ts
+++ src/global.spec.ts
@@ -1,26 +1,36 @@
-import client from "./shared/api/cbioportalClientInstance";
-import Sinon from "sinon";
 import CBioPortalAPI from "./shared/api/generated/CBioPortalAPI";
+import request from 'superagent';
 
 let clientStubs:any[];
-before(()=>{
-    clientStubs = stubClient();
-});
 
-after(()=>{
-    for (const stub of clientStubs) {
-        stub.restore();
-    }
-});
+clientStubs = stubClient();
+
+request.get = (function(){
+    console.log(`Warning: calling stubbed method request.get, which returns a promise that never resolves.`);
+    return new Promise(()=>{});
+} as any);
+
+request.post = (function(){
+    console.log(`Warning: calling stubbed method request.post, which returns a promise that never resolves.`);
+    return new Promise(()=>{});
+} as any);
+
 
 function stubClient():any[] {
     // returns stubs
+
+    var target = CBioPortalAPI;
+
     const stubs:any[] = [];
-    const methodNames:(keyof CBioPortalAPI)[] = (Object.keys(client) as (keyof CBioPortalAPI)[]).filter(m=>(typeof (client as any)[m]) === "function");
-    for (const methodName of methodNames) {
-        stubs.push(Sinon.stub(client, methodName).callsFake(()=>new Promise((resolve, reject)=>{
+
+    var arrObj = Object.getOwnPropertyNames(target.prototype);
+    for( var funcKey in arrObj ) {
+        var methodName = arrObj[funcKey];
+
+        (CBioPortalAPI.prototype as any)[methodName] = function(){
             console.log(`Warning: calling stubbed method cbioportalClient.${methodName}, which returns a promise that never resolves.`);
-        })));
+        }
     }
+
     return stubs;
 }
