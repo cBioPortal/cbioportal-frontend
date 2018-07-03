2f7e0f59b946bc5d1fba30416790d13665cbe7e2
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-07-02 20:54:40 -0400

    StudyViewScatterPlot, applied to Mutation Count vs CNA
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/shared/components/plots/ScatterPlot.tsx
+++ src/shared/components/plots/ScatterPlot.tsx
@@ -205,7 +205,7 @@ export default class ScatterPlot<D extends IBaseScatterPlotData> extends React.C
     }
 
     @computed get plotDomain() {
-        // data extremes plus padding
+        // data extremes
         const max = {x:Number.NEGATIVE_INFINITY, y:Number.NEGATIVE_INFINITY};
         const min = {x:Number.POSITIVE_INFINITY, y:Number.POSITIVE_INFINITY};
         for (const d of this.props.data) {
