fd13bf492a8a5b72a9399ecd94b5ad602082cc8c
James Xu's Developer Account <devaccount@jamess-mbp.mskcc.org>
2018-07-26 19:17:04 -0400

    Update patient view header to show mutational signature confidences. Use hard-coded data

---
+++ src/pages/patientView/patientHeader/MutationalSignatureConfidence.spec.tsx
@@ -0,0 +1,12 @@
+import MutationalSignatureConfidence, {GDDOutput} from './MutationalSignatureConfidence';
+import React from 'react';
+import { assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+const mockData = require("json-loader!./mock/gdd/P-0001055-T01-IM3");
+
+describe('MutationalSignatureConfidence', () => {
+    it('', ()=>{
+
+    });
+});
