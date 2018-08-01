64b81ba99eb6e54340c3eaa85532dc944c3253c0
James Xu's Developer Account <devaccount@Jamess-MacBook-Pro.local>
2018-08-01 15:25:19 -0400

    Update data model for mutational signature header

--- src/pages/patientView/patientHeader/SignificantMutationalSignatures.spec.tsx
+++ src/pages/patientView/patientHeader/SignificantMutationalSignatures.spec.tsx
@@ -1,11 +1,11 @@
-import MutationalSignatureConfidence, {GDDOutput} from './MutationalSignatureConfidence';
+import SignificantMutationalSignatures, {GDDOutput} from './SignificantMutationalSignatures';
 import React from 'react';
 import { assert } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 const mockData = require("json-loader!./mock/gdd/P-0001055-T01-IM3");
 
-describe('MutationalSignatureConfidence', () => {
+describe('SignificantMutationalSignatures', () => {
     it('', ()=>{
 
     });
