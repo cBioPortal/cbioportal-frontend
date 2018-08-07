ec4c5bb8b46bac94dfd0ac2f8f746d32ac58a6b1
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-08-07 17:33:12 -0400

    Adjust look of Oql status banner
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 96c69380c6ddf7baf184316a712d6c50bbd619ca

---
+++ src/shared/components/OqlStatusBanner.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import {ResultsViewPageStore} from "../../pages/resultsView/ResultsViewPageStore";
+import {observer} from "mobx-react";
+
+
+const yesOqlColor = "#117000";
+const noOqlColor = "#a81319";
+
+@observer
+export default class OqlStatusBanner extends React.Component<{store:ResultsViewPageStore, style?:any, tabReflectsOql:boolean}, {}> {
+    render() {
+        if (this.props.store.queryContainsOql) {
+            let className:string;
+            let message:string;
+            let iconClassName:string;
+            let dataTest:string;
+
+            if (this.props.tabReflectsOql) {
+                //color = yesOqlColor;
+                className="alert alert-success";
+                message = "This tab reflects the OQL specification from your query.";
+                iconClassName = "fa fa-md fa-check";
+                dataTest="OqlStatusBannerYes";
+            } else {
+                //color = noOqlColor;
+                className="alert alert-info";
+                message = "This tab does not reflect the OQL specification from your query";
+                iconClassName = "fa fa-md fa-exclamation-triangle";
+                dataTest="OqlStatusBannerNo";
+            }
+            const style = {fontFamily:"arial", fontSize:"13px", fontWeight:"bold", marginBottom:4, padding:8};
+            return (
+                <div data-test={dataTest} className={className} style={Object.assign({}, style, this.props.style || {})}>
+                    <span style={{marginRight:4, verticalAlign:"middle"}}>
+                        <i className={iconClassName}/>
+                    </span>
+                    {message}
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+};
