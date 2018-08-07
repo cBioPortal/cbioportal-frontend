ef726ed5081b861522af527ebe7f0f01720c2dce
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-08-07 17:33:12 -0400

    Adjust look of Oql status banner
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 96c69380c6ddf7baf184316a712d6c50bbd619ca

--- end-to-end-tests/specs/results.spec.js
+++ end-to-end-tests/specs/results.spec.js
@@ -156,7 +156,7 @@ describe('Results Page', function() {
     });
 
     describe('no oql warning banner', function() {
-        const bannerSelector = 'span[data-test="NoOqlWarning"]';
+        const bannerSelector = 'span[data-test="OqlStatusBanner"]';
         const simpleQueryUrl = `${CBIOPORTAL_URL}/index.do?cancer_study_id=coadread_tcga_pub&Z_SCORE_THRESHOLD=2&RPPA_SCORE_THRESHOLD=2&data_priority=0&case_set_id=coadread_tcga_pub_nonhypermut&gene_list=KRAS%2520NRAS%2520BRAF&geneset_list=+&tab_index=tab_visualize&Action=Submit&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=coadread_tcga_pub_mutations&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=coadread_tcga_pub_gistic`;
         const explicitOqlQueryUrl = `${CBIOPORTAL_URL}/index.do?cancer_study_id=coadread_tcga_pub&Z_SCORE_THRESHOLD=2&RPPA_SCORE_THRESHOLD=2&data_priority=0&case_set_id=coadread_tcga_pub_nonhypermut&gene_list=KRAS%2520NRAS%2520%250ABRAF%253AMUT&geneset_list=+&tab_index=tab_visualize&Action=Submit&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=coadread_tcga_pub_mutations&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=coadread_tcga_pub_gistic`;
 
