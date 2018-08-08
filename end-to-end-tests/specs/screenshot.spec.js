50e51a7c339c4c99fb5dcda1c771406bdfd6a382
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-08-08 17:22:28 -0400

    Give more time to coexp tab screenshots
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 8fc6cfec7244953db9f4850d13b7e4a30340b4c0

--- end-to-end-tests/specs/screenshot.spec.js
+++ end-to-end-tests/specs/screenshot.spec.js
@@ -254,7 +254,7 @@ describe("coexpression tab screenshot tests", function() {
         goToUrlAndSetLocalStorage(url);
     });
     it('coexpression tab coadread_tcga_pub initial load', function() {
-        browser.waitForExist('div[data-test="CoExpressionPlot"]', 10000); // wait for plot to show up
+        browser.waitForExist('div[data-test="CoExpressionPlot"]', 20000); // wait for plot to show up
         var res = browser.checkElement('div[data-test="CoExpressionGeneTabContent"]');
         assertScreenShotMatch(res);
     });
@@ -271,20 +271,20 @@ describe("coexpression tab screenshot tests", function() {
     it('coexpression tab coadread_tcga_pub switch tabs', function() {
         browser.click('#coexpressionTabGeneTabs>ul>li:nth-child(2)>a');// click on NRAS
         browser.pause(100); // give time to start loading
-        browser.waitForExist('div[data-test="CoExpressionPlot"]', 10000); // wait for plot to show up
+        browser.waitForExist('div[data-test="CoExpressionPlot"]', 20000); // wait for plot to show up
         var res = browser.checkElement('div[data-test="CoExpressionGeneTabContent"]');
         assertScreenShotMatch(res);
     });
     it('coexpression tab coadread_tcga_pub switch profiles', function() {
         browser.execute(function() { resultsViewCoExpressionTab.onSelectDataSet({ value: "coadread_tcga_pub_mrna"}); });
         browser.pause(100); // give time to start loading
-        browser.waitForExist('div[data-test="CoExpressionPlot"]', 10000); // wait for plot to show up
+        browser.waitForExist('div[data-test="CoExpressionPlot"]', 20000); // wait for plot to show up
         var res = browser.checkElement('div[data-test="CoExpressionGeneTabContent"]');
         assertScreenShotMatch(res);
     });
     it('coexpression tab coadread_tcga_pub with a lot of genes', function() {
         goToUrlAndSetLocalStorage(`${CBIOPORTAL_URL}/index.do?cancer_study_id=coadread_tcga_pub&Z_SCORE_THRESHOLD=2&RPPA_SCORE_THRESHOLD=2&data_priority=0&case_set_id=coadread_tcga_pub_nonhypermut&gene_list=AKR1C3%2520AR%2520CYB5A%2520CYP11A1%2520CYP11B1%2520CYP11B2%2520CYP17A1%2520CYP19A1%2520CYP21A2%2520HSD17B1%2520HSD17B10%2520HSD17B11%2520HSD17B12%2520HSD17B13%2520HSD17B14%2520HSD17B2%2520HSD17B3%2520HSD17B4%2520HSD17B6%2520HSD17B7%2520HSD17B8%2520HSD3B1%2520HSD3B2%2520HSD3B7%2520RDH5%2520SHBG%2520SRD5A1%2520SRD5A2%2520SRD5A3%2520STAR&geneset_list=+&tab_index=tab_visualize&Action=Submit&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=coadread_tcga_pub_mutations&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=coadread_tcga_pub_gistic#coexp`);
-        browser.waitForExist('div[data-test="CoExpressionPlot"]', 10000); // wait for plot to show up
+        browser.waitForExist('div[data-test="CoExpressionPlot"]', 20000); // wait for plot to show up
         var res = browser.checkElement('div[data-test="CoExpressionGeneTabContent"]');
         assertScreenShotMatch(res);
     });
