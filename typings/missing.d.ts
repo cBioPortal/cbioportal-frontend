2554c890f47b8347ab23b3d1319c72d312eac87a
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-08-14 17:21:25 -0400

    Switch svg saving libraries to allow saving png with background
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: c1559c183ee83b046ae32d880568f9626140a7d5

--- typings/missing.d.ts
+++ typings/missing.d.ts
@@ -3,6 +3,7 @@ declare module '*.scss';
 declare module '*.json';
 
 // these packages are missing typings
+declare module 'save-svg-as-png'
 declare module 'react-file-download';
 declare module 'react-select-checked';
 declare module 'react-zeroclipboard';
