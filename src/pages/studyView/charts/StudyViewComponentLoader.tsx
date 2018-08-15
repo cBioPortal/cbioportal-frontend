341c60e4377e3655afbde1887d9a9185643e986d
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-08-15 17:44:00 -0400

    Use flex as aligning style

--- src/pages/studyView/charts/StudyViewComponentLoader.tsx
+++ src/pages/studyView/charts/StudyViewComponentLoader.tsx
@@ -3,6 +3,7 @@ import {If} from 'react-if';
 import MobxPromise from "mobxpromise";
 import LoadingIndicator from "shared/components/loadingIndicator/LoadingIndicator"
 import styles from "../styles.module.scss";
+import classNames from 'classnames';
 
 export interface StudyViewComponentLoaderProps {
     promise: MobxPromise<any>
@@ -11,11 +12,10 @@ export interface StudyViewComponentLoaderProps {
 export class StudyViewComponentLoader extends React.Component<StudyViewComponentLoaderProps> {
     public render() {
         return (
-            <div className={this.props.promise.isPending ? styles.studyViewAutoMargin : null}>
+            <div className={classNames(this.props.promise.isPending ? styles.studyViewAutoMargin : null, styles.studyViewLoadingIndicator)}>
                 <If condition={this.props.promise.isPending}>
                     <LoadingIndicator
                         isLoading={!!this.props.promise.isPending}
-                        style={{position: "absolute", top: "50%", left: "50%", marginLeft: -10}}
                     />
                 </If>
                 <If condition={this.props.promise.isError}>
