9e2817f70393713b8f3430e267483500a192b2ae
aaronlisman <lismanBusiness@gmail.com>
2018-08-22 15:03:59 -0400

    MSK Tabs should switch tabs BEFORE rendering tab children, to prevent locking.  And update loading animations

--- src/shared/components/loadingIndicator/LoadingIndicator.tsx
+++ src/shared/components/loadingIndicator/LoadingIndicator.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {ThreeBounce} from 'better-react-spinkit';
 import { If, Else, Then } from 'react-if';
+import Spinner from "react-spinkit";
 
 export interface ILoader {
     isLoading:Boolean;
@@ -15,7 +16,7 @@ export default class Loader extends React.Component<ILoader, {}> {
             <If condition={this.props.isLoading}>
                 <Then>
                     <div>
-                        <ThreeBounce style={this.props.style || { display:'inline-block', marginLeft:10 }} />
+                        <Spinner fadeIn="none" className={"spinnerColor"} style={this.props.style || { display:'inline-block', marginLeft:10 }} name="line-scale-pulse-out" color="steelblue"/>
                         {
                             this.props.children
                         }
