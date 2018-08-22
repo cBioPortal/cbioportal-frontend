787dfac175f740fea25d6c75f4471237ce4ad4e1
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-08-22 19:09:42 -0400

    Use react-virtualized to populate Mutated/CNA Genes Tables and clincial table
    This relates to https://github.com/cBioPortal/cbioportal/issues/4158
    
    
    Former-commit-id: 10563a54847ea04c8fcb1d5e79f183219e6e108d

--- src/pages/studyView/table/CNAGenesTable.tsx
+++ src/pages/studyView/table/CNAGenesTable.tsx
@@ -1,36 +1,37 @@
 import * as React from "react";
 import * as _ from "lodash";
 import {CNAGenesData} from "pages/studyView/StudyViewPageStore";
-import {default as LazyMobXTable} from "shared/components/lazyMobXTable/LazyMobXTable";
 import {action} from "mobx";
 import {observer} from "mobx-react";
 import styles from "./tables.module.scss";
 import {CopyNumberCountByGene, CopyNumberGeneFilterElement} from "shared/api/generated/CBioPortalAPIInternal";
-import LabeledCheckbox from "../../../shared/components/labeledCheckbox/LabeledCheckbox";
 import MobxPromise from "mobxpromise";
 import {If} from 'react-if';
 import classnames from 'classnames';
 import DefaultTooltip from "shared/components/defaultTooltip/DefaultTooltip";
+import LabeledCheckbox from "shared/components/labeledCheckbox/LabeledCheckbox";
+import FixedHeaderTable from "./FixedHeaderTable";
 
 export interface ICNAGenesTablePros {
     promise: MobxPromise<CNAGenesData>;
     filters: CopyNumberGeneFilterElement[];
     onUserSelection: (entrezGeneId: number, alteration: number) => void;
     numOfSelectedSamples: number;
-    onGeneSelect:(hugoGeneSymbol:string)=>void;
+    onGeneSelect: (hugoGeneSymbol: string) => void;
     selectedGenes: string[]
 }
 
-class CNAGenesTableComponent extends LazyMobXTable<CopyNumberCountByGene> {
+class CNAGenesTableComponent extends FixedHeaderTable<CopyNumberCountByGene> {
 }
 
 @observer
 export class CNAGenesTable extends React.Component<ICNAGenesTablePros, {}> {
-    private _tableColumns = [
+    private columns = [
         {
             name: 'Gene',
             render: (data: CopyNumberCountByGene) => {
-                const overlay = () => <span>{`Click ${data.hugoGeneSymbol} to ${_.includes(this.props.selectedGenes, data.hugoGeneSymbol) ? 'remove' : 'add'} from your query`}</span>;
+                const overlay = () =>
+                    <span>{`Click ${data.hugoGeneSymbol} to ${_.includes(this.props.selectedGenes, data.hugoGeneSymbol) ? 'remove' : 'add'} from your query`}</span>;
                 return (
                     <DefaultTooltip
                         placement="left"
@@ -38,25 +39,40 @@ export class CNAGenesTable extends React.Component<ICNAGenesTablePros, {}> {
                         destroyTooltipOnHide={true}
                     >
                                                     <span
-                                                        className={classnames(styles.geneSymbol, _.includes(this.props.selectedGenes, data.hugoGeneSymbol) ? styles.selectd : undefined)}
+                                                        className={classnames(styles.geneSymbol, _.includes(this.props.selectedGenes, data.hugoGeneSymbol) ? styles.selected : undefined)}
                                                         onClick={() => this.props.onGeneSelect(data.hugoGeneSymbol)}>
                                                         {data.hugoGeneSymbol}
                                                     </span>
                     </DefaultTooltip>
                 )
             },
-            width: "40%"
+            sortBy: (data: CopyNumberCountByGene) => data.hugoGeneSymbol,
+            defaultSortDirection: 'asc' as 'asc',
+            filter: (data: CopyNumberCountByGene, filterString: string, filterStringUpper: string) => {
+                return data.hugoGeneSymbol.indexOf(filterStringUpper) > -1;
+            },
+            width: 85
         },
         {
             name: 'Cytoband',
             render: (data: CopyNumberCountByGene) => <span>{data.cytoband}</span>,
-            width: "20%"
+            sortBy: (data: CopyNumberCountByGene) => data.cytoband,
+            defaultSortDirection: 'asc' as 'asc',
+            filter: (data: CopyNumberCountByGene, filterString: string, filterStringUpper: string) => {
+                return data.cytoband.indexOf(filterStringUpper) > -1;
+            },
+            width: 100
         },
         {
             name: 'CNA',
             render: (data: CopyNumberCountByGene) =>
                 <span>{data.alteration === -2 ? 'DEL' : 'AMP'}</span>,
-            width: "20%"
+            sortBy: (data: CopyNumberCountByGene) => data.alteration,
+            defaultSortDirection: 'asc' as 'asc',
+            filter: (data: CopyNumberCountByGene, filterString: string, filterStringUpper: string) => {
+                return (data.alteration === -2 ? 'DEL' : 'AMP').indexOf(filterStringUpper) > -1;
+            },
+            width: 65
         },
         {
             name: '#',
@@ -67,12 +83,22 @@ export class CNAGenesTable extends React.Component<ICNAGenesTablePros, {}> {
                 >
                     {data.countByEntity}
                 </LabeledCheckbox>,
-            width: "20%"
+            sortBy: (data: CopyNumberCountByGene) => data.countByEntity,
+            defaultSortDirection: 'desc' as 'desc',
+            filter: (data: CopyNumberCountByGene, filterString: string, filterStringUpper: string) => {
+                return _.toString(data.countByEntity).indexOf(filterStringUpper) > -1;
+            },
+            width: 75
         },
         {
             name: 'Freq',
             render: (data: CopyNumberCountByGene) => <span>{data.frequency + '%'}</span>,
-            width: "20%"
+            sortBy: (data: CopyNumberCountByGene) => data.frequency,
+            defaultSortDirection: 'desc' as 'desc',
+            filter: (data: CopyNumberCountByGene, filterString: string, filterStringUpper: string) => {
+                return _.toString(data.frequency).indexOf(filterStringUpper) > -1;
+            },
+            width: 75
         }
     ];
 
@@ -91,16 +117,10 @@ export class CNAGenesTable extends React.Component<ICNAGenesTablePros, {}> {
 
     public render() {
         return (
-            <div className={styles.studyViewTablesTable}>
-                <CNAGenesTableComponent
-                    className={styles.studyViewTablesBody}
-                    initialItemsPerPage={10}
-                    showCopyDownload={false}
-                    showColumnVisibility={false}
-                    data={this.props.promise.result}
-                    columns= {this._tableColumns}
-                />
-            </div>
+            <CNAGenesTableComponent
+                data={this.props.promise.result || []}
+                columns={this.columns}
+            />
         );
     }
 }
