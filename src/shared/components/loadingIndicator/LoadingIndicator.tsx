7c0df7405eb1f5b64aa4e1e9b6b8dd791797adea
aaronlisman <lismanBusiness@gmail.com>
2018-08-23 11:35:38 -0400

    Add new global loading indicator mode
    Former-commit-id: 49a8c8548151ed7d8d172efb833f7c1cbb81440b

--- src/shared/components/loadingIndicator/LoadingIndicator.tsx
+++ src/shared/components/loadingIndicator/LoadingIndicator.tsx
@@ -2,26 +2,46 @@ import * as React from 'react';
 import {ThreeBounce} from 'better-react-spinkit';
 import { If, Else, Then } from 'react-if';
 import Spinner from "react-spinkit";
+import Portal from 'react-portal';
 
 export interface ILoader {
-    isLoading:Boolean;
+    isLoading:boolean;
     style?:any;
+    isGlobal?:boolean;
 }
 
-export default class Loader extends React.Component<ILoader, {}> {
+export default class LoadingIndicator extends React.Component<ILoader, {}> {
 
     public render() {
-        return (
-            <If condition={this.props.isLoading}>
-                <Then>
-                    <div>
-                        <Spinner fadeIn="none" className={"spinnerColor"} style={this.props.style || { display:'inline-block', marginLeft:10 }} name="line-scale-pulse-out" color="steelblue"/>
-                        {
-                            this.props.children
-                        }
-                    </div>
-                </Then>
-            </If>
-        );
+
+        if (this.props.isGlobal) {
+            return <GlobalLoader {...this.props} />
+        } else {
+            return (
+                <If condition={this.props.isLoading}>
+                    <Then>
+                        <div>
+                            <Spinner fadeIn="none" className={"spinnerColor"}
+                                     style={this.props.style || {display: 'inline-block', marginLeft: 10}}
+                                     name="line-scale-pulse-out" color="steelblue"/>
+                            {
+                                this.props.children
+                            }
+                        </div>
+                    </Then>
+                </If>
+            );
+        }
     }
+}
+
+
+export class GlobalLoader extends React.Component<ILoader,{}> {
+
+    public render(){
+        return <Portal isOpened={this.props.isLoading}>
+            <Spinner className={"globalSpinner"} fadeIn="quarter" name="line-scale-pulse-out" color="steelblue"/>
+        </Portal>
+    }
+
 }
