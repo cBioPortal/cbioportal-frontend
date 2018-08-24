40a325bd515e96361bf2a96aa5d2f42135271eaf
Karthik Kalletla <kalletlakarthik@gmail.com>
2018-08-24 11:00:26 -0400

    Update chart loading

--- src/pages/studyView/charts/StudyViewComponentLoader.tsx
+++ src/pages/studyView/charts/StudyViewComponentLoader.tsx
@@ -1,29 +1,26 @@
 import * as React from "react";
-import {If} from 'react-if';
 import MobxPromise from "mobxpromise";
-import LoadingIndicator from "shared/components/loadingIndicator/LoadingIndicator"
 import styles from "../styles.module.scss";
-import classNames from 'classnames';
+import { ThreeBounce } from 'better-react-spinkit';
 
 export interface StudyViewComponentLoaderProps {
     promise: MobxPromise<any>
+    isInitialLoaded:boolean
 }
 
 export class StudyViewComponentLoader extends React.Component<StudyViewComponentLoaderProps> {
     public render() {
         return (
-            <div className={classNames(this.props.promise.isPending ? styles.studyViewAutoMargin : null, styles.studyViewLoadingIndicator)}>
-                <If condition={this.props.promise.isPending}>
-                    <LoadingIndicator
-                        isLoading={!!this.props.promise.isPending}
-                    />
-                </If>
-                <If condition={this.props.promise.isError}>
-                    <div>Error when loading data.</div>
-                </If>
-                <If condition={this.props.promise.isComplete}>
+            <div style={{ position: "relative", height: "100%"}}>
+                {this.props.isInitialLoaded && <div>
                     {this.props.children}
-                </If>
+                </div>
+                }
+                {this.props.promise.isPending &&
+                    <div className={styles.studyViewLoadingIndicator}>
+                        <ThreeBounce style={{ display: 'inline-block', marginLeft: 10 }} />
+                    </div>
+                }
             </div>
         );
     }
