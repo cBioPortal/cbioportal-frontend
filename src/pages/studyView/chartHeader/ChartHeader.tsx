7e3a592415a760e7ad948ba6b6da17570666b170
Onur Sumer <s.onur.sumer@gmail.com>
2018-08-28 16:33:57 -0400

    added download controls for study view charts
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>
    
    Former-commit-id: e25ee0ed73a610098cbd16783e3c61a89ff7a911

--- src/pages/studyView/chartHeader/ChartHeader.tsx
+++ src/pages/studyView/chartHeader/ChartHeader.tsx
@@ -2,6 +2,12 @@ import * as React from "react";
 import "./styles.scss";
 import {If} from 'react-if';
 import {ChartMeta, ChartType} from "pages/studyView/StudyViewPageStore";
+import DefaultTooltip from "shared/components/defaultTooltip/DefaultTooltip";
+import bind from "bind-decorator";
+import fileDownload from 'react-file-download';
+import {action, computed, observable} from "mobx";
+import {observer} from "mobx-react";
+import {IChartContainerDownloadProps} from "../charts/ChartContainer";
 
 export interface IChartHeaderProps {
     chartMeta: ChartMeta;
@@ -11,6 +17,7 @@ export interface IChartHeaderProps {
     hideLabel?       : boolean;
     chartControls?   : ChartControls;
     changeChartType  : (chartType: ChartType) => void;
+    download?        : IChartContainerDownloadProps[];
     setAnalysisGroups  : () => void;
 }
 
@@ -21,9 +28,75 @@ export interface ChartControls {
     showAnalysisGroupsIcon?   : boolean;
 }
 
+@observer
 export class ChartHeader extends React.Component<IChartHeaderProps, {}> {
     constructor(props: IChartHeaderProps) {
         super(props);
+    }
+
+    @observable downloadMenuActive = false;
+    @observable downloadPending = {
+        TSV: false,
+        SVG: false,
+        PDF: false
+    };
+
+    @computed
+    get fileName() {
+        return this.props.chartMeta.displayName.replace(/[ \t]/g, '_');
+    }
+
+    private downloadControls() {
+        const overlay = (
+            <div className="studyViewChartHeaderDownloadControls">
+                {
+                    this.props.download && this.props.download.map( props =>
+                        <button
+                            key={props.type}
+                            className="btn btn-xs"
+                            onClick={() => {
+                                this.downloadPending[props.type] = true;
+                                props.initDownload!().then(data => {
+                                    if (data && data.length > 0) {
+                                        fileDownload(data, `${this.fileName}.${props.type.substring(0, 3).toLowerCase()}`);
+                                    }
+                                    this.downloadPending[props.type] = false;
+                                }).catch(() => {
+                                    // TODO this.triggerDownloadError();
+                                    this.downloadPending[props.type] = false;
+                                });
+                            }}
+                        >
+                            {this.downloadPending[props.type] ? <i className="fa fa-spinner fa-spin" aria-hidden="true" /> : props.type}
+                        </button>
+                    )
+                }
+            </div>
+        );
+
+        return (
+            <DefaultTooltip
+                placement="bottom"
+                overlay={overlay}
+                destroyTooltipOnHide={true}
+                onVisibleChange={this.onTooltipVisibleChange as any}
+                trigger={["click"]}
+            >
+                <button className="btn btn-xs">
+                    <i className="fa fa-download" aria-hidden="true" title="Download" />
+                </button>
+            </DefaultTooltip>
+        );
+    }
+
+    @bind
+    @action
+    onTooltipVisibleChange(visible: boolean) {
+        this.downloadMenuActive = visible;
+    }
+
+    @computed get active() {
+        return this.downloadMenuActive || this.props.active;
     }
 
     public render() {
@@ -33,7 +106,7 @@ export class ChartHeader extends React.Component<IChartHeaderProps, {}> {
                 { !this.props.hideLabel && <span>{this.props.chartMeta.displayName}</span>}
                 </div>
                 <div className='controls'>
-                    <If condition={this.props.active}>
+                    <If condition={this.active}>
                         <div role="group" className="btn-group">
                             <If condition={this.props.chartControls && this.props.chartControls.showResetIcon}>
                                 <button className="btn btn-xs" onClick={() => this.props.resetChart()}>
@@ -61,6 +134,9 @@ export class ChartHeader extends React.Component<IChartHeaderProps, {}> {
                                     <img src="images/survival_icon.svg" style={{verticalAlign:"initial"}} width="10" height="10" className="icon hover" alt="Survival Analysis"/>
                                 </button>
                             </If>
+                            <If condition={this.props.download && this.props.download.length > 0}>
+                                {this.downloadControls()}
+                            </If>
                             <button className="btn btn-xs"  onClick={() => this.props.deleteChart()}>
                                 <i className="fa fa-times" aria-hidden="true" title="Delete chart"></i>
                             </button>
@@ -68,7 +144,7 @@ export class ChartHeader extends React.Component<IChartHeaderProps, {}> {
                     </If>
                 </div>
             </div>
-        )
+        );
     }
 
 }
