f60dcf04fc486075aec968e5b86e11b32424bef1
aaronlisman <lismanBusiness@gmail.com>
2018-08-28 15:20:50 -0400

    g2s client needs domain set

--- src/shared/api/cbioportalClientInstance.ts
+++ src/shared/api/cbioportalClientInstance.ts
@@ -4,32 +4,3 @@ import {getCbioPortalApiUrl} from "./urls";
 const client = new CBioPortalAPI();
 
 export default client;
-
-// function proxyCall(client, methodName) {
-//
-//     let proxied = client[methodName];
-//
-//
-//
-//     client[methodName] = function(){
-//
-//         let ret;
-//
-//         let cachedData = localStorage.getItem(methodName);
-//         if (cachedData) {
-//             ret = Promise.resolve(JSON.parse(cachedData));
-//         }
-//
-//         // let calledPromise = proxied.apply(this,arguments).then((data)=>{
-//         //     localStorage.setItem(methodName, JSON.stringify(data))
-//         //     return data;
-//         // });
-//
-//         console.log(ret);
-//
-//         return ret || calledPromise;
-//     }
-//
-// }
-//
-// proxyCall(client,'getAllStudiesUsingGET');
