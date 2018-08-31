6f025083933dd5ef8756660307fca9f60e47db93
aaronlisman <lismanBusiness@gmail.com>
2018-08-31 16:52:39 -0400

    Fix testimonials link in right bar

--- src/shared/components/testimonials/Testimonials.tsx
+++ src/shared/components/testimonials/Testimonials.tsx
@@ -107,7 +107,7 @@ export default class Testimonials extends React.Component <{}, {}> {
                         <p>"{activeTestimonial.quote}"</p>
                         <cite>--{activeTestimonial.cite}</cite>
                         <div className="testimonial-links">
-                            <Link to={'testimonials'}>View All</Link>
+                            <Link to={'/s/testimonials'}>View All</Link>
                             <a href="mailto:cbioportal@cbio.mskcc.org?subject=cBioPortal Testimonial&body=Job Title:%0D%0APlace of Employment:%0D%0ATestimonial:%0D%0A%0D%0AThank you for taking the time to submit your opinion.">
                                 Tell Us What You Think
                             </a>
