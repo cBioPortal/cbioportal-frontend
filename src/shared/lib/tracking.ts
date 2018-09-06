f30f2363313d2d51bd0445b2abceb619c565df5f
aaronlisman <lismanBusiness@gmail.com>
2018-09-06 17:51:51 -0400

    Add google analytics and associates plugins for spa and activate depending on configuration
    Former-commit-id: 83d636671cf5eb2e398ef6d2782b02c5375286a2

--- src/shared/lib/tracking.ts
+++ src/shared/lib/tracking.ts
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import * as _ from 'lodash';
 import mixpanel from 'mixpanel-browser';
 import AppConfig from "appConfig";
+import getBrowserWindow from "./getBrowserWindow";
 
 interface trackingCodes {
   ga?:string;
@@ -156,4 +157,22 @@ function activateAnalytics(){
 
 }
 
-activateAnalytics();
+//activateAnalytics();
+
+export function embedGoogleAnalytics(){
+
+    $(document).ready(function() {
+        $('<script async src="https://www.google-analytics.com/analytics.js"></script>').appendTo("body");
+        $('<script async src="https://cdnjs.cloudflare.com/ajax/libs/autotrack/2.4.1/autotrack.js"></script>').appendTo("body");
+        getBrowserWindow().ga=getBrowserWindow().ga||function(){(ga.q=ga.q||[]).push(arguments)};
+        const ga:any = getBrowserWindow().ga;
+        ga.l=+new Date;
+        ga('create', 'UA-85438068-3', 'auto');
+        ga('require', 'urlChangeTracker');
+        ga('require', 'cleanUrlTracker', {
+            stripQuery:true,
+            trailingSlash:'remove'
+        });
+        ga('send', 'pageview');
+    });
+}
