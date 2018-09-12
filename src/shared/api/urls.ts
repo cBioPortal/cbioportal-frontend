a39026cb7da6191a226cda2d66ee647ce6c4c4e6
aaronlisman <lismanBusiness@gmail.com>
2018-09-12 16:47:27 -0400

    Add authorization buttons/login state and configuration properties to control
    Former-commit-id: f86ae0e53941d26fd64365e507f49ad739f5a3d2

--- src/shared/api/urls.ts
+++ src/shared/api/urls.ts
@@ -178,3 +178,7 @@ export function getDocsUrl(sourceUrl:string,docsBaseUrl?:string): string {
         return docsBaseUrl + "/" + sourceUrl;
     }
 }
+
+export function getLogoutURL(){
+    return buildCBioPortalPageUrl(AppConfig.authLogoutUrl!)
+}
