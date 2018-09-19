972640c53f44c49115a2996157c8aa3eb3ca1d13
aaronlisman <lismanBusiness@gmail.com>
2018-09-19 09:24:07 -0400

    Add pipe seperator class to handle footer issue with corrupted nbps chars
    Former-commit-id: 9215090e3930b43a8b8bf7c2dbe78e4aa1fd097a

--- src/appShell/App/PortalFooter.tsx
+++ src/appShell/App/PortalFooter.tsx
@@ -6,11 +6,15 @@ export default class PortalFooter extends React.Component<{}, {}> {
         return <div id="footer">
             <a href="http://cbioportal.org">cBioPortal</a>
             <span id="footer-span-version"> Version 1.14.0-SNAPSHOT</span>
-            &nbsp;|&nbsp;<a href="http://www.mskcc.org/mskcc/html/44.cfm">MSKCC</a>&nbsp;|&nbsp;<a
-            href="http://cancergenome.nih.gov/">TCGA</a>
+            <span className="pipeSeperator">|</span><a href="http://www.mskcc.org/mskcc/html/44.cfm">MSKCC</a><span className="pipeSeperator">|</span>
+            <a href="http://cancergenome.nih.gov/">TCGA</a>
             <br/>
-            Questions and feedback: <span className="mailme" title="Contact us">cbioportal at googlegroups dot com</span>&nbsp;|&nbsp;<a target="_blank" href="http://groups.google.com/group/cbioportal">User discussion group</a>
-            &nbsp;|&nbsp;<a target="_blank" href="https://github.com/cBioPortal/"><i className="fa fa-github" aria-hidden="true"></i> GitHub</a>
+            Questions and feedback: <span className="mailme" title="Contact us">cbioportal at googlegroups dot com</span>
+            <span className="pipeSeperator">|</span>
+            <a target="_blank" href="http://groups.google.com/group/cbioportal">User discussion group</a>
+            <span className="pipeSeperator">|</span>
+            <a target="_blank" href="https://github.com/cBioPortal/">
+            <i className="fa fa-github" aria-hidden="true"></i> GitHub</a>
         </div>
     }
 
