c29dc1bc6672ad2ebeb5a2721e77532c8aed82bf
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-09-24 17:09:32 -0400

    SPA - fix study view links from Data Sets page
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/staticPages/datasetView/DatasetList.tsx
+++ src/pages/staticPages/datasetView/DatasetList.tsx
@@ -4,7 +4,7 @@ import {CancerStudy} from 'shared/api/generated/CBioPortalAPI';
 import {ThreeBounce} from 'better-react-spinkit';
 import request from 'superagent';
 import LazyMobXTable from "shared/components/lazyMobXTable/LazyMobXTable";
-import {getStudyDownloadListUrl} from "../../../shared/api/urls";
+import {getStudyDownloadListUrl, getStudySummaryUrl} from "../../../shared/api/urls";
 
 
 interface IDataTableRow {
@@ -53,7 +53,7 @@ class CancerStudyCell extends React.Component<ICancerStudyCellProps,{}> {
         return (
             <span>
                 <a
-                    href={`./study?id=${this.props.studyId}#summary`}
+                    href={getStudySummaryUrl(this.props.studyId)}
                     target='_blank'
                 >
                     {this.props.name}
