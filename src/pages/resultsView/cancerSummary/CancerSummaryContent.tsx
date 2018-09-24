b2183183ea1025bf4eca08b1a84d3b8aef3e7a28
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-09-24 18:05:22 -0400

    SPA cancer types summary - make controls more compact
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 6b51e8a0746fffa808e1477d3eefc793e44c4417

--- src/pages/resultsView/cancerSummary/CancerSummaryContent.tsx
+++ src/pages/resultsView/cancerSummary/CancerSummaryContent.tsx
@@ -358,80 +358,84 @@ export class CancerSummaryContent extends React.Component<ICancerSummaryContentP
 
         return (
 
-            <div style={{display: 'flex'}} className="cancer-summary--form-controls">
+            <div className="cancer-summary--form-controls">
 
-                <div>
-                    <FormGroup>
-                        <ControlLabel>Y-Axis Value:</ControlLabel>
-                        <FormControl componentClass="select" data-test="cancerSummaryYAxisSelect" onChange={this.handleYAxisChange}
-                                     ref={(el: any) => this.inputYAxisEl = el}>
-                            <option value="alt-freq">Alteration Frequency</option>
-                            <option value="abs-count">Absolute Counts</option>
-                        </FormControl>
-                    </FormGroup>
-                    <FormGroup>
-                        <ControlLabel>Sort X-Axis By:</ControlLabel>
-                        <FormControl componentClass="select" data-test="cancerSummaryXAxisSelect" onChange={this.handleXAxisChange}
-                                     ref={(el: any) => this.inputXAxisEl = el}>
-                            <option value="y-axis">Y-Axis Values</option>
-                            <option value="x-axis">Alphabetically</option>
-                        </FormControl>
-                    </FormGroup>
-                </div>
-
-                <div style={{width: 400}}>
-                    <div className="slider-holder">
-                        <FormGroup>
+                <table>
+                    <tr>
+                        <td>
+                            <ControlLabel>Y-Axis Value:</ControlLabel>
+                        </td>
+                        <td className="dashed-border-right">
+                            <FormControl componentClass="select" data-test="cancerSummaryYAxisSelect" onChange={this.handleYAxisChange}
+                                         ref={(el: any) => this.inputYAxisEl = el}>
+                                <option value="alt-freq">Alteration Frequency</option>
+                                <option value="abs-count">Absolute Counts</option>
+                            </FormControl>
+                        </td>
+                        <td>
                             <ControlLabel>Min. # Total Cases:</ControlLabel>
-                            <div className='slider custom-labels'>
-                                <Slider
-                                    min={0}
-                                    max={this.totalCasesMax}
-                                    value={this.tempTotalCasesValue}
-                                    labels={{0: 0, [this.totalCasesMax]: this.totalCasesMax}}
-                                    onChange={this.handleTotalSliderChange}
-                                    onChangeComplete={this.handleTotalSliderChangeComplete}
-                                />
+                        </td>
+                        <td>
+                            <div className="slider-holder">
+                                <div className='slider custom-labels'>
+                                    <Slider
+                                        min={0}
+                                        max={this.totalCasesMax}
+                                        value={this.tempTotalCasesValue}
+                                        labels={{0: 0, [this.totalCasesMax]: this.totalCasesMax}}
+                                        onChange={this.handleTotalSliderChange}
+                                        onChangeComplete={this.handleTotalSliderChangeComplete}
+                                    />
+                                </div>
                             </div>
-                        </FormGroup>
-                        <FormGroup>
-                            <ControlLabel className="invisible">Hidden</ControlLabel>
+                        </td>
+                        <td className="dashed-border-right slider-input">
                             <FormControl type="text" value={this.tempTotalCasesInputValue}
                                          data-test="sampleTotalThresholdInput"
                                          onChange={this.handleTotalInputChange}
                                          onKeyPress={this.handleTotalInputKeyPress}/>
-                        </FormGroup>
-                    </div>
-                    <div className="slider-holder">
-                        <FormGroup>
+                        </td>
+                        <td>
+                            <Checkbox checked={!this.hideGenomicAlterations} onChange={this.handleGenomicCheckboxChange}>
+                                Show Genomic Alteration Types
+                            </Checkbox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><ControlLabel>Sort X-Axis By:</ControlLabel></td>
+                        <td className="dashed-border-right">
+                            <FormControl componentClass="select" data-test="cancerSummaryXAxisSelect" onChange={this.handleXAxisChange}
+                                         ref={(el: any) => this.inputXAxisEl = el}>
+                                <option value="y-axis">Y-Axis Values</option>
+                                <option value="x-axis">Alphabetically</option>
+                            </FormControl>
+                        </td>
+                        <td>
                             <ControlLabel>{`Min. ${this.yAxis === 'alt-freq' ? '%' : '#'} Altered Cases:`}</ControlLabel>
-                            <div className='slider custom-labels'>
-                                <Slider
-                                    min={0}
-                                    max={this.altCasesMax}
-                                    value={this.tempAltCasesValue}
-                                    labels={{0: 0 + symbol, [this.altCasesMax]: Math.ceil(this.altCasesMax) + symbol}}
-                                    format={(val: string) => val + symbol}
-                                    onChange={this.handleAltSliderChange}
-                                    onChangeComplete={this.handleAltSliderChangeComplete}
-                                />
+                        </td>
+                        <td>
+                            <div className="slider-holder">
+                                <div className='slider custom-labels'>
+                                    <Slider
+                                        min={0}
+                                        max={this.altCasesMax}
+                                        value={this.tempAltCasesValue}
+                                        labels={{0: 0 + symbol, [this.altCasesMax]: Math.ceil(this.altCasesMax) + symbol}}
+                                        format={(val: string) => val + symbol}
+                                        onChange={this.handleAltSliderChange}
+                                        onChangeComplete={this.handleAltSliderChangeComplete}
+                                    />
+                                </div>
                             </div>
-                        </FormGroup>
-                        <FormGroup>
-                            <ControlLabel className="invisible">Hidden</ControlLabel>
+                        </td>
+                        <td className="dashed-border-right slider-input">
                             <FormControl type="text" value={this.tempAltCasesInputValue + symbol}
                                          onChange={this.handleAltInputChange}
                                          data-test="alterationThresholdInput"
                                          onKeyPress={this.handleAltInputKeyPress}/>
-                        </FormGroup>
-                    </div>
-                </div>
-
-                <div>
-                    <Checkbox checked={!this.hideGenomicAlterations} onChange={this.handleGenomicCheckboxChange}>
-                        Show Genomic Alteration Types
-                    </Checkbox>
-                </div>
+                        </td>
+                    </tr>
+                </table>
             </div>)
     }
 
@@ -464,10 +468,10 @@ export class CancerSummaryContent extends React.Component<ICancerSummaryContentP
                                 {/*className="fa fa-cog" aria-hidden="true"></i></button>*/}
                         {/*</div>*/}
 
-                        <Panel className={classnames("inlineBlock",{hidden: !this.showControls}, 'cancer-summary-secondary-options')}>
+                        <div className={classnames("inlineBlock",{hidden: !this.showControls}, 'cancer-summary-secondary-options')}>
                             {/*<button type="button" onClick={this.toggleShowControls} className="close">×</button>*/}
                             {this.controls}
-                        </Panel>
+                        </div>
 
                         <CancerSummaryChart key={Date.now()}
                                         data={this.chartData.data}
