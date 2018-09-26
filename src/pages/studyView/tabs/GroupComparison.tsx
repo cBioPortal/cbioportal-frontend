b15f7508d7f7698f6f8ad30ffd6b55522e949952
Karthik Kalletla <kalletlakarthik@gmail.com>
2018-09-26 11:01:04 -0400

    Toggling groups in scatter plot

--- src/pages/studyView/tabs/GroupComparison.tsx
+++ src/pages/studyView/tabs/GroupComparison.tsx
@@ -22,7 +22,7 @@ export interface IGroupComparisonProps {
 export class GroupComparison extends React.Component<IGroupComparisonProps, {}> {
     @observable activeTabId = 'survival';
 
-    @observable private activeTabs = observable.map<boolean>();
+    @observable private activeGroups = observable.map<boolean>();
 
 
     @bind
@@ -32,13 +32,13 @@ export class GroupComparison extends React.Component<IGroupComparisonProps, {}> 
 
     @bind
     @action private toggleActive(name: string) {
-        this.activeTabs.set(name, this.activeTabs.get(name) === undefined ? false : !this.activeTabs.get(name));
+        this.activeGroups.set(name, this.activeGroups.get(name) === undefined ? false : !this.activeGroups.get(name));
     }
 
     @computed get groupWithColors() {
         let count = -1;
-        return this.props.groups.map(group=>{
-            count = count +1;
+        return this.props.groups.map(group => {
+            count = count + 1;
             return {
                 name: group.name,
                 samples: group.samples,
@@ -49,7 +49,7 @@ export class GroupComparison extends React.Component<IGroupComparisonProps, {}> 
 
     @computed get groups() {
         return this.groupWithColors.map(group => {
-            let active = this.activeTabs.get(group.name) === undefined ? true : !!this.activeTabs.get(group.name);
+            let active = this.activeGroups.get(group.name) === undefined ? true : !!this.activeGroups.get(group.name);
             let numOfSamples = group.samples.length;
             let numOfPatients = _.uniq(group.samples.map(sample => sample.uniquePatientKey)).length;
             return (
@@ -59,76 +59,77 @@ export class GroupComparison extends React.Component<IGroupComparisonProps, {}> 
     }
 
     @computed get analysisGroups() {
-        return this.groupWithColors.map(group=>{
-            return {
-                value: group.name,
-                color: group.color
-            } as AnalysisGroup
-        })
+        return _.reduce(this.groupWithColors, (acc, group) => {
+            let isActive = this.activeGroups.get(group.name) === undefined ? true : !!this.activeGroups.get(group.name);
+            if (isActive) {
+                acc.push({
+                    value: group.name,
+                    color: group.color
+                })
+            }
+            return acc;
+        }, [] as AnalysisGroup[]);
     }
 
     @computed get patientToAnalysisGroup() {
-        return _.reduce(this.groupWithColors,(acc, next)=>{
-            next.samples.forEach(sample=>{
+        return _.reduce(this.groupWithColors, (acc, next) => {
+            next.samples.forEach(sample => {
                 acc[sample.uniquePatientKey] = next.name
             })
             return acc;
-        },{} as {[id:string]:string})
+        }, {} as { [id: string]: string })
     }
-    
+
     @computed get survivalCharts() {
-        if (this.props.survivalPlotData.isComplete) {
-            return _.map(this.props.survivalPlotData.result || [],(survivalData)=>{
-                let survivalChartData =  makeSurvivalChartData(
+        if (!_.isEmpty(this.patientToAnalysisGroup) && this.props.survivalPlotData.isComplete) {
+            return _.map(this.props.survivalPlotData.result || [], (survivalData) => {
+                let survivalChartData = makeSurvivalChartData(
                     survivalData.alteredGroup.concat(survivalData.unalteredGroup),
                     this.analysisGroups,
                     this.patientToAnalysisGroup,
                     true,
                     [],
                 );
-                return  (
+                return (
                     <div>
-                    <span>{survivalData.title}</span>
+                        <span>{survivalData.title}</span>
 
-                    <SurvivalChart 
-                patientSurvivals={survivalChartData.patientSurvivals}
-                patientToAnalysisGroup={survivalChartData.patientToAnalysisGroup}
-                analysisGroups={survivalChartData.analysisGroups}
-                legendLocation={LegendLocation.TOOLTIP}
-                title={'Survival'}
-                xAxisLabel="Months Survival"
-                yAxisLabel="Surviving"
-                totalCasesHeader="Number of Cases, Total"
-                statusCasesHeader="Number of Cases, Deceased"
-                medianMonthsHeader="Median Months Survival"
-                yLabelTooltip="Survival estimate"
-                xLabelWithEventTooltip="Time of death"
-                xLabelWithoutEventTooltip="Time of last observation"
-                showDownloadButtons={false}
-                showTable={false}
-                fileName="Overall_Survival"
-                styleOpts={{
-                    width: 400,
-                    height: 380,
-                    legend: {
-                        x: 190,
-                        y: 12
-                    },
-                    axis: {
-                        y: {
-                            axisLabel: {
-                                padding: 40
-                            }
-                        }
-                    }
-                }}
-            />
-                    
+                        <SurvivalChart
+                            patientSurvivals={survivalChartData.patientSurvivals}
+                            patientToAnalysisGroup={survivalChartData.patientToAnalysisGroup}
+                            analysisGroups={survivalChartData.analysisGroups}
+                            legendLocation={LegendLocation.TOOLTIP}
+                            title={'Survival'}
+                            xAxisLabel="Months Survival"
+                            yAxisLabel="Surviving"
+                            totalCasesHeader="Number of Cases, Total"
+                            statusCasesHeader="Number of Cases, Deceased"
+                            medianMonthsHeader="Median Months Survival"
+                            yLabelTooltip="Survival estimate"
+                            xLabelWithEventTooltip="Time of death"
+                            xLabelWithoutEventTooltip="Time of last observation"
+                            showDownloadButtons={false}
+                            showTable={false}
+                            fileName="Overall_Survival"
+                            styleOpts={{
+                                width: 400,
+                                height: 380,
+                                legend: {
+                                    x: 190,
+                                    y: 12
+                                },
+                                axis: {
+                                    y: {
+                                        axisLabel: {
+                                            padding: 40
+                                        }
+                                    }
+                                }
+                            }}
+                        />
                     </div>
                 )
-                
             })
-           
         } else {
             return [];
         }
@@ -136,27 +137,28 @@ export class GroupComparison extends React.Component<IGroupComparisonProps, {}> 
 
     public render() {
         return (
-            <div className={styles.main} style={{margin: '10px'}} >
+            <div className={styles.main} style={{ margin: '10px' }} >
                 <div className={styles.header}>
                     <span>Groups <span className={styles.sub}>(click to toggle, drag to re-order)</span></span>
                     <div className={styles.groups}>{this.groups}</div>
                 </div>
                 <MSKTabs id="groupComparisonTabs" activeTabId={this.activeTabId}
                     onTabClick={(id: string) => this.handleTabChange(id)}
-                         vertical={true}
+                    vertical={true}
                     className="pillTabs">
 
                     <MSKTab key={0} id="overlap" linkText="Overlap">
 
                     </MSKTab>
 
-                    <MSKTab key={1} id="survival" linkText="Survival">
-
-                    <div style={{ display: 'flex', padding: '0 10px'}}>
-                        {this.survivalCharts}
-                    </div>
-
-                    </MSKTab>
+                    {
+                        this.survivalCharts.length > 0 &&
+                        <MSKTab key={1} id="survival" linkText="Survival">
+                            <div style={{ display: 'flex', padding: '0 10px' }}>
+                                {this.survivalCharts}
+                            </div>
+                        </MSKTab>
+                    }
 
                     <MSKTab key={2} id="differentialExpression" linkText="Mutations">
 
@@ -198,7 +200,7 @@ class GroupPill extends React.Component<IGroupPillProps, {}> {
     public render() {
         return (<div className={classNames(styles.groupPill, {
             [styles.active]: this.props.active
-        })} onClick={this.toggleActive}>
+        })} style={{ backgroundColor: this.props.active ? this.props.color : '' }} onClick={this.toggleActive}>
             <span>{this.props.label}</span>
         </div>)
     }
