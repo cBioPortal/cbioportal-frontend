7d940abcb6bd1b877f3e4caadd8b77581659d795
Karthik Kalletla <kalletlakarthik@gmail.com>
2018-10-02 15:58:16 -0400

    Cleanup clinicaldata tab

--- src/pages/studyView/tabs/ClinicalDataTab.tsx
+++ src/pages/studyView/tabs/ClinicalDataTab.tsx
@@ -1,20 +1,17 @@
 import * as React from "react";
 import {Column, default as LazyMobXTable} from "shared/components/lazyMobXTable/LazyMobXTable";
 import {observer} from "mobx-react";
-import {If} from 'react-if';
 import * as _ from 'lodash';
-import {ClinicalAttribute, Sample} from "../../../shared/api/generated/CBioPortalAPI";
-import {computed} from 'mobx';
+import {ClinicalAttribute} from "../../../shared/api/generated/CBioPortalAPI";
 import {getPatientViewUrl, getSampleViewUrl} from "shared/api/urls";
 import SelectedInfo from "../SelectedInfo/SelectedInfo";
 import {getClinicalAttributeUniqueKey} from "../StudyViewUtils";
-import MobxPromise from "mobxpromise";
 import LoadingIndicator from "shared/components/loadingIndicator/LoadingIndicator";
+import { StudyViewPageStore } from "pages/studyView/StudyViewPageStore";
+import { remoteData } from "shared/api/remoteData";
 
 export interface IClinicalDataTabTable {
-    dataPromise: MobxPromise<{ [id: string]: string }[]>;
-    clinicalAttributes: ClinicalAttribute[];
-    selectedSamples: Sample[];
+    store: StudyViewPageStore
 }
 
 class ClinicalDataTabTableComponent extends LazyMobXTable<{ [id: string]: string }> {
@@ -33,60 +30,69 @@ export class ClinicalDataTab extends React.Component<IClinicalDataTabTable, {}> 
         };
     }
 
-    @computed
-    get columns(): Column<{ [id: string]: string }>[] {
-        let defaultColumns: Column<{ [id: string]: string }>[] = [{
-            ...this.getDefaultColumnConfig('patientId'),
-            render: (data: { [id: string]: string }) => {
-                return <a href={getPatientViewUrl(data.studyId, data.patientId)} target='_blank'>{data.patientId}</a>
-            },
-            name: 'Patient ID'
-        }, {
-            ...this.getDefaultColumnConfig('sampleId'),
-            render: (data: { [id: string]: string }) => {
-                return <a href={getSampleViewUrl(data.studyId, data.sampleId)} target='_blank'>{data.sampleId}</a>
-            },
-            name: 'Sample ID'
-        }, {
-            ...this.getDefaultColumnConfig('studyId'),
-            name: 'Cancer Study'
-        }];
-        // Descent sort priority then ascent sort by display name
-        return _.reduce(this.props.clinicalAttributes.sort( (a, b) => {
-            let _a = Number(a.priority) || 0;
-            let _b = Number(b.priority) || 0;
-            let priorityDiff = _b - _a;
-            if (priorityDiff === 0) {
-                return (a.displayName === undefined ? "" : a.displayName).localeCompare(b.displayName);
-            }
-            return priorityDiff;
-        }), (acc: Column<{ [id: string]: string }>[], attr: ClinicalAttribute, index: number) => {
-            acc.push({
-                ...this.getDefaultColumnConfig(getClinicalAttributeUniqueKey(attr)),
-                name: attr.displayName,
-                visible: index < 5
-            });
-            return acc;
-        }, defaultColumns);
-    }
+    readonly columns = remoteData({
+        await: () => [this.props.store.clinicalAttributes],
+        invoke: async () => {
+            
+            let defaultColumns: Column<{ [id: string]: string }>[] = [{
+                ...this.getDefaultColumnConfig('patientId'),
+                render: (data: { [id: string]: string }) => {
+                    return <a href={getPatientViewUrl(data.studyId, data.patientId)} target='_blank'>{data.patientId}</a>
+                },
+                name: 'Patient ID'
+            }, {
+                ...this.getDefaultColumnConfig('sampleId'),
+                render: (data: { [id: string]: string }) => {
+                    return <a href={getSampleViewUrl(data.studyId, data.sampleId)} target='_blank'>{data.sampleId}</a>
+                },
+                name: 'Sample ID'
+            }, {
+                ...this.getDefaultColumnConfig('studyId'),
+                name: 'Cancer Study'
+            }];
+            // Descent sort priority then ascent sort by display name
+            return _.reduce(this.props.store.clinicalAttributes.result.sort( (a, b) => {
+                let _a = Number(a.priority) || 0;
+                let _b = Number(b.priority) || 0;
+                let priorityDiff = _b - _a;
+                if (priorityDiff === 0) {
+                    return (a.displayName === undefined ? "" : a.displayName).localeCompare(b.displayName);
+                }
+                return priorityDiff;
+            }), (acc: Column<{ [id: string]: string }>[], attr: ClinicalAttribute, index: number) => {
+                acc.push({
+                    ...this.getDefaultColumnConfig(getClinicalAttributeUniqueKey(attr)),
+                    name: attr.displayName,
+                    visible: index < 5
+                });
+                return acc;
+            }, defaultColumns);
+        },
+        default: []
+    });
 
     public render() {
         return (
             <div>
-                <SelectedInfo selectedSamples={this.props.selectedSamples} />
-                <LoadingIndicator
-                    isLoading={this.props.dataPromise.isPending}
+                <LoadingIndicator isLoading={this.props.store.selectedSamples.isPending &&
+                    this.columns.isPending &&
+                    this.props.store.getDataForClinicalDataTab.isPending}
                 />
-                {this.props.dataPromise.isComplete &&
+                {
+                    this.props.store.selectedSamples.isComplete &&
+                    <SelectedInfo selectedSamples={this.props.store.selectedSamples.result} />
+                }
+                {
+                    this.props.store.getDataForClinicalDataTab.isComplete &&
+                    this.columns.isComplete &&
                     <ClinicalDataTabTableComponent
                         initialItemsPerPage={10}
                         showCopyDownload={true}
                         showColumnVisibility={true}
-                        data={this.props.dataPromise.result || []}
-                        columns={this.columns}
+                        data={this.props.store.getDataForClinicalDataTab.result || []}
+                        columns={this.columns.result}
                     />
                 }
-
             </div>
         );
     }
