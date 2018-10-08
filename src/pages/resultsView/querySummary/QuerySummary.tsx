17bece65e0ee8c513abaed219870f5552d4af177
aaronlisman <lismanBusiness@gmail.com>
2018-10-08 14:48:45 -0400

    Fix link to study page from results page

--- src/pages/resultsView/querySummary/QuerySummary.tsx
+++ src/pages/resultsView/querySummary/QuerySummary.tsx
@@ -19,7 +19,7 @@ import ExtendedRouterStore from "../../../shared/lib/ExtendedRouterStore";
 
 class StudyLink extends React.Component<{ study: CancerStudy, onClick?: () => void, href?:string }, {}> {
     render() {
-        return (<a href={this.props.href || `study?id=${this.props.study.studyId}`} target="_blank" style={{cursor:'pointer'}} onClick={this.props.onClick || (()=>{})}>{this.props.study.name}</a>);
+        return (<a href={this.props.href || `newstudy?id=${this.props.study.studyId}`} target="_blank" style={{cursor:'pointer'}} onClick={this.props.onClick || (()=>{})}>{this.props.study.name}</a>);
     }
 }
 
