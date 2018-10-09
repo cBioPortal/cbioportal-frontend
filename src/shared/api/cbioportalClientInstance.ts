d9077600fe783ec5e2e0f1f9405fad94d288841d
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-10-09 17:05:04 -0400

    POST cache all API clients
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 152049166ddb4bc7c41353f9b27953c9da9333a7

--- src/shared/api/cbioportalClientInstance.ts
+++ src/shared/api/cbioportalClientInstance.ts
@@ -3,6 +3,4 @@ import {proxyAllPostMethodsOnClient, proxyPost} from "../lib/proxyPost";
 
 const client = new CBioPortalAPI();
 
-proxyAllPostMethodsOnClient(CBioPortalAPI);
-
 export default client;
