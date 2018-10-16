84d0bd616dbebc8ba66a61ce86d409bb3091175c
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2018-10-16 18:19:03 -0400

    Replace logicalOr with lodash.some and make some other tweaks while were here
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: a0f34121ad5eb2b402cbf927047328669656fe84

--- src/shared/lib/LogicUtils.ts
+++ src/shared/lib/LogicUtils.ts
@@ -1,10 +1,5 @@
 import _ from "lodash";
 
-export function logicalOr<T>(array:T[], _iterator?:(t:T)=>boolean):boolean {
-    const iterator = _iterator ? _iterator : ((x:T)=>!!x);
-    return _.reduce(array, (acc:boolean, nextElt:T)=>acc||iterator(nextElt), false);
-}
-
 export function logicalAnd<T>(array:T[], _iterator?:(t:T)=>boolean):boolean {
     const iterator = _iterator ? _iterator : ((x:T)=>!!x);
     return _.reduce(array, (acc:boolean, nextElt:T)=>acc&&iterator(nextElt), true);
