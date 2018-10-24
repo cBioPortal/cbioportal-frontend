aa7b6f0f5779d0e3ef1c42ea64bf5421df285d8f
aaronlisman <lismanBusiness@gmail.com>
2018-10-24 10:50:31 -0400

    Add fourth tutorial to tutorials page

--- src/pages/staticPages/tutorials/Tutorials.tsx
+++ src/pages/staticPages/tutorials/Tutorials.tsx
@@ -28,16 +28,23 @@ export default class Tutorials extends React.Component<{}, {}> {
             <iframe src="https://docs.google.com/presentation/d/1_OGK69lO4Z62WaxHHkNYmWvY0LQN2v0slfaLyY1_IQ0/embed?start=false&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434" allowFullScreen={true}></iframe>
             <hr/>
-            <p></p>
+
             <h2>Tutorial #2: Single Study Query</h2>
             <iframe
                 src="https://docs.google.com/presentation/d/1y9UTIr5vHmsNVWqtGTVGgiuYX9wkK_a_RPNYiR8kYD8/embed?start=false&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434" allowFullScreen={true}></iframe>
             <hr/>
-            <p></p>
+
             <h2>Tutorial #3: Patient View</h2>
             <iframe src="https://docs.google.com/presentation/d/1Jr_2yEfgjKBn4DBiXRk4kmhIbtsRp6gd0iD3k1fIUUk/embed?start=false&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434" allowFullScreen={true}></iframe>
+
+
+            <h2>Tutorial #4: Virtual Studies</h2>
+            <iframe
+                src="https://docs.google.com/presentation/d/1rQE5rbFNdmup-rAtySHFxlLp3i4qa8SBA7MiQpMdn1I/embed?start=false&loop=false&delayms=60000"
+                frameBorder="0" width="720" height="434"
+                allowFullScreen={true}></iframe>
 
 
         </PageLayout>
