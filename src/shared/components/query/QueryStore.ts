f0a463268577ad7625c80f7eadb6a463e1f92821
aaronlisman <lismanBusiness@gmail.com>
2018-10-29 18:28:21 -0400

    Fix integer case_set_id when we're in session mode

--- src/shared/components/query/QueryStore.ts
+++ src/shared/components/query/QueryStore.ts
@@ -118,7 +118,7 @@ export class QueryStore
 
 	constructor(_window:Window, urlWithInitialParams?:string)
 	{
-
+		
 		labelMobxPromises(this);
 		if (urlWithInitialParams)
 			this.setParamsFromUrl(urlWithInitialParams);
@@ -1563,7 +1563,7 @@ export class QueryStore
 		this.zScoreThreshold = params.Z_SCORE_THRESHOLD || '2.0';
 		this.rppaScoreThreshold = params.RPPA_SCORE_THRESHOLD || '2.0';
 		this.dataTypePriorityCode = params.data_priority || '0';
-		this.selectedSampleListId = params.case_set_id || "";
+		this.selectedSampleListId = params.case_set_id ? params.case_set_id.toString() : "";  // must be a string even though it's integer
 		this.caseIds = this.sanitizeQueryParams(params.case_ids);
 		this.caseIdsMode = 'sample'; // url always contains sample IDs
         this.geneQuery = normalizeQuery(decodeURIComponent(params.gene_list||''));
@@ -1606,7 +1606,7 @@ export class QueryStore
             const caseIds = parsedSubmission.case_ids;
             if (caseIds) {
                 if (parsedSubmission.case_set_id == CUSTOM_CASE_LIST_ID) {
-                    this.selectedSampleListId = CUSTOM_CASE_LIST_ID;
+                    this.selectedSampleListId = CUSTOM_CASE_LIST_ID.toString();
                     this.caseIdsMode = 'sample';
                     this.caseIds = caseIds.replace(/\+/g, "\n");
                 }
