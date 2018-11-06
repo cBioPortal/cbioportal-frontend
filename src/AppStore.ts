b69682ef04a8c587aea66909ea2a6ea13f18d830
gaofei zhao <zgfilx@163.com>
2018-11-06 10:28:05 -0500

    Change footer to AppStore

--- src/AppStore.ts
+++ src/AppStore.ts
@@ -1,6 +1,9 @@
 import {computed, observable} from "mobx";
+import {remoteData} from "shared/api/remoteData";
 import {initializeAPIClients} from "./config/config";
 import * as _ from 'lodash';
+import internalClient from "shared/api/cbioportalInternalClientInstance";
+
 
 export class AppStore {
 
@@ -20,4 +23,13 @@ export class AppStore {
         }
     }
 
+    readonly portalVersion = remoteData<string | undefined>({
+        invoke:async()=>{
+            const portalVersionResult = await internalClient.getInfoUsingGET({});
+            if (portalVersionResult && portalVersionResult.portalVersion) {
+                return Promise.resolve("v" + portalVersionResult.portalVersion.split('-')[0]);
+            }
+            return undefined; 
+        }
+    });
 }
