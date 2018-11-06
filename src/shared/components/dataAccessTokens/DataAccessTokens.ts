779e23307863f83ffa4a96ce6d998216ada9c50f
Angelica Ochoa <aochoa4230@gmail.com>
2018-11-06 12:57:16 -0500

    Added check for username type

--- src/shared/components/dataAccessTokens/DataAccessTokens.ts
+++ src/shared/components/dataAccessTokens/DataAccessTokens.ts
@@ -1,9 +1,14 @@
 import * as React from "react";
 import {DataAccessToken} from "../../../shared/api/generated/CBioPortalAPI";
+import * as _ from 'lodash';
 
 export interface IDataAccessTokensProps {
     dataAccessToken?: DataAccessToken;
 }
-export function getDataAccessTokens(username:string){
-    return "myDataAccessToken2" + username;
+export function getDataAccessTokens(username: string | undefined): string {
+    if (_.isString(this.userName)) {
+        return "myDataAccessToken" + username;
+    } else {
+        return "undefined username"
+    }
 }
