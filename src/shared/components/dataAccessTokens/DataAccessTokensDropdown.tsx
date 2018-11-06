b04804d2bdc4052e1db91246c037329be6ecdee7
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2018-11-06 14:54:29 -0500

    1. DataAccessTokens download token details on auth. portals * Added DAT endpoint calls to CBioPortalAPI.ts * Added new react component for DataAccessTokens * Moved sign out link to dropdown * Add DAT file contents build and download link * Suppress DAT download if auth method is social_auth
    2. Migrate DAT endpoint to main API
    
    3. Add DAT method check to hide 'Download Token' option if DAT method is 'none'
    * Added check to determine whether DAT method is "none"
    * If DAT method is "none" then hide 'Token Download' from authorized user dropdown menu.
    
    4. Format DAT dates for download token file (ISO format)
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.comm>

---
+++ src/shared/components/dataAccessTokens/DataAccessTokensDropdown.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import AppConfig from "appConfig";
+import {If, Then, Else} from 'react-if';
+import internalClient from "shared/api/cbioportalInternalClientInstance";
+import {AppStore} from "../../../AppStore";
+import LoadingIndicator from "../loadingIndicator/LoadingIndicator";
+import {observer} from "mobx-react";
+import {observable, computed} from "mobx";
+import {buildCBioPortalPageUrl} from "../../api/urls";
+import {isNullOrUndefined} from "util";
+import fileDownload from 'react-file-download';
+
+export class UserDataAccessToken {
+    @observable token : string;
+    @observable creationDate: string;
+    @observable expirationDate: string;
+    @observable username: string;
+    constructor(token: string, creationDate: string, expirationDate: string, username: string) {
+        this.token = token;
+        this.creationDate = creationDate;
+        this.expirationDate = expirationDate;
+        this.username = username;
+    }
+}
+
+export interface IDataAccessTokensProps {
+    token?:string;
+    creationDate?:string;
+    expirationDate?:string;
+    loadingComponent?:JSX.Element;
+    appStore:AppStore;
+}
+
+function buildDataAccessTokenFileContents(dat:UserDataAccessToken | undefined) {
+    if (!isNullOrUndefined(dat)) {
+        const fileContents = "token: " + dat!.token + "\n" +
+            "creation_date: " + new Date(dat!.creationDate).toISOString()+ "\n" +
+            "expiration_date: " + new Date(dat!.expirationDate).toISOString();
+        return fileContents;
+    } else {
+        alert("Cannot create Data Access Token file for user with non-existent tokens.");
+        return null;
+    }
+}
+
+@observer
+export class DataAccessTokensDropdown extends React.Component<IDataAccessTokensProps, {}> {
+    public static defaultProps: Partial<IDataAccessTokensProps> = {
+        loadingComponent:<LoadingIndicator isLoading={true}/>
+    };
+
+    constructor(props: IDataAccessTokensProps) {
+        super(props);
+    }
+
+    @computed get getDatDropdownList() : any {
+        const listItems = [
+            {
+                id:"signout",
+                action:<a href={buildCBioPortalPageUrl(this.props.appStore.logoutUrl)}>Sign out</a>,
+                hide:false
+            },
+            {
+                id:"datDownload",
+                action:<a onClick={() => this.downloadDataAccessTokenFile()}>Download token</a>,
+                hide:(AppConfig.serverConfig.authenticationMethod === "social_auth" || (AppConfig.serverConfig.dat_method !== "uuid" && AppConfig.serverConfig.dat_method !== "jwt"))
+            }
+        ];
+        const shownListItems = listItems.filter((l)=>{
+            return !l.hide;
+        });
+
+        return shownListItems.map((l)=>{
+            return <li>{l.action}</li>;
+        });
+    }
+
+    async downloadDataAccessTokenFile() {
+        const dat = await this.generateNewDataAccessToken();
+        if (!isNullOrUndefined(dat)) {
+            const fileContents = buildDataAccessTokenFileContents(dat);
+            fileDownload(fileContents, "cbioportal_data_access_token.txt");
+        }
+    }
+
+    async generateNewDataAccessToken() {
+        if (this.props.appStore.isLoggedIn) {
+            let _token = await Promise.resolve(
+                internalClient.createDataAccessTokenUsingPOST(
+                    {'allowRevocationOfOtherTokens':AppConfig.serverConfig.dat_uuid_revoke_other_tokens}))
+            const dat = new UserDataAccessToken(_token.token, _token.creation, _token.expiration, _token.username);
+            return dat;
+        } else {
+            return undefined;
+        }
+    }
+
+    render(){
+        return(
+        <ul className="list-unstyled">
+            {
+                this.getDatDropdownList
+            }
+        </ul>
+        );
+    }
+}
