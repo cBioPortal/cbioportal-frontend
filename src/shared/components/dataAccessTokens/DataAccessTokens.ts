3ea9b94946771099e421258249c6aeb4a69a1ca2
Angelica Ochoa <aochoa4230@gmail.com>
2018-11-07 15:11:11 -0500

    Parsing result from promise..

--- src/shared/components/dataAccessTokens/DataAccessTokens.ts
+++ src/shared/components/dataAccessTokens/DataAccessTokens.ts
@@ -6,7 +6,8 @@ import client from "shared/api/cbioportalClientInstance";
 import {remoteData} from "shared/api/remoteData";
 
 export interface IDataAccessTokensProps {
-    dataAccessToken?: DataAccessToken;
+    dataAccessToken: DataAccessToken;
+    tokenValue: string;
 }
 
 export function getDataAccessTokens(username: string | undefined): string {
@@ -15,11 +16,26 @@ export function getDataAccessTokens(username: string | undefined): string {
         const result = Promise.resolve(
             client.createDataAccessTokenUsingPOST(
                 {'allowRevocationOfOtherTokens':true}));
+        console.log("RESULT AS PROMISE");
         console.log(result);
+
+
         token = result.then(function(response) {
             return response.token as string;
         });
+        console.log("-------TOKEN AS PROMISE--------");
         console.log(token);
+
+        const dat = remoteData<DataAccessToken>({
+            invoke: () => {
+                return Promise.resolve(
+                        client.createDataAccessTokenUsingPOST(
+                            {'allowRevocationOfOtherTokens':true}));
+            }});
+        console.log("-------RESULT AS READONLY--------");
+        console.log(dat.result!);
+        console.log(dat.result!.token);
+
         return "mytoken-" + username;
     } else {
         return "undefined username";
