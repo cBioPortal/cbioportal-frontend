a81171add3e3a056856df1b617bb084ba102b340
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-11-09 17:49:05 -0500

    update
    Former-commit-id: f1c478abea347f9e5392f5f577412b9fef71b64f

--- src/pages/studyView/SelectedInfo/SelectedInfo.tsx
+++ src/pages/studyView/SelectedInfo/SelectedInfo.tsx
@@ -4,10 +4,9 @@ import {computed} from "mobx";
 import {observer} from "mobx-react";
 import {Sample} from 'shared/api/generated/CBioPortalAPI';
 import styles from './styles.module.scss';
-import MobxPromise from 'mobxpromise';
 
 export interface ISelectedInfoProps {
-    promise: MobxPromise<Sample[]>;
+    selectedSamples: Sample[];
 }
 
 @observer
@@ -15,14 +14,14 @@ export default class SelectedInfo extends React.Component<ISelectedInfoProps, {}
 
     @computed
     get selectedPatientsCount() {
-        return _.uniq(this.props.promise.result!.map(sample => sample.uniquePatientKey)).length;
+        return _.uniq(this.props.selectedSamples.map(sample => sample.uniquePatientKey)).length;
     }
 
     render() {
         return <div className={styles.main}>
             <span className={styles.title}>Selected:</span>
             <span className={styles.content}>
-                {this.selectedPatientsCount} patients | {this.props.promise.result!.length} samples
+                {this.selectedPatientsCount} patients | {this.props.selectedSamples.length} samples
             </span>
         </div>
     }
