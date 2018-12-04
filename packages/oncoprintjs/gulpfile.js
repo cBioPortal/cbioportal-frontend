722d34fba5341080fc0a222044eccf39b1355d60
Adam Abeshouse <adamabeshouse@gmail.com>
2018-12-04 16:19:10 -0500

    Merge pull request #85 from cBioPortal/fix-vuln
    Uninstall unused packages

--- packages/oncoprintjs/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-
-gulp.task('default', shell.task([
-	'mkdir -p dist/img',
-	'browserify src/js/main.js -o dist/oncoprint-bundle.js',
-	'cp src/css/* dist/',
-	'cp src/img/* dist/img/',
-]));
