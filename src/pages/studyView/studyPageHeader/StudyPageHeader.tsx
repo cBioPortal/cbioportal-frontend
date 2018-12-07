c1db8b96c9aef1fa39ebfcf4aa39146f0c282e67
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-12-07 10:20:35 -0500

    move filters to study view header
    Signed-off-by: Hongxin Zhang <hongxin@cbio.mskcc.org>

--- src/pages/studyView/studyPageHeader/StudyPageHeader.tsx
+++ src/pages/studyView/studyPageHeader/StudyPageHeader.tsx
@@ -5,6 +5,7 @@ import {StudyViewPageStore} from 'pages/studyView/StudyViewPageStore';
 import RightPanel from "./rightPanel/RightPanel";
 import StudySummary from "./studySummary/StudySummary";
 import {ServerConfigHelpers} from "../../../config/config";
+import UserSelections from "../UserSelections";
 
 
 export interface IStudyPageHeaderProps {
@@ -15,7 +16,8 @@ export interface IStudyPageHeaderProps {
 export default class StudyPageHeader extends React.Component<IStudyPageHeaderProps, {}> {
     render() {
         return (
-            <div style={{display: 'flex', margin: '5px 20px'}}>
+            <div style={{display: 'flex', flexDirection:'column',margin: '5px 20px'}}>
+                <div style={{display: 'flex'}}>
                 <StudySummary
                     studies={this.props.store.displayedStudies.result}
                     originStudies={this.props.store.originStudies}
@@ -24,6 +26,24 @@ export default class StudyPageHeader extends React.Component<IStudyPageHeaderPro
                 <RightPanel
                     user={ServerConfigHelpers.getUserEmailAddress()}
                     store={this.props.store}/>
+                </div>
+
+                <UserSelections
+                    filter={this.props.store.userSelections}
+                    getSelectedGene={this.props.store.getKnownHugoGeneSymbolByEntrezGeneId}
+                    attributesMetaSet={this.props.store.chartMetaSet}
+                    updateClinicalDataEqualityFilter={this.props.store.updateClinicalDataEqualityFilters}
+                    updateClinicalDataIntervalFilter={this.props.store.updateClinicalDataIntervalFiltersByValues}
+                    removeGeneFilter={this.props.store.removeGeneFilter}
+                    removeCNAGeneFilter={this.props.store.removeCNAGeneFilters}
+                    resetMutationCountVsCNAFilter={this.props.store.resetMutationCountVsCNAFilter}
+                    clearCNAGeneFilter={this.props.store.clearCNAGeneFilter}
+                    clearGeneFilter={this.props.store.clearGeneFilter}
+                    removeWithMutationDataFilter={this.props.store.removeWithMutationDataFilter}
+                    removeWithCNADataFilter={this.props.store.removeWithCNADataFilter}
+                    clearChartSampleIdentifierFilter={this.props.store.clearChartSampleIdentifierFilter}
+                    clearAllFilters={this.props.store.clearAllFilters}
+                />
             </div>
         )
     }
