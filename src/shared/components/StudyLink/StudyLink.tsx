4fca392f936c4215074eb6ae0196d4a7dda0bef0
aaronlisman <lismanBusiness@gmail.com>
2018-12-10 14:33:58 -0500

    Use media queries to adjust size of results view header to prevent excessive wrapping on small screens
    Former-commit-id: d3ad54447cd6c40b61f45f49e739901b47754be4

--- src/shared/components/StudyLink/StudyLink.tsx
+++ src/shared/components/StudyLink/StudyLink.tsx
@@ -5,6 +5,6 @@ import {buildCBioLink} from "../../api/urls";
 export class StudyLink extends React.Component<{ study: CancerStudy, onClick?: () => void, href?:string }, {}> {
     render() {
         const url = this.props.href || `study?id=${this.props.study.studyId}`;
-        return (<a href={buildCBioLink(url)} target="_blank" style={{cursor:'pointer'}} onClick={this.props.onClick || (()=>{})}>{this.props.study.name}</a>);
+        return (<a href={buildCBioLink(url)} title={this.props.study.name} target="_blank" style={{cursor:'pointer'}} onClick={this.props.onClick || (()=>{})}>{this.props.study.name}</a>);
     }
 }
