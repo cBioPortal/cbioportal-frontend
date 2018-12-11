e10cee3e255fad43c57958c61bc8e48539b41928
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-12-11 16:00:44 -0500

    Include study view screenshot references

--- end-to-end-tests/specs/studyview.spec.js
+++ end-to-end-tests/specs/studyview.spec.js
@@ -7,18 +7,18 @@ var assertScreenShotMatch = require('../lib/testUtils').assertScreenShotMatch;
 
 const CBIOPORTAL_URL = process.env.CBIOPORTAL_URL.replace(/\/$/, "");
 
-describe('new study view screenshot test', function(){
+describe('study view screenshot test', function(){
     before(function(){
         var url = `${CBIOPORTAL_URL}/study?id=laml_tcga`;
         goToUrlAndSetLocalStorage(url);
     });
-     it('new study view laml_tcga', function() {
+     it('study view laml_tcga', function() {
         browser.waitForVisible('#mainColumn',10000);
         waitForNetworkQuiet();
         var res = browser.checkElement('#mainColumn', {hide:['.qtip', '#footer-span-version'] });
         assertScreenShotMatch(res);
     });
-     it('new study view laml_tcga clinical data clicked', function() {
+     it('study view laml_tcga clinical data clicked', function() {
         browser.click('.tabAnchor_clinicalData');
         browser.waitForVisible('#mainColumn',10000);
         waitForNetworkQuiet();
