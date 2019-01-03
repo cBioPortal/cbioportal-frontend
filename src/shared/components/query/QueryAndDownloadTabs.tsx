db3fef7294e7b0de82081739e1b17903973ab769
aaronlisman <lismanBusiness@gmail.com>
2019-01-03 13:48:51 -0500

    Handle invalid genes error by directing use to correct query in query selector
    Former-commit-id: d90c7981932427fb8e0c7f1e7f9573ca72909fd4

--- src/shared/components/query/QueryAndDownloadTabs.tsx
+++ src/shared/components/query/QueryAndDownloadTabs.tsx
@@ -4,6 +4,7 @@ import * as styles_any from './styles/styles.module.scss';
 import {observer} from 'mobx-react';
 import QueryContainer from "./QueryContainer";
 import { QueryStore} from "./QueryStore";
+import {observable} from "mobx";
 
 const styles = styles_any as {
 	QueryAndDownloadTabs: string,
@@ -14,9 +15,10 @@ const QUERY = 'query';
 
 interface IQueryAndDownloadTabsProps
 {
-	store:QueryStore;
 	onSubmit?:()=>void;
 	showDownloadTab?:boolean;
+    getQueryStore:()=>QueryStore;
+    showAlerts?:boolean;
 }
 
 
@@ -24,10 +26,17 @@ interface IQueryAndDownloadTabsProps
 export default class QueryAndDownloadTabs extends React.Component<IQueryAndDownloadTabsProps, {}>
 {
 
-	get store()
-	{
-		return this.props.store;
+	constructor(props:IQueryAndDownloadTabsProps){
+		super();
+
+		// the query store models a single use of the query component and therefor a new one should
+		// be made when the component is instantiated and it should be destroyed when the component unmounts
+		// we use a callback because we want to create the query store at the moment the query form is instantiated/shown
+		// because it needs the state of route at that moment (so that on the results page it reflects the current query)
+		this.store = props.getQueryStore();
 	}
+
+	@observable.ref store: QueryStore;
 
 	onSelectTab = (eventKey:string) =>
 	{
@@ -39,6 +48,13 @@ export default class QueryAndDownloadTabs extends React.Component<IQueryAndDownl
 	{
 		return (
 			<div className={styles.QueryAndDownloadTabs}>
+
+				{
+					(this.props.showAlerts && this.store.genes.isComplete && this.store.genes.result.suggestions.length > 0) && (
+                        <div className="alert alert-danger"><i className={"fa fa-exclamation-triangle"} /> Your query has invalid or out-dated gene symbols. Please correct below.</div>
+					)
+				}
+
 				<Tabs
 					id='QueryAndDownloadTabs'
 					animation={false}
@@ -53,7 +69,7 @@ export default class QueryAndDownloadTabs extends React.Component<IQueryAndDownl
 					}
 
 				</Tabs>
-				<QueryContainer onSubmit={this.props.onSubmit} store={this.props.store}/>
+				<QueryContainer onSubmit={this.props.onSubmit} store={this.store}/>
 			</div>
 		);
 	}
