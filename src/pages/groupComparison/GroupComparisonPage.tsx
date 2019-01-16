6c3ddea099ffebfe5945cbb71b8903817151b1fa
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-01-16 16:44:42 -0500

    group comparison - add enrichment group select dropdowns
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/groupComparison/GroupComparisonPage.tsx
+++ src/pages/groupComparison/GroupComparisonPage.tsx
@@ -4,6 +4,8 @@ import GroupComparisonStore from "./GroupComparisonStore";
 import MutationEnrichments from "./MutationEnrichments";
 import {MSKTab, MSKTabs} from "../../shared/components/MSKTabs/MSKTabs";
 import {PageLayout} from "../../shared/components/PageLayout/PageLayout";
+import ReactSelect from "react-select";
+import 'react-select/dist/react-select.css';
 
 export enum GroupComparisonTab {
     MUTATIONS, CNA, MRNA, PROTEIN
@@ -20,20 +22,50 @@ export default class GroupComparisonPage extends React.Component<{}, {}> {
     render() {
         return (
             <PageLayout noMargin={true} hideFooter={true} className={"subhead-dark"}>
-                <MSKTabs unmountOnHide={false} activeTabId={this.store.currentTabId} onTabClick={this.store.setTabId} className="secondaryTabs">
-                    <MSKTab id={GroupComparisonTab.MUTATIONS.toString()} linkText="Mutations">
-                        <MutationEnrichments store={this.store}/>
-                    </MSKTab>
-                    <MSKTab id={GroupComparisonTab.CNA.toString()} linkText="Copy-number">
-                        <span>GOES HERE</span>
-                    </MSKTab>
-                    <MSKTab id={GroupComparisonTab.MRNA.toString()} linkText="mRNA">
-                        <span>GOES HERE</span>
-                    </MSKTab>
-                    <MSKTab id={GroupComparisonTab.PROTEIN.toString()} linkText="Protein">
-                        <span>GOES HERE</span>
-                    </MSKTab>
-                </MSKTabs>
+                <div style={{display:"flex", flexDirection:"column"}}>
+                    <div style={{marginBottom:30}}>
+                        { this.store.sampleGroups.isComplete && (
+                            <div style={{display:"flex", alignItems:"center"}}>
+                                <h3 style={{marginRight:10}}>Enrichment analysis groups:</h3>
+                                <div style={{width:200, marginRight:10}}>
+                                    <ReactSelect
+                                        options={this.store.sampleGroups.result!.map((g, i)=>({ label: `Group ${i}`, value: g.id, group: g }))}
+                                        onChange={(option:any)=>(option && (this.store.enrichmentsGroup1 = option.group))}
+                                        value={this.store.enrichmentsGroup1.id}
+                                        clearable={false}
+                                        searchable={false}
+                                    />
+                                </div>
+                                <span>vs.&nbsp;&nbsp;</span>
+                                <div style={{width:200, marginRight:10}}>
+                                    <ReactSelect
+                                        options={this.store.sampleGroups.result!.map((g, i)=>({ label: `Group ${i}`, value: g.id, group: g }))}
+                                        onChange={(option:any)=>(option && (this.store.enrichmentsGroup2 = option.group))}
+                                        value={this.store.enrichmentsGroup2.id}
+                                        clearable={false}
+                                        searchable={false}
+                                    />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        <MSKTabs unmountOnHide={false} activeTabId={this.store.currentTabId} onTabClick={this.store.setTabId} className="secondaryTabs">
+                            <MSKTab id={GroupComparisonTab.MUTATIONS.toString()} linkText="Mutations">
+                                <MutationEnrichments store={this.store}/>
+                            </MSKTab>
+                            <MSKTab id={GroupComparisonTab.CNA.toString()} linkText="Copy-number">
+                                <span>GOES HERE</span>
+                            </MSKTab>
+                            <MSKTab id={GroupComparisonTab.MRNA.toString()} linkText="mRNA">
+                                <span>GOES HERE</span>
+                            </MSKTab>
+                            <MSKTab id={GroupComparisonTab.PROTEIN.toString()} linkText="Protein">
+                                <span>GOES HERE</span>
+                            </MSKTab>
+                        </MSKTabs>
+                    </div>
+                </div>
             </PageLayout>
         );
     }
