bf127db2faa1f39dbb761a024ed31c5ac60df443
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-01-23 15:42:28 -0500

    group comparison - adjust group choosing UI
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/groupComparison/GroupComparisonPage.tsx
+++ src/pages/groupComparison/GroupComparisonPage.tsx
@@ -14,6 +14,10 @@ import ProteinEnrichments from "./ProteinEnrichments";
 import {MakeMobxView} from "../../shared/components/MobxView";
 import LoadingIndicator from "../../shared/components/loadingIndicator/LoadingIndicator";
 import ErrorMessage from "../../shared/components/ErrorMessage";
+import GroupSelector from "./GroupSelector";
+import { Checkbox, Button } from "react-bootstrap";
+import InfoIcon from "shared/components/InfoIcon";
+import { caseCountsInParens } from "./GroupComparisonUtils";
 
 export enum GroupComparisonTab {
     OVERLAP, MUTATIONS, CNA, MRNA, PROTEIN, SURVIVAL
@@ -34,7 +38,8 @@ export default class GroupComparisonPage extends React.Component<{}, {}> {
             this.store.copyNumberEnrichmentProfiles,
             this.store.mRNAEnrichmentProfiles,
             this.store.proteinEnrichmentProfiles,
-            this.store.survivalClinicalDataExists
+            this.store.survivalClinicalDataExists,
+            this.store.studyIds
         ],
         render:()=>{
             if ((this.store.mutationEnrichmentProfiles.result!.length > 0) ||
@@ -43,6 +48,10 @@ export default class GroupComparisonPage extends React.Component<{}, {}> {
                 (this.store.proteinEnrichmentProfiles.result!.length > 0) ||
                 this.store.showSurvivalTab
             ) {
+                let mrnaAndProteinAnchorStyle:any = {};
+                if(this.store.studyIds.result!.length > 1) {
+                    mrnaAndProteinAnchorStyle = {color:"#afafaf"};
+                }
                 return <MSKTabs unmountOnHide={false} activeTabId={this.store.currentTabId} onTabClick={this.store.setTabId} className="primaryTabs">
                     {
                         this.store.showSurvivalTab &&
@@ -61,12 +70,16 @@ export default class GroupComparisonPage extends React.Component<{}, {}> {
                         </MSKTab>
                     )}
                     {this.store.mRNAEnrichmentProfiles.result!.length > 0 && (
-                        <MSKTab id={GroupComparisonTab.MRNA.toString()} linkText="mRNA">
+                        <MSKTab id={GroupComparisonTab.MRNA.toString()} linkText="mRNA"
+                            anchorStyle={mrnaAndProteinAnchorStyle}
+                        >
                             <MRNAEnrichments store={this.store}/>
                         </MSKTab>
                     )}
                     {this.store.proteinEnrichmentProfiles.result!.length > 0 && (
-                        <MSKTab id={GroupComparisonTab.PROTEIN.toString()} linkText="Protein">
+                        <MSKTab id={GroupComparisonTab.PROTEIN.toString()} linkText="Protein"
+                            anchorStyle={mrnaAndProteinAnchorStyle}
+                        >
                             <ProteinEnrichments store={this.store}/>
                         </MSKTab>
                     )}
@@ -84,32 +97,31 @@ export default class GroupComparisonPage extends React.Component<{}, {}> {
             <PageLayout>
                 <div style={{display:"flex", flexDirection:"column"}}>
                     <div style={{marginBottom:30}}>
-                        { this.store.sampleGroups.isComplete && (
-                            <div style={{display:"flex", alignItems:"center"}}>
-                                <h3 style={{marginRight:10}}>Enrichment analysis groups:</h3>
-                                <div style={{width:200, marginRight:10}}>
-                                    <ReactSelect
-                                        options={this.store.sampleGroups.result!.map((g, i)=>({ label: g.name || "Group", value: g.id, group: g }))}
-                                        onChange={(option:any)=>(option && (this.store.setEnrichmentsGroup1(option.group)))}
-                                        value={this.store.enrichmentsGroup1.result && this.store.enrichmentsGroup1.result.id}
-                                        placeholder="Loading sample groups.."
-                                        clearable={false}
-                                        searchable={false}
+                        <div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
+                            <span style={{marginRight:10}}>Groups (click to toggle):</span>
+                            <GroupSelector
+                                store = {this.store}
+                            />
+                            <span style={{marginLeft:10}}>
+                                <button type="button" className="btn" 
+                                    style={{border:"1px solid #dddddd"}}
+                                    onClick={this.store.toggleExcludeOverlapping}
+                                >
+                                    <input
+                                        type="checkbox"
+                                        checked={this.store.excludeOverlapping}
                                     />
-                                </div>
-                                <span>vs.&nbsp;&nbsp;</span>
-                                <div style={{width:200, marginRight:10}}>
-                                    <ReactSelect
-                                        options={this.store.sampleGroups.result!.map((g, i)=>({ label: `Group ${i}`, value: g.id, group: g }))}
-                                        onChange={(option:any)=>(option && (this.store.setEnrichmentsGroup2(option.group)))}
-                                        value={this.store.enrichmentsGroup2.result && this.store.enrichmentsGroup2.result.id}
-                                        placeholder="Loading sample groups.."
-                                        clearable={false}
-                                        searchable={false}
-                                    />
-                                </div>
-                            </div>
-                        )}
+                                    <span style={{marginLeft:7}}>
+                                        {`Exclude overlapping samples/patients ${caseCountsInParens(this.store.overlappingSelectedSamples, this.store.overlappingSelectedPatients)}`}
+                                    </span> 
+                                    <div style={{display:"inline-block", marginLeft:7}}>
+                                        <InfoIcon 
+                                            tooltip={<span style={{maxWidth:200}}>Exclude samples from analysis which occur in more than one selected group.</span>}
+                                        />
+                                    </div>
+                                </button>
+                            </span>
+                        </div>
                     </div>
                     <div>
                         {this.tabs.component}
