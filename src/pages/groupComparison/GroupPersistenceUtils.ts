e177932ebfea6b24f69ede0b3eb593a1fea3dcde
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-02-13 17:03:29 -0500

    group comparison - pie chart comparison shortcut
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: af03c0dfee61ea63f2f593e9ddc4d32fed7ba997

--- src/pages/groupComparison/GroupPersistenceUtils.ts
+++ src/pages/groupComparison/GroupPersistenceUtils.ts
@@ -1,11 +1,23 @@
-import {ComparisonSampleGroup, TEMP_localStorageGroupsKey} from "./GroupComparisonUtils";
-import {SampleIdentifier} from "../../shared/api/generated/CBioPortalAPI";
+import {ComparisonSampleGroup} from "./GroupComparisonUtils";
+import {ClinicalAttribute, SampleIdentifier} from "../../shared/api/generated/CBioPortalAPI";
 import hashString from "../../shared/lib/hashString";
 import {observable} from "mobx";
 import _ from "lodash";
 import ListIndexedMap from "../../shared/lib/ListIndexedMap";
+import {StudyViewFilter} from "../../shared/api/generated/CBioPortalAPIInternal";
 
 // TODO: use web service
+
+/// TYPES
+export type SampleGroupWithoutId = Pick<ComparisonSampleGroup, Exclude<keyof ComparisonSampleGroup, "id">>;
+export type FromChartSpec = {
+    clinicalAttribute:ClinicalAttribute, values:string[], filters:StudyViewFilter,
+    created:number
+};
+
+/// VARIABLES
+const LSGroupsKey = "__tmp__groupComparisonGroups";
+const LSChartKey = "__tmp__fromChart_specs";
 
 // keep and update a local observable copy so that users can react to changes
 const observableGroupsCopy = observable.shallowBox<ComparisonSampleGroup[]>([]);
@@ -13,41 +25,72 @@ const observableGroupsCopy = observable.shallowBox<ComparisonSampleGroup[]>([]);
 // hold onto recently deleted groups for "undo" purpose
 let recentlyDeletedGroups:ComparisonSampleGroup[] = [];
 
+
+/// FUNCTIONS
 function updateObservableGroupsCopy() {
     // update local observable version for users to react to
-    observableGroupsCopy.set(JSON.parse(localStorage.getItem(TEMP_localStorageGroupsKey) || "[]"));
+    observableGroupsCopy.set(JSON.parse(localStorage.getItem(LSGroupsKey) || "[]"));
 }
-
-// initial update
-updateObservableGroupsCopy();
 
 function updateLocalStorageGroups(groups?:ComparisonSampleGroup[]) {
     if (!groups) {
         groups = observableGroupsCopy.get();
     }
-    localStorage.setItem(TEMP_localStorageGroupsKey, JSON.stringify(groups));
+    localStorage.setItem(LSGroupsKey, JSON.stringify(groups));
 }
 
-export function getLocalStorageGroups() {
+export function getLSChartGroupsSpec(key:string):FromChartSpec | null {
+    const lsData = localStorage.getItem(LSChartKey);
+    if (lsData) {
+        const groups = JSON.parse(lsData);
+        if (key in groups) {
+            return groups[key];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+}
+
+export function getLSGroups() {
     return observableGroupsCopy.get().slice();
 }
-
-export type SampleGroupWithoutId = Pick<ComparisonSampleGroup, Exclude<keyof ComparisonSampleGroup, "id">>;
 
 export function addGroupToLocalStorage(
     newGroup:SampleGroupWithoutId
 ) {
-    const groups:ComparisonSampleGroup[] = getLocalStorageGroups();
+    const groups:ComparisonSampleGroup[] = getLSGroups();
     groups.push(Object.assign({id:createGroupId(newGroup)}, newGroup));
     updateLocalStorageGroups(groups);
     updateObservableGroupsCopy();
+}
+
+export function addChartGroupsSpec(
+    clinicalAttribute:ClinicalAttribute,
+    values:string[],
+    filters:StudyViewFilter
+) {
+    const lsData = localStorage.getItem(LSChartKey);
+    let groups:{[key:string]:FromChartSpec};
+    if (lsData) {
+        groups = JSON.parse(lsData);
+    } else {
+        groups = {};
+    }
+    const key = hashString(JSON.stringify({ clinicalAttribute, values, filters})).toString();
+    groups[key] = {
+        clinicalAttribute, values, filters, created:Date.now()
+    };
+    localStorage.setItem(LSChartKey, JSON.stringify(groups));
+    return key
 }
 
 export function deleteGroups(
     groupIds:string[]
 ) {
     const idsToDelete = _.keyBy(groupIds);
-    const groups = getLocalStorageGroups();
+    const groups = getLSGroups();
     const filteredGroups = groups.filter(group=>!(group.id in idsToDelete));
     recentlyDeletedGroups = groups.filter(group=>(group.id in idsToDelete));
     updateLocalStorageGroups(filteredGroups);
@@ -84,7 +127,7 @@ export function addSamplesToGroup(
 function getGroupById(
     groupId:string
 ):ComparisonSampleGroup|undefined {
-    const allGroups = getLocalStorageGroups();
+    const allGroups = getLSGroups();
     return allGroups.find(group=>(group.id === groupId));
 }
 
@@ -93,3 +136,27 @@ function createGroupId(
 ) {
     return hashString(`${group.name}:${JSON.stringify(group.sampleIdentifiers)}:${Math.random()}`).toString();
 }
+
+function deleteExpiredGroups() {
+    const lsData = localStorage.getItem(LSChartKey);
+    if (lsData) {
+        const groups = JSON.parse(lsData);
+        // delete groups that are more than one week old
+        const weekAgo = new Date();
+        weekAgo.setDate(weekAgo.getDate()-7);
+        const weekAgoTime = weekAgo.getTime();
+
+        _.forEach(groups, (group, key)=>{
+            if ((group as FromChartSpec).created < weekAgoTime) {
+                delete groups[key];
+            }
+        });
+        localStorage.setItem(LSChartKey, JSON.stringify(groups));
+    }
+}
+
+/// ACTIONS TAKEN ON LOAD
+// initial update
+updateObservableGroupsCopy();
+
+deleteExpiredGroups();
