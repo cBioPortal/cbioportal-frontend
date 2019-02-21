0a4b94fd6c6b9606dba7c17b2e8a7dc1132dc230
Ino de Bruijn <ino@ino.pm>
2019-02-21 14:16:14 -0500

    e2e fixes exon/hgvsc column
    Former-commit-id: 018518e3eed1d9d71f390aa078ffc157ec3b7d1e

--- end-to-end-tests/specs/mutationTable.spec.js
+++ end-to-end-tests/specs/mutationTable.spec.js
@@ -12,13 +12,13 @@ function waitForGenomeNexusAnnotation() {
     browser.pause(5000);// wait for annotation
 }
 
-describe('Mutation Mapper Tool', function() {
+describe('Mutation Table', function() {
     before(function(){
         goToUrlAndSetLocalStorage(`${CBIOPORTAL_URL}`);
     });
 
-    describe('example genomic changes input', ()=>{
-        beforeEach(()=>{
+    describe('try getting exon and hgvsc info from genome nexus', ()=>{
+        before(()=>{
             var url = `${CBIOPORTAL_URL}/results/mutations?Action=Submit&Z_SCORE_THRESHOLD=1.0&cancer_study_id=gbm_tcga_pub&cancer_study_list=gbm_tcga_pub&case_ids=&case_set_id=gbm_tcga_pub_sequenced&clinicallist=PROFILED_IN_gbm_tcga_pub_cna_rae&gene_list=TP53%20MDM2%20MDM4&gene_set_choice=user-defined_list&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=gbm_tcga_pub_cna_rae&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=gbm_tcga_pub_mutations&show_samples=false`;
 
             goToUrlAndSetLocalStorage(url);
@@ -35,64 +35,49 @@ describe('Mutation Mapper Tool', function() {
             // click on column button
             browser.click("button*=Columns");
             // scroll down to activated "exon" selection
-            browser.scroll(0, 1000);
+            browser.scroll(1000, 1000);
             // click "exon"
             browser.click('//*[text()="Exon"]');
-            waitForNetworkQuiet();
-            // check if 3 exact matches for 6 appear
+            // check if three exact matches for 6 appear
+            browser.waitUntil(() => {
+                textValuesWith6 = browser.getText('//*[text()="6"]');
+                return textValuesWith6.length > 1;
+            }, 60000, "Exon data not in Exon column");
             textValuesWith6 = browser.getText('//*[text()="6"]');
-            assert.ok(textValuesWith6.length === 4);
+            assert.equal(textValuesWith6.length, 3);
         });
 
         it('should show more exon number after clicking "Show more"', ()=>{
-            // check if 6 appears once in COSMIC column
-            browser.waitForText('//*[text()="6"]',30000);
-            var textValuesWith6 = browser.getText('//*[text()="6"]');
-            assert.ok(textValuesWith6.length === 1);
-            // click on column button
-            browser.click("button*=Columns");
-            // scroll down to activated "exon" selection
-            browser.scroll(0, 1000);
-            // click "exon"
-            browser.click('//*[text()="Exon"]');
-            waitForNetworkQuiet();
             // click "show more" to add more data
             browser.click("button*=Show more");
-            waitForNetworkQuiet();
             // check if 6 exact matches for 6 appear
-            textValuesWith6 = browser.getText('//*[text()="6"]');
-            assert.ok(textValuesWith6.length === 6);
+            browser.waitUntil(() => {
+                textValuesWith6 = browser.getText('//*[text()="6"]');
+                return textValuesWith6.length === 6;
+            }, 20000, "Exon data not in Exon column after clikcing show more button");
         });
 
-        it('should show the HGVSc data after adding the HGVSc column', ()=>{           
-            // click on column button
+        it('should show the HGVSc data after adding the HGVSc column', ()=>{
+            // reopen columns
             browser.click("button*=Columns");
-            // scroll down to activated "HGVSc" selection
-            browser.scroll(0, 1000);
             // click "HGVSc"
-            browser.waitForText('//*[text()="HGVSc"]',30000);
             browser.click('//*[text()="HGVSc"]');
-            waitForNetworkQuiet();
+
             // check if "ENST00000269305.4:c.817C>T" appears
-            var hgvscValues = browser.getText('//*[text()[contains(.,"ENST00000269305.4:c.817C>T")]]');
-            assert.ok(hgvscValues.length > 0);
+            browser.waitUntil(() => {
+                var hgvscValues = browser.getText('//*[text()[contains(.,"ENST00000269305.4:c.817C>T")]]');
+                return hgvscValues.length > 0;
+            }, 20000, "HGVSc values not in hgvs column");
         });
 
-        it('should show more HGVSc data after clicking "Show more"', ()=>{           
-            // click on column button
-            browser.click("button*=Columns");
-            // scroll down to activated "HGVSc" selection
-            browser.scroll(0, 1000);
-            // click "HGVSc"
-            browser.waitForText('//*[text()="HGVSc"]',30000);
-            browser.click('//*[text()="HGVSc"]');
-            waitForNetworkQuiet();
+        it('should show more HGVSc data after clicking "Show more"', ()=>{
             // click "show more" to add more data
             browser.click("button*=Show more");
-            waitForNetworkQuiet();
             // check if "C>T" exact matches for 12 appear
-            var hgvscValues = browser.getText('//*[text()[contains(.,"C>T")]]');
-            assert.ok(hgvscValues.length === 12);
+            browser.waitUntil(() => {
+                var hgvscValues = browser.getText('//*[text()[contains(.,"C>T")]]');
+                return hgvscValues.length > 0;
+            }, 20000, "HGVSc values not in hgvs column after clicking show more button");
         });
     });
 
