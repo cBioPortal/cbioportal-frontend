22db5c058fab6b42d9d7b30bc4130dba5d34d4a4
Pim van Nierop <pim@thehyve.nl>
2019-03-05 08:53:40 +0100

    Change dependency to react-select-checked

--- src/shared/components/oncoprint/controls/OncoprintControls.tsx
+++ src/shared/components/oncoprint/controls/OncoprintControls.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {observer, Observer} from "mobx-react";
 import {Button, ButtonGroup} from "react-bootstrap";
 import CustomDropdown from "./CustomDropdown";
-import ReactSelect from "react-select";
+import ReactSelect from "react-select-checked";
 import {MobxPromise} from "mobxpromise";
 import {computed, IObservableObject, observable, ObservableMap, reaction} from "mobx";
 import _ from "lodash";
