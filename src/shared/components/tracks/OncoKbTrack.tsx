670a4602b9eaf3897a3bebc6b4bfa67fa3cde348
Onur Sumer <s.onur.sumer@gmail.com>
2019-03-05 17:34:25 -0500

    added PTM track for mutation mapper (fix https://github.com/cBioPortal/cbioportal/issues/5837)
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>
    
    Former-commit-id: d23c76a3f892dc41b498079ea42d9c5750d24a75

--- src/shared/components/tracks/OncoKbTrack.tsx
+++ src/shared/components/tracks/OncoKbTrack.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
+import * as _ from "lodash";
 import {observer} from "mobx-react";
 import {computed} from "mobx";
 
 import {Mutation} from "shared/api/generated/CBioPortalAPI";
 import {IOncoKbData} from "shared/model/OncoKB";
 import {defaultOncoKbIndicatorFilter, getIndicatorData} from "shared/lib/OncoKbUtils";
+import MutationMapperStore from "shared/components/mutationMapper/MutationMapperStore";
 import {default as Track, TrackProps} from "./Track";
+import {TrackItemSpec} from "./TrackCircle";
+import OncoKbTrackTooltip from "./OncoKbTrackTooltip";
+
 
 type OncoKbTrackProps = TrackProps & {
+    store: MutationMapperStore;
     oncoKbData?: IOncoKbData;
 };
 
@@ -34,8 +40,28 @@ function defaultFilter(d: Mutation[], oncoKbData?: IOncoKbData): boolean
 @observer
 export default class OncoKbTrack extends React.Component<OncoKbTrackProps, {}>
 {
-    constructor(props: OncoKbTrackProps) {
-        super(props);
+    @computed get oncoKbSpecs(): TrackItemSpec[] {
+        const filteredOncoKbDataByProteinPosStart = this.props.store.filteredOncoKbDataByProteinPosStart;
+
+        if(!_.isEmpty(filteredOncoKbDataByProteinPosStart)) {
+            return _.keys(filteredOncoKbDataByProteinPosStart)
+                .filter(position => Number(position) >= 0)
+                .map(position => ({
+                    codon: Number(position),
+                    color: "#007FFF",
+                    tooltip: (
+                        <OncoKbTrackTooltip
+                            mutations={this.props.store.filteredMutationsByPosition[Number(position)]}
+                            indicatorData={filteredOncoKbDataByProteinPosStart[Number(position)]}
+                            oncoKbData={this.props.oncoKbData}
+                            hugoGeneSymbol={this.props.store.gene.hugoGeneSymbol}
+                        />
+                    )
+                }));
+        }
+        else {
+            return [];
+        }
     }
 
     @computed get trackTitle() {
@@ -58,7 +84,7 @@ export default class OncoKbTrack extends React.Component<OncoKbTrackProps, {}>
                 xOffset={this.props.xOffset}
                 proteinLength={this.props.proteinLength}
                 trackTitle={this.trackTitle}
-                trackItems={this.props.trackItems}
+                trackItems={this.oncoKbSpecs}
                 dataHighlightFilter={(d: Mutation[]) => defaultFilter(d, this.props.oncoKbData)}
                 dataSelectFilter={(d: Mutation[]) => defaultFilter(d, this.props.oncoKbData)}
                 idClassPrefix={ONCOKB_ID_CLASS_PREFIX}
