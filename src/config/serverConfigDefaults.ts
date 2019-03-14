7f744274a04dec808f06f3f816bc3d8f3aa37854
Ino de Bruijn <ino@ino.pm>
2019-03-14 14:25:30 -0400

    Use quick search config for gene query's studylist
    Was a hardcoded list before
    
    
    Former-commit-id: 77b70bb39e91377797c4a5259b57892e7868a41d

--- src/config/serverConfigDefaults.ts
+++ src/config/serverConfigDefaults.ts
@@ -52,6 +52,17 @@ const ServerConfigDefaults: Partial<IServerConfig> = {
     skin_show_web_api_tab:true,
     skin_show_tweet_button:false,
     skin_show_tissue_image_tab:true,
+    skin_quick_search_show: false,
+    skin_quick_search_gene_query_cancer_study_list:
+        "laml_tcga_pan_can_atlas_2018,acc_tcga_pan_can_atlas_2018,blca_tcga_pan_can_atlas_2018," +
+        "lgg_tcga_pan_can_atlas_2018,brca_tcga_pan_can_atlas_2018,cesc_tcga_pan_can_atlas_2018,chol_tcga_pan_can_atlas_2018," +
+        "coadread_tcga_pan_can_atlas_2018,dlbc_tcga_pan_can_atlas_2018,esca_tcga_pan_can_atlas_2018,gbm_tcga_pan_can_atlas_2018," +
+        "hnsc_tcga_pan_can_atlas_2018,kich_tcga_pan_can_atlas_2018,kirc_tcga_pan_can_atlas_2018,kirp_tcga_pan_can_atlas_2018," +
+        "lihc_tcga_pan_can_atlas_2018,luad_tcga_pan_can_atlas_2018,lusc_tcga_pan_can_atlas_2018,meso_tcga_pan_can_atlas_2018," +
+        "ov_tcga_pan_can_atlas_2018,paad_tcga_pan_can_atlas_2018,pcpg_tcga_pan_can_atlas_2018,prad_tcga_pan_can_atlas_2018," +
+        "sarc_tcga_pan_can_atlas_2018,skcm_tcga_pan_can_atlas_2018,stad_tcga_pan_can_atlas_2018," +
+        "tgct_tcga_pan_can_atlas_2018,thym_tcga_pan_can_atlas_2018,thca_tcga_pan_can_atlas_2018,ucs_tcga_pan_can_atlas_2018," +
+        "ucec_tcga_pan_can_atlas_2018,uvm_tcga_pan_can_atlas_2018",
     skin_title:"cBioPortal for Cancer Genomics",
 
     skin_blurb:`The cBioPortal for Cancer Genomics provides 
