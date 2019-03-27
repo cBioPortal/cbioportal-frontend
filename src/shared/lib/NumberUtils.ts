2e5ebac64e1ed6ae0deea8298f22c07e72d2a4b4
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-03-27 16:55:45 -0400

    Use superscript for power instead of ^ notation
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/shared/lib/NumberUtils.ts
+++ src/shared/lib/NumberUtils.ts
@@ -6,16 +6,3 @@ export function toConditionalPrecision(number: number, precision: number, thresh
 
     return number.toPrecision(precision);
 };
-
-export function toConditionalPrecisionWithMinimum(
-    positiveNumber:number,
-    precision:number,
-    precisionThreshold:number,
-    minimumExponent:number
-):string {
-    if (positiveNumber === 0 || Math.log10(positiveNumber) < minimumExponent) {
-        return `< 10^${minimumExponent}`;
-    } else {
-        return toConditionalPrecision(positiveNumber, precision, precisionThreshold);
-    }
-}
