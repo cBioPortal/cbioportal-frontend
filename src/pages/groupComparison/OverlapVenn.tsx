56345f14ba82ca8bd77cfb38002ce35fef112981
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-03-28 16:42:33 -0400

    Rewrite Overlap Venn diagram so that we can highlight set complements
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 9128516930df578e7105e7dac08d32773fd74f8f

--- src/pages/groupComparison/OverlapVenn.tsx
+++ src/pages/groupComparison/OverlapVenn.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { observer } from "mobx-react";
-import * as d3 from 'd3';
-import autobind from 'autobind-decorator';
-const venn = require('venn.js');
 import { VictoryLabel, VictoryLegend } from 'victory';
 import CBIOPORTAL_VICTORY_THEME from 'shared/theme/cBioPoralTheme';
 import _ from "lodash";
 import { computed } from 'mobx';
-import {ComparisonGroup, getCombinations, getVennPlotData} from './GroupComparisonUtils';
+import {ComparisonGroup} from './GroupComparisonUtils';
+import VennSimple from "./VennSimple";
 
 export interface IVennProps {
     svgId?: string;
@@ -22,99 +20,28 @@ export interface IVennProps {
     uidToGroup: { [uid: string]: ComparisonGroup };
 }
 
-const VENN_PLOT_WIDTH = 250;
-const PADDING_BTWN_SAMPLE_AND_PATIENT = 50;
-const VENN_PLOT_HEIGHT = 200;
+const VENN_PLOT_WIDTH = 400;
+const PADDING_BTWN_SAMPLE_AND_PATIENT = 10;
+const VENN_PLOT_HEIGHT = 400;
+const PADDING_BTWN_VENN_AND_LEGEND = 20;
+const LEGEND_WIDTH = 180;
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            feDropShadow:any; // for some reason typescript doesnt know about this SVG tag
+        }
+    }
+}
 
 @observer
 export default class Venn extends React.Component<IVennProps, {}> {
-
-    constructor(props: IVennProps, context: any) {
-        super(props, context);
-    }
-
-    componentDidMount() {
-        this.createVennDiagram(this.sampleSets, 'sampleVennDiagram')
-        this.createVennDiagram(this.patientSets, 'patientVennDiagram')
-    }
-
-    componentDidUpdate() {
-        this.createVennDiagram(this.sampleSets, 'sampleVennDiagram')
-        this.createVennDiagram(this.patientSets, 'patientVennDiagram')
-    }
-
-    @computed get sampleSets() {
-        return getVennPlotData(getCombinations(this.props.sampleGroups));
-    }
-
-    @computed get patientSets() {
-        return getVennPlotData(getCombinations(this.props.patientGroups));
-    }
-
-    @autobind
-    getColor(categories: string[]) {
-        return categories.length === 1 ? this.props.uidToGroup[categories[0]].color : undefined
-    }
-
-    @autobind
-    private createVennDiagram(sets: {
-        label: string;
-        size: number;
-        sets: string[];
-    }[], id: string) {
-        let self = this
-
-        let vennDiagram = venn.VennDiagram();
-        vennDiagram.width(VENN_PLOT_WIDTH);
-        vennDiagram.height(VENN_PLOT_HEIGHT);
-        vennDiagram.padding(5);
-        vennDiagram.fontSize('14px');
-
-        let vennDiagramDiv = d3.select(`#${id}`)
-        vennDiagramDiv.datum(sets).call(vennDiagram);
-
-        vennDiagramDiv.selectAll(".venn-circle path")
-            .style("fill-opacity", .8)
-            .style("fill", function (d: any) { return self.getColor(d.sets); });
-
-        vennDiagramDiv.selectAll(".venn-intersection text")
-            .style("font-size", '10px');
-
-        vennDiagramDiv.selectAll("text")
-            .style("fill", "black")
-
-        vennDiagramDiv.selectAll("path")
-            .style("stroke-opacity", 0)
-            .style("stroke", "#fff")
-            .style("stroke-width", 3);
-
-        vennDiagramDiv.selectAll("g")
-            .on("mouseover", function (d: any) {
-                // highlight the current path
-                d3.select(this)
-                    .select("path")
-                    .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
-                    .style("stroke-opacity", 1);
-            })
-            .on("mouseout", function (d: any) {
-                d3.select(this)
-                    .select("path")
-                    .style("fill-opacity", d.sets.length == 1 ? .8 : .0)
-                    .style("stroke-opacity", 0);
-            });
-
-    }
-
     @computed get chartWidth() {
-        return this.vennPlotAreaWidth + 200;
+        return this.vennPlotAreaWidth + PADDING_BTWN_VENN_AND_LEGEND + LEGEND_WIDTH;
     }
 
     @computed get chartHeight() {
         return 500;
-    }
-
-    @computed get topPadding() {
-        return 100
     }
 
     @computed get legendData() {
@@ -136,21 +63,20 @@ export default class Venn extends React.Component<IVennProps, {}> {
     }
 
     public render() {
-        return (<div
-            style={{ width: this.chartWidth, height: this.chartHeight }}
-        >
+        return (
             <svg
                 id={this.props.svgId || ""}
-                style={{
-                    width: this.chartWidth,
-                    height: this.chartHeight,
-                    pointerEvents: "all"
-                }}
-                height={this.chartHeight}
+                xmlns="http://www.w3.org/2000/svg"
                 width={this.chartWidth}
+                height={this.chartHeight}
                 role="img"
                 viewBox={`0 0 ${this.chartWidth} ${this.chartHeight}`}
             >
+                <defs>
+                    <filter x="0" y="0" width="100" height="100" id="caseCountBackground">
+                        <feDropShadow as any dx="0" dy="0" floodColor="white" stdDeviation="2" floodOpacity="0.5"/>
+                    </filter>
+                </defs>
                 <VictoryLabel
                     style={{
                         fontWeight: "bold",
@@ -161,7 +87,16 @@ export default class Venn extends React.Component<IVennProps, {}> {
                     y="1.2em"
                     text={'Samples overlap'}
                 />
-                <g id="sampleVennDiagram" transform={`translate(0,${this.topPadding})`} />
+                <VennSimple
+                    x={0}
+                    y={15}
+                    groups={this.props.sampleGroups}
+                    uidToGroup={this.props.uidToGroup}
+                    width={VENN_PLOT_WIDTH}
+                    height={VENN_PLOT_HEIGHT}
+                    caseCountFilterName="caseCountBackground"
+                    onClickRegion={()=>{}}
+                />
 
                 <VictoryLabel
                     style={{
@@ -174,18 +109,28 @@ export default class Venn extends React.Component<IVennProps, {}> {
                     y="1.2em"
                     text={'Patients overlap'}
                 />
-                <g id="patientVennDiagram" transform={`translate(${VENN_PLOT_WIDTH+PADDING_BTWN_SAMPLE_AND_PATIENT},${this.topPadding})`} />
+
+                <VennSimple
+                    x={VENN_PLOT_WIDTH+PADDING_BTWN_SAMPLE_AND_PATIENT}
+                    y={15}
+                    groups={this.props.patientGroups}
+                    uidToGroup={this.props.uidToGroup}
+                    width={VENN_PLOT_WIDTH}
+                    height={VENN_PLOT_HEIGHT}
+                    caseCountFilterName="caseCountBackground"
+                    onClickRegion={()=>{}}
+                />
 
                 {this.legendData.length > 0 && (
                     <VictoryLegend
-                        x={this.vennPlotAreaWidth}
-                        y={this.topPadding}
+                        x={this.vennPlotAreaWidth + PADDING_BTWN_VENN_AND_LEGEND}
+                        y={100}
                         theme={CBIOPORTAL_VICTORY_THEME}
                         standalone={false}
                         data={this.legendData}
                     />
                 )}
             </svg>
-        </div>)
+        );
     }
 }
