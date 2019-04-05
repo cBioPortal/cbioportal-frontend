0aaa81e56c1152ec922e14277275e98082cc9a41
Onur Sumer <s.onur.sumer@gmail.com>
2019-04-05 16:41:36 -0400

    Updated data store interface

--- packages/react-mutation-mapper/src/model/DataStore.ts
+++ packages/react-mutation-mapper/src/model/DataStore.ts
@@ -3,7 +3,9 @@ export interface DataStore {
     setPositionHighlighted: (position:number, newVal:boolean) => void;
     clearSelectedPositions: () => void;
     clearHighlightedPositions: () => void;
+    setDataSelectFilter?: (dataSelectorFilter: <T>(d: T) => boolean) => void;
     clearDataSelectFilter: () => void;
+    setDataHighlightFilter?: (dataHighlightFilter: <T>(d: T) => boolean) => void;
     clearDataHighlightFilter: () => void;
     isPositionSelected: (position:number) => boolean;
     isPositionHighlighted: (position:number) => boolean;
