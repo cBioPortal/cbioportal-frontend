cd631fd5a57f7723bd36ab87041f2efae10a2435
aaronlisman <lismanBusiness@gmail.com>
2019-04-08 12:17:09 -0400

    Update patient view screenshot

--- end-to-end-tests/wdio/webdriver-manager.conf.js
+++ end-to-end-tests/wdio/webdriver-manager.conf.js
@@ -45,7 +45,7 @@ var config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 1,
+    maxInstances: 5,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
