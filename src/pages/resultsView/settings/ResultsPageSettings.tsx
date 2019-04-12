a75da5ce4f12c24b26c7b5b5b3a76b8f12ef3313
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-04-12 15:17:22 -0400

    Improve UI of global settings menu
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/pages/resultsView/settings/ResultsPageSettings.tsx
+++ src/pages/resultsView/settings/ResultsPageSettings.tsx
@@ -9,7 +9,13 @@ import DriverAnnotationControls, {
     IDriverAnnotationControlsState
 } from "./DriverAnnotationControls";
 import {IObservableObject} from "mobx";
-import {buildDriverAnnotationControlsHandlers, buildDriverAnnotationControlsState} from "./ResultsPageSettingsUtils";
+import {
+    boldedTabList,
+    buildDriverAnnotationControlsHandlers,
+    buildDriverAnnotationControlsState
+} from "./ResultsPageSettingsUtils";
+import CustomDropdown from "../../../shared/components/oncoprint/controls/CustomDropdown";
+import InfoIcon from "../../../shared/components/InfoIcon";
 
 export interface IResultsPageSettingsProps {
     store:ResultsViewPageStore;
@@ -17,7 +23,10 @@ export interface IResultsPageSettingsProps {
 
 enum EVENT_KEY {
     hidePutativePassengers="0",
-    showGermlineMutations="1"
+    showGermlineMutations="1",
+
+    dataTypeSample="2",
+    dataTypePatient="3"
 }
 
 @observer
@@ -44,52 +53,53 @@ export default class ResultsPageSettings extends React.Component<IResultsPageSet
             case EVENT_KEY.showGermlineMutations:
                 this.props.store.setShowGermlineMutations(!this.props.store.showGermlineMutations);
                 break;
+            case EVENT_KEY.dataTypeSample:
+                this.props.store.setCaseType("sample");
+                break;
+            case EVENT_KEY.dataTypePatient:
+                this.props.store.setCaseType("patient");
+                break;
         }
     }
 
     render() {
         return (
-            <div className="cbioportal-frontend" style={{padding:5}}>
-                <h4>Settings</h4>
-                <div
-                    style={{
-                        padding:5,
-                        display:"flex",
-                        alignItems:"center"
-                    }}
-                >
-                    <span style={{
-                        width:60
-                    }}>
-                        Case type:
-                    </span>
-                    <div
-                        style={{
-                            width:170,
-                            marginLeft:10
-                        }}
-                    >
-                        <ReactSelect
-                            value={{label:capitalize(this.props.store.caseType), value:this.props.store.caseType}}
-                            options={[{
-                                label:"Patient",
-                                value:"patient"
-                            }, {
-                                label:"Sample",
-                                value:"sample"
-                            }]}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                </div>
+            <div id="globalSettingsDropdown" className="cbioportal-frontend" style={{padding:5}}>
+                <h5>Data Type</h5>
+                <InfoIcon style={{color:"rgb(54, 134, 194)"}} tooltip={<span>This setting affects the following tabs: {boldedTabList(["Oncoprint"])}</span>}/>
+                <div className="radio"><label>
+                    <input
+                        type="radio"
+                        value={EVENT_KEY.dataTypeSample}
+                        checked={this.props.store.caseType === "sample"}
+                        onClick={this.onInputClick}
+                    /> Events per sample
+                </label></div>
+                <div className="radio"><label>
+                    <input
+                        type="radio"
+                        value={EVENT_KEY.dataTypePatient}
+                        checked={this.props.store.caseType === "patient"}
+                        onClick={this.onInputClick}
+                    /> Events per patient
+                </label></div>
+                <hr/>
 
                 <h5>Annotate Data</h5>
+                <InfoIcon style={{color:"rgb(54, 134, 194)"}} tooltip={
+                    <span>These settings affect the following tabs: {boldedTabList(["Oncoprint", "Mutual Exclusivity", "Mutations", "Enrichments", "Survival"])}</span>
+                }/>
                 <DriverAnnotationControls
                     state={this.driverSettingsState}
                     handlers={this.driverSettingsHandlers}
                 />
 
+                <hr/>
+
                 <h5>Filter Data</h5>
+                <InfoIcon style={{color:"rgb(54, 134, 194)"}} tooltip={
+                    <span>These settings affect the following tabs: {boldedTabList(["Oncoprint", "Mutual Exclusivity", "Mutations", "Enrichments", "Survival"])}</span>
+                }/>
                 <div style={{marginLeft:10}}>
                     <div className="checkbox"><label>
                         <input
