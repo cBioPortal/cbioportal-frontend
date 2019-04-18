2593877262ebb71fc66af91d268a5864c29ad559
Xiang Li <xlvv9@mail.missouri.edu>
2019-04-18 17:56:27 -0400

    gnomad table

---
+++ src/shared/components/mutationTable/column/GnomadColumnFormatter.tsx
@@ -0,0 +1,262 @@
+import * as React from 'react';
+import {Circle} from "better-react-spinkit";
+import * as _ from 'lodash';
+import 'rc-tooltip/assets/bootstrap_white.css';
+import styles from "./gnomad.module.scss";
+import generalStyles from "./styles.module.scss";
+import {Mutation} from "shared/api/generated/CBioPortalAPI";
+import DefaultTooltip from 'shared/components/defaultTooltip/DefaultTooltip';
+import {default as TableCellStatusIndicator, TableCellStatus} from "shared/components/TableCellStatus";
+import {MyVariantInfo, MyVariantInfoAnnotation, Gnomad, AlleleCount, AlleleNumber, Homozygotes, AlleleFrequency} from 'shared/api/generated/GenomeNexusAPI';
+import GenomeNexusCache, { GenomeNexusCacheDataType } from "shared/cache/GenomeNexusCache";
+import GnomadFrequencyTable from 'shared/components/gnomad/GnomadFrequencyTable';
+
+export type GnomadData = {
+    'population': string
+
+        'alleleCount': number
+
+        'alleleNumber': number
+
+        'homezygotes': number
+
+        'alleleFrequency': number
+};
+
+const PopulationName: {[key:string]: string} = {
+    "African" : "afr",
+        
+        "Latino" : "amr",
+
+        "Other" : "oth",
+
+        "European (Non-Finnish)" : "nfe",
+
+        "European (Finnish)" : "fin",
+
+        "Ashkenazi Jewish" : "asj",
+
+        "East Asian" : "eas",
+
+        "South Asian" : "sas",
+
+        "Total" : ""
+}
+
+export enum ColumnName {
+    population = 'population',
+        
+        alleleCount = 'ac',
+    
+        alleleNumber = 'an',
+    
+        homezygotes = 'hom',
+    
+        alleleFrequency = 'af'
+}
+
+export default class GnomadColumnFormatter {
+
+    public static renderFunction(data:Mutation[],
+                                 genomeNexusCache:GenomeNexusCache|undefined) {
+        const genomeNexusCacheData = GnomadColumnFormatter.getGenomeNexusDataFromCache(data, genomeNexusCache);
+        return (
+            <div>
+                <span>{GnomadColumnFormatter.getGnomadDataViz(genomeNexusCacheData)}</span>
+            </div>
+        );
+    }
+
+    private static getGenomeNexusDataFromCache(data:Mutation[], cache:GenomeNexusCache|undefined):GenomeNexusCacheDataType | null {
+        
+        if (data.length === 0 || !cache) {
+            return null;
+        }
+        return cache.get(data[0]);
+    }
+
+    private static getGnomadDataViz(genomeNexusCacheData:GenomeNexusCacheDataType|null) {
+        let status:TableCellStatus | null = null;
+
+        if (genomeNexusCacheData == null) {
+            status = TableCellStatus.LOADING;
+        } else if (genomeNexusCacheData.status === "error") {
+            status = TableCellStatus.ERROR;
+        } else if (genomeNexusCacheData.data == null) {
+            status = TableCellStatus.NA;
+        } else {
+
+            const gnomadData = GnomadColumnFormatter.getData(genomeNexusCacheData.data.my_variant_info);
+
+            let display:string = "";
+            let overlay:(() => JSX.Element)|null = null;
+            let content:JSX.Element;
+            let result : {[key:string]: GnomadData} = {};
+            
+            // Checking if gnomad data is valid
+            if (gnomadData && (gnomadData.gnomadExome || gnomadData.gnomadGenome)) {
+                const gnomadExome : {[key:string]: GnomadData} = {};
+                const gnomadGenome : {[key:string]: GnomadData} = {};
+                const gnomadResult : {[key:string]: GnomadData} = {};
+
+                // If only gnomadExome data exist, show gnomadExome result in the table
+                if (gnomadData.gnomadExome) {
+                    Object.keys(PopulationName).map(key => {
+                        this.setGnomadTableData(key, gnomadData.gnomadExome, gnomadExome);
+                    })
+                    result = gnomadExome;
+                }
+
+                // If only gnomadGenome data exist, show gnomadGenome result in the table
+                if (gnomadData.gnomadGenome) {
+                    Object.keys(PopulationName).map(key => {
+                        this.setGnomadTableData(key, gnomadData.gnomadGenome, gnomadGenome);
+                    })
+                    result = gnomadGenome;
+                }
+
+                // If both gnomadExome and gnomadGenome exist, combine gnomadExome and gnomadGenome together
+                if (gnomadData.gnomadExome && gnomadData.gnomadGenome) {
+                    Object.keys(PopulationName).map(key => {
+                        gnomadResult[key] = {
+                            'population': key,
+                            'alleleCount': gnomadExome[key].alleleCount + gnomadGenome[key].alleleCount,
+                            'alleleNumber':gnomadExome[key].alleleNumber + gnomadGenome[key].alleleNumber,
+                            'homezygotes': gnomadExome[key].homezygotes + gnomadGenome[key].homezygotes,
+                            'alleleFrequency': parseFloat(((gnomadExome[key].alleleCount + gnomadGenome[key].alleleCount) / 
+                                            (gnomadExome[key].alleleNumber + gnomadGenome[key].alleleNumber)).toFixed(10))
+                        } as GnomadData;
+                    })
+                    result = gnomadResult;
+                }
+
+                // The column will show the frequency in total
+                display = parseFloat(result["Total"].alleleFrequency.toFixed(10)).toString();
+                
+                overlay = () => (
+                    <span className={styles["gnomad-table"]}>
+                        <GnomadFrequencyTable data={Object.values(result)}/>
+                    </span>
+                    
+                );
+            }
+
+            // if there is no gnomad data, show empty in the column
+            else {
+                display = "";
+            }
+
+            content = (
+                <div className={generalStyles["integer-data"]}>
+                    {display}
+                </div>
+            );
+
+            // add a tooltip if the gnomad value is valid
+            if (overlay)
+            {
+                content = (
+                    <DefaultTooltip
+                        overlay={overlay}
+                        placement="topLeft"
+                        trigger={['hover', 'focus']}
+                        destroyTooltipOnHide={true}
+                    >
+                        {content}
+                    </DefaultTooltip>
+                );
+            }
+
+            return content;
+            
+        }
+
+            if (status !== null) {
+                // show loading circle
+                if (status === TableCellStatus.LOADING) {
+                    return <Circle size={18} scaleEnd={0.5} scaleStart={0.2} color="#aaa" className="pull-right"/>;
+                } 
+                else {
+                    return <TableCellStatusIndicator status={status}/>;
+                }
+            }
+    }
+
+    public static getData(genomeNexusData: MyVariantInfoAnnotation | null): MyVariantInfo | null
+    {
+        if (!genomeNexusData)
+        {
+            return null;
+        }
+        return genomeNexusData.annotation;
+    }
+
+    public static setGnomadTableData (key: string, data: Gnomad, result: {[key:string]: GnomadData}) {
+
+        // Access data by population name and column name in the format of: columnName_populationName, e.g. "ac_afr"
+        const alleleCountName : keyof AlleleCount = (PopulationName[key] ? ColumnName.alleleCount + "_" + PopulationName[key] : ColumnName.alleleCount).toString() as keyof AlleleCount;
+        const alleleNumberName : keyof AlleleNumber = (PopulationName[key] ? ColumnName.alleleNumber + "_" + PopulationName[key] : ColumnName.alleleNumber).toString() as keyof AlleleNumber;
+        const homozygotesName : keyof Homozygotes = (PopulationName[key] ? ColumnName.homezygotes + "_" + PopulationName[key] : ColumnName.homezygotes).toString() as keyof Homozygotes;
+        const alleleFrequencyName : keyof AlleleFrequency = (PopulationName[key] ? ColumnName.alleleFrequency + "_" + PopulationName[key] : ColumnName.alleleFrequency).toString() as keyof AlleleFrequency;
+
+        result[key] = 
+        {
+            'population' : key,
+            'alleleCount': data.alleleCount[alleleCountName],
+            'alleleNumber': data.alleleNumber[alleleNumberName],
+            'homezygotes': data.homozygotes[homozygotesName],
+            'alleleFrequency': data.alleleFrequency[alleleFrequencyName]
+        } as GnomadData;
+    }
+
+    public static getSortValue(data:Mutation[], genomeNexusCache:GenomeNexusCache): number|null {
+        const genomeNexusCacheData = GnomadColumnFormatter.getGenomeNexusDataFromCache(data, genomeNexusCache);
+        if (genomeNexusCacheData && genomeNexusCacheData.data) {
+            let gnomadData = GnomadColumnFormatter.getData(genomeNexusCacheData.data.my_variant_info);
+
+            // If has both gnomadExome and gnomadGenome, sort by the total frequency
+            if (gnomadData && gnomadData.gnomadExome && gnomadData.gnomadGenome) {
+                return parseFloat(((gnomadData.gnomadExome.alleleCount.ac + gnomadData.gnomadGenome.alleleCount.ac) /
+                                    (gnomadData.gnomadExome.alleleNumber.an + gnomadData.gnomadGenome.alleleNumber.an)).toFixed(10));               
+            }
+
+            // If only has gnomadExome, sort by gnomadExome frequency
+            if (gnomadData && gnomadData.gnomadExome) {
+                return parseFloat(gnomadData.gnomadExome.alleleFrequency.af.toString());
+            }
+
+            // If only has gnomadGenome, sort by gnomadGenome frequency
+            if (gnomadData && gnomadData.gnomadGenome) {
+                return parseFloat(gnomadData.gnomadGenome.alleleFrequency.af.toString());
+            }
+        }
+        // If genomeNexusCacheData is null or gnomadData is null, return null
+        return null;
+
+    }
+
+    public static download(data:Mutation[], genomeNexusCache:GenomeNexusCache): string
+    {
+        const genomeNexusCacheData = GnomadColumnFormatter.getGenomeNexusDataFromCache(data, genomeNexusCache);
+
+        if (genomeNexusCacheData && genomeNexusCacheData.data) {
+            let gnomadData = GnomadColumnFormatter.getData(genomeNexusCacheData.data.my_variant_info);
+
+            if (gnomadData && gnomadData.gnomadExome && gnomadData.gnomadGenome) {
+                return ((gnomadData.gnomadExome.alleleCount.ac + gnomadData.gnomadGenome.alleleCount.ac) /
+                        (gnomadData.gnomadExome.alleleNumber.an + gnomadData.gnomadGenome.alleleNumber.an)).toFixed(10);               
+            }
+
+            if (gnomadData && gnomadData.gnomadExome) {
+                return gnomadData.gnomadExome.alleleFrequency.af.toString();
+            }
+
+            if (gnomadData && gnomadData.gnomadGenome) {
+                return gnomadData.gnomadGenome.alleleFrequency.af.toString();
+            }
+        }
+        
+        return "";
+
+    }
+} 
