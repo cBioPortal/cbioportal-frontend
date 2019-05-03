5b5f4358537c0e79a1999806a1bb543f9d109c1d
Xiang Li <xlvv9@mail.missouri.edu>
2019-05-03 14:07:13 -0400

    bold header bottom border

--- src/shared/components/gnomad/GnomadFrequencyTable.tsx
+++ src/shared/components/gnomad/GnomadFrequencyTable.tsx
@@ -1,19 +1,10 @@
 import * as React from 'react';
 import { GnomadData, frequencyOutput} from '../mutationTable/column/GnomadColumnFormatter';
-import {Column, default as LazyMobXTable} from "../lazyMobXTable/LazyMobXTable";
 import SimpleTable from '../simpleTable/SimpleTable';
 export interface IGnomadFrequencyTableProps
 {
     data: GnomadData[];
-    columns?: Array<Column<GnomadData>>;
-    initialSortColumn?: string;
-    initialSortDirection?: 'asc'|'desc';
-    initialItemsPerPage?: number;
 }
-
-// LazyMobXTable is a generic component which requires data type argument
-class GnomadTable extends LazyMobXTable<GnomadData> {}
-
 export default class GnomadFrequencyTable extends React.Component<IGnomadFrequencyTableProps, {}>
 {
     constructor(props: IGnomadFrequencyTableProps)
@@ -36,30 +27,21 @@ export default class GnomadFrequencyTable extends React.Component<IGnomadFrequen
     private getRows(): JSX.Element[] {
         const ret: JSX.Element[] = [];
         this.props.data.forEach(d => {
-            ret.push(
-
-            <tr>
-                <td>{d.population}</td>
-                <td>{d.alleleCount}</td>
-                <td>{d.alleleNumber}</td>
-                <td>{d.homezygotes}</td>
-                <td>{frequencyOutput(d.alleleFrequency)}</td>
-            </tr>
-            );
+                ret.push(
+                    <tr>
+                        <td>{d.population}</td>
+                        <td>{d.alleleCount}</td>
+                        <td>{d.alleleNumber}</td>
+                        <td>{d.homezygotes}</td>
+                        <td>{frequencyOutput(d.alleleFrequency)}</td>
+                    </tr>
+                    );
         });
         return ret;
     }
 
     public render()
     {
-        const {
-            data,
-            columns,
-            initialSortColumn,
-            initialSortDirection,
-            initialItemsPerPage,
-        } = this.props;
-
         return (
             <div className='cbioportal-frontend'>
                 <SimpleTable
