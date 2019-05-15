3a9a93259d474b8ba951bc3f6d2d15289629f133
Pim van Nierop <pim@thehyve.nl>
2019-05-15 11:33:31 +0200

    WIP

--- end-to-end-tests/local_database/runtime-config/run_screenshot_test.sh
+++ end-to-end-tests/local_database/runtime-config/run_screenshot_test.sh
@@ -9,7 +9,7 @@ CUR_DIR=$PWD
 cp $TEST_HOME/local_database/docker/Dockerfile.screenshottest $PORTAL_SOURCE_DIR
 cd $PORTAL_SOURCE_DIR
 docker build -f Dockerfile.screenshottest -t cbioportal:screenshot .
-docker run --network="$DOCKER_NETWORK_NAME" -e CBIOPORTAL_URL=$CBIOPORTAL_URL cbioportal:screenshot
+docker run --rm -ti --name screenshot-e2e --network="$DOCKER_NETWORK_NAME" -e CBIOPORTAL_URL="$CBIOPORTAL_URL" -e DIFF_DIRECTORY="$TEST_HOME/screenshots/$DIFF_SCREENSHOT_DIR_NAME" cbioportal:screenshot
 
 cd $CUR_DIR
 exit 0
