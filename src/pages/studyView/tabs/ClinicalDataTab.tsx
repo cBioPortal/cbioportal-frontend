15a9331997e69205c9587c6f94ec00d6090a42e1
Hongxin Zhang <jackson.zhang.828@gmail.com>
2019-05-15 16:20:53 -0400

    Only show cancer study column in clinical data type if the chart/column is visible
    Former-commit-id: 4a56896e6d5e00d5ca6698ef234dc7804a132130

--- src/pages/studyView/tabs/ClinicalDataTab.tsx
+++ src/pages/studyView/tabs/ClinicalDataTab.tsx
@@ -3,14 +3,13 @@ import {Column, default as LazyMobXTable} from "shared/components/lazyMobXTable/
 import {observer} from "mobx-react";
 import * as _ from 'lodash';
 import {getPatientViewUrl, getSampleViewUrl} from "shared/api/urls";
-import {getClinicalAttributeOverlay, getClinicalAttributeUniqueKey, chartMetaComparator} from "../StudyViewUtils";
+import {chartMetaComparator, getClinicalAttributeOverlay, getClinicalAttributeUniqueKey} from "../StudyViewUtils";
 import LoadingIndicator from "shared/components/loadingIndicator/LoadingIndicator";
-import {ChartMeta, StudyViewPageStore} from "pages/studyView/StudyViewPageStore";
+import {ChartMeta, StudyViewPageStore, UniqueKey} from "pages/studyView/StudyViewPageStore";
 import {remoteData} from "shared/api/remoteData";
 import {Else, If, Then} from 'react-if';
 import ProgressIndicator, {IProgressIndicatorItem} from "../../../shared/components/progressIndicator/ProgressIndicator";
 import autobind from 'autobind-decorator';
-import windowStore from "../../../shared/components/window/WindowStore";
 import {WindowWidthBox} from "../../../shared/components/WindowWidthBox/WindowWidthBox";
 
 export interface IClinicalDataTabTable {
@@ -57,9 +56,13 @@ export class ClinicalDataTab extends React.Component<IClinicalDataTabTable, {}> 
                 render: (data: { [id: string]: string }) => {
                     return <a href={getSampleViewUrl(data.studyId, data.sampleId)} target='_blank'>{data.sampleId}</a>
                 }
-            }, {
-                ...this.getDefaultColumnConfig('studyId', 'Cancer Study')
             }];
+
+            if (_.find(this.props.store.visibleAttributes, chartMeta => chartMeta.uniqueKey === UniqueKey.CANCER_STUDIES) !== undefined) {
+                defaultColumns.push({
+                    ...this.getDefaultColumnConfig('studyId', 'Cancer Study')
+                });
+            }
             return _.reduce(this.props.store.visibleAttributes.sort(chartMetaComparator),
                 (acc: Column<{ [id: string]: string }>[], chartMeta: ChartMeta, index: number) => {
                     if (chartMeta.clinicalAttribute !== undefined) {
