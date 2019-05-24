1176e7c11ebf13bfcdd01430b02f11041162a8e5
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-05-24 17:33:19 -0400

    fix layout for alert headers on group comparison page
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 49e2ab67f706256629e1f4522323114ce90b7649

--- src/pages/groupComparison/GroupComparisonPage.tsx
+++ src/pages/groupComparison/GroupComparisonPage.tsx
@@ -240,9 +240,8 @@ export default class GroupComparisonPage extends React.Component<IGroupCompariso
         const pluralUnsaved = this.store.unsavedGroupNamesWithOrdinal.length > 1;
 
         if (this.store.unsavedGroupNamesWithOrdinal.length > 0) {
-            return [
-                <br/>,
-                <div className="alert alert-warning" style={{display:"inline-flex", marginBottom:3, marginTop:7}}>
+            return (
+                <div className="alert alert-warning" style={{display:"flex", marginBottom:3, marginTop:7}}>
                     <i className="fa fa-md fa-exclamation-triangle" style={{marginRight:12, marginTop:3}}/>
                     <div style={{maxWidth:500, display:"inline-block", marginRight: 6}}>
                         {joinNames(this.store.unsavedGroupNamesWithOrdinal, "and")} {pluralUnsaved ? "are" : "is"} not saved. Others visiting this link will not see {pluralUnsaved ? "them" : "it"}.
@@ -265,7 +264,7 @@ export default class GroupComparisonPage extends React.Component<IGroupCompariso
                         </button>
                     </div>
                 </div>
-            ];
+            );
         } else {
             return null;
         }
@@ -274,7 +273,7 @@ export default class GroupComparisonPage extends React.Component<IGroupCompariso
     @computed get unsavedOrderWarning() {
         if (this.store.dragUidOrder) {
             return (
-                <div className="alert alert-warning" style={{display:"inline-flex", marginBottom:3}}>
+                <div className="alert alert-warning" style={{display:"flex", marginBottom:3, marginTop:7}}>
                     <i className="fa fa-md fa-exclamation-triangle" style={{marginRight:12, marginTop:3}}/>
                     <div style={{maxWidth:500, display:"inline-block", marginRight: 6}}>
                         Your group order is not saved.
