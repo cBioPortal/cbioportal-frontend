c48b5e7c70694eda2f2524b02617ecf470db139d
Karthik Kalletla <kalletlakarthik@gmail.com>
2019-06-04 09:06:27 -0400

    Support multi-study for mutation and copy-number enrichments
    Former-commit-id: 7790f465a8531f9158a2a66eecaa8fff905a7101

--- src/pages/resultsView/enrichments/CopyNumberEnrichmentsTab.tsx
+++ src/pages/resultsView/enrichments/CopyNumberEnrichmentsTab.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { observer } from "mobx-react";
 import { ResultsViewPageStore } from "../ResultsViewPageStore";
-import { observable } from 'mobx';
 import AlterationEnrichmentContainer from 'pages/resultsView/enrichments/AlterationEnrichmentsContainer';
 import Loader from 'shared/components/loadingIndicator/LoadingIndicator';
 import EnrichmentsDataSetDropdown from 'pages/resultsView/enrichments/EnrichmentsDataSetDropdown';
 import { MolecularProfile } from 'shared/api/generated/CBioPortalAPI';
 import autobind from 'autobind-decorator';
-import {getMobxPromiseGroupStatus} from "../../../shared/lib/getMobxPromiseGroupStatus";
 import ErrorMessage from "../../../shared/components/ErrorMessage";
 import { AlterationContainerType } from './EnrichmentsUtil';
 import {makeUniqueColorGetter} from "shared/components/plots/PlotUtils";
@@ -25,8 +23,8 @@ export default class CopyNumberEnrichmentsTab extends React.Component<ICopyNumbe
     private unalteredColor = this.uniqueColorGetter();
 
     @autobind
-    private onProfileChange(molecularProfile: MolecularProfile) {
-        this.props.store._selectedEnrichmentCopyNumberProfile = molecularProfile;
+    private onProfileChange(profileMap:{[studyId:string]:MolecularProfile}) {
+        this.props.store.setCopyNumberEnrichmentProfileMap(profileMap);
     }
 
     readonly tabUI = MakeMobxView({
@@ -36,15 +34,27 @@ export default class CopyNumberEnrichmentsTab extends React.Component<ICopyNumbe
             this.props.store.alteredSampleKeys,
             this.props.store.unalteredSampleKeys,
             this.props.store.alteredPatientKeys,
-            this.props.store.unalteredPatientKeys
+            this.props.store.unalteredPatientKeys,
+            this.props.store.studies,
+            this.props.store.selectedCopyNumberEnrichmentProfileMap
         ],
         render:()=>{
             const patientLevel = this.props.store.usePatientLevelEnrichments;
+            let profileName: string = "";
+            const studies = this.props.store.studies.result!;
+            if (studies.length === 1) {
+                profileName = ' - ' + this.props.store.selectedMutationEnrichmentProfileMap.result![studies[0].studyId].name;
+            }
+
             return (
-                <div>
-                    <EnrichmentsDataSetDropdown dataSets={this.props.store.copyNumberEnrichmentProfiles} onChange={this.onProfileChange}
-                                                selectedValue={this.props.store.selectedEnrichmentCopyNumberProfile.molecularProfileId}
-                                                molecularProfileIdToProfiledSampleCount={this.props.store.molecularProfileIdToProfiledSampleCount}/>
+                <div data-test="CopyNumberEnrichmentsTab">
+                    <EnrichmentsDataSetDropdown
+                        dataSets={this.props.store.copyNumberEnrichmentProfiles}
+                        onChange={this.onProfileChange}
+                        selectedProfileByStudyId={this.props.store.selectedCopyNumberEnrichmentProfileMap.result!}
+                        molecularProfileIdToProfiledSampleCount={this.props.store.molecularProfileIdToProfiledSampleCount}
+                        studies={this.props.store.studies.result!}
+                    />
                     <AlterationEnrichmentContainer data={this.props.store.copyNumberHomdelEnrichmentData.result!}
                                                    groups={[
                                                        {
@@ -61,7 +71,7 @@ export default class CopyNumberEnrichmentsTab extends React.Component<ICopyNumbe
                                                            color: this.unalteredColor
                                                        }
                                                    ]}
-                                                   headerName={"Deep Deletion - " + this.props.store.selectedEnrichmentCopyNumberProfile.name}
+                                                   headerName={"Deep Deletion" + profileName}
                                                    store={this.props.store}
                                                    containerType={AlterationContainerType.COPY_NUMBER}
                                                    patientLevelEnrichments={this.props.store.usePatientLevelEnrichments}
@@ -84,7 +94,7 @@ export default class CopyNumberEnrichmentsTab extends React.Component<ICopyNumbe
                                                            color: this.unalteredColor
                                                        }
                                                    ]}
-                                                   headerName={"Amplification - " + this.props.store.selectedEnrichmentCopyNumberProfile.name}
+                                                   headerName={"Amplification - " + profileName}
                                                    store={this.props.store}
                                                    containerType={AlterationContainerType.COPY_NUMBER}
                                                    patientLevelEnrichments={this.props.store.usePatientLevelEnrichments}
