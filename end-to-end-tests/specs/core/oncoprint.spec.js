e5b620d974d8dab5ec7c5cc1dad11b1632bb4e02
lismana <lismanBusiness@gmail.com>
2019-06-13 17:40:41 -0400

    Remote separate test jobs and move all e2e tests back to core
    Former-commit-id: 0ab5c1bbdea4ac508737f11fb8bc54ebddbece69

--- end-to-end-tests/specs/core/oncoprint.spec.js
+++ end-to-end-tests/specs/core/oncoprint.spec.js
@@ -474,5 +474,20 @@ describe('oncoprint', function() {
             assert(legendText.indexOf("Sex") > -1, "Sex legend is shown (in sample mode)");
             assert(legendText.indexOf("Female") > -1, "Female item is shown (in sample mode)");
         });
+
+
+
     });
+
+    describe.skip("other mutation", ()=>{
+        it("should have Other mutations in oncoprint", ()=>{
+            browser.url(CBIOPORTAL_URL+'/results?Z_SCORE_THRESHOLD=2.0&RPPA_SCORE_THRESHOLD=2.0&data_priority=0&case_set_id=prad_fhcrc_cnaseq&gene_list=YEATS2&geneset_list=%20&tab_index=tab_visualize&Action=Submit&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=prad_fhcrc_mutations&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=prad_fhcrc_cna&cancer_study_list=prad_fhcrc');
+            waitForOncoprint(ONCOPRINT_TIMEOUT);
+
+            const legendText = getTextInOncoprintLegend();
+            assert(legendText.indexOf("Other Mutation") > -1);
+        });
+    });
+
+
 });
