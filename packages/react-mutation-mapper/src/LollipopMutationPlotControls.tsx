7852fd71b9a0f36a21a8f67abf715bb9ffc71b55
Onur Sumer <s.onur.sumer@gmail.com>
2019-06-26 12:37:56 -0400

    Added options to customize lollipop plot controls
    Former-commit-id: 17757ef7ff3aaae556343375bf3b06e4777222ee

--- packages/react-mutation-mapper/src/LollipopMutationPlotControls.tsx
+++ packages/react-mutation-mapper/src/LollipopMutationPlotControls.tsx
@@ -3,7 +3,7 @@ import {observer} from "mobx-react";
 import classnames from "classnames";
 import {DownloadControls, EditableSpan} from "cbioportal-frontend-commons";
 
-import TrackSelector, {TrackDataStatus, TrackVisibility} from "./TrackSelector";
+import TrackSelector, {TrackDataStatus, TrackName, TrackVisibility} from "./TrackSelector";
 
 import styles from "./lollipopMutationPlot.module.scss";
 
@@ -20,8 +20,12 @@ interface ILollipopMutationPlotControlsProps
     onToggleLegend: () => void;
     yMaxSlider: number;
     yMaxInput: number;
+    tracks?: TrackName[];
     trackVisibility?: TrackVisibility;
     trackDataStatus?: TrackDataStatus;
+    showYMaxSlider?: boolean;
+    showLegendToggle?: boolean;
+    showDownloadControls?: boolean;
     onTrackVisibilityChange?: (selectedTrackIds: string[]) => void;
     getSVG: () => SVGElement;
 }
@@ -29,6 +33,80 @@ interface ILollipopMutationPlotControlsProps
 @observer
 export default class LollipopMutationPlotControls extends React.Component<ILollipopMutationPlotControlsProps, {}>
 {
+    public static defaultProps: Partial<ILollipopMutationPlotControlsProps> = {
+        showYMaxSlider: true,
+        showLegendToggle: true,
+        showDownloadControls: true
+    };
+
+    protected get yMaxSlider()
+    {
+        return (
+            <div className="small" style={{display: "flex", alignItems: "center", marginLeft: 10}}>
+                <span>Y-Axis Max:</span>
+                <input
+                    style={{display:"inline-block", padding:0, width:200, marginLeft:10, marginRight:10}}
+                    type="range"
+                    min={this.props.countRange[0]}
+                    max={this.props.countRange[1]}
+                    step="1"
+                    onChange={this.props.onYAxisMaxSliderChange}
+                    value={this.props.yMaxSlider}
+                />
+                <EditableSpan
+                    className={styles["ymax-number-input"]}
+                    value={`${this.props.yMaxInput}`}
+                    setValue={this.props.onYAxisMaxChange}
+                    numericOnly={true}
+                    onFocus={this.props.onYMaxInputFocused}
+                    onBlur={this.props.onYMaxInputBlurred}
+                />
+            </div>
+        );
+    }
+
+    protected get trackSelector()
+    {
+        return (
+            <div
+                className={classnames("annotation-track-selector", "small")}
+                style={{width: 180, marginRight: 7}}
+            >
+                <TrackSelector
+                    tracks={this.props.tracks}
+                    trackVisibility={this.props.trackVisibility}
+                    trackDataStatus={this.props.trackDataStatus}
+                    onChange={this.props.onTrackVisibilityChange}
+                />
+            </div>
+        );
+    }
+
+    protected get legendToggle()
+    {
+        return (
+            <button
+                className="btn btn-default btn-xs"
+                onClick={this.props.onToggleLegend}
+                style={{marginRight: 7}}
+            >
+                Legend <i className="fa fa-eye" aria-hidden="true" />
+            </button>
+        );
+    }
+
+    protected get downloadControls()
+    {
+        return (
+            <DownloadControls
+                getSvg={this.props.getSVG}
+                filename={`${this.props.hugoGeneSymbol}_lollipop.svg`}
+                dontFade={true}
+                collapse={true}
+            />
+        );
+    }
+
     public render()
     {
         return (
@@ -37,53 +115,11 @@ export default class LollipopMutationPlotControls extends React.Component<ILolli
                     this.props.showControls ? styles["fade-in"] : styles["fade-out"])}
             >
                 <div style={{display:"flex", alignItems:"center"}}>
-                    {
-                        this.props.trackVisibility && this.props.onTrackVisibilityChange &&
-                        <div
-                            className={classnames("annotation-track-selector", "small")}
-                            style={{width: 180, marginRight: 7}}
-                        >
-                            <TrackSelector
-                                trackVisibility={this.props.trackVisibility}
-                                trackDataStatus={this.props.trackDataStatus}
-                                onChange={this.props.onTrackVisibilityChange}
-                            />
-                        </div>
-                    }
-                    <div className="small" style={{display: "flex", alignItems: "center", marginLeft: 10}}>
-                        <span>Y-Axis Max:</span>
-                        <input
-                            style={{display:"inline-block", padding:0, width:200, marginLeft:10, marginRight:10}}
-                            type="range"
-                            min={this.props.countRange[0]}
-                            max={this.props.countRange[1]}
-                            step="1"
-                            onChange={this.props.onYAxisMaxSliderChange}
-                            value={this.props.yMaxSlider}
-                        />
-                        <EditableSpan
-                            className={styles["ymax-number-input"]}
-                            value={`${this.props.yMaxInput}`}
-                            setValue={this.props.onYAxisMaxChange}
-                            numericOnly={true}
-                            onFocus={this.props.onYMaxInputFocused}
-                            onBlur={this.props.onYMaxInputBlurred}
-                        />
-                    </div>
+                    {this.props.trackVisibility && this.props.onTrackVisibilityChange && this.trackSelector}
+                    {this.props.showYMaxSlider && this.yMaxSlider}
                     <div style={{display: "flex", marginLeft: "auto"}}>
-                        <button
-                            className="btn btn-default btn-xs"
-                            onClick={this.props.onToggleLegend}
-                            style={{marginRight: 7}}
-                        >
-                            Legend <i className="fa fa-eye" aria-hidden="true" />
-                        </button>
-                        <DownloadControls
-                            getSvg={this.props.getSVG}
-                            filename={`${this.props.hugoGeneSymbol}_lollipop.svg`}
-                            dontFade={true}
-                            collapse={true}
-                        />
+                        {this.props.showLegendToggle && this.legendToggle}
+                        {this.props.showDownloadControls && this.downloadControls}
                     </div>
                 </div>
                 {'  '}
