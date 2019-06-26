6594f24472b82138b6e109f203da2332d6383fd1
gaofei zhao <zgfilx@163.com>
2019-06-26 17:40:42 -0400

    update tutorial link

--- src/pages/staticPages/tutorials/Tutorials.tsx
+++ src/pages/staticPages/tutorials/Tutorials.tsx
@@ -4,6 +4,7 @@ import {PageLayout} from "../../../shared/components/PageLayout/PageLayout";
 import './styles.scss';
 import Helmet from "react-helmet";
 import {getNCBIlink} from "../../../public-lib/lib/urls";
+import AppConfig from 'appConfig';
 
 @observer
 export default class Tutorials extends React.Component<{}, {}> {
@@ -24,23 +25,26 @@ export default class Tutorials extends React.Component<{}, {}> {
                 [<a href={getNCBIlink('/pubmed/23550210')}>Reprint</a>].</p>
 
             <hr />
-            <h2>View tutorial slides below or download slides <a href="https://drive.google.com/drive/u/0/folders/0B9KTQJAGhFhhRi1qaTdUWmpLQTA">here</a>.</h2>
+            <h2 className={'tutorialsFolderText'}>View tutorial slides below or download from <a href="https://drive.google.com/drive/u/0/folders/0B9KTQJAGhFhhRi1qaTdUWmpLQTA">Google Drive</a> or <a href={`${AppConfig.serverConfig.skin_documentation_baseurl!.replace("raw.githubusercontent.com", "www.github.com").replace("/master/", "/tree/master/")}tutorials`}>GitHub</a>.</h2>
             <hr/>
 
             <h2 id={"single-study-exploration"}>Tutorial #1: Single Study Exploration</h2>
             <iframe src="https://docs.google.com/presentation/d/1_OGK69lO4Z62WaxHHkNYmWvY0LQN2v0slfaLyY1_IQ0/embed?start=false&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434" allowFullScreen={true}></iframe>
+            <h4><a href={`${AppConfig.serverConfig.skin_documentation_baseurl!}tutorials/cBioPortal Tutorial 1 Single Study Exploration.pdf`}>Download tutorial</a></h4>
             <hr/>
 
             <h2 id={"single-study-query"}>Tutorial #2: Single Study Query</h2>
             <iframe
                 src="https://docs.google.com/presentation/d/1y9UTIr5vHmsNVWqtGTVGgiuYX9wkK_a_RPNYiR8kYD8/embed?start=false&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434" allowFullScreen={true}></iframe>
+            <h4><a href={`${AppConfig.serverConfig.skin_documentation_baseurl!}tutorials/cBioPortal Tutorial 2 Single Study Query.pdf`}>Download tutorial</a></h4>
             <hr/>
 
             <h2 id={"patient-view"}>Tutorial #3: Patient View</h2>
             <iframe src="https://docs.google.com/presentation/d/1Jr_2yEfgjKBn4DBiXRk4kmhIbtsRp6gd0iD3k1fIUUk/embed?start=false&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434" allowFullScreen={true}></iframe>
+            <h4><a href={`${AppConfig.serverConfig.skin_documentation_baseurl!}tutorials/cBioPortal Tutorial 3 Patient View.pdf`}>Download tutorial</a></h4>
             <hr/>
 
 
@@ -49,6 +53,7 @@ export default class Tutorials extends React.Component<{}, {}> {
                 src="https://docs.google.com/presentation/d/1rQE5rbFNdmup-rAtySHFxlLp3i4qa8SBA7MiQpMdn1I/embed?start=false&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434"
                 allowFullScreen={true}></iframe>
+            <h4><a href={`${AppConfig.serverConfig.skin_documentation_baseurl!}tutorials/cBioPortal Tutorial 4 Virtual Studies.pdf`}>Download tutorial</a></h4>
             <hr />
 
             <h2 id={"oql"}>Tutorial #5: Onco Query Language (OQL)</h2>
@@ -56,6 +61,7 @@ export default class Tutorials extends React.Component<{}, {}> {
                 src="https://docs.google.com/presentation/d/1U39xgVujtBodwW20qIfcGu4E5n2zzaKkl2KmzzHqj4A/embed?startfalse&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434"
                 allowFullScreen={true}></iframe>
+            <h4><a href={`${AppConfig.serverConfig.skin_documentation_baseurl!}tutorials/cBioPortal Tutorial 5 Onco Query Language.pdf`}>Download tutorial</a></h4>
             <hr />
 
             <h2 id={"group-comparison"}>Tutorial #6: Group Comparison</h2>
@@ -63,6 +69,7 @@ export default class Tutorials extends React.Component<{}, {}> {
                 src="https://docs.google.com/presentation/d/1P2boDph8IfpvjxoxDj_496CLHGtshzJnbbZhszPsmf4/embed?startfalse&loop=false&delayms=60000"
                 frameBorder="0" width="720" height="434"
                 allowFullScreen={true}></iframe>
+            <h4><a href={`${AppConfig.serverConfig.skin_documentation_baseurl!}tutorials/cBioPortal Tutorial 6 Group Comparison.pdf`}>Download tutorial</a></h4>
             
 
         </PageLayout>
