04579b38baa43207a3250aa03332afba5c57be23
Pim van Nierop <pim@thehyve.nl>
2019-07-01 16:41:38 +0200

    Implement `e2e-localdb` tests against local dockerized backends (#2408)
    The e2e-localdb feature creates an environment where e2e-tests are conducted using a local dockerized backend. Custom (minimal) studies with essential data for tests can be provided alongside the test code. These studies will be imported before running the tests. Moreover, custom backend versions can be specified that are used for tests. A safety mechanism (based on GitHub pull request draft state) is included that prevents merging pull requests that specify a custom backend versions. e2e-localdb runs on CircleCI and local environments.
    
    For a detailed explanation see the README.md, section 'Local cBioPortal database for e2e-tests'.
    
    * Impl. e2e tests that use a local database
    * Refactor testing directory structure
    * Refactor evaluation of github API url to get base branch
    * Load study_es_0 by default for e2e-localdb tests
    * Load gene set data for study_es_0
    * Improve local context testing
    * Add react-select util functions
    * Fix webdriver-manager config (read debug option)
    * Update ERROR shot script for better debugging
    * Update docs for local and CI testing
    * Fix maxInstances to 1 for localdb tests
    * Supress verbose output
    * Fix test summary for e2e remote tests
    * Update .gitignore with screenshot paths for local e2e
    * Correct urls for cbioportal for validation and import scripts
    * Impl. cache for local database

---
+++ end-to-end-test/shared/image-compare/upload_failed_screenshots.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo UPLOADING ERROR SCREENSHOTS
+
+# paths are different on CI and local testing
+# TODO implement these different paths
+# low prio since I consider running imageCompare locally as a rare event.
+# if [[ ${TRAVIS} || ${CIRCLECI} ]]; then
+#     reference_path=$DIR
+# else
+#     reference_path=...
+# fi
+
+cd ${DIR}/../../$SCREENSHOT_DIRECTORY
+
+if (ls diff/*.png 2> /dev/null > /dev/null); then
+    references=()
+    # collect reference images for each screenshot in diff directory
+    for image_path in diff/*.png; do
+        reference=${image_path/diff/reference}
+        references=( ${references[@]} $reference )
+        echo "FAILED SCREENSHOT TEST: ${reference}"
+    done
+
+    echo "var errorImages = '${references[@]}'.split(' ')" > ${DIR}/errors.js
+
+
+    if [[ ${TRAVIS} || ${CIRCLECI} ]]; then
+        echo "${YELLOW}See Artifacts tab above for image comparison UI (imageCompare.html)${NC}"
+    else
+        repo_url=file://${PWD}/../../..
+        echo -e "${GREEN}COPY+PASTE in BROWSER TO COMPARE FAILED SCREENSHOTS: ${YELLOW}${repo_url}/end-to-end-test/shared/imageCompare.html${NC}";
+    fi
+
+
+fi
+exit 1 # always exit 1 since we are assuming that you call this when screenshots have failed
