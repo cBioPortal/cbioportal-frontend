91e4ee6c1a547c7b49d429bcff8d7e431a5348a2
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-07-19 17:40:30 -0400

    Patient view mutations VAF oncoprint prototype
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

---
+++ src/pages/patientView/oncoprint/MutationOncoprint.tsx
@@ -0,0 +1,229 @@
+import * as React from "react";
+import {observer} from "mobx-react";
+import Oncoprint, {IGeneHeatmapTrackSpec} from "../../../shared/components/oncoprint/Oncoprint";
+import {remoteData} from "../../../public-lib";
+import {MakeMobxView} from "../../../shared/components/MobxView";
+import {PatientViewPageStore} from "../clinicalInformation/PatientViewPageStore";
+import $ from "jquery";
+import {
+    getDownloadData,
+    IMutationOncoprintTrackDatum, IMutationOncoprintTrackSpec,
+    makeMutationHeatmapData,
+    MutationStatus
+} from "./MutationOncoprintUtils";
+import LoadingIndicator from "../../../shared/components/loadingIndicator/LoadingIndicator";
+import ErrorMessage from "../../../shared/components/ErrorMessage";
+import {computed, observable} from "mobx";
+import ReactSelect from "react-select2";
+import OncoprintJS from "oncoprintjs";
+import autobind from "autobind-decorator";
+import DownloadControls, {DownloadControlsButton} from "../../../public-lib/components/downloadControls/DownloadControls";
+import _ from "lodash";
+
+export interface IMutationOncoprintProps {
+    store:PatientViewPageStore;
+}
+
+const TRACK_GROUP_INDEX = 2;
+
+@observer
+export default class MutationOncoprint extends React.Component<IMutationOncoprintProps, {}> {
+
+    private oncoprint:OncoprintJS<any>|null = null;
+
+    constructor(props:IMutationOncoprintProps) {
+        super(props);
+
+        (window as any).mutationOncoprint = this;
+    }
+
+    @autobind
+    private oncoprintRef(oncoprint:OncoprintJS<any>|null) {
+        this.oncoprint = oncoprint;
+    }
+
+    @observable clustered = true;
+
+    @computed get sortConfig() {
+        if (this.clustered) {
+            return {
+                clusterHeatmapTrackGroupIndex: TRACK_GROUP_INDEX
+            };
+        } else {
+            return {};
+        }
+    }
+
+    @computed get heatmapTracksOrder() {
+        if (this.clustered || !this.props.store.samples.isComplete) {
+            return undefined;
+        } else {
+            return {
+                [TRACK_GROUP_INDEX]:this.props.store.samples.result!.map(s=>s.uniqueSampleKey)
+            };
+        }
+    }
+
+    private readonly heatmapTracks = remoteData<IMutationOncoprintTrackSpec[]>({
+        await:()=>[
+            this.props.store.samples,
+            this.props.store.mutationData,
+            this.props.store.mutationMolecularProfile,
+            this.props.store.coverageInformation
+        ],
+        invoke:()=>{
+            if (this.props.store.mutationData.result!.length === 0) {
+                return Promise.resolve([]);
+            }
+            const profile = this.props.store.mutationMolecularProfile.result!;
+            const data = makeMutationHeatmapData(
+                this.props.store.samples.result!,
+                this.props.store.mutationData.result!,
+                this.props.store.coverageInformation.result!
+            );
+            return Promise.resolve(this.props.store.samples.result!.map(sample=>{
+                return {
+                    key: sample.uniqueSampleKey,
+                    label: sample.sampleId,
+                    molecularProfileId: this.props.store.mutationMolecularProfileId.result!,
+                    molecularAlterationType: profile.molecularAlterationType,
+                    datatype: profile.datatype,
+                    data: data[sample.uniqueSampleKey],
+                    trackGroupIndex:TRACK_GROUP_INDEX,
+                    naLegendLabel:"Not sequenced",
+                    tooltip:(data:IMutationOncoprintTrackDatum[])=>{
+                        const d = data[0];
+                        let vafSection:string;
+                        switch (d.mutationStatus) {
+                            case MutationStatus.MUTATED:
+                                vafSection = "<span>VAF:"+d.profile_data!.toFixed(2)+"</span>";
+                                break;
+                            case MutationStatus.MUTATED_BUT_NO_VAF:
+                                vafSection = "<span>Mutated, but no VAF data.</span>";
+                                break;
+                            case MutationStatus.PROFILED_BUT_NOT_MUTATED:
+                                vafSection = "<span>Wild type</span>";
+                                break;
+                            case MutationStatus.NOT_PROFILED:
+                            default:
+                                vafSection = "<span>Not sequenced</span>";
+                                break;
+                        }
+                        const html = "<div>"+
+                                        "<span>"+d.mutation.gene.hugoGeneSymbol!+"</span><br/>"+
+                                        "<span>"+d.mutation.proteinChange+"</span><br/>"+
+                                        vafSection +
+                                    "</div>"
+                        ;
+                        return $(html);
+                    },
+                    sortDirectionChangeable: false,
+                    initSortDirection:-1 as -1,
+                    movable: false
+                };
+            }));
+        }
+    });
+
+    private readonly oncoprintUI = MakeMobxView({
+        await:()=>[this.heatmapTracks],
+        render:()=>{
+            if (this.heatmapTracks.result!.length === 0) {
+                return null;
+            } else {
+                return (
+                    <div>
+                        <div style={{display:"flex", alignItems:"center", justifyContent:"space-between", marginBottom:5}}>
+                            <div style={{display:"flex", alignItems:"center"}}>
+                                <span>Sort configuration:&nbsp;</span>
+                                <div style={{ width: 250, marginRight: 7 }} >
+                                    <ReactSelect
+                                        name="select sort configuration"
+                                        onChange={(option:any|null)=>{
+                                            if (option) {
+                                                this.clustered = option.value;
+                                            }
+                                        }}
+                                        options={[
+                                            { label: "Cluster", value: true},
+                                            { label: "Sample order", value: false}
+                                        ]}
+                                        clearable={false}
+                                        searchable={false}
+                                        value={{ label: this.clustered ? "Cluster" : "Sample order", value:this.clustered}}
+                                        styles={{
+                                            control: (provided:any)=>({
+                                                ...provided,
+                                                height:36,
+                                                minHeight:36,
+                                                border: "1px solid rgb(204,204,204)"
+                                            }),
+                                            menu: (provided:any)=>({
+                                                ...provided,
+                                                maxHeight: 400
+                                            }),
+                                            menuList: (provided:any)=>({
+                                                ...provided,
+                                                maxHeight:400
+                                            }),
+                                            placeholder:(provided:any)=>({
+                                                ...provided,
+                                                color: "#000000"
+                                            }),
+                                            dropdownIndicator:(provided:any)=>({
+                                                ...provided,
+                                                color:"#000000"
+                                            }),
+                                            option:(provided:any, state:any)=>{
+                                                return {
+                                                    ...provided,
+                                                    cursor:"pointer",
+                                                };
+                                            }
+                                        }}
+                                        theme={(theme:any)=>({
+                                            ...theme,
+                                            colors: {
+                                                ...theme.colors,
+                                                neutral80:"black",
+                                                //primary: theme.colors.primary50
+                                            },
+                                        })}
+                                    />
+                                </div>
+                            </div>
+                            <DownloadControls
+                                filename="vafHeatmap"
+                                getSvg={()=>(this.oncoprint ? this.oncoprint.toSVG(true) : null)}
+                                getData={()=>{
+                                    const data = _.flatMap(this.heatmapTracks.result!, track=>track.data);
+                                    return getDownloadData(data);
+                                }}
+                                buttons={["SVG", "PNG", "PDF", "Data"]}
+                                dontFade
+                            />
+                        </div>
+                        <Oncoprint
+                            oncoprintRef={this.oncoprintRef}
+                            clinicalTracks={[]}
+                            geneticTracks={[]}
+                            genesetHeatmapTracks={[]}
+                            heatmapTracks={this.heatmapTracks.result!}
+                            heatmapTracksOrder={this.heatmapTracksOrder}
+                            divId="MutationHeatmap"
+                            width={900}
+                            caseLinkOutInTooltips={false}
+                            sortConfig={this.sortConfig}
+                        />
+                    </div>
+                );
+            }
+        },
+        renderPending:()=><LoadingIndicator isLoading={true}/>,
+        renderError:()=><ErrorMessage/>
+    });
+
+    render() {
+        return this.oncoprintUI.component;
+    }
+}
