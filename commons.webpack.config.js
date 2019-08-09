971b65a257dcf33aceec017f15bbf3799724f799
Onur Sumer <s.onur.sumer@gmail.com>
2019-08-09 16:10:06 -0400

    using enum instead of types for mutation type constants
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>
    
    Former-commit-id: 877cfa6ea6fa663007e3b5eb0883b099695f3fcc

--- commons.webpack.config.js
+++ commons.webpack.config.js
@@ -7,7 +7,7 @@ var GeneratePackageJsonPlugin =require('generate-package-json-webpack-plugin');
 var TerserPlugin = require('terser-webpack-plugin');
 
 var commit = 'unknown';
-var version = '0.0.15';
+var version = '0.0.16';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
