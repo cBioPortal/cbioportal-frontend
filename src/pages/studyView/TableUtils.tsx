ae78e44966f5e259b6d40543f9c519907e8c1d7e
lismana <lismanBusiness@gmail.com>
2019-08-13 11:24:33 -0400

    fix type stuff
    Former-commit-id: 8cf9aadba2250d012780a08a028ba6c87fc7d800

--- src/pages/studyView/TableUtils.tsx
+++ src/pages/studyView/TableUtils.tsx
@@ -6,7 +6,8 @@ import DefaultTooltip from "public-lib/components/defaultTooltip/DefaultTooltip"
 import {ICON_FILTER_OFF, ICON_FILTER_ON} from "shared/lib/Colors";
 import {GenePanelList} from "pages/studyView/table/GenePanelModal";
 import {getFrequencyStr} from "pages/studyView/StudyViewUtils";
-import {GenePanel, GenePanelToGene} from "shared/api/generated/CBioPortalAPIInternal";
+import {GenePanel, GenePanelToGene} from "shared/api/generated/CBioPortalAPI";
+import {CSSProperties} from "react";
 
 export function getGeneColumnHeaderRender(cellMargin: number, headerName: string, cancerGeneListFilterEnabled: boolean, isFilteredByCancerGeneList: boolean, cancerGeneIconToggle: (event: any) => void) {
     return <div style={{marginLeft: cellMargin}} className={styles.displayFlex} data-test='gene-column-header'>
@@ -46,7 +47,7 @@ export function getCancerGeneFilterToggleIcon(isFilteredByCancerGeneList:boolean
     return <span data-test='cancer-gene-filter' className={classnames(styles.cancerGeneIcon, styles.displayFlex)} style={{color: isFilteredByCancerGeneList ? ICON_FILTER_ON : ICON_FILTER_OFF}}><i className='fa fa-filter'></i></span>;
 }
 
-export function getFreqColumnRender(type: 'mutation' | 'cna', numberOfSamplesProfiled: number, numberOfAlteredCases: number, matchingGenePanels: GenePanel[], toggleModal: (panelName: string, genes: GenePanelToGene[]) => void, style?:CSSProperties) {
+export function getFreqColumnRender(type: 'mutation' | 'cna', numberOfSamplesProfiled: number, numberOfAlteredCases: number, matchingGenePanelIds: string[], toggleModal: (panelName: string) => void, style?:CSSProperties) {
     const addTotalProfiledOverlay = () => (
         <span style={{display: 'flex', flexDirection: 'column'}} data-test='freq-cell-tooltip'>
             <span>{`# of samples profiled for ${type === 'mutation' ? 'mutations' : 'copy number alterations'} in this gene: ${numberOfSamplesProfiled.toLocaleString()}`}</span>
