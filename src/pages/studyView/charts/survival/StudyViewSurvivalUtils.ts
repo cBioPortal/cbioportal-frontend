8dd4932c56d52ecd82e5e51b8f1a4b85041763b4
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-08-19 11:11:10 -0400

    Bring back survival plot and only show selected patients or all patients
    Also disable the NA filtering since that there is no comparison anymore
    
    Signed-off-by: Hongxin Zhang <hongxin@cbio.mskcc.org>

--- src/pages/studyView/charts/survival/StudyViewSurvivalUtils.ts
+++ src/pages/studyView/charts/survival/StudyViewSurvivalUtils.ts
@@ -6,18 +6,8 @@ import _ from "lodash";
 export function makeSurvivalChartData(
     patientSurvivals: ReadonlyArray<PatientSurvival>,
     analysisGroups: ReadonlyArray<AnalysisGroup>,
-    patientToAnalysisGroup: {[uniquePatientKey:string]:string},
-    naPatientsHiddenInSurvival: boolean,
-    patientKeysWithNAInSelectedClinicalData?:string[]
+    patientToAnalysisGroup: {[uniquePatientKey:string]:string}
 ) {
-    if (naPatientsHiddenInSurvival && patientKeysWithNAInSelectedClinicalData) {
-        // filter out NA
-        const clinicalNAPatientKeysMap = _.keyBy(patientKeysWithNAInSelectedClinicalData);
-        patientSurvivals = patientSurvivals.filter(s=>{
-            return (patientToAnalysisGroup[s.uniquePatientKey] !== "NA") && !(s.uniquePatientKey in clinicalNAPatientKeysMap);
-        });
-    }
-
     let patientToAnalysisGroups = _.mapValues(patientToAnalysisGroup, group => [group])
 
     return {
