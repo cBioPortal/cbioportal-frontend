ba73dafe5bee54e3c159f730581742e51cdb2ce4
lismana <lismanBusiness@gmail.com>
2019-08-20 15:18:04 -0400

    Update screenshots to fix some local tests

--- end-to-end-test/shared/wdio/webdriver-manager.conf.js
+++ end-to-end-test/shared/wdio/webdriver-manager.conf.js
@@ -57,7 +57,7 @@ var config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: debug? 1 : defaultMaxInstances,
+    maxInstances: 1, //debug? 1 : defaultMaxInstances,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
