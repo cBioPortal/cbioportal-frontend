e26d1e0777c57f65be307c29e9d1a0868c5d882c
Onur Sumer <s.onur.sumer@gmail.com>
2019-08-28 17:27:58 -0400

    added a checklist select component
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>
    
    Former-commit-id: 9d9dda4fff5d88417a3b4c4bc89d96dc2872ed68

--- commons.webpack.config.js
+++ commons.webpack.config.js
@@ -7,7 +7,7 @@ var GeneratePackageJsonPlugin =require('generate-package-json-webpack-plugin');
 var TerserPlugin = require('terser-webpack-plugin');
 
 var commit = 'unknown';
-var version = '0.0.19';
+var version = '0.0.20';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
