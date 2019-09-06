92dc86a5cd7e53fbbfc53d1b627218c4905bb5a4
Pim van Nierop <pim@thehyve.nl>
2019-09-06 16:45:28 +0200

    Correct tooltip position for search labels in horizontal waterfall plot
    Former-commit-id: ec1cdeceec8a4317e33a282894489d703867b330

--- src/shared/components/plots/WaterfallPlot.tsx
+++ src/shared/components/plots/WaterfallPlot.tsx
@@ -101,7 +101,7 @@ export default class WaterfallPlot<D extends IBaseWaterfallPlotData> extends Rea
                             mutation: (props: any) => {
 
                                 // swap x and y label pos when in horizontal mode
-                                if (this.props.horizontal) {
+                                if (this.props.horizontal && !props.datum.search_label) {
                                     const x = props.x;
                                     props.x = props.y;
                                     props.y = x;
@@ -420,6 +420,8 @@ export default class WaterfallPlot<D extends IBaseWaterfallPlotData> extends Rea
         const dataPoints = _.filter(this.waterfallPlotData, (d:any) => this.props.highlight!(d) );
 
         const searchLabels = _.cloneDeep(dataPoints);
+        // add marker field to search label
+        _.each(searchLabels, (o) => o.search_label = true);
 
         const range = this.props.horizontal ? this.plotDomainX : this.plotDomainY;
         const min_value = range[0];
