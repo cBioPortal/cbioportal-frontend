7f15d9322400ff749c5c6269084d8d15634ff0fc
Gaofei Zhao <gzd23@mail.missouri.edu>
2019-09-09 14:41:35 -0400

    move profile download function from query page to result view page
    Former-commit-id: 4a3956636006d70fb08d79bd5f59bba275e77e2c

--- src/pages/resultsView/download/DownloadTab.tsx
+++ src/pages/resultsView/download/DownloadTab.tsx
@@ -3,7 +3,7 @@ import * as _ from 'lodash';
 import {computed, observable, action} from "mobx";
 import {observer} from 'mobx-react';
 import fileDownload from 'react-file-download';
-import {AnnotatedExtendedAlteration, ExtendedAlteration, ResultsViewPageStore, ModifyQueryParams} from "../ResultsViewPageStore";
+import {AnnotatedExtendedAlteration, ExtendedAlteration, ResultsViewPageStore, ModifyQueryParams, CaseAggregatedData, AlterationTypeConstants} from "../ResultsViewPageStore";
 import {CoverageInformation, getSingleGeneResultKey, getMultipleGeneResultKey} from "../ResultsViewPageStoreUtils";
 import {OQLLineFilterOutput, UnflattenedOQLLineFilterOutput, MergedTrackLineFilterOutput} from "shared/lib/oql/oqlfilter";
 import FeatureTitle from "shared/components/featureTitle/FeatureTitle";
@@ -13,7 +13,7 @@ import {default as CaseAlterationTable, ICaseAlteration} from "./CaseAlterationT
 import {
     generateCaseAlterationData, generateCnaData, generateDownloadData, generateGeneAlterationData, generateMrnaData,
     generateMutationData, generateMutationDownloadData,
-    generateProteinData, hasValidData, hasValidMutationData, stringify2DArray
+    generateProteinData, hasValidData, hasValidMutationData, stringify2DArray, generateOtherMolecularProfileData, generateOtherMolecularProfileDownloadData
 } from "./DownloadUtils";
 
 import styles from "./styles.module.scss";
@@ -24,7 +24,7 @@ import {WindowWidthBox} from "../../../shared/components/WindowWidthBox/WindowWi
 import {remoteData} from "../../../public-lib/api/remoteData";
 import LoadingIndicator from "shared/components/loadingIndicator/LoadingIndicator";
 import onMobxPromise from "shared/lib/onMobxPromise";
-import {MolecularProfile, Sample} from "shared/api/generated/CBioPortalAPI";
+import {MolecularProfile, Sample, CancerStudy} from "shared/api/generated/CBioPortalAPI";
 import {getMobxPromiseGroupStatus} from "../../../shared/lib/getMobxPromiseGroupStatus";
 import ErrorMessage from "../../../shared/components/ErrorMessage";
 import AlterationFilterWarning from "../../../shared/components/banners/AlterationFilterWarning";
@@ -34,6 +34,14 @@ import { MakeMobxView } from 'shared/components/MobxView';
 import { CUSTOM_CASE_LIST_ID } from 'shared/components/query/QueryStore';
 import { IVirtualStudyProps } from 'pages/studyView/virtualStudy/VirtualStudy';
 import { Alteration } from 'shared/lib/oql/oql-parser';
+import ReactSelect from "react-select";
+import Select from "react-select1";
+import { Dropdown, Checkbox } from 'react-bootstrap';
+import { DropdownToggleProps } from 'react-bootstrap/lib/DropdownToggle';
+import { DropdownMenuProps } from 'react-bootstrap/lib/DropdownMenu';
+import autobind from 'autobind-decorator';
+import DefaultTooltip from 'public-lib/components/defaultTooltip/DefaultTooltip';
+import FontAwesome from 'react-fontawesome';
 
 export interface IDownloadTabProps {
     store: ResultsViewPageStore;
@@ -96,7 +104,9 @@ export default class DownloadTab extends React.Component<IDownloadTabProps, {}>
 
     readonly mutationData = remoteData<{[key: string]: ExtendedAlteration[]}>({
         await:()=>[this.props.store.nonOqlFilteredCaseAggregatedData],
-        invoke:()=>Promise.resolve(generateMutationData(this.props.store.nonOqlFilteredCaseAggregatedData.result!))
+        invoke:()=>{
+            return Promise.resolve(generateMutationData(this.props.store.nonOqlFilteredCaseAggregatedData.result!));
+        }
     });
 
     readonly mutationDownloadData = remoteData<string[][]>({
@@ -119,6 +129,45 @@ export default class DownloadTab extends React.Component<IDownloadTabProps, {}>
     readonly transposedMutationDataText = remoteData<string>({
         await:()=>[this.transposedMutationDownloadData],
         invoke:()=>Promise.resolve(stringify2DArray(this.transposedMutationDownloadData.result!))
+    });
+
+    readonly otherMolecularProfileData = remoteData<{[key: string]: ExtendedAlteration[]}>({
+        await:()=>[this.props.store.nonQueriedMolecularData, this.props.store.nonSelectedMolecularProfilesGroupByName],
+        invoke:()=>{
+            const profileNames = _.keys(this.props.store.nonSelectedMolecularProfilesGroupByName.result);
+            const selectedProfileName = this.props.store.selectedMolecularProfileNameForDownload ? this.props.store.selectedMolecularProfileNameForDownload : profileNames[0];
+            if (this.props.store.doNonSelectedMolecularProfilesExist) {
+                const data = {
+                    "samples": _.groupBy(this.props.store.nonQueriedMolecularData.result!, (data) => data.uniqueSampleKey)
+                } as CaseAggregatedData<ExtendedAlteration>
+                return Promise.resolve(generateOtherMolecularProfileData(this.props.store.nonSelectedMolecularProfilesGroupByName.result[selectedProfileName].map(profile => profile.molecularProfileId), data))
+            }
+            return Promise.resolve({});
+        }
+    });
+
+    readonly otherMolecularProfileDownloadData = remoteData<string[][]>({
+        await:()=>[this.otherMolecularProfileData, this.props.store.samples, this.props.store.genes],
+        invoke:()=>Promise.resolve(generateOtherMolecularProfileDownloadData(
+            this.otherMolecularProfileData.result!, this.props.store.samples.result!, this.props.store.genes.result!
+        ))
+    });
+
+    readonly transposedOtherMolecularProfileDownloadData = remoteData<string[][]>({
+        await:()=>[this.otherMolecularProfileDownloadData],
+        invoke:()=>Promise.resolve(_.unzip(this.otherMolecularProfileDownloadData.result!))
+    });
+
+    readonly otherMolecularProfileDataText = remoteData<string>({
+        await:()=>[this.otherMolecularProfileDownloadData],
+        invoke:()=>Promise.resolve(stringify2DArray(this.otherMolecularProfileDownloadData.result!))
+    });
+
+    readonly transposedOtherMolecularProfileDataText = remoteData<string>({
+        await:()=>[this.transposedOtherMolecularProfileDownloadData],
+        invoke:()=>{
+            return Promise.resolve(stringify2DArray(this.transposedOtherMolecularProfileDownloadData.result!))
+        }
     });
 
     readonly mrnaData = remoteData<{[key: string]: ExtendedAlteration[]}>({
@@ -334,7 +383,8 @@ export default class DownloadTab extends React.Component<IDownloadTabProps, {}>
 
     public render() {
         const status = getMobxPromiseGroupStatus(this.geneAlterationData, this.caseAlterationData, this.oqls, this.trackLabels, this.trackAlterationTypesMap, this.geneAlterationMap, this.cnaData, this.mutationData, 
-            this.mrnaData, this.proteinData, this.unalteredCaseAlterationData, this.alteredCaseAlterationData, this.props.store.virtualStudyParams, this.sampleMatrixText);
+            this.mrnaData, this.proteinData, this.unalteredCaseAlterationData, this.alteredCaseAlterationData, this.props.store.virtualStudyParams, this.sampleMatrixText, this.props.store.nonSelectedMolecularProfilesGroupByName,
+            this.props.store.studies, this.props.store.selectedMolecularProfiles);
 
         switch (status) {
             case "pending":
@@ -359,11 +409,12 @@ export default class DownloadTab extends React.Component<IDownloadTabProps, {}>
                                 <tbody>
                                     {hasValidData(this.cnaData.result!) && this.cnaDownloadControls()}
                                     {hasValidMutationData(this.mutationData.result!) && this.mutationDownloadControls()}
-                                    {hasValidData(this.mrnaData.result!) && this.mrnaExprDownloadControls()}
-                                    {hasValidData(this.proteinData.result!) && this.proteinExprDownloadControls()}
+                                    {hasValidData(this.mrnaData.result!) && this.mrnaExprDownloadControls(this.props.store.selectedMolecularProfiles.result!.find((profile) => profile.molecularAlterationType === AlterationTypeConstants.MRNA_EXPRESSION)!.name)}
+                                    {hasValidData(this.proteinData.result!) && this.proteinExprDownloadControls(this.props.store.selectedMolecularProfiles.result!.find((profile) => profile.molecularAlterationType === AlterationTypeConstants.PROTEIN_LEVEL)!.name)}
                                     {this.alteredSamplesDownloadControls(this.alteredCaseAlterationData.result!, this.props.store.virtualStudyParams.result!)}
                                     {this.unalteredSamplesDownloadControls(this.unalteredCaseAlterationData.result!, this.props.store.virtualStudyParams.result!)}
                                     {this.sampleMatrixDownloadControls(this.sampleMatrixText.result!)}
+                                    {(this.props.store.doNonSelectedMolecularProfilesExist) && this.nonSelectedProfileDownloadRow(this.props.store.nonSelectedMolecularProfilesGroupByName.result!)}
                                 </tbody>
                             </table>
                         </div>
@@ -412,38 +463,126 @@ export default class DownloadTab extends React.Component<IDownloadTabProps, {}>
                                         this.handleTransposedMutationDownload);
     }
 
-    private mrnaExprDownloadControls(): JSX.Element
+    private mrnaExprDownloadControls(profileName: string): JSX.Element
     {
-        return this.downloadControlsRow("mRNA Expression",
+        return this.downloadControlsRow(profileName,
                                         this.handleMrnaDownload,
                                         this.handleTransposedMrnaDownload);
     }
 
-    private proteinExprDownloadControls(): JSX.Element
+    private proteinExprDownloadControls(profileName: string): JSX.Element
     {
-        return this.downloadControlsRow("Protein Expression",
+        return this.downloadControlsRow(profileName,
                                         this.handleProteinDownload,
                                         this.handleTransposedProteinDownload);
     }
 
-    private downloadControlsRow(title:string,
-                                handleTabDelimitedDownload: () => void,
-                                handleTransposedMatrixDownload: () => void)
+    private downloadControlsRow(profileName:string,
+                                handleTabDelimitedDownload: (name: string) => void,
+                                handleTransposedMatrixDownload: (name: string) => void)
     {
         return (
             <tr>
-                <td style={{width: 500}}>{title}</td>
+                <td style={{width: 500}}>{profileName}</td>
                 <td>
-                    <a onClick={handleTabDelimitedDownload}>
+                    <a onClick={(event) => handleTabDelimitedDownload(profileName)}>
                         <i className='fa fa-cloud-download' style={{marginRight: 5}}/>Tab Delimited Format
                     </a>
                     <span style={{margin:'0px 10px'}}>|</span>
-                    <a onClick={handleTransposedMatrixDownload}>
+                    <a onClick={(event) => handleTransposedMatrixDownload(profileName)}>
                         <i className='fa fa-cloud-download' style={{marginRight: 5}}/>Transposed Matrix
                     </a>
                 </td>
             </tr>
         );
+    }
+
+    private nonSelectedProfileDownloadRow(nonSelectedMolecularProfilesGroupByName:_.Dictionary<MolecularProfile[]>)
+    {
+        const profileOptions = _.keys(nonSelectedMolecularProfilesGroupByName).map((name) => {
+            if (this.props.store.studies.result!.length === 1) {
+                const singleStudyProfile = nonSelectedMolecularProfilesGroupByName[name][0];
+                return {
+                    "value": name,
+                    "label": name,
+                    "description": singleStudyProfile.description
+                }
+            }
+            return {"value": name, "label": name}
+        });
+
+        const selectedOption = profileOptions.find((option) => {
+            if (this.props.store.selectedMolecularProfileNameForDownload && _.keys(nonSelectedMolecularProfilesGroupByName).includes(this.props.store.selectedMolecularProfileNameForDownload)) {
+                return option.value === this.props.store.selectedMolecularProfileNameForDownload;
+            }
+            return option.value === _.keys(nonSelectedMolecularProfilesGroupByName)[0];
+        });
+        const selectedProfileName = selectedOption!.value;
+        
+        return (
+            <tr>
+                <td style={{width: 500}}>
+                    <div style={{display: "flex", alignItems:"center"}}>
+                        {"Other profiles"}
+                        <div className={styles["other-profiles-select"]}>
+                            <ReactSelect
+                                    value={selectedOption}
+                                    onChange={this.handleSelect}
+                                    options={profileOptions}
+                                    formatOptionLabel={this.formatOptionLabel}
+                                    clearable={false}
+                                    autosize={false}
+                            />
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    {
+                        (this.props.store.doNonSelectedMolecularProfilesExist) && (
+                            <div>
+                                <a onClick={(event) => this.handleOtherMolecularProfileDownload(event, selectedProfileName)}>
+                                <i className='fa fa-cloud-download' style={{marginRight: 5}}/>Tab Delimited Format
+                                </a>
+                                <span style={{margin:'0px 10px'}}>|</span>
+                                <a onClick={(event) => this.handleTransposedOtherMolecularProfileDownload(event, selectedProfileName)}>
+                                    <i className='fa fa-cloud-download' style={{marginRight: 5}}/>Transposed Matrix
+                                </a>
+                            </div>
+                        )
+                    }
+
+
+                </td>
+            </tr>
+        );
+    }
+
+    @autobind
+    @action
+    private formatOptionLabel(props: any) {
+        return (
+            <div style={{ display: "flex" }}>
+                <div>{props.label}</div>
+                {
+                    (props.description) && (
+                        <DefaultTooltip
+                            mouseEnterDelay={0}
+                            placement="right"
+                            overlay={<div className={styles.tooltip}>{props.description}</div>}
+                        >
+                            <FontAwesome className={styles.infoIcon} name='question-circle'/>
+                        </DefaultTooltip>
+                    )
+                }
+            </div>
+        )
+    };
+
+    @autobind
+    private handleSelect(option: any) {
+        if (option && option.value) {
+            this.props.store.selectedMolecularProfileNameForDownload = option.value;
+        }
     }
 
     private copyDownloadControlsRow(title:string,
@@ -549,24 +688,36 @@ export default class DownloadTab extends React.Component<IDownloadTabProps, {}>
         onMobxPromise(this.transposedMutationDataText, text=>fileDownload(text, "mutations_transposed.txt"));
     }
 
-    private handleMrnaDownload()
+    @autobind
+    private handleOtherMolecularProfileDownload(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, profileName: string)
     {
-        onMobxPromise(this.mrnaDataText, text=>fileDownload(text, "mRNA_exp.txt"));
+        onMobxPromise(this.otherMolecularProfileDataText, text=>fileDownload(text, `${profileName}.txt`));
     }
 
-    private handleTransposedMrnaDownload()
+    @autobind
+    private handleTransposedOtherMolecularProfileDownload(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, profileName: string)
     {
-        onMobxPromise(this.transposedMrnaDataText, text=>fileDownload(text, "mRNA_exp_transposed.txt"));
+        onMobxPromise(this.transposedOtherMolecularProfileDataText, text=>fileDownload(text, `${profileName}.txt`));
     }
 
-    private handleProteinDownload()
+    private handleMrnaDownload(profileName: string)
     {
-        onMobxPromise(this.proteinDataText, text=>fileDownload(text, "protein_exp.txt"));
+        onMobxPromise(this.mrnaDataText, text=>fileDownload(text, `${profileName}.txt`));
     }
 
-    private handleTransposedProteinDownload()
+    private handleTransposedMrnaDownload(profileName: string)
     {
-        onMobxPromise(this.transposedProteinDataText, text=>fileDownload(text, "protein_exp_transposed.txt"));
+        onMobxPromise(this.transposedMrnaDataText, text=>fileDownload(text, `${profileName}.txt`));
+    }
+
+    private handleProteinDownload(profileName: string)
+    {
+        onMobxPromise(this.proteinDataText, text=>fileDownload(text, `${profileName}.txt`));
+    }
+
+    private handleTransposedProteinDownload(profileName: string)
+    {
+        onMobxPromise(this.transposedProteinDataText, text=>fileDownload(text, `${profileName}.txt`));
     }
 
     private handleCnaDownload()
