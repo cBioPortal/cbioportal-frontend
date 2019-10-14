c4cda8b62e53b85d98e0555ca7a02b7d60289df3
Pim van Nierop <pim@thehyve.nl>
2019-10-14 17:38:24 +0200

    Fix flaky e2e-localdb test for utilities menu

--- end-to-end-test/local/specs/core/plotstab.spec.js
+++ end-to-end-test/local/specs/core/plotstab.spec.js
@@ -14,14 +14,14 @@ describe('plots tab', function() {
             it('is shown when plot data available',function(){
                 var url = `${CBIOPORTAL_URL}/results/plots?Action=Submit&RPPA_SCORE_THRESHOLD=2.0&Z_SCORE_THRESHOLD=2.0&cancer_study_list=study_es_0&case_set_id=study_es_0_all&clinicallist=NUM_SAMPLES_PER_PATIENT%2CPROFILED_IN_study_es_0_mutations%2CPROFILED_IN_study_es_0_gistic%2CPROFILED_IN_study_es_0_mrna_median_Zscores&data_priority=0&gene_list=RPS11&geneset_list=%20&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=study_es_0_gistic&genetic_profile_ids_PROFILE_MRNA_EXPRESSION=study_es_0_mrna_median_Zscores&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=study_es_0_mutations&show_samples=false&tab_index=tab_visualize`;
                 goToUrlAndSetLocalStorage(url);
-                waitForPlotsTab();
+                browser.waitForVisible('div[data-test="PlotsTabPlotDiv"]');
                 assert( $('div.utilities-menu').isExisting());
             });
 
             it('is hidden when plot data unavailable',function(){
                 var url = `${CBIOPORTAL_URL}/results/plots?Action=Submit&RPPA_SCORE_THRESHOLD=2.0&Z_SCORE_THRESHOLD=2.0&cancer_study_list=study_es_0&case_set_id=study_es_0_all&clinicallist=NUM_SAMPLES_PER_PATIENT%2CPROFILED_IN_study_es_0_mutations%2CPROFILED_IN_study_es_0_gistic%2CPROFILED_IN_study_es_0_mrna_median_Zscores&data_priority=0&gene_list=AR%2520RPS11&geneset_list=%20&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=study_es_0_gistic&genetic_profile_ids_PROFILE_MRNA_EXPRESSION=study_es_0_mrna_median_Zscores&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=study_es_0_mutations&show_samples=false&tab_index=tab_visualize`;
                 goToUrlAndSetLocalStorage(url);
-                waitForPlotsTab();
+                browser.waitForVisible('div[data-test="PlotsTabPlotDiv"]');
                 assert(!$('div.utilities-menu').isExisting());
             });
 
--- env/custom.sh
+++ env/custom.sh
@@ -1,3 +1,2 @@
 # set e.g.
 # export CBIOPORTAL_URL="http://localhost:8080"
-export BACKEND=sheridancbio:hide-internal-identifiers-in-treatments-api
