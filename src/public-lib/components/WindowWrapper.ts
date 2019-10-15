f77d3a2305b757768d51eaccca8354da59a484bd
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-10-15 12:29:58 -0400

    Resize oncoprint instantly for e2e tests
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- src/public-lib/components/WindowWrapper.ts
+++ src/public-lib/components/WindowWrapper.ts
@@ -10,25 +10,39 @@ export interface WindowSize {
 export default class WindowWrapper
 {
     @observable public size: WindowSize;
-    @observable public size500Ms: WindowSize;
+    @observable public sizeByMs:{
+        _0: WindowSize,
+        _500: WindowSize
+    } = {
+        _0: { width:-1, height:-1},
+        _500: { width:-1, height:-1}
+    };
 
-    private handleWindowResize = _.debounce(this.setWindowSize, 200);
-    private handleWindowResize500Ms = _.debounce(this.setWindowSize500Ms, 500);
+    private setWindowSize = _.debounce(this._setWindowSize, 200);
+    private setWindowSize500Ms = _.debounce(this._setWindowSize500Ms, 500);
     private windowObj: any;
 
     constructor() {
         if (typeof window === 'object') {
             this.windowObj = window;
-            this.setWindowSize();
-            this.setWindowSize500Ms();
+            this._setWindowSize();
+            this._setWindowSize500Ms();
+            this.setWindowSize0Ms();
             this.windowObj.addEventListener("resize", this.handleWindowResize);
-            this.windowObj.addEventListener("resize", this.handleWindowResize500Ms);
         }
     }
 
     @autobind
     @action
-    private setWindowSize() {
+    private handleWindowResize() {
+        this.setWindowSize();
+        this.setWindowSize0Ms();
+        this.setWindowSize500Ms();
+    }
+
+    @autobind
+    @action
+    private _setWindowSize() {
         this.size = {
             width: this.windowObj.innerWidth,
             height: this.windowObj.innerHeight
@@ -37,8 +51,17 @@ export default class WindowWrapper
 
     @autobind
     @action
-    private setWindowSize500Ms() {
-        this.size500Ms = {
+    private setWindowSize0Ms() {
+        this.sizeByMs._0 = {
+            width: this.windowObj.innerWidth,
+            height: this.windowObj.innerHeight
+        };
+    }
+
+    @autobind
+    @action
+    private _setWindowSize500Ms() {
+        this.sizeByMs._500 = {
             width: this.windowObj.innerWidth,
             height: this.windowObj.innerHeight
         };
