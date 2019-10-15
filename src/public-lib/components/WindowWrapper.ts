9c3645f4f4ddb06e6a8f0faa496b8e607277b0ab
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-10-15 16:55:32 -0400

    Simplify handling of window resizing
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 191575e0ec2020e01a61e8caf0b959665fe399e1

--- src/public-lib/components/WindowWrapper.ts
+++ src/public-lib/components/WindowWrapper.ts
@@ -1,6 +1,7 @@
 import autobind from "autobind-decorator";
 import _ from 'lodash';
 import {action, observable} from 'mobx';
+import {isWebdriver} from "../../shared/lib/tracking";
 
 export interface WindowSize {
     width: number,
@@ -10,58 +11,22 @@ export interface WindowSize {
 export default class WindowWrapper
 {
     @observable public size: WindowSize;
-    @observable public sizeByMs:{
-        _0: WindowSize,
-        _500: WindowSize
-    } = {
-        _0: { width:-1, height:-1},
-        _500: { width:-1, height:-1}
-    };
 
-    private setWindowSize = _.debounce(this._setWindowSize, 200);
-    private setWindowSize500Ms = _.debounce(this._setWindowSize500Ms, 500);
+    private handleWindowResize = isWebdriver() ? this.setWindowSize : _.debounce(this.setWindowSize, 500);
     private windowObj: any;
 
     constructor() {
         if (typeof window === 'object') {
             this.windowObj = window;
-            this._setWindowSize();
-            this._setWindowSize500Ms();
-            this.setWindowSize0Ms();
-            this.windowObj.addEventListener("resize", this.handleWindowResize);
+            this.setWindowSize();
+            this.windowObj.addEventListener("resize", this.handleWindowResize)
         }
     }
 
     @autobind
     @action
-    private handleWindowResize() {
-        this.setWindowSize();
-        this.setWindowSize0Ms();
-        this.setWindowSize500Ms();
-    }
-
-    @autobind
-    @action
-    private _setWindowSize() {
+    private setWindowSize() {
         this.size = {
-            width: this.windowObj.innerWidth,
-            height: this.windowObj.innerHeight
-        };
-    }
-
-    @autobind
-    @action
-    private setWindowSize0Ms() {
-        this.sizeByMs._0 = {
-            width: this.windowObj.innerWidth,
-            height: this.windowObj.innerHeight
-        };
-    }
-
-    @autobind
-    @action
-    private _setWindowSize500Ms() {
-        this.sizeByMs._500 = {
             width: this.windowObj.innerWidth,
             height: this.windowObj.innerHeight
         };
