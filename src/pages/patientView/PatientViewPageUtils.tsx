665c28eb5eecaac0708cd3d51d9dc931e33a6001
Pim van Nierop <pim@thehyve.nl>
2019-10-22 11:37:53 +0200

    Clean up code for more efficient implementation

--- src/pages/patientView/PatientViewPageUtils.tsx
+++ src/pages/patientView/PatientViewPageUtils.tsx
@@ -1,7 +1,4 @@
-import SampleManager from "./SampleManager";
-import { PatientViewPageStore } from "./clinicalInformation/PatientViewPageStore";
 import TumorColumnFormatter from "./mutation/column/TumorColumnFormatter";
-import { ClinicalDataBySampleId } from "shared/api/api-types-extended";
 import _ from "lodash";
 
 export function checkNonProfiledGenesExist( sampleIds:string[],
@@ -9,7 +6,7 @@ export function checkNonProfiledGenesExist( sampleIds:string[],
                                             sampleToGenePanelId:{[sampleId:string]:string},
                                             genePanelIdToEntrezGeneIds:{[genePanelId:string]:number[]}
 ):boolean {
-    return !! _.find(entrezGeneIds, entrezGeneId => {
+    return _.some(entrezGeneIds, entrezGeneId => {
         const profiledSamples = TumorColumnFormatter.getProfiledSamplesForGene( entrezGeneId, 
                                                                                 sampleIds,
                                                                                 sampleToGenePanelId,
