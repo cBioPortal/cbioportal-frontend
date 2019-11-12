6b519836c6312345c8222dc680c73386fc480e6a
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-11-12 14:29:27 -0500

    Update tests and screenshots
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 1e295572970b4ffa74e302fefe9dee3ff2519fae

--- end-to-end-test/remote/specs/core/mutationsTab.spec.js
+++ end-to-end-test/remote/specs/core/mutationsTab.spec.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var waitForOncoprint = require('../../../shared/specUtils').waitForOncoprint;
-var setOncoprintMutationsMenuOpen = require('../../../shared/specUtils').setOncoprintMutationsMenuOpen;
+var setResultsPageSettingsMenuOpen = require('../../../shared/specUtils').setResultsPageSettingsMenuOpen;
 var goToUrlAndSetLocalStorage = require('../../../shared/specUtils').goToUrlAndSetLocalStorage;
 
 const CBIOPORTAL_URL = process.env.CBIOPORTAL_URL.replace(/\/$/, "");
@@ -9,8 +9,9 @@ describe("mutations tab", function() {
     it("uses VUS filtering", function() {
         goToUrlAndSetLocalStorage(`${CBIOPORTAL_URL}/results/oncoprint?Action=Submit&RPPA_SCORE_THRESHOLD=2.0&Z_SCORE_THRESHOLD=2.0&cancer_study_list=acc_tcga_pan_can_atlas_2018&case_set_id=acc_tcga_pan_can_atlas_2018_cnaseq&data_priority=0&gene_list=HSD17B4&geneset_list=%20&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=acc_tcga_pan_can_atlas_2018_gistic&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=acc_tcga_pan_can_atlas_2018_mutations&tab_index=tab_visualize`);
         waitForOncoprint(60000);
-        setOncoprintMutationsMenuOpen(true);
+        setResultsPageSettingsMenuOpen(true);
         browser.click('input[data-test="HideVUS"]');
+        setResultsPageSettingsMenuOpen(false);
         browser.waitForExist('a.tabAnchor_mutations');
         browser.click('a.tabAnchor_mutations');
         browser.waitForVisible('[data-test="LazyMobXTable_CountHeader"]');
