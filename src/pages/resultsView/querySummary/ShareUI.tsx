6b74e3fdd2cedd79ad130427b02f9fd34d299251
lismana <lismanBusiness@gmail.com>
2019-12-18 17:46:16 -0500

    Bookmark feature should give user fullURL (and shortened version if bitly enabled) so that all url params (not just session) are preserved
    Former-commit-id: 1f9655cbc77c00ed1af609b7b416135f28678b90

--- src/pages/resultsView/querySummary/ShareUI.tsx
+++ src/pages/resultsView/querySummary/ShareUI.tsx
@@ -12,7 +12,6 @@ import ResultsViewURLWrapper from "pages/resultsView/ResultsViewURLWrapper";
 
 interface IShareUI {
     sessionEnabled: boolean;
-    //routingStore: ExtendedRouterStore;
     urlWrapper:ResultsViewURLWrapper;
     bitlyAccessToken?: string | null;
 }
@@ -20,9 +19,9 @@ interface IShareUI {
 const win = getBrowserWindow();
 
 export interface ShareUrls {
-    sessionUrl:string|undefined;
-    bitlyUrl:any|undefined;
+    bitlyUrl:string|undefined;
     fullUrl:string;
+    sessionUrl:string|undefined;
 }
 
 @observer
@@ -30,28 +29,11 @@ export class ShareUI extends React.Component<IShareUI, {}> {
 
     @observable showBookmarkDialog:boolean = false;
 
-    async getUrl():Promise<ShareUrls> {
+    async getUrls():Promise<ShareUrls> {
 
         let sessionUrl = win.location.href;
 
         let bitlyResponse;
-
-        if (this.props.sessionEnabled) {
-            if (this.props.urlWrapper.hasSessionId) {
-                sessionUrl = getBrowserWindow().location.href;
-            } else {
-                const resp = await this.props.urlWrapper.saveRemoteSession(this.props.urlWrapper.query);
-                // for testing purposes we don't have links to localhost
-                sessionUrl = URL.format({
-                    hostname: (win.location.hostname.includes("localhost") ? "www.cbioportal.org" : win.location.hostname),
-                    pathname: win.location.pathname,
-                    protocol: win.location.protocol,
-                    query: {
-                        session_id: resp.id
-                    }
-                });
-            }
-        }
 
         // now lets shorten with bityly, if we have key
         // WE ARE DISABLING BITLY PENDING DISCUSSION
@@ -66,40 +48,17 @@ export class ShareUI extends React.Component<IShareUI, {}> {
         }
 
         return {
-            sessionUrl,
             bitlyUrl: ((bitlyResponse && bitlyResponse.data && bitlyResponse.data.url) ? bitlyResponse.data.url : undefined),
-            fullUrl: win.location.href
+            fullUrl: win.location.href,
+            sessionUrl:sessionUrl
         }
 
-    }
-
-    @autobind
-    shareTwitter() {
-        this.getUrl().then((urlData:ShareUrls) => {
-            win.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(this.shortenedUrl(urlData))}&text=${encodeURIComponent(document.title)}&via=cbioportal`);
-        });
-    }
-
-    @autobind
-    openEmail() {
-        this.getUrl().then((urlData)=>{
-            window.location.href =
-                `mailto:?subject=${encodeURIComponent(document.title)}&body=${encodeURIComponent(this.shortenedUrl(urlData))}%20${encodeURIComponent(document.title)}`;
-        });
     }
 
     @autobind
     @action
     toggleBookmarkDialog(){
         this.showBookmarkDialog = !this.showBookmarkDialog;
-    }
-
-    shortenedUrl(urlData:ShareUrls){
-        const url = urlData.bitlyUrl || urlData.sessionUrl || urlData.fullUrl
-        if (!url) {
-            throw("URL bookmarking error");
-        }
-        return url;
     }
 
     render() {
@@ -114,7 +73,7 @@ export class ShareUI extends React.Component<IShareUI, {}> {
                 </a>
             </DefaultTooltip>
             {
-                (this.showBookmarkDialog) && (<BookmarkModal onHide={this.toggleBookmarkDialog} urlPromise={this.getUrl()}/>)
+                (this.showBookmarkDialog) && (<BookmarkModal onHide={this.toggleBookmarkDialog} urlPromise={this.getUrls()}/>)
             }
         </div>
     }
