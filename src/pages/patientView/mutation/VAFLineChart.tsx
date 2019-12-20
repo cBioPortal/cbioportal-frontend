9e40a043a4c8acf2c6e53c22a24012712ad630f4
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2019-12-20 15:01:30 -0500

    PVGE - vaf line chart unit tests
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 3b91c9551347b971abc75db1ae697fe76e5c2891

--- src/pages/patientView/mutation/VAFLineChart.tsx
+++ src/pages/patientView/mutation/VAFLineChart.tsx
@@ -26,6 +26,7 @@ import invertIncreasingFunction, {invertDecreasingFunction} from "../../../share
 import {MutationStatus, mutationTooltip} from "./PatientViewMutationsTabUtils";
 import {tickFormatNumeral} from "../../../shared/components/plots/TickUtils";
 import {once} from "mobx/lib/utils/utils";
+import {computeRenderData, IPoint} from "./VAFLineChartUtils";
 
 
 export interface IVAFLineChartProps {
@@ -37,14 +38,6 @@ export interface IVAFLineChartProps {
     svgRef:(elt:SVGElement|null)=>void;
     logScale:boolean;
     zeroToOneAxis:boolean;
-}
-
-interface IPoint {
-    x:number,
-    y:number,
-    sampleId:string,
-    mutation:Mutation;
-    mutationStatus:MutationStatus
 }
 
 export const SHOW_ONLY_SELECTED_LABEL = "Show only selected mutations";
@@ -65,11 +58,6 @@ class ScaleCapturer extends React.Component<any, any>{
 
         return <g/>;
     }
-}
-
-function isPointFoundedOnRealVAF(d:{ mutationStatus: MutationStatus }) {
-    return d.mutationStatus === MutationStatus.MUTATED_WITH_VAF ||
-        d.mutationStatus === MutationStatus.PROFILED_WITH_READS_BUT_UNCALLED;
 }
 
 class Tick extends React.Component<any, any>{
@@ -290,132 +278,13 @@ export default class VAFLineChart extends React.Component<IVAFLineChartProps, {}
     }
 
     @computed get renderData() {
-
-        const grayPoints:IPoint[] = []; // points that are purely interpolated for rendering, dont have data of their own
-        const lineData:IPoint[][] = [];
-
-        for (const mergedMutation of this.mutations) {
-            // determine data points in line for this mutation
-
-            // first add data points for each mutation
-            let thisLineData:Partial<IPoint>[] = [];
-            // keep track of which samples have mutations
-            const samplesWithData:{[uniqueSampleKey:string]:boolean} = {};
-            for (const mutation of mergedMutation) {
-                const sampleKey = mutation.uniqueSampleKey;
-                const sampleId = mutation.sampleId;
-                const vaf = getVariantAlleleFrequency(mutation);
-                if (vaf !== null) {
-                    // has VAF data
-
-                    if (mutation.mutationStatus.toLowerCase() === "uncalled") {
-                        if (mutation.tumorAltCount > 0) {
-                            // add point for uncalled mutation with supporting reads
-                            thisLineData.push({
-                                x: this.sampleIdIndex[sampleId],
-                                y: vaf,
-                                sampleId,
-                                mutation,
-                                mutationStatus: MutationStatus.PROFILED_WITH_READS_BUT_UNCALLED
-                            });
-                            samplesWithData[sampleKey] = true;
-                        }
-                    } else {
-                        // add point for called mutation with VAF data
-                        thisLineData.push({
-                            x: this.sampleIdIndex[sampleId],
-                            y: vaf,
-                            sampleId,
-                            mutation,
-                            mutationStatus: MutationStatus.MUTATED_WITH_VAF
-                        });
-                        samplesWithData[sampleKey] = true;
-                    }
-                } else {
-                    // no VAF data - add point which will be extrapolated
-                    thisLineData.push({
-                        x: this.sampleIdIndex[sampleId],
-                        sampleId, mutation,
-                        mutationStatus: MutationStatus.MUTATED_BUT_NO_VAF,
-                    });
-                    samplesWithData[sampleKey] = true;
-                }
-            }
-
-            const mutation = mergedMutation[0];
-            // add data points for samples without mutations
-            for (const sample of this.props.samples) {
-                if (!(sample.uniqueSampleKey in samplesWithData)) {
-                    if (!isSampleProfiled(
-                        sample.uniqueSampleKey,
-                        this.props.mutationProfileId,
-                        mutation.gene.hugoGeneSymbol,
-                        this.props.coverageInformation
-                    )) {
-                        // not profiled
-                        thisLineData.push({
-                            x: this.sampleIdIndex[sample.sampleId],
-                            sampleId: sample.sampleId, mutation,
-                            mutationStatus: MutationStatus.NOT_PROFILED,
-                        });
-                    } else {
-                        thisLineData.push({
-                            x: this.sampleIdIndex[sample.sampleId],
-                            y: 0,
-                            sampleId: sample.sampleId, mutation,
-                            mutationStatus: MutationStatus.PROFILED_BUT_NOT_MUTATED,
-                        });
-                    }
-                }
-            }
-            // sort by sample order
-            thisLineData = _.sortBy(thisLineData, d=>d.x);
-            // interpolate missing y values
-            // take out anything from the left and right that dont have data - we'll only interpolate points with data to their left and right
-            while (thisLineData.length > 0 && !isPointFoundedOnRealVAF(thisLineData[0] as IPoint)) {
-                thisLineData.shift();
-            }
-            while (thisLineData.length > 0 && !isPointFoundedOnRealVAF(thisLineData[thisLineData.length-1] as IPoint)) {
-                thisLineData.pop();
-            }
-            if (thisLineData.length === 0) {
-                // skip this mutation if theres no line data left to plot
-                continue;
-            }
-            // interpolate, and pull out interpolated points into gray points array
-            const thisLineDataWithoutGrayPoints:IPoint[] = [];
-            const thisGrayPoints:IPoint[] = [];
-            for (let i=0; i<thisLineData.length; i++) {
-                if (i !== 0 && i !== thisLineData.length-1 && thisLineData[i].y === undefined) {
-                    // find closest defined data to the left and right
-                    let leftIndex = 0, rightIndex = 0;
-                    for (leftIndex=i; leftIndex>=0; leftIndex--) {
-                        if (thisLineData[leftIndex].y !== undefined) {
-                            break;
-                        }
-                    }
-                    for (rightIndex=i; rightIndex<=thisLineData.length-1; rightIndex++) {
-                        if (thisLineData[rightIndex].y !== undefined) {
-                            break;
-                        }
-                    }
-                    const step = 1/(rightIndex - leftIndex);
-                    thisLineData[i].y = (i-leftIndex)*step*(thisLineData[leftIndex].y! + thisLineData[rightIndex].y!);
-                    // add to grayPoints
-                    thisGrayPoints.push(thisLineData[i] as IPoint);
-                } else {
-                    thisLineDataWithoutGrayPoints.push(thisLineData[i] as IPoint);
-                }
-            }
-            // we know thisLineDataWithoutGrayPoints is nonempty because it could only be empty if every point in
-            //  it was gray, in which case it would have been made empty and skipped above.
-            grayPoints.push(...thisGrayPoints);
-            lineData.push(thisLineDataWithoutGrayPoints);
-        }
-        return {
-            lineData,
-            grayPoints,
-        };
+        return computeRenderData(
+            this.props.samples,
+            this.mutations,
+            this.sampleIdIndex,
+            this.props.mutationProfileId,
+            this.props.coverageInformation
+        );
     }
 
     @computed get mutationToDataPoints() {
