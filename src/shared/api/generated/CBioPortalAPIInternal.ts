6b3b1d5ef70a874fe04f9d4603d67221c8cba4f7
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2019-12-24 13:48:31 -0500

    update api docs
    Former-commit-id: bf383d952a1051f0dccc90c4057a4c9bf544133a

--- src/shared/api/generated/CBioPortalAPIInternal.ts
+++ src/shared/api/generated/CBioPortalAPIInternal.ts
@@ -388,10 +388,6 @@ export type MolecularProfileSampleCount = {
 
         'numberOfCNSegmentSamples': number
 
-        'numberOfFusionProfiledSamples': number
-
-        'numberOfFusionUnprofiledSamples': number
-
         'numberOfMutationProfiledSamples': number
 
         'numberOfMutationUnprofiledSamples': number
@@ -500,8 +496,6 @@ export type Sample = {
 
         'patientId': string
 
-        'profiledForFusions': boolean
-
         'sampleId': string
 
         'sampleType': "Primary Solid Tumor" | "Recurrent Solid Tumor" | "Primary Blood Tumor" | "Recurrent Blood Tumor" | "Metastatic" | "Blood Derived Normal" | "Solid Tissues Normal"
@@ -519,6 +513,10 @@ export type SampleIdentifier = {
     'sampleId': string
 
         'studyId': string
+
+};
+export type ServerStatusMessage = {
+    'status': string
 
 };
 export type StudyViewFilter = {
@@ -539,8 +537,6 @@ export type StudyViewFilter = {
         'studyIds': Array < string >
 
         'withCNAData': boolean
-
-        'withFusionData': boolean
 
         'withMutationData': boolean
 
@@ -2603,6 +2599,67 @@ export default class CBioPortalAPIInternal {
                 return response.body;
             });
         };
+    getServerStatusUsingGETURL(parameters: {
+        $queryParameters ? : any
+    }): string {
+        let queryParameters: any = {};
+        let path = '/health';
+
+        if (parameters.$queryParameters) {
+            Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                var parameter = parameters.$queryParameters[parameterName];
+                queryParameters[parameterName] = parameter;
+            });
+        }
+        let keys = Object.keys(queryParameters);
+        return this.domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '');
+    };
+
+    /**
+     * Get the running status of the server
+     * @method
+     * @name CBioPortalAPIInternal#getServerStatusUsingGET
+     */
+    getServerStatusUsingGETWithHttpInfo(parameters: {
+        $queryParameters ? : any,
+            $domain ? : string
+    }): Promise < request.Response > {
+        const domain = parameters.$domain ? parameters.$domain : this.domain;
+        const errorHandlers = this.errorHandlers;
+        const request = this.request;
+        let path = '/health';
+        let body: any;
+        let queryParameters: any = {};
+        let headers: any = {};
+        let form: any = {};
+        return new Promise(function(resolve, reject) {
+            headers['Accept'] = 'application/json';
+
+            if (parameters.$queryParameters) {
+                Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+                    var parameter = parameters.$queryParameters[parameterName];
+                    queryParameters[parameterName] = parameter;
+                });
+            }
+
+            request('GET', domain + path, body, headers, queryParameters, form, reject, resolve, errorHandlers);
+
+        });
+    };
+
+    /**
+     * Get the running status of the server
+     * @method
+     * @name CBioPortalAPIInternal#getServerStatusUsingGET
+     */
+    getServerStatusUsingGET(parameters: {
+        $queryParameters ? : any,
+            $domain ? : string
+    }): Promise < ServerStatusMessage > {
+        return this.getServerStatusUsingGETWithHttpInfo(parameters).then(function(response: request.Response) {
+            return response.body;
+        });
+    };
     getInfoUsingGETURL(parameters: {
         $queryParameters ? : any
     }): string {
