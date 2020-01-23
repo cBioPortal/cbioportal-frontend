2e543451103380624bae81f50d4138bd6c2d1ef1
lismana <lismanBusiness@gmail.com>
2020-01-23 12:22:10 -0500

    Add caching logic

--- scripts/cached_build.sh
+++ scripts/cached_build.sh
@@ -1,6 +1,10 @@
 cat yarn.lock webpack.config.js vendor-bundles.webpack.config.js > has_vender_build_changed
 
-vendorKey=$(md5sum has_vender_build_changed | cut -d ' ' -f 1)
+if hash md5 2>/dev/null; then
+    vendorKey=$(md5 -q has_vender_build_changed)
+else
+    vendorKey=$(md5sum has_vender_build_changed | cut -d ' ' -f 1)
+fi
 
 CACHE_ROOT=$CACHE_ROOT_FOLDER
 
@@ -19,9 +23,16 @@ else
       rm -r "./packages"
       cp -r "$CACHE_DIR/vendor_$vendorKey/packages" "./packages"
   else
+      echo "no cache detected for $CACHE_DIR/vendor_$vendorKey"
       mkdir "${CACHE_DIR}/vendor_$vendorKey"
       yarn run buildModules;
       cp -r ./packages "$CACHE_DIR/vendor_$vendorKey"
+
+       if [ -d "$CACHE_DIR/vendor_$vendorKey" ]; then
+          echo "cache primed for $CACHE_DIR/vendor_$vendorKey"
+       else
+          echo "cachr prime FAILED for $CACHE_DIR/vendor_$vendorKey"
+       fi
   fi
 fi
 
