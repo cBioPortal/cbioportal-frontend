33924a3e91e18cfb00ebfd8aea8ce694bc9c344b
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2020-03-04 16:08:52 -0500

    use base64 encoding and redirecting with bitly
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: 4d58752fd93df28ec6bcb1ee67d19b35c7366d16

--- src/shared/lib/bitly.ts
+++ src/shared/lib/bitly.ts
@@ -1,9 +1,17 @@
 import request from 'superagent';
+import { getEncodedRedirectUrl } from '../api/urls';
+
+export const EncodedURLParam = 'encodedURL';
 
 export async function getBitlyShortenedUrl(
     url: string,
     bitlyAccessToken?: string | null
 ): Promise<string | undefined> {
+    // we have to pass through a base64 encoded URL, and decode it, because bitly messes with URI-encoded URL params
+    //  in ways that can break the page (for example, if we have a URI-encoded square bracket in a query param value,
+    //  bitly decodes it, thus creating an invalid URL.
+    const encodedRedirectUrl = getEncodedRedirectUrl(url);
+
     let bitlyResponse;
 
     // now lets shorten with bityly, if we have key
@@ -13,7 +21,7 @@ export async function getBitlyShortenedUrl(
             bitlyResponse = await request
                 .post('https://api-ssl.bitly.com/v4/bitlinks')
                 .send({
-                    long_url: url,
+                    long_url: encodedRedirectUrl,
                 })
                 .set({
                     Authorization: `Bearer ${bitlyAccessToken}`,
