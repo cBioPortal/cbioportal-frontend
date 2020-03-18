39350a5dee4a4cb5bc09ca805564d0c5b5661855
lismana <lismanBusiness@gmail.com>
2020-03-18 15:24:02 -0400

    Load pathway mapper module dynamically and avoid doing so in IE11
    Former-commit-id: 3857863c7341680b48ca5b128092673665b9b438

--- vendor-bundles.webpack.config.js
+++ vendor-bundles.webpack.config.js
@@ -26,7 +26,6 @@ const config = {
             'd3',
             'datatables.net',
             'webpack-raphael',
-            'pathway-mapper',
             'pluralize',
             'react-if',
             'react-select1',
