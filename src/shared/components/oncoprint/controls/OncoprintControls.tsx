b9d5b9bce803a1644e4aabba1e17f3cc90c6028c
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2020-04-22 19:47:21 -0400

    oncoprint - improve clinical attrs dropdown rendering so as to remove jitter and loss of scroll location
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
    
    Former-commit-id: ca9faa858ffdfad05247d6d49e6a245309217d52

--- src/shared/components/oncoprint/controls/OncoprintControls.tsx
+++ src/shared/components/oncoprint/controls/OncoprintControls.tsx
@@ -734,6 +734,11 @@ export default class OncoprintControls extends React.Component<
         return 'Select all';
     }
 
+    @autobind
+    private getSelectedClinicalAttributeIds() {
+        return this.props.state.selectedClinicalAttributeIds;
+    }
+
     private getClinicalTracksMenu() {
         // TODO: put onFocus handler on CheckedSelect when possible
         // TODO: pass unmodified string array as value prop when possible
@@ -746,8 +751,8 @@ export default class OncoprintControls extends React.Component<
             return (
                 <AddClinicalTracks
                     store={this.props.store}
-                    selectedClinicalAttributeIds={
-                        this.props.state.selectedClinicalAttributeIds
+                    getSelectedClinicalAttributeIds={
+                        this.getSelectedClinicalAttributeIds as () => string[]
                     }
                     onChangeSelectedClinicalTracks={
                         this.props.handlers.onChangeSelectedClinicalTracks
