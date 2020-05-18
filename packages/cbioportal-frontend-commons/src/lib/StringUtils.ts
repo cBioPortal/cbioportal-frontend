26ec477c5b7a8a55bc4b603888459bff353b0f65
Hongxin Zhang <hongxin@cbio.mskcc.org>
2020-05-18 13:12:05 -0400

    Trim off html entities coming from NCI API
    Signed-off-by: Hongxin Zhang <hongxin@cbio.mskcc.org>
    
    revert back to string split method to trim off html tag entities
    
    The library string-strip-html has an issue supporting in ie11
    
    
    Former-commit-id: 0f2706600b3036f55e6574aed2298ae0d04c108f

--- packages/cbioportal-frontend-commons/src/lib/StringUtils.ts
+++ packages/cbioportal-frontend-commons/src/lib/StringUtils.ts
@@ -72,3 +72,24 @@ export function isUrl(str: string) {
     const pattern = /^http[s]?:\/\//;
     return pattern.test(str);
 }
+
+export const LESS_THAN_HTML_ENTITY = '&lt;';
+export const GREATER_THAN_HTML_ENTITY = '&gt;';
+
+// This method is only designed to trim off html tags <**> </**>
+// A full solution could be https://www.npmjs.com/package/string-strip-html
+// But there is an issue related to template literals (https://gitlab.com/codsen/codsen/-/issues/31)
+export function trimOffHtmlTagEntities(str: string) {
+    // I previously explored Regex but does not work really well in multiple matches
+    return (str || '')
+        .split(LESS_THAN_HTML_ENTITY)
+        .map(match => {
+            const elements = match.split(GREATER_THAN_HTML_ENTITY);
+            if (elements.length > 1) {
+                return elements[1];
+            } else {
+                return elements.length === 0 ? '' : elements[0];
+            }
+        })
+        .join('');
+}
