7224c9395fb1da17241d111c7560b04bb3b47d04
Ino de Bruijn <ino@ino.pm>
2020-05-28 18:20:11 -0400

    Add webinar 5 bilibili link
    Former-commit-id: f0e9f05bb62a9b6e78181463c87b731972a2976d

--- src/pages/staticPages/tutorials/Tutorials.tsx
+++ src/pages/staticPages/tutorials/Tutorials.tsx
@@ -346,15 +346,15 @@ export default class Tutorials extends React.Component<{}, {}> {
                             YouTube.com
                         </a>
                     </h4>
-                    {/*<span style={{ fontSize: 'large' }}> or </span>
+                    <span style={{ fontSize: 'large' }}> or </span>
                     <h4 style={{ display: 'inline' }}>
                         <a
                             target="_blank"
-                            href="https://www.bilibili.com/video/BV1VZ4y1W76p"
+                            href="https://www.bilibili.com/video/BV1jz4y197iU"
                         >
                             bilibili.com
                         </a>
-                    </h4>*/}
+                    </h4>
                     <span style={{ color: '#eee' }}> | </span>
                     <h4 style={{ display: 'inline' }}>
                         <a
