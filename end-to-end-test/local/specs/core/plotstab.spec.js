bc78f85226188965a68b04300fcd71dfc96af016
lismana <lismanBusiness@gmail.com>
2020-06-23 14:47:07 -0400

    Fix localdb tests after plots tab changes mistakenly merged
    Former-commit-id: ecfc5ba3d3d684f1291524f7764fa19fda06d041

--- end-to-end-test/local/specs/core/plotstab.spec.js
+++ end-to-end-test/local/specs/core/plotstab.spec.js
@@ -18,7 +18,8 @@ describe('plots tab', function() {
                 var url = `${CBIOPORTAL_URL}/results/plots?Action=Submit&RPPA_SCORE_THRESHOLD=2.0&Z_SCORE_THRESHOLD=2.0&cancer_study_list=study_es_0&case_set_id=study_es_0_all&clinicallist=NUM_SAMPLES_PER_PATIENT%2CPROFILED_IN_study_es_0_mutations%2CPROFILED_IN_study_es_0_gistic%2CPROFILED_IN_study_es_0_mrna_median_Zscores&data_priority=0&gene_list=RPS11&geneset_list=%20&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=study_es_0_gistic&genetic_profile_ids_PROFILE_MRNA_EXPRESSION=study_es_0_mrna_median_Zscores&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=study_es_0_mutations&show_samples=false&tab_index=tab_visualize`;
                 goToUrlAndSetLocalStorage(url);
                 browser.waitForVisible('div[data-test="PlotsTabPlotDiv"]');
-                assert($('div.utilities-menu').isExisting());
+                //browser.debug();
+                assert($('div.color-samples-toolbar-elt').isExisting());
             });
 
             it('is hidden when plot data unavailable', function() {
@@ -39,44 +40,36 @@ describe('plots tab', function() {
                 selectReactSelectOption(vertDataSelect, 'Treatment Response');
                 browser.waitForVisible('div[data-test="PlotsTabPlotDiv"]');
 
-                assert($('div.utilities-menu').isExisting());
+                assert($('div.coloring-menu').isExisting());
                 assert(
-                    $(
-                        'div.utilities-menu div.gene-select-container'
-                    ).isExisting()
-                );
-                assert(
-                    $('div.utilities-menu').$$('input[type="radio"]').length ===
-                        3
+                    $('div.coloring-menu').$$('input[type="checkbox"]')
+                        .length === 2
                 );
             });
 
-            it('shows mutation types in clinical attribute vs treatment plot', () => {
+            it('shows mutation and copy number by default', () => {
                 const res = browser.checkElement('[id=plots-tab-plot-svg]');
                 assertScreenShotMatch(res);
             });
 
-            it('shows CNA types in clinical attribute vs treatment plot', () => {
-                $(
-                    'div.utilities-menu input[data-test="ViewCopyNumber"]'
-                ).click();
+            it('shows only mutation types when copy number is de-selected', () => {
+                $('input[data-test="ViewCopyNumber"]').click();
                 const res = browser.checkElement('[id=plots-tab-plot-svg]');
                 assertScreenShotMatch(res);
+                // set it back
+                $('input[data-test="ViewCopyNumber"]').click();
             });
 
-            it('shows both mutation and CNA types in clinical attribute vs treatment plot', () => {
-                $(
-                    'div.utilities-menu input[data-test="ViewMutationAndCNA"]'
-                ).click();
+            it('shows only CNA types when mutation checkbox is deselected', () => {
+                $('input[data-test="ViewMutationType"]').click();
                 const res = browser.checkElement('[id=plots-tab-plot-svg]');
                 assertScreenShotMatch(res);
+                $('input[data-test="ViewMutationType"]').click();
             });
 
-            it('removes sample stylings when selecting "None" in gene selection box', () => {
-                $('.gene-select-container .Select-control').click();
-                $('.gene-select-container')
-                    .$$('.Select-option')[0]
-                    .click();
+            it('removes sample stylings when selecting None in gene selection box', () => {
+                $('.gene-select').click();
+                $('#react-select-4-option-0').click();
                 const res = browser.checkElement('[id=plots-tab-plot-svg]');
                 assertScreenShotMatch(res);
             });
