47c86be9c2e05ea9cf5abd6910b0bfe5779b8870
Adam Abeshouse <abeshoua@mskcc.org>
2020-06-24 15:44:36 -0400

    Automatically import localdb env variables when running tests
    Signed-off-by: Adam Abeshouse <abeshoua@mskcc.org>

--- scripts/setup_localdb.sh
+++ scripts/setup_localdb.sh
@@ -1,14 +1,9 @@
-export BACKEND=cbioportal:master
-export PORTAL_SOURCE_DIR=$(pwd) # change path if needed
-export DB_DATA_DIR=/tmp/mysql # change path if needed
-export TEST_HOME="$PORTAL_SOURCE_DIR/end-to-end-test"
-export CHROMEDRIVER_VERSION=83.0.4103.39
+eval "$(./localdb_env_vars.sh)"
 source $PORTAL_SOURCE_DIR/env/custom.sh
 cd $TEST_HOME/local/runtime-config
 eval "$(./setup_environment.sh)"
 cd $PORTAL_SOURCE_DIR
 
-export CBIOPORTAL_URL='http://localhost:8081'
 $TEST_HOME/local/runtime-config/setup_local_context.sh -p -d -e # remove flags to exclude specific stages if desired (see below)
 cd $PORTAL_SOURCE_DIR
 
