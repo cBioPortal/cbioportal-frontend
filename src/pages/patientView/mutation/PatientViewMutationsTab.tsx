69d66222631fa5ce4fc68eabc438431535538658
lismana <lismanBusiness@gmail.com>
2020-09-30 16:25:20 -0400

    VAF should take up entire width of portal

--- src/pages/patientView/mutation/PatientViewMutationsTab.tsx
+++ src/pages/patientView/mutation/PatientViewMutationsTab.tsx
@@ -140,7 +140,7 @@ export default class PatientViewMutationsTab extends React.Component<
         ],
         renderPending: () => <LoadingIndicator isLoading={true} size="small" />,
         render: () => (
-            <div className="borderedChart" style={{ display: 'inline-block' }}>
+            <div style={{ width: WindowStore.size.width - 50 }}>
                 {/*<div*/}
                 {/*    style={{*/}
                 {/*        display: 'flex',*/}
@@ -422,7 +422,6 @@ export default class PatientViewMutationsTab extends React.Component<
             ) {
                 return (
                     <div
-                        className="borderedChart"
                         style={{
                             marginBottom: 20,
                             padding: this.showTimeline ? 10 : 5,
