1a98a8dc59d77c6573e73824131e49082ca14dc7
Adam Abeshouse <abeshoua@mskcc.org>
2020-10-01 17:53:58 -0400

    Use bordered comparison vs icon everywhere
    Signed-off-by: Adam Abeshouse <abeshoua@mskcc.org>
    
    Former-commit-id: 4cd98bafc15f06d2c7c761ea9abfd5ecc5b7d3b4

--- src/shared/components/ComparisonVsIcon.tsx
+++ src/shared/components/ComparisonVsIcon.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 
 interface IComparisonVsIconProps {
-    width: number;
-    style: any;
+    width?: number;
+    style?: any;
+    className?: string;
 }
 const ComparisonVsIcon: React.FunctionComponent<IComparisonVsIconProps> = ({
     width,
     style,
+    className,
 }: IComparisonVsIconProps) => {
     return (
         <img
             src={require('../../rootImages/compare_vs.svg')}
+            className={className || ''}
             style={{
                 width,
                 border: '1px solid black',
                 padding: 2,
-                ...style,
+                ...(style || {}),
             }}
         />
     );
