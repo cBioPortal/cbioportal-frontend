7639e3bd0b97ad593af2888fb7fda86c2fe0c661
pnp300 <pim@thehyve.nl>
2020-10-08 10:44:52 +0200

    Small updates

--- end-to-end-test/local/docker/build_portal_image.sh
+++ end-to-end-test/local/docker/build_portal_image.sh
@@ -7,6 +7,7 @@ set -o pipefail
 # rc, master and tagged releases (e.g. 3.0.1) of cbioportal are available as prebuilt images
 # build a image of a specified backend if no prebuilt image exists
 if [[ $BACKEND_IMAGE_NAME == $CUSTOM_BACKEND_IMAGE_NAME ]]; then
+	echo "Building custom backend ..."
    dir=$PWD
    git clone "https://github.com/$BACKEND_PROJECT_USERNAME/cbioportal.git"
    cd $BACKEND_SOURCE_DIR
@@ -15,6 +16,5 @@ if [[ $BACKEND_IMAGE_NAME == $CUSTOM_BACKEND_IMAGE_NAME ]]; then
    mvn clean install -DskipTests
    unzip $BACKEND_SOURCE_DIR/portal/target/cbioportal*.war -d $BACKEND_SOURCE_DIR/portal/target/war-exploded
    cd $PWD
-else
-    docker pull $BACKEND_IMAGE_NAME
 fi
+docker pull $BACKEND_IMAGE_NAME
