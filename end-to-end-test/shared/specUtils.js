1d8bf693e54779d5de5f6854bd538df7043bf623
Aaron Lisman <lisman.aaron@gmail.com>
2020-11-16 11:20:58 -0500

    Update some e2e tests broken by data changes. Improve hiding mouse

--- end-to-end-test/shared/specUtils.js
+++ end-to-end-test/shared/specUtils.js
@@ -327,13 +327,32 @@ function checkElementWithTemporaryClass(
 }
 
 function checkElementWithMouseDisabled(selector, pauseTime, options) {
-    return checkElementWithTemporaryClass(
+    browser.execute(function() {
+        $("<div id='blockUIToDisableMouse'></div>")
+            .appendTo('body')
+            .css({
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                zIndex: 999999999,
+            });
+    });
+
+    const ret = checkElementWithTemporaryClass(
         selector,
         selector,
         'disablePointerEvents',
         pauseTime || 0,
         options
     );
+
+    browser.execute(function() {
+        $('#blockUIToDisableMouse').remove();
+    });
+
+    return ret;
 }
 
 function checkElementWithElementHidden(selector, selectorToHide, options) {
