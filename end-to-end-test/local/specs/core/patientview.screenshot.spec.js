94fd4f9e6e339aec60d350d293999edf9e1405f3
Gaofei Zhao <gzd23@mail.missouri.edu>
2020-12-04 10:08:38 -0500

    test more

--- end-to-end-test/local/specs/core/patientview.screenshot.spec.js
+++ end-to-end-test/local/specs/core/patientview.screenshot.spec.js
@@ -15,70 +15,7 @@ const genericAssayPatientViewUrl =
     '/patient?studyId=lgg_ucsf_2014_test_generic_assay&sampleId=P01_Pri';
 
 describe('patient view page', function() {
-    describe('mutation table for study with ASCN data', () => {
-        beforeEach(() => {
-            goToUrlAndSetLocalStorage(ascnPatientViewUrl);
-            waitForPatientView();
-        });
-
-        it('displays ASCN columns', () => {
-            var res = browser.checkElement(
-                'div[data-test=patientview-mutation-table] table'
-            );
-            assertScreenShotMatch(res);
-        });
-    });
-
-    describe('mutation table for study with no ASCN data', () => {
-        beforeEach(() => {
-            goToUrlAndSetLocalStorage(patientViewUrl);
-            waitForPatientView();
-        });
-
-        it('does not display ASCN columns for studies with no ASCN data', () => {
-            var res = browser.checkElement(
-                'div[data-test=patientview-mutation-table] table'
-            );
-            assertScreenShotMatch(res);
-        });
-    });
-
-    describe('gene panel icons', () => {
-        const iconIndexGenePanelSample = 2;
-        const iconIndexWholeGenomeSample = 3;
-
-        beforeEach(() => {
-            goToUrlAndSetLocalStorage(patientViewUrl);
-            waitForPatientView();
-        });
-
-        it('shows gene panel icons behind mutation and CNA genomic tracks', () => {
-            var res = browser.checkElement(
-                'div.genomicOverviewTracksContainer'
-            );
-            assertScreenShotMatch(res);
-        });
-
-        it('filters mutation tracks based on gene filter setting', () => {
-            switchGeneFilter('allSamples');
-            var res = browser.checkElement(
-                'div.genomicOverviewTracksContainer'
-            );
-            assertScreenShotMatch(res);
-        });
-
-        it('filters VAF plot based on gene filter setting when switching to "all samples"', () => {
-            switchGeneFilter('allSamples');
-            doVafPlotScreenshotTest();
-        });
-
-        it('filters VAF plot based on gene filter setting when switching to "any sample"', () => {
-            switchGeneFilter('anySample');
-            doVafPlotScreenshotTest();
-        });
-    });
-
-    describe.only('patient view mutational signatures', () => {
+    describe('patient view mutational signatures', () => {
         beforeEach(() => {
             goToUrlAndSetLocalStorage(genericAssayPatientViewUrl);
             waitForPatientView();
@@ -99,6 +36,11 @@ describe('patient view page', function() {
         it('show tooltip for patient who has significant v2 significant signatures', () => {
             browser.waitForVisible('div.progress', 20000);
             browser.moveToObject('div.progress', 0, 0);
+
+            browser.waitForVisible(
+                'div[data-test="SignificantMutationalSignaturesTooltip"]'
+            );
+
             assertScreenShotMatch(browser.checkElement('div.patientViewPage'));
         });
 
@@ -137,26 +79,6 @@ describe('patient view page', function() {
         });
     });
 });
-
-const switchGeneFilter = selectedOption => {
-    var filterIcon = $('div[data-test=patientview-mutation-table]').$(
-        'i[data-test=gene-filter-icon]'
-    );
-    filterIcon.click();
-    var selectMenu = $('.rc-tooltip');
-    const allGenesRadio = selectMenu.$('input[value=' + selectedOption + ']');
-    allGenesRadio.click();
-    filterIcon.click();
-};
-
-const doVafPlotScreenshotTest = () => {
-    var res = browser.checkElement('[data-test=vaf-plot]'); // grabs the full plot
-    browser.moveToObject('svg[data-test=vaf-plot]'); // moves pointer to plot thumbnail
-    var res = browser.checkElement('div[role=tooltip] [data-test=vaf-plot]'); // grabs the full plot
-    $('div[role=tooltip] [data-test=vaf-plot]').waitForExist();
-    res = browser.checkElement('div[role=tooltip] [data-test=vaf-plot]'); // grabs the full plot
-    assertScreenShotMatch(res);
-};
 
 const selectMutationalSignaturesVersion3 = () => {
     browser.waitForVisible(
