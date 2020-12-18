44fb87a111951d52bd19faaef84ff832644b661c
Pim van Nierop <pim@thehyve.nl>
2020-12-18 13:40:53 +0100

    Remove redundant type definitions
    PR feedback Gaofei
    
    
    Former-commit-id: ba34f088233a5ec28f07cf5f8fa7a96219b527a4

--- src/pages/groupComparison/AlterationsEnrichments.tsx
+++ src/pages/groupComparison/AlterationsEnrichments.tsx
@@ -14,63 +14,6 @@ export interface IAlterationsEnrichmentsProps {
     resultsViewStore?: ResultsViewPageStore;
 }
 
-export type CopyNumberEnrichmentTypeSelectorSettings = {
-    HOMDEL: boolean;
-    AMP: boolean;
-};
-
-export type MutationEnrichmentTypeSelectorSettings = {
-    missense_mutation: boolean;
-    missense: boolean;
-    missense_variant: boolean;
-    frame_shift_ins: boolean;
-    frame_shift_del: boolean;
-    frameshift: boolean;
-    frameshift_deletion: boolean;
-    frameshift_insertion: boolean;
-    de_novo_start_outofframe: boolean;
-    frameshift_variant: boolean;
-    nonsense_mutation: boolean;
-    nonsense: boolean;
-    stopgain_snv: boolean;
-    stop_gained: boolean;
-    splice_site: boolean;
-    splice: boolean;
-    splicing: boolean;
-    splice_site_snp: boolean;
-    splice_site_del: boolean;
-    splice_site_indel: boolean;
-    splice_region_variant: boolean;
-    splice_region: boolean;
-    translation_start_site: boolean;
-    initiator_codon_variant: boolean;
-    start_codon_snp: boolean;
-    start_codon_del: boolean;
-    nonstop_mutation: boolean;
-    stop_lost: boolean;
-    inframe_del: boolean;
-    inframe_deletion: boolean;
-    in_frame_del: boolean;
-    in_frame_deletion: boolean;
-    inframe_ins: boolean;
-    inframe_insertion: boolean;
-    in_frame_ins: boolean;
-    in_frame_insertion: boolean;
-    indel: boolean;
-    nonframeshift_deletion: boolean;
-    nonframeshift: boolean;
-    nonframeshift_insertion: boolean;
-    targeted_region: boolean;
-    inframe: boolean;
-    truncating: boolean;
-    feature_truncation: boolean;
-    fusion: boolean;
-    silent: boolean;
-    synonymous_variant: boolean;
-    any: boolean;
-    other: boolean;
-};
-
 @observer
 export default class AlterationsEnrichments extends React.Component<
     IAlterationsEnrichmentsProps,
