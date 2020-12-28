7e9d2ae78c07f7e95ff9744aa83801c41929fa67
Onur Sumer <s.onur.sumer@gmail.com>
2020-12-28 17:14:12 -0500

    rename germline mutation status values for Signal (related to knowledgesystems/signal#66)
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>

--- packages/cbioportal-utils/src/model/SignalMutationStatus.ts
+++ packages/cbioportal-utils/src/model/SignalMutationStatus.ts
@@ -1,7 +1,7 @@
 export enum SignalMutationStatus {
     SOMATIC = 'Somatic',
     GERMLINE = 'Germline',
-    BENIGN_GERMLINE = 'Rare Benign/VUS Germline',
-    PATHOGENIC_GERMLINE = 'Pathogenic Germline',
-    BIALLELIC_PATHOGENIC_GERMLINE = 'Biallelic Pathogenic Germline',
+    BENIGN_GERMLINE = 'Germline - Benign/VUS',
+    PATHOGENIC_GERMLINE = 'Germline - Pathogenic',
+    BIALLELIC_PATHOGENIC_GERMLINE = 'Germline - Biallelic Pathogenic',
 }
