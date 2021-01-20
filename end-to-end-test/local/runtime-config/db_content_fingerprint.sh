4ce5a445e6e00378a50fab39e4ca9e5ae2cad392
Pim van Nierop <pim@thehyve.nl>
2021-01-20 11:11:13 +0100

    Add genesets to db fingerprint

--- end-to-end-test/local/runtime-config/db_content_fingerprint.sh
+++ end-to-end-test/local/runtime-config/db_content_fingerprint.sh
@@ -13,6 +13,7 @@ fi
 
 cd $TEST_HOME/local/docker_compose/cbioportal-docker-compose
 CHECKSUM_ES_0=$(docker-compose $compose_extensions run --rm cbioportal sh -c 'tar -cf - /cbioportal/core/src/test/scripts/test_data/study_es_0/ 2> /dev/null | shasum -a 1')
+CHECKSUM_ES_0_GENESETS=$(docker-compose $compose_extensions run --rm cbioportal sh -c 'tar -cf - /cbioportal/core/src/test/resources/genesets/study_es_0* 2> /dev/null | shasum -a 1')
 CHECKSUM_TEST_STUDIES=$(find "$TEST_HOME"/local/studies/ -type f -exec md5sum {} \; | shasum -a 1)
 CHECKSUM_MIGRATION_SQL=$(docker-compose $compose_extensions run --rm cbioportal sh -c 'shasum -a 1 /cbioportal/db-scripts/src/main/resources/migration.sql')
 CHECKSUM_SEED_URL=$(echo "$DB_SEED_URL" | shasum -a 1)
@@ -20,9 +21,10 @@ CHECKSUM_SCHEMA_URL=$(echo "$DB_CGDS_URL" | shasum -a 1)
 
 cd $DIR
 CHECKSUM_ES_0=$(echo $CHECKSUM_ES_0 | cut -c 1-10)
+CHECKSUM_ES_0_GENESETS=$(echo CHECKSUM_ES_0_GENESETS | cut -c 1-10)
 CHECKSUM_TEST_STUDIES=$(echo $CHECKSUM_TEST_STUDIES | cut -c 1-10)
 CHECKSUM_MIGRATION_SQL=$(echo $CHECKSUM_MIGRATION_SQL | cut -c 1-10)
 CHECKSUM_SEED_URL=$(echo $CHECKSUM_SEED_URL | cut -c 1-10)
 CHECKSUM_SCHEMA_URL=$(echo $CHECKSUM_SCHEMA_URL | cut -c 1-10)
 
-echo $CHECKSUM_ES_0$CHECKSUM_TEST_STUDIES$CHECKSUM_MIGRATION_SQL$CHECKSUM_SEED_URL$CHECKSUM_SCHEMA_URL
+echo $CHECKSUM_ES_0$CHECKSUM_ES_0_GENESETS$CHECKSUM_TEST_STUDIES$CHECKSUM_MIGRATION_SQL$CHECKSUM_SEED_URL$CHECKSUM_SCHEMA_URL
