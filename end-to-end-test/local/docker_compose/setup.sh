e34e483d846201d3aa15ba05b4fbc58ecd192d1a
Pim van Nierop <pim@thehyve.nl>
2021-01-23 06:42:46 +0100

    Add persmissions for test studies to testuser account

--- end-to-end-test/local/docker_compose/setup.sh
+++ end-to-end-test/local/docker_compose/setup.sh
@@ -16,6 +16,17 @@ if [[ -n "$BACKEND_BUILD_URL" ]]; then
   compose_extensions="$compose_extensions -f ../cbioportal-custombranch.yml"
 fi
 
+# add permissions for test studies to testuser
+config_json="$(cat ../keycloak/keycloak-config.json)"
+studies=$(find $TEST_HOME/local/studies -name meta_study.txt -exec grep cancer_study_identifier {} \; | awk 'NF>1{print $NF}')
+studies=("study_es_0" $studies)
+studies_json_array=$(printf '%s\n' "${studies[@]}" | jq -R . | jq -s .)
+studies_json_object=$(jq '[.[] | {name: .}]' <<< $studies_json_array)
+config_json=$(jq '.roles.client.cbioportal='"$studies_json_object" <<< $config_json)
+config_json=$(jq '(.groups[] | select (.name == "PUBLIC_STUDIES") | .clientRoles.cbioportal) = '"$studies_json_array" <<< $config_json)
+echo 'Creating realm ... (see keycloak-config-generated.json for settings)'
+echo "$config_json" > ../keycloak/keycloak-config-generated.json
+
 echo "docker-compose $compose_extensions build"
 docker-compose $compose_extensions build
 
