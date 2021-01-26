963d6c2e8cc2bab12058918a6cc5ec5b71e6ced3
Karthik Kalletla <kalletlakarthik@gmail.com>
2021-01-25 22:47:07 -0500

    Point to appropriate backend

--- env/custom.sh
+++ env/custom.sh
@@ -1,2 +1,4 @@
-export BRANCH_ENV="http://localhost:8080"
+# set e.g.
+# export CBIOPORTAL_URL="http://localhost:8080"
+export BACKEND=cbioportal:fusion-sv-migration
 export GENOME_NEXUS_URL="https://www.genomenexus.org"
