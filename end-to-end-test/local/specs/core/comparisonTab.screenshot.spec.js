356cb60b95f3edc95e06d82222f9e2a4ad7a9bd9
Pim van Nierop <pim@thehyve.nl>
2021-02-08 17:05:42 +0100

    Change waitForVisible into waitForExist

--- end-to-end-test/local/specs/core/comparisonTab.screenshot.spec.js
+++ end-to-end-test/local/specs/core/comparisonTab.screenshot.spec.js
@@ -15,7 +15,7 @@ describe('results view comparison tab screenshot tests', function() {
             goToUrlAndSetLocalStorage(
                 `${CBIOPORTAL_URL}/results/comparison?genetic_profile_ids_PROFILE_MUTATION_EXTENDED=lgg_ucsf_2014_test_generic_assay_mutations&cancer_study_list=lgg_ucsf_2014_test_generic_assay&Z_SCORE_THRESHOLD=2.0&RPPA_SCORE_THRESHOLD=2.0&data_priority=0&profileFilter=0&case_set_id=lgg_ucsf_2014_test_generic_assay_sequenced&gene_list=TOPAZ1%2520ANK1%2520ACAN%2520INTS4&geneset_list=%20&tab_index=tab_visualize&Action=Submit&comparison_subtab=alterations&comparison_selectedGroups=%5B"TOPAZ1"%2C"ANK1"%2C"ACAN"%2C"INTS4"%5D`
             );
-            browser.waitForVisible(
+            browser.waitForExist(
                 'div[data-test="GroupComparisonAlterationEnrichments"]',
                 20000
             );
@@ -33,7 +33,7 @@ describe('results view comparison tab screenshot tests', function() {
             browser.execute(function() {
                 comparisonTab.store.setUsePatientLevelEnrichments(true);
             });
-            browser.waitForVisible(
+            browser.waitForExist(
                 'div[data-test="GroupComparisonAlterationEnrichments"]',
                 10000
             );
@@ -116,7 +116,7 @@ describe('results view comparison tab screenshot tests', function() {
             goToUrlAndSetLocalStorage(
                 `${CBIOPORTAL_URL}/results/comparison?genetic_profile_ids_PROFILE_MUTATION_EXTENDED=lgg_ucsf_2014_test_generic_assay_mutations&cancer_study_list=lgg_ucsf_2014_test_generic_assay&Z_SCORE_THRESHOLD=2.0&RPPA_SCORE_THRESHOLD=2.0&data_priority=0&profileFilter=0&case_set_id=lgg_ucsf_2014_test_generic_assay_sequenced&gene_list=TOPAZ1%2520ANK1%2520ACAN%2520INTS4&geneset_list=%20&tab_index=tab_visualize&Action=Submit&comparison_subtab=alterations&comparison_selectedGroups=%5B"ACAN"%2C"INTS4"%5D`
             );
-            browser.waitForVisible(
+            browser.waitForExist(
                 'div[data-test="GroupComparisonAlterationEnrichments"]',
                 10000
             );
