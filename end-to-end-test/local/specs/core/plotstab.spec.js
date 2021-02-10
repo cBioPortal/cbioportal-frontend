8a1b51abea41a65749f1f09609b81e8a7de48d47
Pim van Nierop <pim@thehyve.nl>
2021-02-10 06:22:52 +0100

    Fix gene selection

--- end-to-end-test/local/specs/core/plotstab.spec.js
+++ end-to-end-test/local/specs/core/plotstab.spec.js
@@ -75,7 +75,13 @@ describe('plots tab', function() {
                 );
                 selectTreatmentProfile();
                 $('.gene-select').click();
-                $('#react-select-4-option-0').click();
+                // select gene menu entries
+                var geneMenuEntries = $('[data-test=GeneColoringMenu]')
+                    .$('div=Genes')
+                    .$('..')
+                    .$$('div')[1]
+                    .$$('div');
+                geneMenuEntries[0].click();
                 $('[data-test=PlotsTabPlotDiv]').waitForVisible();
                 const res = browser.checkElement('[id=plots-tab-plot-svg]');
                 assertScreenShotMatch(res);
