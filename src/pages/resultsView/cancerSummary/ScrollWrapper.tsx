3c3ad2e766a1f8ab1398d07962dfa24d0749c597
Luke Sikina <lucas@ds.dfci.harvard.edu>
2021-02-24 14:16:14 -0500

    fixed

--- src/pages/resultsView/cancerSummary/ScrollWrapper.tsx
+++ src/pages/resultsView/cancerSummary/ScrollWrapper.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Observer } from 'mobx-react';
+import { observer } from 'mobx-react';
 
 interface ScrollWrapperProps {
     children: React.ReactNode;
@@ -7,36 +7,33 @@ interface ScrollWrapperProps {
     assignDummyScrollPaneRef: (el: HTMLDivElement) => void;
 }
 
-function ScrollWrapper(props: ScrollWrapperProps) {
-    return (
-        <>
-            <Observer>
-                {() => (
-                    <div
-                        className="dummyScrollDiv scrollbarAlwaysVisible"
-                        style={{
-                            position: 'relative',
-                            width: '100%',
-                            maxWidth: props.plotElementWidth,
-                            overflow: 'scroll',
-                            marginTop: 35,
-                            marginBottom: -25, // reduce excessive padding caused by the marginTop
-                            zIndex: 1, // make sure it receives mouse even though marginBottom pulls the plot on top of it
-                        }}
-                        ref={props.assignDummyScrollPaneRef}
-                    >
-                        <div
-                            style={{
-                                minWidth: props.plotElementWidth - 8, // subtract 8 due to the pseudo-scrollbar element adding bulk
-                                height: 1,
-                            }}
-                        />
-                    </div>
-                )}
-            </Observer>
-            {props.children}
-        </>
-    );
+@observer
+export default class ScrollWrapper extends React.Component<
+    ScrollWrapperProps,
+    {}
+> {
+    render() {
+        return (
+            <div
+                className="dummyScrollDiv scrollbarAlwaysVisible"
+                style={{
+                    position: 'relative',
+                    width: '100%',
+                    maxWidth: this.props.plotElementWidth,
+                    overflow: 'scroll',
+                    marginTop: 35,
+                    marginBottom: -25, // reduce excessive padding caused by the marginTop
+                    zIndex: 1, // make sure it receives mouse even though marginBottom pulls the plot on top of it
+                }}
+                ref={this.props.assignDummyScrollPaneRef}
+            >
+                <div
+                    style={{
+                        minWidth: this.props.plotElementWidth - 8, // subtract 8 due to the pseudo-scrollbar element adding bulk
+                        height: 1,
+                    }}
+                />
+            </div>
+        );
+    }
 }
-
-export default ScrollWrapper;
