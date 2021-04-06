084177e7b3a81782f902b0f51e5e03257d9b3cf3
Pim van Nierop <pim@thehyve.nl>
2021-04-06 09:45:28 +0200

    Remove redundant tests in custom-driver-annotations.spec.js

--- end-to-end-test/local/specs/custom-driver-annotations.spec.js
+++ end-to-end-test/local/specs/custom-driver-annotations.spec.js
@@ -2,11 +2,6 @@ var assert = require('assert');
 var goToUrlAndSetLocalStorage = require('../../shared/specUtils')
     .goToUrlAndSetLocalStorage;
 var waitForOncoprint = require('../../shared/specUtils').waitForOncoprint;
-var reactSelectOption = require('../../shared/specUtils').reactSelectOption;
-var getReactSelectOptions = require('../../shared/specUtils')
-    .getReactSelectOptions;
-var selectReactSelectOption = require('../../shared/specUtils')
-    .selectReactSelectOption;
 var useExternalFrontend = require('../../shared/specUtils').useExternalFrontend;
 var setSettingsMenuOpen = require('../../shared/specUtils').setSettingsMenuOpen;
 
@@ -18,15 +13,6 @@ const oncoprintTabUrl =
 const oncoprintTabUrlCna =
     CBIOPORTAL_URL +
     '/results/oncoprint?Action=Submit&RPPA_SCORE_THRESHOLD=2.0&Z_SCORE_THRESHOLD=2.0&cancer_study_list=study_es_0&case_set_id=study_es_0_cnaseq&data_priority=0&gene_list=ACAP3%2520AGRN&geneset_list=%20&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=study_es_0_gistic&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=study_es_0_mutations&profileFilter=0&tab_index=tab_visualize&show_samples=true';
-
-const studyViewUrlEs_0 = CBIOPORTAL_URL + '/study/summary?id=study_es_0';
-
-const studyViewUrlGenPanels =
-    CBIOPORTAL_URL + '/study/summary?id=teststudy_genepanels';
-
-const HIDDEN_MUTATION_ALTERATIONS = '[data-test=hidden-mutation-alterations]';
-const HIDDEN_FUSION_ALTERATIONS = '[data-test=hidden-fusion-alterations]';
-const HIDDEN_CNA_ALTERATIONS = '[data-test=hidden-cna-alterations]';
 
 describe('custom driver annotations feature', function() {
     if (useExternalFrontend) {
@@ -183,172 +169,6 @@ describe('custom driver annotations feature', function() {
                 assert(topCheckBox.isSelected());
                 assert(tiersCheckboxes[0].isSelected());
                 assert(tiersCheckboxes[1].isSelected());
-            });
-        });
-
-        describe('study view', () => {
-            describe('hint with instructions when hovering menu', () => {
-                it('not show the annotation filter button when no custom annotations', () => {
-                    goToUrlAndSetLocalStorage(studyViewUrlGenPanels);
-                    $('#comparisonGroupManagerContainer').waitForVisible();
-                    assert($('[data-test=AlterationFilterButton]').isVisible());
-                    $('[data-test=AlterationFilterButton]').click();
-                    $('[data-test=GlobalSettingsButtonHint]').waitForVisible();
-                });
-            });
-
-            describe('study with custom driver annotations', () => {
-                beforeEach(() => {
-                    goToUrlAndSetLocalStorage(studyViewUrlEs_0);
-                    $('#comparisonGroupManagerContainer').waitForVisible();
-                    $('[data-test=mutations-table]').waitForVisible();
-                    $$('[data-test=cancer-gene-filter]').forEach(f =>
-                        f.click()
-                    );
-                    $('[data-test=mutations-table]').waitForVisible();
-                    $('[data-test=AlterationFilterButton]').waitForVisible();
-                    setSettingsMenuOpen(true, 'AlterationFilterButton');
-                });
-
-                it('shows settings menu', () => {
-                    // shows custom driver options
-                    var topCheckBox = $(
-                        'input[data-test=annotateCustomBinary]'
-                    );
-                    assert(topCheckBox.isSelected());
-
-                    var tiersCheckboxes = $(
-                        'span[data-test=annotateCustomTiers]'
-                    ).$$('input');
-                    assert(tiersCheckboxes.length > 0);
-
-                    // does not show remote options (oncokb, ...)
-                    assert(!$('[data-test=annotateOncoKb]').isExisting());
-
-                    // does show VUS and germline filtering options
-                    assert($('[data-test=HideVUS]').isVisible());
-                    assert($('[data-test=HideGermline]').isVisible());
-
-                    // does not show exlude samples option
-                    assert(!$('[data-test=HideUnprofiled]').isExisting());
-                });
-
-                it('updates mutated genes counts when VUS alterations are excluded', () => {
-                    var values = $$(
-                        '[data-test="mutations-table"] [data-test=numberOfAlteredCasesText]'
-                    );
-
-                    assert.strictEqual(values.length, 13);
-                    assert(
-                        !$(
-                            '[data-test=mutations-table] [data-test=gene-column-header]'
-                        )
-                            .getText()
-                            .includes('⚠️')
-                    );
-
-                    $('input[data-test=HideVUS]').click();
-
-                    $('[data-test=mutations-table]').waitForVisible();
-                    values = $$(
-                        '[data-test="mutations-table"] [data-test=numberOfAlteredCasesText]'
-                    );
-                    assert.strictEqual(values.length, 2);
-                    assert(
-                        $(
-                            '[data-test=mutations-table] [data-test=gene-column-header]'
-                        )
-                            .getText()
-                            .includes('⚠️')
-                    );
-                    browser.moveToObject(
-                        '[data-test=mutations-table] [data-test=gene-column-header]'
-                    );
-                    $(HIDDEN_MUTATION_ALTERATIONS).waitForVisible();
-                    assert(
-                        $(HIDDEN_MUTATION_ALTERATIONS)
-                            .getText()
-                            .includes('17 mutations in 6 genes')
-                    );
-                });
-
-                it('updates CNA genes counts when VUS alterations are excluded', () => {
-                    var values = $$(
-                        '[data-test="copy number alterations-table"] [data-test=numberOfAlteredCasesText]'
-                    );
-                    assert.strictEqual(values.length, 14);
-                    assert(
-                        !$(
-                            '[data-test="copy number alterations-table"] [data-test=gene-column-header]'
-                        )
-                            .getText()
-                            .includes('⚠️')
-                    );
-
-                    $('input[data-test=HideVUS]').click();
-                    $(
-                        '[data-test="copy number alterations-table"]'
-                    ).waitForVisible();
-
-                    values = $$(
-                        '[data-test="copy number alterations-table"] [data-test=numberOfAlteredCasesText]'
-                    );
-                    assert.strictEqual(values.length, 3);
-                    assert(
-                        $(
-                            '[data-test="copy number alterations-table"] [data-test=gene-column-header]'
-                        )
-                            .getText()
-                            .includes('⚠️')
-                    );
-                    browser.moveToObject(
-                        '[data-test="copy number alterations-table"] [data-test=gene-column-header]'
-                    );
-                    $(HIDDEN_CNA_ALTERATIONS).waitForVisible();
-                    assert(
-                        $(HIDDEN_CNA_ALTERATIONS)
-                            .getText()
-                            .includes('7 copy number alterations in 1 gene')
-                    );
-                });
-
-                it('updates fusion genes counts when VUS alterations are excluded', () => {
-                    var values = $$(
-                        '[data-test="fusions-table"] [data-test=numberOfAlteredCasesText]'
-                    );
-                    assert.strictEqual(values.length, 3);
-                    assert(
-                        !$(
-                            '[data-test=mutations-table] [data-test=gene-column-header]'
-                        )
-                            .getText()
-                            .includes('⚠️')
-                    );
-
-                    $('input[data-test=HideVUS]').click();
-                    $('[data-test=fusions-table]').waitForVisible();
-
-                    values = $$(
-                        '[data-test="fusions-table"] [data-test=numberOfAlteredCasesText]'
-                    );
-                    assert.strictEqual(values.length, 0);
-                    assert(
-                        $(
-                            '[data-test=fusions-table] [data-test=gene-column-header]'
-                        )
-                            .getText()
-                            .includes('⚠️')
-                    );
-                    browser.moveToObject(
-                        '[data-test=fusions-table] [data-test=gene-column-header]'
-                    );
-                    $(HIDDEN_FUSION_ALTERATIONS).waitForVisible();
-                    assert(
-                        $(HIDDEN_FUSION_ALTERATIONS)
-                            .getText()
-                            .includes('3 fusions in 2 genes')
-                    );
-                });
             });
         });
     }
