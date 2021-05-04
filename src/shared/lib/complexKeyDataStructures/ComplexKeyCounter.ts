25b6cd107a65a8f9e0042841599e8fa41b930dc2
Adam Abeshouse <abeshoua@mskcc.org>
2021-05-04 12:44:38 -0400

    Optimize annotation with cosmic and cbio count
    Signed-off-by: Adam Abeshouse <abeshoua@mskcc.org>
    
    Former-commit-id: de53a5f10840fcda85fdd6ae150ea2cc71575d29

--- src/shared/lib/complexKeyDataStructures/ComplexKeyCounter.ts
+++ src/shared/lib/complexKeyDataStructures/ComplexKeyCounter.ts
@@ -3,8 +3,12 @@ import ComplexKeyMap, { ComplexKey } from './ComplexKeyMap';
 export default class ComplexKeyCounter {
     private map: ComplexKeyMap<number> = new ComplexKeyMap<number>();
 
+    public add(key: ComplexKey, amount: number) {
+        this.map.set(key, (this.map.get(key) || 0) + amount);
+    }
+
     public increment(key: ComplexKey) {
-        this.map.set(key, (this.map.get(key) || 0) + 1);
+        this.add(key, 1);
     }
 
     public get(key: ComplexKey) {
