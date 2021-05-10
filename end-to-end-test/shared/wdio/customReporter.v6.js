dae79b056fb11f01980f953831b1ca8877770b1f
Adam Abeshouse <abeshoua@mskcc.org>
2021-05-10 18:35:04 -0400

    modify custom reporter
    Signed-off-by: Adam Abeshouse <abeshoua@mskcc.org>

--- end-to-end-test/shared/wdio/customReporter.v6.js
+++ end-to-end-test/shared/wdio/customReporter.v6.js
@@ -45,22 +45,14 @@ class CustomReporter extends WDIOReporter {
     }
 
     addTest(test) {
-        const existingTest = this.report.tests.find(
-            t => test.title === t.title
-        );
+        const existingTest = this.testsByUid[test.uid];
         const locked = existingTest && existingTest.locked === true;
         if (!locked) {
-            // attempt to remove it (could be undefined)
-            this.report.tests = _.remove(this.report.tests, existingTest);
-            this.report.tests.push(test);
+            this.testsByUid[test.uid] = test;
         }
     }
 
-    report = {
-        meta: {},
-
-        tests: [],
-    };
+    testsByUid = {};
 
     onRunnerStart() {}
     onBeforeCommand() {}
@@ -70,22 +62,31 @@ class CustomReporter extends WDIOReporter {
     onHookEnd() {}
     onTestStart() {}
     onTestPass(test) {}
-    onTestFail() {}
+    onTestFail(test) {}
     onTestSkip(test) {
         //this.report.tests.push(test);
     }
     onTestEnd(test) {
-        const filtered = this.report.tests.filter(t => {
-            return t.title !== test.title;
-        });
-        filtered.push(test);
-        this.report.tests = filtered;
+        this.testsByUid[test.uid] = test;
     }
     onSuiteEnd() {}
     onRunnerEnd() {
-        this.report.testHome = this.options[0].testHome;
+        let tests = Object.keys(this.testsByUid).map(k => {
+            const test = this.testsByUid[k];
+            const { output, ...simplified } = test;
+            return simplified;
+        });
 
-        const strReport = JSON.stringify(this.report, null, 5);
+        const outputPath = `${this.options[0].outputDir}/customReport.json`;
+        if (fs.existsSync(outputPath)) {
+            // combine existing tests with new tests
+            const existingTests = JSON.parse(
+                fs.readFileSync(outputPath).toString()
+            ).tests;
+            tests = [...existingTests, ...tests];
+        }
+
+        const strReport = JSON.stringify({ tests }, null, 5);
 
         fs.writeFileSync(
             `${this.options[0].outputDir}/customReportJSONP.js`,
@@ -96,14 +97,10 @@ class CustomReporter extends WDIOReporter {
             }
         );
 
-        fs.writeFileSync(
-            `${this.options[0].outputDir}/customReport.json`,
-            strReport,
-            err => {
-                if (err) console.log(err);
-                console.log('Successfully Written to File.');
-            }
-        );
+        fs.writeFileSync(outputPath, strReport, err => {
+            if (err) console.log(err);
+            console.log('Successfully Written to File.');
+        });
     }
 }
 
