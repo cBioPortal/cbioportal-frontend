982967c70885d50d29b2cc338bfefbd8d36afae2
alisman <lisman.aaron@gmail.com>
2021-06-22 11:12:21 -0400

    ignore ts error

--- src/shared/lib/comparison/ComparisonStore.ts
+++ src/shared/lib/comparison/ComparisonStore.ts
@@ -1035,6 +1035,7 @@ export default abstract class ComparisonStore {
                     enrichmentType: this.usePatientLevelEnrichments
                         ? 'PATIENT'
                         : 'SAMPLE',
+                    //@ts-ignore
                     groupsAndAlterationTypes,
                 });
             }
