cb2bd2216740e520c5f59ae991aaca4f2bfad608
Rajat Sirohi <sirohirajat6@gmail.com>
2021-08-01 15:12:36 -0400

    add link to filtered study view + fix URL generation
    Former-commit-id: 334366b33cca81a76463fbaefd63fd053d04b66d

--- src/pages/resultsView/querySummary/QuerySummary.tsx
+++ src/pages/resultsView/querySummary/QuerySummary.tsx
@@ -71,10 +71,10 @@ export default class QuerySummary extends React.Component<
                     submitToStudyViewPage(
                         this.props.store.queriedStudies.result,
                         this.props.store.filteredSamples.result!,
-                        this.props.store.queriedVirtualStudies.result.length >
-                            0,
                         this.props.store.filteredSamples.result!.length <
                             this.props.store.samples.result!.length,
+                        this.props.store.queriedVirtualStudies.result.length >
+                            0,
                         this.props.store.sampleLists.result
                     );
                 }}
