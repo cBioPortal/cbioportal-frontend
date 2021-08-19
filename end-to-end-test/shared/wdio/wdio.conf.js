32180ab25f630dde866fc833c73212152cc87287
alisman <lisman.aaron@gmail.com>
2021-08-19 15:52:14 -0400

    Adjust circleci and parrelization settings for e2e

--- end-to-end-test/shared/wdio/wdio.conf.js
+++ end-to-end-test/shared/wdio/wdio.conf.js
@@ -146,7 +146,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: debug ? 1 : defaultMaxInstances,
+    maxInstances: TEST_TYPE === 'remote' ? 5 : 5,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -154,11 +154,6 @@ exports.config = {
     //
     capabilities: [
         {
-            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-            // grid with only 5 firefox instances available you can make sure that not more than
-            // 5 instances get started at a time.
-            maxInstances: 5,
-            //
             browserName: 'chrome',
             'goog:chromeOptions': {
                 args: chromeArgs,
