3bd28e293054c9b2280cc72c533d24bd2f1e1ef6
Hongxin <5400599+zhx828@users.noreply.github.com>
2021-08-19 15:51:41 -0400

    Do not filter out structural variant when one of the genes is missing
    Signed-off-by: Hongxin <5400599+zhx828@users.noreply.github.com>

--- packages/cbioportal-utils/src/oncokb/OncoKbUtils.spec.tsx
+++ packages/cbioportal-utils/src/oncokb/OncoKbUtils.spec.tsx
@@ -1,6 +1,10 @@
 import { assert } from 'chai';
 
-import { generateQueryVariantId } from './OncoKbUtils';
+import {
+    generateAnnotateStructuralVariantQueryFromGenes,
+    generateQueryVariantId,
+    StructuralVariantType,
+} from './OncoKbUtils';
 
 describe('OncoKbUtils', () => {
     describe('generateQueryVariantId', () => {
@@ -10,6 +14,46 @@ describe('OncoKbUtils', () => {
 
         it('properly generates query variant id when only entrezGeneId is valid', () => {
             assert.equal(generateQueryVariantId(451, null), '451');
+        });
+    });
+
+    describe('generateAnnotateStructuralVariantQueryFromGenes', () => {
+        it('Handle regular fusion', () => {
+            const query = generateAnnotateStructuralVariantQueryFromGenes(
+                673,
+                55750,
+                'MEL',
+                StructuralVariantType.FUSION
+            );
+            assert.equal(query.geneA.entrezGeneId, 673);
+            assert.equal(query.geneB.entrezGeneId, 55750);
+            assert.equal(query.functionalFusion, true);
+            assert.equal(
+                query.structuralVariantType,
+                StructuralVariantType.FUSION
+            );
+        });
+        it('Handle intragenic alteration query', () => {
+            const query = generateAnnotateStructuralVariantQueryFromGenes(
+                673,
+                undefined,
+                'MEL',
+                StructuralVariantType.DELETION
+            );
+            assert.equal(query.geneA.entrezGeneId, 673);
+            assert.equal(query.geneB.entrezGeneId, 673);
+            assert.equal(query.functionalFusion, false);
+        });
+        it('Handle fusion when one gene is missing', () => {
+            const query = generateAnnotateStructuralVariantQueryFromGenes(
+                673,
+                undefined,
+                'MEL',
+                StructuralVariantType.FUSION
+            );
+            assert.equal(query.geneA.entrezGeneId, 673);
+            assert.isTrue(query.geneB.entrezGeneId === undefined);
+            assert.equal(query.functionalFusion, true);
         });
     });
 });
