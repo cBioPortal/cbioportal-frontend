f8f88f9b92c8f25c6990b72d47d7127d7d84a219
Diana Baiceanu <diana@thehyve.nl>
2021-08-20 20:38:55 +0200

    RFC 59 - show studies for which a user is not authorized
    Updated code after review comments (2)
    
    
    Former-commit-id: a3931324caaf8afa7a2a66e451ef0ba9854fe0a3

--- src/shared/components/lazyMobXTable/utils.ts
+++ src/shared/components/lazyMobXTable/utils.ts
@@ -1,10 +1,20 @@
 import { SHOW_ALL_PAGE_SIZE } from '../paginationControls/PaginationControls';
+import { CancerStudy } from 'cbioportal-ts-api-client';
+import AppConfig from 'appConfig';
 export function maxPage(displayDataLength: number, itemsPerPage: number) {
     if (itemsPerPage === SHOW_ALL_PAGE_SIZE) {
         return 0;
     } else {
         return Math.floor(Math.max(displayDataLength - 1, 0) / itemsPerPage);
     }
+}
+
+export function isQueriedStudyAuthorized(study: CancerStudy) {
+    return (
+        !AppConfig.serverConfig.skin_show_unauthorized_studies ||
+        (AppConfig.serverConfig.skin_show_unauthorized_studies &&
+            study.isAuthorized !== false)
+    );
 }
 
 const validSymbols = ['<', '<=', '>', '>=', '='];
