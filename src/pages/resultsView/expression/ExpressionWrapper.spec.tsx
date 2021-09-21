2ef1bcdc408184fc9506e260bddef1ff7cfa574c
Onur Sumer <s.onur.sumer@gmail.com>
2021-09-21 09:23:55 -0400

    remove defunct tests

--- src/pages/resultsView/expression/ExpressionWrapper.spec.tsx
+++ src/pages/resultsView/expression/ExpressionWrapper.spec.tsx
@@ -17,14 +17,12 @@ describe('Expression Wrapper', () => {
         instance = wrapper.instance() as ExpressionWrapper;
     });
 
-    // this is failing. we need new data to debug
-    /*it.skip('studies are sorted properly depending on sortBy setting',()=>{
-
-        instance.sortBy = "alphabetic";
-        assert.equal(instance.sortedData[2][0].studyId,  'chol_tcga', 'sorting is alphabetical');
-
-        instance.sortBy = "median";
-        assert.equal(instance.sortedData[2][0].studyId,  'laml_tcga', 'sort according to median values');
-
-    });*/
+    it.skip('studies are sorted properly depending on sortBy setting', () => {
+        // TODO this is failing. we need new data to debug
+        // instance.sortBy = "alphabetic";
+        // assert.equal(instance.sortedData[2][0].studyId,  'chol_tcga', 'sorting is alphabetical');
+        //
+        // instance.sortBy = "median";
+        // assert.equal(instance.sortedData[2][0].studyId,  'laml_tcga', 'sort according to median values');
+    });
 });
