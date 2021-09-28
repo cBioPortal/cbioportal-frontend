02f4bd9b9f3f55f671006c276c815a0eb63a61ec
Onur Sumer <s.onur.sumer@gmail.com>
2021-09-28 09:15:12 -0400

    mock session client for URLWrapper tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// mock global modules to prevent remote calls
+jest.mock('cbioportal-ts-api-client');
