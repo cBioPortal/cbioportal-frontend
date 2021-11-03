f13842e13cde523a82f0a6c04246589008d11435
Gaofei Zhao <gzd23@mail.missouri.edu>
2021-11-03 15:37:56 -0400

    Update tests
    Former-commit-id: f419b2b2d5be0a86b8e1c9cd50cb5d24dae9a224

--- end-to-end-test/local/specs/core/studyview.screenshot.spec.js
+++ end-to-end-test/local/specs/core/studyview.screenshot.spec.js
@@ -11,6 +11,10 @@ const CBIOPORTAL_URL = process.env.CBIOPORTAL_URL.replace(/\/$/, '');
 const ADD_CHART_BUTTON = "[data-test='add-charts-button']";
 const ADD_CHART_GENERIC_ASSAY_TAB =
     '.addChartTabs a.tabAnchor_MUTATIONAL_SIGNATURE_TEST';
+const GENERIC_ASSAY_PROFILE_SELECTION =
+    "[data-test='GenericAssayProfileSelection']";
+const CATEGORY_MUTATIONAL_SIGNATURE_PROFILE_TEXT =
+    'div=mutational signature category v2 (61 samples)';
 const WAIT_FOR_VISIBLE_TIMEOUT = 30000;
 
 describe('study view generic assay categorical/binary features', function() {
@@ -31,6 +35,21 @@ describe('study view generic assay categorical/binary features', function() {
             timeout: WAIT_FOR_VISIBLE_TIMEOUT,
         });
         $(ADD_CHART_GENERIC_ASSAY_TAB).click();
+
+        // Select category mutational signature profile
+        $(GENERIC_ASSAY_PROFILE_SELECTION).waitForDisplayed({
+            timeout: WAIT_FOR_VISIBLE_TIMEOUT,
+        });
+        $(GENERIC_ASSAY_PROFILE_SELECTION).click();
+
+        $(GENERIC_ASSAY_PROFILE_SELECTION)
+            .$(CATEGORY_MUTATIONAL_SIGNATURE_PROFILE_TEXT)
+            .waitForDisplayed({
+                timeout: WAIT_FOR_VISIBLE_TIMEOUT,
+            });
+        $(GENERIC_ASSAY_PROFILE_SELECTION)
+            .$(CATEGORY_MUTATIONAL_SIGNATURE_PROFILE_TEXT)
+            .click();
 
         // wait for generic assay data loading complete
         // and select a option
