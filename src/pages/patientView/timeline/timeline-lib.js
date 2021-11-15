b5a88e3b60ae6980b6977231237dbe724aa48387
alisman <lisman.aaron@gmail.com>
2021-11-15 17:55:52 -0500

    Remove legacy timeline component
    Former-commit-id: 0746c339dafa67274d837b2d8c7ed1d355ff70cb

--- src/pages/patientView/timeline/timeline-lib.js
+++
@@ -1,9 +0,0 @@
-// we have to shim both exports and imports for this library
-// so better to do so in once place and then export
-let clinicalTimelineExports = require('imports-loader?$=jquery&d3=d3&_=underscore!exports-loader' +
-    '?clinicalTimeline=clinicalTimeline' +
-    '&trimClinicalTimeline=trimClinicalTimeline' +
-    '&clinicalTimelineZoom=clinicalTimelineZoom' +
-    '&clinicalTimelineParser=clinicalTimelineParser!clinical-timeline');
-
-export default clinicalTimelineExports;
