0db0fa9e15186dd1419164317c980e611d7997ab
alisman <lisman.aaron@gmail.com>
2021-11-17 12:05:42 -0500

    Fix mocks of removed deps

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -13,5 +13,4 @@ jest.mock('shared/components/copyDownloadControls/CopyDownloadQueryLinks.tsx');
 
 // explicitly set mock for legacy packages
 jest.setMock('webpack-raphael', {});
-jest.setMock('qtip2', {});
 jest.setMock('jquery-migrate', {});
