fc48e380cdb4d0529945dcfb2cad3113c4fc31ce
alisman <lisman.aaron@gmail.com>
2021-11-22 13:49:13 -0500

    Do not retry specs
    Former-commit-id: a33f2ca42db83a4627bcd349c32b338c250d637f

--- end-to-end-test/shared/wdio/wdio.conf.js
+++ end-to-end-test/shared/wdio/wdio.conf.js
@@ -300,13 +300,13 @@ exports.config = {
     framework: 'mocha',
     //
     // The number of times to retry the entire specfile when it fails as a whole
-    specFileRetries: 2,
+    specFileRetries: 0,
     //
     // Delay in seconds between the spec file retry attempts
     // specFileRetriesDelay: 0,
     //
     // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-    specFileRetriesDeferred: true,
+    specFileRetriesDeferred: false,
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
