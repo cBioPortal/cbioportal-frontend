293734e1c75b4e78b9ca693e18a958bec6233b9b
Pim van Nierop <pim@thehyve.nl>
2022-01-20 16:11:27 +0100

    REMOVEME - add BACKEND var to custom.sh

--- env/custom.sh
+++ env/custom.sh
@@ -1,2 +1,3 @@
-#export CBIOPORTAL_URL="http://www.cbioportal.org"
+export CBIOPORTAL_URL="http://localhost:8080"
 export GENOME_NEXUS_URL="https://www.genomenexus.org"
+export BACKEND=cbioportal:master
