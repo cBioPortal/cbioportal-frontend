390e51c9fd5f3d5efd06c0f65792814cf5b6c4bf
Ino de Bruijn <ino@ino.bio>
2022-03-22 20:40:24 -0400

    fix missing comma

--- src/pages/patientView/timeline/TimelineWrapper.tsx
+++ src/pages/patientView/timeline/TimelineWrapper.tsx
@@ -65,7 +65,7 @@ const TimelineWrapper: React.FunctionComponent<ITimelineProps> = observer(
                 window.location.href.includes('htan_test_2021') &&
                 window.location.href.includes('HTA9_1');
             const isToxicityPortal = [
-                'www.cbioportal.org'
+                'www.cbioportal.org',
                 'triage.cbioportal.mskcc.org',
                 'cbioportal.mskcc.org',
                 'private.cbioportal.mskcc.org',
