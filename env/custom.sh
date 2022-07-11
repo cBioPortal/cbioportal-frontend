bc6fdf1711c6f490d42927e92598b06f7ea4604f
alisman <lisman.aaron@gmail.com>
2022-07-11 12:06:36 -0400

    Point at new sv backend branch

--- env/custom.sh
+++ env/custom.sh
@@ -1,3 +1,3 @@
 export CBIOPORTAL_URL="http://localhost:8080"
 export GENOME_NEXUS_URL="https://www.genomenexus.org"
-export BACKEND=averyniceday:rebased-sv-on-master
+export BACKEND=alisman:rebased-sv-on-master
