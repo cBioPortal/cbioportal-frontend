2374b7089714006c2e6e46ea2546b04238ddc4bf
Ino de Bruijn <ino@ino.bio>
2022-07-13 15:42:55 -0400

    use alisman backend image

--- env/custom.sh
+++ env/custom.sh
@@ -1,3 +1,3 @@
 #export CBIOPORTAL_URL="http://localhost:8080"
 #export GENOME_NEXUS_URL="https://www.genomenexus.org"
-export BACKEND="cbioportal:release-5.0.0"
+export BACKEND="alisman:release-5.0.0"
