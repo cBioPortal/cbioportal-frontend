bd74cfb9f2af1f91a296784a46f6001fc6458360
alisman <lisman.aaron@gmail.com>
2022-07-20 14:03:28 -0400

    Update backend

--- env/custom.sh
+++ env/custom.sh
@@ -1,3 +1,3 @@
 #export CBIOPORTAL_URL="http://localhost:8080"
 #export GENOME_NEXUS_URL="https://www.genomenexus.org"
-export BACKEND="alisman:release-5.0.0"
+export BACKEND="averyniceday:release-5.0.0"
