8fa60886bf670afef76bb115c0c404880040eb0c
alisman <lisman.aaron@gmail.com>
2022-07-26 16:22:11 -0400

    Run against onur's test branch

--- env/custom.sh
+++ env/custom.sh
@@ -1,3 +1,3 @@
 #export CBIOPORTAL_URL="http://localhost:8080"
 #export GENOME_NEXUS_URL="https://www.genomenexus.org"
-export BACKEND="averyniceday:release-5.0.0"
+export BACKEND="onursumer:fix-cases-without-panel-data"
