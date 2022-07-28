94c095d067598520c8d58a6c97062bc55a19bfef
Prasanna Kumar Jagannathan <37613906+jagnathan@users.noreply.github.com>
2022-07-28 18:03:33 -0400

    Update custom.sh
    Adding full branch name fix-9594-API-endpoints-cleanup instead of hash d6283f0a

--- env/custom.sh
+++ env/custom.sh
@@ -1,3 +1,3 @@
 #export CBIOPORTAL_URL="http://localhost:8080"
-export BACKEND=jagnathan:d6283f0ab
+export BACKEND=jagnathan:fix-9594-API-endpoints-cleanup
 #export GENOME_NEXUS_URL="https://www.genomenexus.org"
