50ac7246616c79118a450d3a0458d4d0996d9e97
alisman <lisman.aaron@gmail.com>
2022-08-11 11:03:16 -0400

    Cleanup commented-out code

--- src/shared/lib/oql/AccessorsForOqlFilter.ts
+++ src/shared/lib/oql/AccessorsForOqlFilter.ts
@@ -150,16 +150,6 @@ export default class AccessorsForOqlFilter
         );
     }
 
-    // public gene(d: Datum) {
-    //     if (
-    //         this.molecularAlterationType(d.molecularProfileId) ===
-    //         AlterationTypeConstants.STRUCTURAL_VARIANT
-    //     ) {
-    //         return (d as StructuralVariant).site1HugoSymbol;
-    //     }
-    //     return (d as any).gene.hugoGeneSymbol;
-    // }
-
     public gene(d: Datum) {
         if (
             this.molecularAlterationType(d.molecularProfileId) ===
