9a8271a52c1e3baf38525d56143635463b5313c9
oplantalech <oleguer@thehyve.nl>
2022-12-14 11:38:38 +0100

    Add feedback

--- src/shared/components/alterationsTableUtils.tsx
+++ src/shared/components/alterationsTableUtils.tsx
@@ -1,8 +1,4 @@
-import {
-    ExtendedMutationTableColumnType,
-    MutationTableColumn,
-} from 'shared/components/mutationTable/MutationTable';
-import { getServerConfig } from 'config/config';
+import _ from 'lodash';
 import { Column } from 'shared/components/lazyMobXTable/LazyMobXTable';
 export function adjustVisibility(
     tableColumns: Record<string, Column<any>>,
@@ -14,15 +10,15 @@ export function adjustVisibility(
         return;
     }
     const visibleColumns = visibleColumnsProperty.split(',');
-    Object.keys(tableColumns).forEach(column => {
-        if (visibleColumns.includes(column)) {
-            tableColumns[column].visible = true;
+    _.forIn(tableColumns, function(column, columnName) {
+        if (visibleColumns.includes(columnName)) {
+            column.visible = true;
         } else {
             // If not specified by name, only show the column when it is a namespace column and
             // the skin_mutation_table_namespace_column_show_by_default is 'true'.
             const isVisible =
-                namespaceColumns.includes(column) && showNamespaceColumns;
-            tableColumns[column].visible = isVisible;
+                namespaceColumns.includes(columnName) && showNamespaceColumns;
+            column.visible = isVisible;
         }
     });
 }
