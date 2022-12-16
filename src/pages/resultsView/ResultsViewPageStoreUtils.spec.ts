a740aa6513b36c0f54187ff112c05c0ad1f0c390
Pim van Nierop <pim@thehyve.nl>
2022-12-16 15:15:24 +0100

    Add '::' structural variant notation to OQL syntax

--- src/pages/resultsView/ResultsViewPageStoreUtils.spec.ts
+++ src/pages/resultsView/ResultsViewPageStoreUtils.spec.ts
@@ -1328,7 +1328,7 @@ const defaultOqlAlterations = (oql_parser.parse(
 )![0] as SingleGeneQuery).alterations;
 
 describe('getSampleAlteredMap', () => {
-    const filteredAlterationData = [
+    const filteredAlterationData = ([
         {
             cases: {
                 samples: {},
@@ -1518,6 +1518,7 @@ describe('getSampleAlteredMap', () => {
         {
             cases: {
                 samples: {},
+                patients: {},
             },
             oql: {
                 gene: 'KRAS',
@@ -1538,7 +1539,7 @@ describe('getSampleAlteredMap', () => {
                 ],
             },
         },
-    ] as IQueriedMergedTrackCaseData[];
+    ] as unknown) as IQueriedMergedTrackCaseData[];
 
     var samples = ([
         {
