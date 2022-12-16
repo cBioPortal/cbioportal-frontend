8e576c6bcdc4ec9f4b79e6df0064d8d4c14adb8e
alisman <lisman.aaron@gmail.com>
2022-12-16 15:34:26 -0500

    Cloodge to get lgggbm study overlap detection (other tcga studies are prefixed "lgg")

--- src/shared/lib/getOverlappingStudies.ts
+++ src/shared/lib/getOverlappingStudies.ts
@@ -9,10 +9,15 @@ export default function getOverlappingStudies(
         (memo, study: CancerStudy) => {
             if (/_tcga/.test(study.studyId)) {
                 // we need to find when root of study name is in duplicate, so strip out the modifiers (pub or pancan)
-                const initial = study.studyId.replace(
-                    /(_\d\d\d\d|_pub|(_pub\d\d\d\d)|_pan_can_atlas_\d\d\d\d)$/g,
-                    ''
-                );
+                const initial = study.studyId
+                    .replace(
+                        /(_\d\d\d\d|_pub|(_pub\d\d\d\d)|_pan_can_atlas_\d\d\d\d)$/g,
+                        ''
+                    )
+                    // this is a cloodge for a tcga study which does not respect convention of it's siblings with
+                    // which it overlaps
+                    .replace('lgggbm_', 'lgg_');
+
                 if (initial) {
                     if (initial in memo) {
                         memo[initial].push(study);
