e47032b0e8a34e114ed499f04a678a45a3c87df6
dubincorey <dubersincorporated@gmail.com>
2023-03-03 10:59:21 -0500

    add comment clarifying formatPercentValue function

--- src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.tsx
+++ src/pages/patientView/clinicalInformation/ClinicalInformationPatientTable.tsx
@@ -37,6 +37,7 @@ export default class ClinicalInformationPatientTable extends React.Component<
         const parsedFloat = parseFloat(data.value);
 
         if (parsedFloat != parseInt(data.value)) {
+            // the values formatted are not percentages in this case however the function cleanly rounds any float to 1 decimal place
             ret = formatPercentValue(parsedFloat);
         } else {
             ret = data.value;
