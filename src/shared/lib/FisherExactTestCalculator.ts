b261695ec79040a27eaf10a8426bef5904072967
zhajia <zhajia@umich.edu>
2023-04-05 13:58:54 -0400

    changes for #9943 on frontend

--- src/shared/lib/FisherExactTestCalculator.ts
+++ src/shared/lib/FisherExactTestCalculator.ts
@@ -1,3 +1,5 @@
+// Calculates the probability of observing the observed counts (a,b,c,d) in a 2x2 contingency table
+// given the total count (n) and the factorial of the counts (logFactorial).
 function getProbability(
     a: number,
     b: number,
@@ -19,6 +21,7 @@ function getProbability(
     return Math.exp(p);
 }
 
+// Cumulative p-value for a 2x2 contingency table using Fisher's exact test
 export function getCumulativePValue(
     a: number,
     b: number,
@@ -35,7 +38,9 @@ export function getCumulativePValue(
         logFactorial[j] = logFactorial[j - 1] + Math.log(j);
     }
 
+    // probability for the given contingency table
     p += getProbability(a, b, c, d, logFactorial);
+    // cumulative p-value for all tables with greater proportion of observations in 1st row and 1st column
     if (a * d >= b * c) {
         min = c < b ? c : b;
         for (i = 0; i < min; i++) {
@@ -43,6 +48,7 @@ export function getCumulativePValue(
         }
     }
 
+    // cumulative p-value for all tables with greater proportion of observations in 2nd row and 2nd column
     if (a * d < b * c) {
         min = a < d ? a : d;
         for (i = 0; i < min; i++) {
