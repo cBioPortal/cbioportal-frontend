72ff76a3a5f46b24c465aaeb5205a5af6c788060
Niklas Reimer <n.reimer@uni-luebeck.de>
2023-04-20 14:27:36 +0200

    start with oncokb treatment table

--- packages/oncokb-frontend-commons/src/components/OncoKbTreatmentTable.tsx
+++ packages/oncokb-frontend-commons/src/components/OncoKbTreatmentTable.tsx
@@ -15,6 +15,7 @@ import { EvidenceReferenceContent } from './oncokbCard/EvidenceReferenceContent'
 import mainStyles from './main.module.scss';
 import './oncoKbTreatmentTable.scss';
 import request from 'superagent';
+import { observable, observe } from 'mobx';
 
 let cancerdrugsUrl = localStorage.getItem('cancerdrugsUrl') || '';
 let cancerdrugsJsonUrl = localStorage.getItem('cancerdrugsJsonUrl') || '';
@@ -23,6 +24,8 @@ type OncoKbTreatmentTableProps = {
     variant: string;
     treatments: IndicatorQueryTreatment[];
 };
+
+const drugInfos = new Object() as DrugInfo;
 
 type EmaDrugInfo = {
     infoAvailable: boolean;
@@ -37,10 +40,6 @@ type EmaDrugInfo = {
 interface DrugInfo {
     [key: string]: [EmaDrugInfo];
 }
-
-type OncoKbTreatmentTableState = {
-    drugInfos: DrugInfo;
-};
 
 // export const OncoKbTreatmentTable: React.Component<OncoKbTreatmentTableProps, OncoKbTreatmentTableState> = ({variant, treatments,}) => {
 //     constructor(props: OncoKbTreatmentTableProps) {
@@ -91,131 +90,129 @@ export const OncoKbTreatmentTable: React.FunctionComponent<OncoKbTreatmentTableP
         );
     };
 
-    // promiseDrugInfo = (drug: string) =>
-    //     new Promise<EmaDrugInfo>((resolve, reject) => {
-    //         request
-    //             .get(cancerdrugsJsonUrl + drug.replace(' ', '_') + '.json')
-    //             .end((err, res) => {
-    //                 if (!err && res.ok) {
-    //                     const response = JSON.parse(res.text);
-    //                     const emaEpar = response.emaEpar;
-    //                     if (emaEpar.length === 0) {
-    //                         const emaInfo = {
-    //                             infoAvailable: false,
-    //                         } as EmaDrugInfo;
-    //                         this.setState({
-    //                             drugInfos: _.extend(this.state.drugInfos, {
-    //                                 [drug]: [emaInfo],
-    //                             }),
-    //                         });
-    //                     } else {
-    //                         const emaInfos = new Array<EmaDrugInfo>();
-    //                         emaEpar.map((emaEparEntry: any) => {
-    //                             const emaInfo = {
-    //                                 infoAvailable: true,
-    //                                 activeSubstance:
-    //                                     emaEparEntry.activeSubstance,
-    //                                 conditionIndication:
-    //                                     emaEparEntry.conditionIndication,
-    //                                 authorisationDate:
-    //                                     emaEparEntry.marketingAuthorisationDate,
-    //                                 authorisationHolder:
-    //                                     emaEparEntry.marketingAuthorisationHolder,
-    //                                 medicineName: emaEparEntry.medicineName,
-    //                                 url: emaEparEntry.url,
-    //                             } as EmaDrugInfo;
-    //                             emaInfos.push(emaInfo);
-    //                         });
-    //                         this.setState({
-    //                             drugInfos: _.extend(this.state.drugInfos, {
-    //                                 [drug]: emaInfos,
-    //                             }),
-    //                         });
-    //                     }
-    //                 } else {
-    //                     this.setState({
-    //                         drugInfos: _.extend(this.state.drugInfos, {
-    //                             [drug]: [],
-    //                         }),
-    //                     });
-    //                 }
-    //             });
-    //     });
+    const promiseDrugInfo = (drug: string) =>
+        new Promise<EmaDrugInfo>((resolve, reject) => {
+            request
+                .get(cancerdrugsJsonUrl + drug.replace(' ', '_') + '.json')
+                .end((err, res) => {
+                    if (!err && res.ok) {
+                        const response = JSON.parse(res.text);
+                        const emaEpar = response.emaEpar;
+                        if (emaEpar.length === 0) {
+                            const emaInfo = {
+                                infoAvailable: false,
+                            } as EmaDrugInfo;
+                            _.extend(drugInfos, {
+                                [drug]: [emaInfo],
+                            });
+                        } else {
+                            const emaInfos = new Array<EmaDrugInfo>();
+                            emaEpar.map((emaEparEntry: any) => {
+                                const emaInfo = {
+                                    infoAvailable: true,
+                                    activeSubstance:
+                                        emaEparEntry.activeSubstance,
+                                    conditionIndication:
+                                        emaEparEntry.conditionIndication,
+                                    authorisationDate:
+                                        emaEparEntry.marketingAuthorisationDate,
+                                    authorisationHolder:
+                                        emaEparEntry.marketingAuthorisationHolder,
+                                    medicineName: emaEparEntry.medicineName,
+                                    url: emaEparEntry.url,
+                                } as EmaDrugInfo;
+                                emaInfos.push(emaInfo);
+                            });
+                            _.extend(drugInfos, {
+                                [drug]: emaInfos,
+                            });
+                        }
+                    } else {
+                        _.extend(drugInfos, {
+                            [drug]: [],
+                        });
+                    }
+                });
+        });
 
-    // emaTooltipStyle = (drugName: string) => {
-    //     const drugInfo = this.state.drugInfos[drugName];
-    //     if (!drugInfo) {
-    //         return 'fa fa-spinner fa-spin fa-lg';
-    //     } else if (drugInfo.length < 1) {
-    //         return 'fa fa-eur text-muted fa-lg';
-    //     } else if (!drugInfo[0].infoAvailable) {
-    //         return 'fa fa-eur text-danger fa-lg';
-    //     } else {
-    //         return 'fa fa-eur text-primary fa-lg';
-    //     }
-    // };
+    const emaTooltipStyle = (drugName: string) => {
+        const drugInfo = drugInfos[drugName];
+        if (!drugInfo) {
+            return 'fa fa-spinner fa-spin fa-lg';
+        } else if (drugInfo.length < 1) {
+            return 'fa fa-eur text-muted fa-lg';
+        } else if (!drugInfo[0].infoAvailable) {
+            return 'fa fa-eur text-danger fa-lg';
+        } else {
+            return 'fa fa-eur text-primary fa-lg';
+        }
+    };
 
-    // emaTooltipContent = (drugName: string) => {
-    //     const drugInfo = this.state.drugInfos[drugName];
-    //     if (!drugInfo) {
-    //         return (
-    //             <div style={{ maxWidth: '400px' }}>
-    //                 Getting EMA information...
-    //             </div>
-    //         );
-    //     } else if (drugInfo.length < 1) {
-    //         return (
-    //             <div style={{ maxWidth: '400px' }}>
-    //                 No entry found in cancerdrugs. <br />
-    //                 <a href={cancerdrugsUrl} target={'_blank'}>
-    //                     Search on cancerdrugs
-    //                 </a>
-    //             </div>
-    //         );
-    //     } else if (!drugInfo[0].infoAvailable) {
-    //         return (
-    //             <div style={{ maxWidth: '400px' }}>
-    //                 {drugName} is <b>not</b> authorized in the EU. <br />
-    //                 <a
-    //                     href={cancerdrugsUrl + '/drugs/' + drugName}
-    //                     target={'_blank'}
-    //                 >
-    //                     More info on cancerdrugs
-    //                 </a>
-    //             </div>
-    //         );
-    //     } else {
-    //         return (
-    //             <div style={{ maxWidth: '400px' }}>
-    //                 {drugInfo.map(drugInfoEntry =>
-    //                     this.emaTooltipEntry(drugName, drugInfoEntry)
-    //                 )}
-    //                 <a
-    //                     href={cancerdrugsUrl + '/drugs/' + drugName}
-    //                     target={'_blank'}
-    //                 >
-    //                     More info on cancerdrugs
-    //                 </a>
-    //             </div>
-    //         );
-    //     }
-    // };
+    const emaTooltipContent = (drugName: string) => {
+        const drugInfo = drugInfos[drugName];
+        if (!drugInfo) {
+            return (
+                <div style={{ maxWidth: '400px' }}>
+                    Getting EMA information...
+                </div>
+            );
+        } else if (drugInfo.length < 1) {
+            return (
+                <div style={{ maxWidth: '400px' }}>
+                    No entry found in cancerdrugs. <br />
+                    <a href={cancerdrugsUrl} target={'_blank'}>
+                        Search on cancerdrugs
+                    </a>
+                </div>
+            );
+        } else if (!drugInfo[0].infoAvailable) {
+            return (
+                <div style={{ maxWidth: '400px' }}>
+                    {drugName} is <b>not</b> authorized in the EU. <br />
+                    <a
+                        href={cancerdrugsUrl + '/drugs/' + drugName}
+                        target={'_blank'}
+                    >
+                        More info on cancerdrugs
+                    </a>
+                </div>
+            );
+        } else {
+            return (
+                <div style={{ maxWidth: '400px' }}>
+                    {drugInfo.map(drugInfoEntry =>
+                        emaTooltipEntry(drugName, drugInfoEntry)
+                    )}
+                    <a
+                        href={cancerdrugsUrl + '/drugs/' + drugName}
+                        target={'_blank'}
+                    >
+                        More info on cancerdrugs
+                    </a>
+                </div>
+            );
+        }
+    };
 
-    // emaTooltipEntry = (drugName: string, drugInfo: EmaDrugInfo) => {
-    //     return (
-    //         <span>
-    //             {drugName} is authorized in the EU under the name of{' '}
-    //             {drugInfo.medicineName} since{' '}
-    //             {drugInfo.authorisationDate.split(' ')[0]} by{' '}
-    //             {drugInfo.authorisationHolder} (
-    //             <a href={drugInfo.url} target={'_blank'}>
-    //                 more info
-    //             </a>
-    //             ). <br />
-    //             Authorized indication: {drugInfo.conditionIndication} <br />
-    //         </span>
-    //     );
-    // };
+    const emaTooltipEntry = (drugName: string, drugInfo: EmaDrugInfo) => {
+        return (
+            <span>
+                {drugName} is authorized in the EU under the name of{' '}
+                {drugInfo.medicineName} since{' '}
+                {drugInfo.authorisationDate.split(' ')[0]} by{' '}
+                {drugInfo.authorisationHolder} (
+                <a href={drugInfo.url} target={'_blank'}>
+                    more info
+                </a>
+                ). <br />
+                Authorized indication: {drugInfo.conditionIndication} <br />
+            </span>
+        );
+    };
+
+    treatments.map(treatment =>
+        treatment.drugs.map(drug => promiseDrugInfo(drug.drugName))
+    );
 
     const columns = [
         OncoKbHelper.getDefaultColumnDefinition('level'),
@@ -234,10 +231,10 @@ export const OncoKbTreatmentTable: React.FunctionComponent<OncoKbTreatmentTableP
             accessor: 'drugs',
             Cell: (props: { original: IndicatorQueryTreatment }) => (
                 <div style={{ whiteSpace: 'normal', lineHeight: '1rem' }}>
-                    {props.original.drugs
+                    {/* {props.original.drugs
                         .map(drug => drug.drugName)
-                        .join(' + ')}
-                    {/* {props.original.drugs.map(drug => (
+                        .join(' + ')} */}
+                    {props.original.drugs.map(drug => (
                         <div>
                             <span style={{ marginRight: '5px' }}>
                                 {drug.drugName}
@@ -247,19 +244,17 @@ export const OncoKbTreatmentTable: React.FunctionComponent<OncoKbTreatmentTableP
                                 trigger={['hover', 'focus']}
                                 overlay={
                                     <div>
-                                        {this.emaTooltipContent(drug.drugName)}
+                                        {emaTooltipContent(drug.drugName)}
                                     </div>
                                 }
                                 destroyTooltipOnHide={true}
                             >
                                 <i
-                                    className={this.emaTooltipStyle(
-                                        drug.drugName
-                                    )}
+                                    className={emaTooltipStyle(drug.drugName)}
                                 ></i>
                             </DefaultTooltip>
                         </div>
-                    ))} */}
+                    ))}
                 </div>
             ),
         },
