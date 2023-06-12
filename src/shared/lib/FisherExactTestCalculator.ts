7c11e8f505351c6786657a97bd7a4ebf0cb8cf58
Bryan Lai <laib1@mskcc.org>
2023-06-12 16:33:28 -0400

    change fisher test to use two-sided throughout

--- src/shared/lib/FisherExactTestCalculator.ts
+++ src/shared/lib/FisherExactTestCalculator.ts
@@ -21,39 +21,6 @@ function getProbability(
     return Math.exp(p);
 }
 
-export function getCumulativePValue(
-    a: number,
-    b: number,
-    c: number,
-    d: number
-): number {
-    let min: number, i: number;
-    const n: number = a + b + c + d;
-    let p: number = 0;
-    let logFactorial: number[] = new Array(n + 1);
-    logFactorial[0] = 0.0;
-
-    for (let j: number = 1; j <= n; j++) {
-        logFactorial[j] = logFactorial[j - 1] + Math.log(j);
-    }
-
-    p += getProbability(a, b, c, d, logFactorial);
-    if (a * d >= b * c) {
-        min = c < b ? c : b;
-        for (i = 0; i < min; i++) {
-            p += getProbability(++a, --b, --c, ++d, logFactorial);
-        }
-    }
-
-    if (a * d < b * c) {
-        min = a < d ? a : d;
-        for (i = 0; i < min; i++) {
-            p += getProbability(--a, ++b, ++c, --d, logFactorial);
-        }
-    }
-    return p;
-}
-
 export function getTwoTailedPValue(
     a: number,
     b: number,
