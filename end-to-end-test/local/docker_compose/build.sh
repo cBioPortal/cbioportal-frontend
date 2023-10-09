5206bb565651e221e9aa68f3a8165e30d367e832
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-10-09 18:18:35 -0400

    attempt at getting demo-rfc72 localdb tests to run

--- end-to-end-test/local/docker_compose/build.sh
+++ end-to-end-test/local/docker_compose/build.sh
@@ -11,7 +11,6 @@ cd $E2E_WORKSPACE
 git clone --depth 1 --branch $BACKEND_BRANCH --single-branch https://github.com/$BACKEND_PROJECT_USERNAME/cbioportal.git
 cd cbioportal
 mvn -DskipTests clean install
-unzip portal/target/cbioportal*.war -d portal/target/war-exploded
 
 cd $PWD
 
--- env/custom.sh
+++ env/custom.sh
@@ -1,2 +1,3 @@
 #export CBIOPORTAL_URL="http://localhost:8081"
 #export GENOME_NEXUS_URL="https://www.genomenexus.org"
+BACKEND=cbioportal:demo-rfc72
