e2c896c76aa6416aaf0dcbed4e07022365b14fb9
Bryan Lai <laib1@mskcc.org>
2023-10-18 16:55:09 -0400

    fix incorrect cancer type and oncokb levels in mutations table

--- src/shared/lib/StoreUtils.ts
+++ src/shared/lib/StoreUtils.ts
@@ -1122,17 +1122,43 @@ export function findMrnaRankMolecularProfileId(
 export function generateUniqueSampleKeyToTumorTypeMap(
     clinicalDataForSamples: MobxPromise<ClinicalData[]>,
     studies?: MobxPromise<CancerStudy[]>,
-    samples?: MobxPromise<Sample[]>
+    samples?: MobxPromise<Sample[]>,
+    useCancerTypeAttribute?: boolean
 ): { [sampleId: string]: string } {
     const map: { [sampleId: string]: string } = {};
 
     if (clinicalDataForSamples.result) {
-        // first priority is CANCER_TYPE_DETAILED in clinical data
-        _.each(clinicalDataForSamples.result, (clinicalData: ClinicalData) => {
-            if (clinicalData.clinicalAttributeId === 'CANCER_TYPE_DETAILED') {
-                map[clinicalData.uniqueSampleKey] = clinicalData.value;
-            }
-        });
+        // if useCancerTypeAttribute is true, use CANCER_TYPE in clinical data
+        // else, use CANCER_TYPE_DETAILED in clinical data
+        if (useCancerTypeAttribute) {
+            _.each(
+                clinicalDataForSamples.result,
+                (clinicalData: ClinicalData) => {
+                    if (clinicalData.clinicalAttributeId === 'CANCER_TYPE') {
+                        map[clinicalData.uniqueSampleKey] = clinicalData.value;
+                    }
+                }
+            );
+        } else {
+            _.each(
+                clinicalDataForSamples.result,
+                (clinicalData: ClinicalData) => {
+                    if (
+                        clinicalData.clinicalAttributeId ===
+                        'CANCER_TYPE_DETAILED'
+                    ) {
+                        map[clinicalData.uniqueSampleKey] = clinicalData.value;
+                    }
+                }
+            );
+        }
+
+        // // first priority is CANCER_TYPE_DETAILED in clinical data
+        // _.each(clinicalDataForSamples.result, (clinicalData: ClinicalData) => {
+        //     if (clinicalData.clinicalAttributeId === 'CANCER_TYPE_DETAILED') {
+        //         map[clinicalData.uniqueSampleKey] = clinicalData.value;
+        //     }
+        // });
 
         // // second priority is CANCER_TYPE in clinical data
         // _.each(clinicalDataForSamples.result, (clinicalData: ClinicalData) => {
