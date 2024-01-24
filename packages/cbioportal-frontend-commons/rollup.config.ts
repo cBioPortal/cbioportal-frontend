327c9ab3782711a6790f5862ed8144434dea3381
Onur Sumer <s.onur.sumer@gmail.com>
2024-01-24 14:00:20 -0500

    revert packages/cbioportal-frontend-commons/rollup.config.ts

--- packages/cbioportal-frontend-commons/rollup.config.ts
+++ packages/cbioportal-frontend-commons/rollup.config.ts
@@ -1,4 +1,9 @@
 import getRollupOptions from '../config/rollup.config';
 import pkg from './package.json';
 
-export default getRollupOptions('src/index.tsx', pkg.main, pkg.module);
+export default getRollupOptions(
+    'src/index.tsx',
+    pkg.main,
+    pkg.module,
+    pkg.styles
+);
