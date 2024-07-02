f3e95861e0cf890d526c233d522361fd6737b29d
Denis Papp <pappde@users.noreply.github.com>
2024-07-02 17:52:49 -0500

    ExternalToolConfig tests

---
+++ src/shared/components/ExternalTools/ExternalToolConfigUtils.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import { ExternalToolConfig} from './ExternalToolConfig';
+import { isExternalToolAvailable } from './ExternalToolConfigUtils';
+
+describe('checkToolRequirementsFontFamily', () => {
+    const mockPropsUndefined = {
+        required_installed_font_family: undefined,
+    } as ExternalToolConfig;
+
+    const mockPropsDoesNotExist = {
+        required_installed_font_family: 'FooDoesNotExist',
+    } as ExternalToolConfig;
+
+    const mockPropsDoesExist = {
+        required_installed_font_family: 'Arial',
+    } as ExternalToolConfig;
+
+
+    it('should return true if required_installed_font_family is not provided', () => {
+        expect(isExternalToolAvailable(mockPropsUndefined)).to.be.true;
+    });
+
+    it('should return true if the required font family is installed', () => {
+        expect(isExternalToolAvailable(mockPropsDoesExist)).to.be.true;
+    });
+
+    it('should return false if the required font family is not installed', () => {
+        expect(isExternalToolAvailable(mockPropsDoesNotExist)).to.be.true;
+    });
+});
