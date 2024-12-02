d200de4d36479fee90dba95e31c7aaaf7ae8e06b
alisman <lisman.aaron@gmail.com>
2024-12-02 15:13:03 -0500

    run e2e on master.cbio

--- env/master.sh
+++ env/master.sh
@@ -1,3 +1,2 @@
 export CBIOPORTAL_URL="${CBIOPORTAL_URL:-https://master.cbioportal.org}"
 export GENOME_NEXUS_URL="${GENOME_NEXUS_URL:-https://www.genomenexus.org}"
-export BACKEND=cbioportal:demo-rfc80-poc
