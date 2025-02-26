581f13f7e2c43bb1417e38fed86077ac31030b7c
Bryan Lai <laib1@mskcc.org>
2025-02-26 15:51:22 -0500

    improve loading behavior

--- src/pages/groupComparison/AlterationEnrichments.tsx
+++ src/pages/groupComparison/AlterationEnrichments.tsx
@@ -38,6 +38,7 @@ export default class AlterationEnrichments extends React.Component<
 
     readonly enrichmentsUI = MakeMobxView({
         await: () => [
+            this.props.store.alterationEnrichmentRowData,
             this.props.store.alterationsEnrichmentData,
             this.props.store.alterationsEnrichmentAnalysisGroups,
             this.props.store.selectedStudyMutationEnrichmentProfileMap,
