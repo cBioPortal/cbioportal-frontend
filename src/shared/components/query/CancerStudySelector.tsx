f3dd549850adca8dde0fb6a596f9b5fc0bd73d46
Bryan Lai <laib1@mskcc.org>
2025-05-14 15:51:55 -0400

    add resource type filter options on home page

--- src/shared/components/query/CancerStudySelector.tsx
+++ src/shared/components/query/CancerStudySelector.tsx
@@ -83,8 +83,68 @@ const StudyFilterOptionsFormatted = [
         checked: false,
     },
     {
-        id: 'imagingSampleCount',
-        name: 'Imaging',
+        id: 'pathologySlideSampleCount',
+        name: 'Pathology Slide',
+        checked: false,
+    },
+    {
+        id: 'heSampleCount',
+        name: 'H&E Slide',
+        checked: false,
+    },
+    {
+        id: 'mxifSampleCount',
+        name: 'MxIF Image',
+        checked: false,
+    },
+    {
+        id: 'mxif2SampleCount',
+        name: 'MxIF Image 2',
+        checked: false,
+    },
+    {
+        id: 'mxif3SampleCount',
+        name: 'MxIF Image 3',
+        checked: false,
+    },
+    {
+        id: 'mxif4SampleCount',
+        name: 'MxIF Image 4',
+        checked: false,
+    },
+    {
+        id: 'mxif5SampleCount',
+        name: 'MxIF Image 5',
+        checked: false,
+    },
+    {
+        id: 'mxif6SampleCount',
+        name: 'MxIF Image 6',
+        checked: false,
+    },
+    {
+        id: 'mxif7SampleCount',
+        name: 'MxIF Image 7',
+        checked: false,
+    },
+    {
+        id: 'mxif8SampleCount',
+        name: 'MxIF Image 8',
+        checked: false,
+    },
+    {
+        id: 'mxif9SampleCount',
+        name: 'MxIF Image 9',
+        checked: false,
+    },
+    {
+        id: 'minervaStorySampleCount',
+        name: 'Minerva Story',
+        checked: false,
+    },
+    {
+        id: 'cycifSampleCount',
+        name: 'CyCIF',
         checked: false,
     },
 ];
