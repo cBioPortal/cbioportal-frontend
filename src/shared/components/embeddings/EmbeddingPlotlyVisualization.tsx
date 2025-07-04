03f20eb0839a7dfc873a244685a1621c3565ebce
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2025-07-04 10:34:41 -0400

    use latest plotly (non-react version)

--- src/shared/components/embeddings/EmbeddingPlotlyVisualization.tsx
+++ src/shared/components/embeddings/EmbeddingPlotlyVisualization.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as Plotly from 'plotly.js';
+import * as Plotly from 'plotly.js-dist';
 import { EmbeddingVisualizationProps, EmbeddingPoint } from './EmbeddingTypes';
 
 /**
