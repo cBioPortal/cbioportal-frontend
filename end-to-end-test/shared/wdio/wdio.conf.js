a9fe2d3f7d942688e2e90d83174cbcfd0ca5a0a7
alisman <lisman.aaron@gmail.com>
2025-09-10 22:09:31 -0400

    update chrome version

--- end-to-end-test/shared/wdio/wdio.conf.js
+++ end-to-end-test/shared/wdio/wdio.conf.js
@@ -269,7 +269,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: debug ? 1 : 2,
+    maxInstances: debug ? 1 : 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
