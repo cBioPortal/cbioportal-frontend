cb4649f77a26ae8362b6a9b4411d065aec45f0d9
alisman <lisman.aaron@gmail.com>
2025-09-12 16:42:04 -0400

    update chrome version

--- end-to-end-test/shared/wdio/wdio.conf.js
+++ end-to-end-test/shared/wdio/wdio.conf.js
@@ -352,7 +352,7 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
 
-    automationProtocol: 'devtools',
+    //automationProtocol: 'devtools',
 
     services: [
         [
