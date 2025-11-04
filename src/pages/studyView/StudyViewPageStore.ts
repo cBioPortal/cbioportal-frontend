66db1c41d6fbdbdc4e4b53cf45fb4f6cd2bb6dd7
alisman <lisman.aaron@gmail.com>
2025-11-04 15:41:39 -0500

    Fix namespace typing in studyviewpagestore

--- src/pages/studyView/StudyViewPageStore.ts
+++ src/pages/studyView/StudyViewPageStore.ts
@@ -6178,7 +6178,7 @@ export class StudyViewPageStore
         },
     });
 
-    readonly namespaceAttributes = remoteData({
+    readonly namespaceAttributes = remoteData<NamespaceAttribute[]>({
         await: () => [this.queriedPhysicalStudyIds],
         invoke: async () => {
             if (this.queriedPhysicalStudyIds.result.length > 0) {
