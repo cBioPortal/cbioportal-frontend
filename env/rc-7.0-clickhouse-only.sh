3bab06a88f1cdbf786d94d5f190d5577c6659126
alisman <lisman.aaron@gmail.com>
2025-11-12 11:24:51 -0500

    update screenshots after api ordering PR

--- env/rc-7.0-clickhouse-only.sh
+++ env/rc-7.0-clickhouse-only.sh
@@ -1,2 +1,2 @@
-export CBIOPORTAL_URL="${CBIOPORTAL_URL:-https://staging.cbioportal.org}"
+export CBIOPORTAL_URL="https://staging.cbioportal.org"
 export GENOME_NEXUS_URL="${GENOME_NEXUS_URL:-https://www.genomenexus.org}"
