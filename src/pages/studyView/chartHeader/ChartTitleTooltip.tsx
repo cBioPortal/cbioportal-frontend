c68fb08b74d6c7597c118455edf0dc1f2f9b9850
lismana <lisman.aaron@gmail.com>
2025-12-22 16:04:16 -0500

    Update the data and add tabs

--- src/pages/studyView/chartHeader/ChartTitleTooltip.tsx
+++ src/pages/studyView/chartHeader/ChartTitleTooltip.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { DefaultTooltip } from 'cbioportal-frontend-commons';
 import { IChartHeaderProps } from './ChartHeader';
 import summaryData from '../summary.json';
+import { Tabs, Tab } from 'react-bootstrap';
 
 interface IChartTitleTooltipProps {
     chartProps: IChartHeaderProps;
@@ -30,7 +31,29 @@ export const ChartTitleTooltip: React.FC<IChartTitleTooltipProps> = ({
         <div style={{ maxWidth: 500 }}>
             <strong>{chartInfo.chart_name}</strong>
             <div style={{ marginTop: 8, fontSize: '13px' }}>
-                {chartInfo.summary}
+                <Tabs defaultActiveKey="summary" id="chart-tooltip-tabs">
+                    <Tab eventKey="summary" title="From Paper">
+                        <div style={{ padding: '10px 0' }}>
+                            {chartInfo.summary_a_paper_only}
+                        </div>
+                    </Tab>
+                    <Tab eventKey="details" title="From Internet">
+                        <div style={{ padding: '10px 0' }}>
+                            {chartInfo.summary_b_extended_analysis}
+                        </div>
+                    </Tab>
+                    <Tab eventKey="references" title="Follow-ups">
+                        <div style={{ padding: '10px 0' }}>
+                            <ul>
+                                {chartInfo.follow_up_questions.map(
+                                    (t: string) => (
+                                        <li>{t}</li>
+                                    )
+                                )}
+                            </ul>
+                        </div>
+                    </Tab>
+                </Tabs>
             </div>
             <div
                 style={{
