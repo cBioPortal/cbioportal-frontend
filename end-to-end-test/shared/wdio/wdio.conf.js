f296225ff49433626e0f53339e0af62bd30afd3d
lismana <lisman.aaron@gmail.com>
2026-01-07 13:28:29 -0500

    no virtual study

--- end-to-end-test/shared/wdio/wdio.conf.js
+++ end-to-end-test/shared/wdio/wdio.conf.js
@@ -268,7 +268,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: debug ? 1 : 2,
+    maxInstances: 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -279,7 +279,6 @@ exports.config = {
             // maxInstances can get overwritten per capability. So if you have an in-house Selenium
             // grid with only 5 firefox instances available you can make sure that not more than
             // 5 instances get started at a time.
-            maxInstances: 5,
             //
             browserName: 'chrome',
             'goog:chromeOptions': {
