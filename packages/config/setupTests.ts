ac657626a0090cc66669d28d6f59416895b6f439
Gaofei Zhao <zhaogaofeimail@gmail.com>
2026-02-11 12:59:23 -0500

    chore: remove Node 15 TextEncoder/TextDecoder polyfill
    - Remove unnecessary polyfill for TextEncoder/TextDecoder
    - Node 22 natively supports these APIs
    - Simplify test setup configuration

--- packages/config/setupTests.ts
+++ packages/config/setupTests.ts
@@ -2,11 +2,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-// Polyfill TextEncoder/TextDecoder for Node 15 compatibility with @noble/hashes
-// These are available in Node 16+ but need to be polyfilled for Node 15 and Jest/jsdom environment
-if (typeof (global as any).TextEncoder === 'undefined') {
-    const { TextEncoder, TextDecoder } = require('util');
-    (global as any).TextEncoder = TextEncoder;
-    (global as any).TextDecoder = TextDecoder;
-}
