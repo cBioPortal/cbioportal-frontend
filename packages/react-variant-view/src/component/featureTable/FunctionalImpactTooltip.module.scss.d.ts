a5bf6dc3a9aa1c70ebd6f6e2e8494053020416dc
Jatin Yadav <heroraoshab2005@gmail.com>
2026-03-07 17:21:10 +0000

    fix(study-view): format SV filters to fix table checkbox selection

--- packages/react-variant-view/src/component/featureTable/FunctionalImpactTooltip.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "high": string;
-  readonly "medium": string;
-  readonly "low": string;
-  readonly "neutral": string;
-  readonly "unknown": string;
-};
-export = styles;
-
