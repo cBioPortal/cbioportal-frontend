0219c8158df4e4026499528857a7799aa8641854
Lai <laib1@lski5346.mskcc.root.mskcc.org>
2026-04-06 16:37:16 -0400

    add missing mouse move

--- end-to-end-test/local/specs/core/patientview.screenshot.spec.js
+++ end-to-end-test/local/specs/core/patientview.screenshot.spec.js
@@ -111,6 +111,12 @@ describe('patient view page', function() {
         });
 
         it('show mutational signatures table for patient who has significant SBS signatures', async () => {
+            await (await getElement('body')).moveTo({ xOffset: 0, yOffset: 0 });
+            await (
+                await $(
+                    'div[data-test="SignificantMutationalSignaturesTooltip"]'
+                )
+            ).waitForDisplayed({ timeout: 5000, reverse: true });
             var res = await browser.checkElement(
                 'div[data-test="MutationalSignaturesContainer"]'
             );
@@ -118,12 +124,6 @@ describe('patient view page', function() {
         });
 
         it('show stacked bar chart for patient who has significant ID signatures', async () => {
-            await (await getElement('body')).moveTo({ xOffset: 0, yOffset: 0 });
-            await (
-                await $(
-                    'div[data-test="SignificantMutationalSignaturesTooltip"]'
-                )
-            ).waitForDisplayed({ timeout: 5000, reverse: true });
             await selectMutationalSignaturesVersionID();
 
             await (await $('div.patientSamples')).waitForDisplayed({
